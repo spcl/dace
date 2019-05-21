70c23bf36c863e07318c1c524c7d9dcf750162c5
Saurabh Raje <saurabh.mraje@gmail.com>
2019-05-21 15:05:08 +0200

    bugfix in conv backprop filter

--- tests/tensorflow_conv_test.py
+++ tests/tensorflow_conv_test.py
@@ -63,3 +63,40 @@ for p in paddings:
                 print(output_dace)
                 print(tf.linalg.norm(output_tf - output_dace).eval(session=sess_tf))
                 raise AssertionError("Convolution grad test failed")
+
+##### Conv filter backprop ##################
+inp_shape = [10, 10, 10, 10]
+filters = [[i, i, 10, 3] for i in [1, 2, 3, 4, 7]]
+strides = [[1, i, i, 1] for i in [1, 3, 4, 7, 8]]
+paddings = ["SAME", "VALID"]
+for p in paddings:
+    for f in filters:
+        for s in strides:
+            input_placeholder = tf.placeholder(tf.float64, inp_shape)
+            filter = tf.placeholder(tf.float64, f)
+            outp = tf.nn.conv2d(inp, filter, s, padding=p, data_format="NHWC")
+            out_backprop = tf.placeholder(tf.float64, outp.shape)
+            filter_gradients = gen_nn_ops.conv2d_backprop_filter(
+                input_placeholder, f, out_backprop, s, padding=p
+            )
+            test_grads = np.random.uniform(size=outp.shape).astype(np.float64)
+            test_input = np.random.uniform(size=tuple(inp_shape)).astype(np.float64)
+
+            output_tf = sess_tf.run(
+                filter_gradients, feed_dict={input_placeholder: test_input, out_backprop: test_grads}
+            )
+            output_dace = sess_dace.run(
+                filter_gradients, feed_dict={input_placeholder: test_input, out_backprop: test_grads}
+            )
+
+            try:
+                assert tf.norm(output_dace - output_tf).eval(session=sess_tf) < 1e-10
+            except:
+                print(p)
+                print(f)
+                print(s)
+                print(output_tf)
+                print(output_dace)
+                print(tf.linalg.norm(output_tf - output_dace).eval(session=sess_tf))
+                raise AssertionError("Convolution filter grad test failed")
+
