9a0707c3adcd399c29b76ba245bc2067d1705b2f
Saurabh Manish Raje <saurabh.mraje@gmail.com>
2019-06-17 10:41:32 +0200

    relaxed test bounds

--- tests/tensorflow_pool_test.py
+++ tests/tensorflow_pool_test.py
@@ -5,13 +5,14 @@ from dace.frontend.tensorflow import TFSession
 
 size_in = [1, 112, 112, 3]
 # size_in = [4, 4, 4, 4]
-input_tensor = np.random.uniform(size=size_in).astype(np.float64)
-input_placeholder = tf.placeholder(tf.float64, size_in)
+np.random.seed(0)
+input_tensor = np.random.uniform(size=size_in).astype(np.float32)
+input_placeholder = tf.placeholder(tf.float32, size_in)
 ksize = [1, 3, 3, 1]
 stride = [1, 2, 2, 1]
 # need to fix bug in padding SAME
-max_pool_outp = tf.nn.max_pool(input_placeholder, ksize, stride, "VALID")
-avg_pool_outp = tf.nn.avg_pool(input_placeholder, ksize, stride, "VALID")
+max_pool_outp = tf.nn.max_pool(input_placeholder, ksize, stride, "VALID", data_format="NHWC")
+avg_pool_outp = tf.nn.avg_pool(input_placeholder, ksize, stride, "VALID", data_format="NHWC")
 sess_tf = tf.Session()
 sess_dace = TFSession()
 # MAX pool test
@@ -24,25 +25,29 @@ except:
     print(tf_output.shape)
     print(tf.norm(dace_output - tf_output).eval(session=sess_tf))
     raise AssertionError("max pool test failed")
+print("Max pool test passed")
+
 # AVG pool test
 tf_output = sess_tf.run(avg_pool_outp, feed_dict={input_placeholder: input_tensor})
 dace_output = sess_dace.run(avg_pool_outp, feed_dict={input_placeholder: input_tensor})
 try:
-    assert tf.norm(dace_output - tf_output).eval(session=sess_tf) < 1e-10
+    assert tf.norm(dace_output - tf_output).eval(session=sess_tf) < 1e-5
 except:
-    print(dace_output)
-    print(tf_output)
+    print(dace_output.shape)
+    print(tf_output.shape)
     print(tf.norm(dace_output - tf_output).eval(session=sess_tf))
     raise AssertionError("avg pool test failed")
+print("Average pool test passed")
 
 # AVG pool gradient test
-loss_placeholder = tf.placeholder(tf.float64, avg_pool_outp.shape)
-loss_tensor = np.random.uniform(size=avg_pool_outp.shape)
+np.random.seed(0)
+loss_placeholder = tf.placeholder(tf.float32, avg_pool_outp.shape)
+loss_tensor = np.random.uniform(size=avg_pool_outp.shape).astype(np.float32)
 grads_avg = tf.gradients(avg_pool_outp, input_placeholder, grad_ys=loss_placeholder)
 dace_output = sess_dace.run(grads_avg, feed_dict={loss_placeholder: loss_tensor})
 tf_output = sess_tf.run(grads_avg, feed_dict={loss_placeholder: loss_tensor})
 try:
-    assert tf.norm(dace_output[0] - tf_output[0]).eval(session=sess_tf) < 1e-10
+    assert tf.norm(dace_output[0] - tf_output[0]).eval(session=sess_tf) < 1e-5
 except:
     print(dace_output)
     print(tf_output)
@@ -50,8 +55,9 @@ except:
     raise AssertionError("avg pool gradient test failed")
 
 # Max pool gradient test
-loss_placeholder = tf.placeholder(tf.float64, max_pool_outp.shape)
-loss_tensor = np.random.uniform(size=max_pool_outp.shape)
+loss_placeholder = tf.placeholder(tf.float32, max_pool_outp.shape)
+np.random.seed(0)
+loss_tensor = np.random.uniform(size=max_pool_outp.shape).astype(np.float32)
 grads_max = tf.gradients(max_pool_outp, input_placeholder, grad_ys=loss_placeholder)
 dace_output = sess_dace.run(
     grads_max,
@@ -62,7 +68,7 @@ tf_output = sess_tf.run(
     feed_dict={input_placeholder: input_tensor, loss_placeholder: loss_tensor},
 )
 try:
-    assert tf.norm(dace_output[0] - tf_output[0]).eval(session=sess_tf) < 1e-10
+    assert tf.norm(dace_output[0] - tf_output[0]).eval(session=sess_tf) < 1e-5
 except:
     print(dace_output)
     print(tf_output)
