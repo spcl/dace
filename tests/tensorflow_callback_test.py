c3ff29b308ae0876df5a886170f54bff6a5e4679
Saurabh Raje <saurabh.mraje@gmail.com>
2019-06-21 13:06:24 +0200

    added tests

--- tests/tensorflow_callback_test.py
+++ tests/tensorflow_callback_test.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import dace
 from dace.frontend.tensorflow import TFSession
 
-input_image = tf.constant(0.69, tf.float64, [2, 2, 2, 2, 2])
+input_image = tf.constant(0.69, tf.float64, [2, 2, 5, 5, 2])
 conv_filter = tf.constant(0.01, tf.float64, [1, 1, 1, 2, 2])
 tests = []
 tests.append(
@@ -12,6 +12,13 @@ tests.append(
 
 myinput = tf.constant(0.69, tf.float64, [2, 2])
 tests.append(tf.sigmoid(myinput))
+
+myinput = np.random.rand(2, 3, 4).astype(np.float64)
+tests.append(tf.math.reduce_max(myinput))
+
+myinput = np.random.rand(10).astype(np.float64)
+tests.append(tf.nn.top_k(myinput, 4)[0])
+tests.append(tf.nn.top_k(myinput, 4)[1])
 
 sess_tf = tf.Session()
 sess_dace = TFSession()
@@ -22,3 +29,4 @@ for test in tests:
     print(output_dace)
     print(output_tf)
     assert (output_dace == output_tf).all()
+
