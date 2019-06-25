28ebaa9ae6ef4da85049cd570a9c7ce2cda9a7f4
Saurabh Raje <saurabh.mraje@gmail.com>
2019-06-25 12:17:51 +0200

    working example of dataset reading in DACE

--- samples/tensorflow/tensorflow_dataset_reader.py
+++ samples/tensorflow/tensorflow_dataset_reader.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
+import numpy as np
 import dace
 from dace.frontend.tensorflow import TFSession
+import matplotlib.pyplot as plt
 
-def data_input_fn(filenames, batch_size=1000, shuffle=False):
+def data_input_fn(filenames, batch_size=2, shuffle=False):
     def _parser(record):
         features = {
             "label": tf.FixedLenFeature([], tf.int64),
@@ -10,20 +12,18 @@ def data_input_fn(filenames, batch_size=1000, shuffle=False):
         }
         parsed_record = tf.parse_single_example(record, features)
         image = tf.decode_raw(parsed_record["image_raw"], tf.float32)
+        image = tf.reshape(image, [28,28])
 
         label = tf.cast(parsed_record["label"], tf.int32)
-
+        label = tf.one_hot(indices=label, depth=10, on_value=1, off_value=0)
         return image, tf.one_hot(label, depth=10)
 
     def _input_fn():
         dataset = tf.data.TFRecordDataset(filenames).map(_parser)
-        if shuffle:
-            dataset = dataset.shuffle(buffer_size=10_000)
+        #if shuffle:
+        #    dataset = dataset.shuffle(buffer_size=10_000)
 
-        dataset = dataset.repeat(
-            None
-        )
-        dataset = dataset.batch(batch_size)
+        dataset = dataset.batch(batch_size, drop_remainder=True)
 
         iterator = dataset.make_one_shot_iterator()
         features, labels = iterator.get_next()
@@ -34,16 +34,29 @@ def data_input_fn(filenames, batch_size=1000, shuffle=False):
 
 filenames = ["/home/saurabh/data/mnist/validation.tfrecords"]
 
-with TFSession() as sess:
-    try:
-        while True:
-            print(sess.run(data_input_fn(filenames)()))
-    except tf.errors.OutOfRangeError:
-        pass
+#with TFSession() as sess:
+#    output_dace = sess.run(data_input_fn(filenames)())
 
 with tf.Session() as sess:
-    try:
-        while True:
-            print(sess.run(data_input_fn(filenames)()))
-    except tf.errors.OutOfRangeError:
-        pass
+    output_tf = sess.run(data_input_fn(filenames)())[0]
+    #print(output_tf)
+    #print(type(output_tf))
+    #print(output_tf.shape)
+    for _out in output_tf:
+        _out = np.multiply(255.0, _out)
+        _out = _out.astype(np.uint8)
+        plt.imshow(_out)
+        plt.show()
+
+with TFSession() as sess:
+    output_dace = sess.run(data_input_fn(filenames)())[0]
+    #print(output_tf)
+    #print(type(output_tf))
+    #print(output_tf.shape)
+    for _out in output_dace:
+        _out = np.multiply(255.0, _out)
+        _out = _out.astype(np.uint8)
+        plt.imshow(_out)
+        plt.show()
+
+#print(tf.linalg.norm(output_dace[0] - output_tf[0]).eval(session=tf.Session()))
