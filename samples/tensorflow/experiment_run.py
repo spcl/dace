411309fa3dbfb92dc70ba363fb5611e0dcb9a0c4
Saurabh Manish Raje <saurabh.mraje@gmail.com>
2019-06-27 13:57:37 +0200

    experimenting with array removal after gpu transform

--- samples/tensorflow/experiment_run.py
+++ samples/tensorflow/experiment_run.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
 import resnet_small
+import resnet50
 import numpy as np
 import dace
 from dace.frontend.tensorflow import TFSession
 from dace.transformation.dataflow import (
+    GPUTransformLocalStorage,
     TensorflowRedundantArray,
     MapFusion,
     RedundantArray,
@@ -14,7 +16,7 @@ from dace.transformation.dataflow import (
 from resnet_small import SEED
 
 learning_rate = 0.01
-batch_size = 1
+batch_size = 128
 num_classes = 10
 
 # dace.Config.set(
@@ -38,24 +40,45 @@ def random_batch(batch_size):
 
 
 images, labels = random_batch(batch_size)
-# Graph building
-myresnet = resnet_small.ResNet50_small(
-    "channels_last", classes=num_classes
-)  # trainable=False)
-input_placeholder = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
-label_placeholder = tf.placeholder(dtype=tf.int32, shape=(batch_size))
-logits = myresnet(input_placeholder)
-softmax = tf.nn.sparse_softmax_cross_entropy_with_logits(
-    labels=label_placeholder, logits=logits
+
+# Small Graph
+#small_resnet = resnet_small.ResNet50_small("channels_last", classes=num_classes)
+#input_placeholder = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
+#label_placeholder = tf.placeholder(dtype=tf.int32, shape=(batch_size))
+#logits = small_resnet(input_placeholder)
+#softmax = tf.nn.sparse_softmax_cross_entropy_with_logits(
+#    labels=label_placeholder, logits=logits
+#)
+#loss = tf.reduce_mean(softmax, name="loss")
+
+# Large Graph
+big_resnet = resnet50.ResNet50("channels_last", classes=num_classes)
+input_placeholder_1 = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
+label_placeholder_1 = tf.placeholder(dtype=tf.int32, shape=(batch_size))
+logits_big = big_resnet(input_placeholder_1)
+softmax_big = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    labels=label_placeholder_1, logits=logits_big
 )
-loss = tf.reduce_mean(softmax, name="loss")
+loss_big = tf.reduce_mean(softmax_big, name="loss")
+update_big = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_big)
+
+sess_dace = TFSession(seed=SEED)
+outputs_dace = sess_dace.run(
+    update_big,
+    gpu=False,
+    feed_dict={input_placeholder_1: images, label_placeholder_1: labels},
+    transformations=[
+        [TensorflowRedundantArray],
+        [GPUTransformLocalStorage],
+        [RedundantArray, RedundantArrayCopying],
+    ],
+)
 # gradients = tf.train.GradientDescentOptimizer(learning_rate).compute_gradients(loss)
 # gradient_tensors = []
 # for tup in gradients:
 #    gradient_tensors.append(tup[0])
 # update_op = tf.train.GradientDescentOptimizer(learning_rate).apply_gradients(gradients)
 # input_gradients = tf.gradients(loss, input_placeholder)
-sess_dace = TFSession(seed=SEED)
 # sess_tf = tf.Session()
 # init = tf.global_variables_initializer()
 # gradients_dace = sess_dace.train(
@@ -85,18 +108,6 @@ sess_dace = TFSession(seed=SEED)
 #    gradient_tensors[0],
 #    feed_dict={input_placeholder: images, label_placeholder: labels},
 # )
-outputs_dace = sess_dace.run(
-    loss,
-    gpu=False,
-    feed_dict={input_placeholder: images, label_placeholder: labels},
-    transformations=[
-        [
-            TensorflowRedundantArray,
-            RedundantArrayCopying,
-        ],
-        [MapFusion],
-    ],
-)
 # tf.summary.FileWriter("./", sess_tf.graph)
 # sess_tf.run(init)
 ##outputs_tf = sess_tf.run(
