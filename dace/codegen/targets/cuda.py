208a417038529056c9a10723758206ebc59208f6
Saurabh Manish Raje <saurabh.mraje@gmail.com>
2019-06-28 14:46:45 +0200

    added cudaDeviceReset for profiler

--- dace/codegen/targets/cuda.py
+++ dace/codegen/targets/cuda.py
@@ -193,6 +193,7 @@ void __dace_exit_cuda({params}) {{
     for(int i = 0; i < {nevents}; ++i) {{
         cudaEventDestroy(dace::cuda::__events[i]);
     }}
+    cudaDeviceReset();
 }}
 
 {localcode}
