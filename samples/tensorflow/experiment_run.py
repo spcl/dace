9669b623f59734760c517b10d6d680ab3e6ee628
Saurabh Manish Raje <saurabh.mraje@gmail.com>
2019-07-02 16:07:21 +0200

    added flags for profiling

--- samples/tensorflow/experiment_run.py
+++ samples/tensorflow/experiment_run.py
@@ -16,17 +16,15 @@ from dace.transformation.dataflow import (
 from resnet_small import SEED
 
 learning_rate = 0.01
-batch_size = 128
+batch_size = 1
 num_classes = 10
 
-# dace.Config.set(
-#    "compiler",
-#    "cpu",
-#    "args",
-#    value=dace.Config.get("compiler", "cpu", "args")
-#    .replace("O3", "O0")
-#    .replace("-ffast-math", ""),
-# )
+dace.Config.set(
+   "compiler",
+   "cuda",
+   "additional_args",
+    value="-g -G"
+)
 
 
 def random_batch(batch_size):
@@ -42,31 +40,32 @@ def random_batch(batch_size):
 images, labels = random_batch(batch_size)
 
 # Small Graph
-#small_resnet = resnet_small.ResNet50_small("channels_last", classes=num_classes)
-#input_placeholder = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
-#label_placeholder = tf.placeholder(dtype=tf.int32, shape=(batch_size))
-#logits = small_resnet(input_placeholder)
-#softmax = tf.nn.sparse_softmax_cross_entropy_with_logits(
-#    labels=label_placeholder, logits=logits
-#)
-#loss = tf.reduce_mean(softmax, name="loss")
+small_resnet = resnet_small.ResNet50_small("channels_last", classes=num_classes)
+input_placeholder = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
+label_placeholder = tf.placeholder(dtype=tf.int32, shape=(batch_size))
+logits = small_resnet(input_placeholder)
+softmax = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    labels=label_placeholder, logits=logits
+)
+loss = tf.reduce_mean(softmax, name="loss")
+update_small = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
 # Large Graph
-big_resnet = resnet50.ResNet50("channels_last", classes=num_classes)
-input_placeholder_1 = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
-label_placeholder_1 = tf.placeholder(dtype=tf.int32, shape=(batch_size))
-logits_big = big_resnet(input_placeholder_1)
-softmax_big = tf.nn.sparse_softmax_cross_entropy_with_logits(
-    labels=label_placeholder_1, logits=logits_big
-)
-loss_big = tf.reduce_mean(softmax_big, name="loss")
-update_big = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_big)
+#big_resnet = resnet50.ResNet50("channels_last", classes=num_classes)
+#input_placeholder_1 = tf.placeholder(dtype=tf.float32, shape=(batch_size, 224, 224, 3))
+#label_placeholder_1 = tf.placeholder(dtype=tf.int32, shape=(batch_size))
+#logits_big = big_resnet(input_placeholder_1)
+#softmax_big = tf.nn.sparse_softmax_cross_entropy_with_logits(
+#    labels=label_placeholder_1, logits=logits_big
+#)
+#loss_big = tf.reduce_mean(softmax_big, name="loss")
+#update_big = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_big)
 
 sess_dace = TFSession(seed=SEED)
 outputs_dace = sess_dace.run(
-    update_big,
+    update_small,
     gpu=False,
-    feed_dict={input_placeholder_1: images, label_placeholder_1: labels},
+    feed_dict={input_placeholder: images, label_placeholder: labels},
     transformations=[
         [TensorflowRedundantArray],
         [GPUTransformLocalStorage],
