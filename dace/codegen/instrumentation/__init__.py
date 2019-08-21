856b88edd885662d1e1427c3fc36e3610a5f4b0c
Tal Ben-Nun <talbn@inf.ethz.ch>
2019-08-21 17:39:53 +0200

    CUDA event instrumentation provider

--- dace/codegen/instrumentation/__init__.py
+++ dace/codegen/instrumentation/__init__.py
@@ -3,10 +3,11 @@ from .provider import InstrumentationProvider
 
 from .papi import PAPIInstrumentation
 from .timer import TimerProvider
+from .cuda_events import CUDAEventProvider
 
 INSTRUMENTATION_PROVIDERS = {
     InstrumentationType.No_Instrumentation: None,
     InstrumentationType.PAPI_Counters: PAPIInstrumentation,
     InstrumentationType.Timer: TimerProvider,
-    InstrumentationType.CUDA_Events: None  # TODO
+    InstrumentationType.CUDA_Events: CUDAEventProvider
 }
