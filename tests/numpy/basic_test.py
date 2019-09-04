0711a6663cb70df75df624801a4fc4ce5f917e0b
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2019-09-04 17:44:02 +0200

    Fixed validation code

--- tests/numpy/basic_test.py
+++ tests/numpy/basic_test.py
@@ -14,10 +14,12 @@ def gemm(A: dace.float32[M, K], B: dace.float32[K, N], C: dace.float32[M, N],
 if __name__ == '__main__':
     A = np.random.rand(M, K).astype(np.float32)
     B = np.random.rand(K, N).astype(np.float32)
-    C = np.zeros([M, N], dtype=np.float32)
+    C = np.random.rand(M, N).astype(np.float32)
+    origC = np.zeros([M, N], dtype=np.float32)
+    origC[:] = C
     gemm(A, B, C, 1.0, 1.0)
 
-    realC = 1.0 * (A @ B) + 1.0 * C
+    realC = 1.0 * (A @ B) + 1.0 * origC
     diff = np.linalg.norm(C - realC) / (M * N)
     print('Difference:', diff)
     exit(1 if diff >= 1e-5 else 0)
