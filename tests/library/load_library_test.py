f8ced66c4f36c4022d0fa1b7f84d0fcead6a8eb5
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2019-10-24 10:44:26 +0200

    Refactor

--- tests/library/load_library_test.py
+++ tests/library/load_library_test.py
@@ -4,12 +4,37 @@ import dacelibs.blas as blas
 import numpy as np
 import sys
 
-dtype = dace.float32
+n = dace.symbol("n")
 
 ###############################################################################
 
 
-def run(compiled_sdfg):
+def test_dot(dtype):
+
+    sdfg = dace.SDFG("dot_product")
+    state = sdfg.add_state("dataflow")
+
+    sdfg.add_array("x", [n], dtype)
+    sdfg.add_array("y", [n], dtype)
+    sdfg.add_array("result", [1], dtype)
+
+    x = state.add_read("x")
+    y = state.add_read("y")
+    result = state.add_write("result")
+
+    dot_node = blas.nodes.Dot("dot", dtype)
+
+    state.add_memlet_path(
+        x, dot_node, dst_conn="_x", memlet=Memlet.simple(x, "0:n", num_accesses=n))
+    state.add_memlet_path(
+        y, dot_node, dst_conn="_y", memlet=Memlet.simple(y, "0:n", num_accesses=n))
+    state.add_memlet_path(
+        dot_node,
+        result,
+        src_conn="_result",
+        memlet=Memlet.simple(result, "0", num_accesses=1))
+
+    dot = sdfg.compile()
 
     x = dace.ndarray([n], dtype=dtype)
     y = dace.ndarray([n], dtype=dtype)
@@ -19,7 +44,7 @@ def run(compiled_sdfg):
     x[:] = 2.5
     y[:] = 2
 
-    compiled_sdfg(x=x, y=y, result=result, n=n.get())
+    dot(x=x, y=y, result=result, n=n.get())
 
     diff = abs(result[0] - 32 * 5)
     if diff >= 1e-6 * 32 * 5:
@@ -29,33 +54,6 @@ def run(compiled_sdfg):
 
 ###############################################################################
 
-n = dace.symbol("n")
-
-sdfg = dace.SDFG("dot_product")
-state = sdfg.add_state("dataflow")
-
-sdfg.add_array("x", [n], dtype)
-sdfg.add_array("y", [n], dtype)
-sdfg.add_array("result", [1], dtype)
-
-x = state.add_read("x")
-y = state.add_read("y")
-result = state.add_write("result")
-
-dot_node = blas.nodes.Dot("dot", dtype)
-
-state.add_memlet_path(
-    x, dot_node, dst_conn="_x", memlet=Memlet.simple(x, "0:n", num_accesses=n))
-state.add_memlet_path(
-    y, dot_node, dst_conn="_y", memlet=Memlet.simple(y, "0:n", num_accesses=n))
-state.add_memlet_path(
-    dot_node,
-    result,
-    src_conn="_result",
-    memlet=Memlet.simple(result, "0", num_accesses=1))
-
-sdot = sdfg.compile()
-
-run(sdot)
+test_dot(dace.float64)
 
 ###############################################################################
