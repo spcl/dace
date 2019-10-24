e949c221421d8ceb5dc5ab713153e9e735600008
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2019-10-24 20:30:07 +0200

    Found bug in InlineSDFG. Made new test for it.

---
+++ tests/numpy/dTGL_test.py
@@ -0,0 +1,22 @@
+import dace
+import numpy as np
+
+# Declaration of symbolic variables
+N, BS = (dace.symbol(name) for name in ['N', 'BS'])
+
+
+@dace.program
+def dTGL_test(
+        HD: dace.complex128[N, BS, BS],
+        HE: dace.complex128[N, BS, BS],
+        dTGL: dace.complex128[N]):
+    
+    for n in range(1, N - 1):
+        trace_tmp = HD[n] @ HE[n]
+        for i in dace.map[0:BS]:
+            dTGL[n] += trace_tmp[i, i]
+
+
+if __name__ == '__main__':
+
+    dTGL_test.compile()
