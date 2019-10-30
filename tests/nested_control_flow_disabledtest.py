894b9b9abf2bf07f7effcae4dd0a36df6a95df89
Tal Ben-Nun <talbn@inf.ethz.ch>
2019-10-30 10:57:53 +0100

    Add (disabled) test for directly nested control flow constructs

---
+++ tests/nested_control_flow_disabledtest.py
@@ -0,0 +1,34 @@
+import dace
+import numpy as np
+
+
+@dace.program
+def nested_cflow_test(A: dace.int32[1]):
+    if A[0] == 0:
+        for i in range(11):
+            for j in range(5):
+                for k in range(6):
+                    if j < 3:
+                        with dace.tasklet:
+                            in_a << A[0]
+                            out_a >> A[0]
+                            out_a = in_a + 1
+                    else:
+                        with dace.tasklet:
+                            in_a << A[0]
+                            out_a >> A[0]
+                            out_a = in_a + 2
+
+
+if __name__ == '__main__':
+    A = np.zeros(1).astype(np.int32)
+    nested_cflow_test(A)
+
+    expected_result = 11 * 6 * 3 + 11 * 6 * 2 * 2
+
+    if A[0] != expected_result:
+        print('ERROR: %d != %d' % (A[0], expected_result))
+        exit(1)
+
+    print('OK')
+    exit(0)
