8640bd632bc4beadec1c1f3e3a89b19b192082af
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2019-11-05 17:02:14 +0100

    Allocate and provide complex zero

--- dacelibs/blas/include/dace_cublas.h
+++ dacelibs/blas/include/dace_cublas.h
@@ -13,13 +13,21 @@ namespace blas {
 
 template <typename Key = size_t>
 class CublasHelper {
-  static cublasHandle_t& GetHandle(Key i) {
+  static cublasHandle_t& GetHandle() {
     static thread_local CublasHelper singleton;
-    return singleton._GetHandle(i);
+    return singleton;
   }
 
+  cuDoubleComplex const* ComplexZero() const { return complex_zero_; }
+
  private:
-  CublasHelper() = default;
+  CublasHelper() {
+    // Allocate constant complex zero
+    cudaMalloc(&complex_zero_, sizeof(cuDoubleComplex) * 1);
+    cuDoubleComplex zero = make_cuDoubleComplex(0.0, 0.0);
+    cudaMemcpy(complex_zero_, &zero, sizeof(cuDoubleComplex) * 1,
+               cudaMemcpyHostToDevice);
+  }
 
   CublasHelper(CublasHelper const&) = delete;
 
@@ -27,11 +35,12 @@ class CublasHelper {
     for (auto& h : handles_) {
       CheckError(cublasDestroy(h.second));
     }
+    cudaFree(complex_zero_);
   }
 
   CublasHelper& operator=(CublasHelper const&) = delete;
 
-  cublasHandle_t& _GetHandle(Key i) {
+  cublasHandle_t& GetHandle(Key i) {
     auto f = handles_.find(i);
     if (f == handles_.end()) {
       // Lazily construct new cuBLAS handle
@@ -50,6 +59,7 @@ class CublasHelper {
   }
 
   std::unordered_map<Key, cublasHandle_t> handles_;
+  cuDoubleComplex* complex_zero_;
 };
 
 }  // namespace blas
