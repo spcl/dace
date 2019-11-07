e0acf7d536b2a0b4d61473c640e1862cef082ca8
Tiziano De Matteis <tdematt@inf.ethz.ch>
2019-11-07 11:51:17 +0100

    Fix: nesting sdfg

--- dace/codegen/targets/fpga.py
+++ dace/codegen/targets/fpga.py
@@ -935,14 +935,15 @@ class FPGACodeGen(TargetCodeGenerator):
                     # decide type of loop variable
                     loop_var_type = "int"
 
-                    if dace.symbolic.eval(begin) >= 0 and dace.symbolic.eval(skip) > 0:
-                        # it could be an unsigned (uint32) variable: we need to check to the type of 'end',
-                        # if we are able to determine it
-                        end_type = dace.symbolic.symbol.s_types.get(cpu.sym2cpp(end+1))
-                        if end_type is not None and end_type.dtype.type > np.dtype('uint32'):
-                            loop_var_type = end.ctype
-                        elif np.issubdtype(end_type.dtype.type, np.unsignedinteger):
-                            loop_var_type = "size_t"
+                    #TODO restore this
+                    # if dace.symbolic.eval(begin) >= 0 and dace.symbolic.eval(skip) > 0:
+                    #     # it could be an unsigned (uint32) variable: we need to check to the type of 'end',
+                    #     # if we are able to determine it
+                    #     end_type = dace.symbolic.symbol.s_types.get(cpu.sym2cpp(end+1))
+                    #     if end_type is not None and end_type.dtype.type > np.dtype('uint32'):
+                    #         loop_var_type = end.ctype
+                    #     elif np.issubdtype(end_type.dtype.type, np.unsignedinteger):
+                    #         loop_var_type = "size_t"
 
                     result.write(
                         "for ({} {} = {}; {} < {}; {} += {}) {{\n".format(loop_var_type,
