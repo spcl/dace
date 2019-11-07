edfaf6985e1e71c31db6f9278e81e027299afd5c
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2019-11-07 15:23:36 +0100

    Fixed c++ errors in BlasConstants class.

--- dacelibs/blas/include/dace_blas.h
+++ dacelibs/blas/include/dace_blas.h
@@ -7,16 +7,17 @@ namespace dacelib {
 namespace blas {
 
 class BlasConstants {
-    constexpr void* Complex64Zero() { return &complex64_zero_; }
-    constexpr void* Complex128Zero() const { return &complex128_zero_; }
-    constexpr void* Complex64Pone() const { return &complex64_pone_; }
-    constexpr void* Complex128Pone() const { return &complex128_pone_; }
+public:
+    static constexpr const void* Complex64Zero() { return &complex64_zero_; }
+    static constexpr const void* Complex128Zero() { return &complex128_zero_; }
+    static constexpr const void* Complex64Pone() { return &complex64_pone_; }
+    static constexpr const void* Complex128Pone() { return &complex128_pone_; }
 
 private:
-    constexpr complex64_zero_ = std::complex<float>(0.0f, 0.0f);
-    constexpr complex128_zero_ = std::complex<double>(0.0, 0.0);
-    constexpr complex64_pone_ = std::complex<float>(1.0f, 0.0f);
-    constexpr complex128_pone_ = std::complex<double>(1.0, 0.0);
+    static constexpr std::complex<float> complex64_zero_ = std::complex<float>(0.0f, 0.0f);
+    static constexpr std::complex<double> complex128_zero_ = std::complex<double>(0.0, 0.0);
+    static constexpr std::complex<float> complex64_pone_ = std::complex<float>(1.0f, 0.0f);
+    static constexpr std::complex<double> complex128_pone_ = std::complex<double>(1.0, 0.0);
 };
 
 }   // namespace blas
