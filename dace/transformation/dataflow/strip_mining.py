05c1315ce9cab62a83ee766ab28d330fba6fe8d6
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2019-11-15 20:12:27 +0100

    Rewrote strip-mining to work with all types of connectors.

--- dace/transformation/dataflow/strip_mining.py
+++ dace/transformation/dataflow/strip_mining.py
@@ -35,6 +35,7 @@ def calc_set_image_range(map_idx, map_set, array_range):
             new_range = [
                 new_range[i].subs(symbol,
                                   dace.symbolic.overapproximate(m_range[i]))
+                if isinstance(new_range[i], dace.symbol) else new_range[i]
                 for i in range(0, 3)
             ]
         image.append(new_range)
@@ -119,10 +120,8 @@ class StripMining(pattern_matching.Transformation):
     def apply(self, sdfg):
         graph = sdfg.nodes()[self.state_id]
         # Strip-mine selected dimension.
-        target_dim, new_dim, new_map = self.__stripmine(
+        _, _, new_map = self.__stripmine(
             sdfg, graph, self.subgraph)
-        # Modify edges to match strip-mined dimension.
-        # StripMining.__modify_edges(sdfg, graph, self.subgraph, target_dim, new_dim)
         return new_map
 
     # def __init__(self, tag=True):
@@ -156,7 +155,7 @@ class StripMining(pattern_matching.Transformation):
 
         # Retrieve map entry and exit nodes.
         map_entry = graph.nodes()[candidate[StripMining._map_entry]]
-        map_exits = graph.exit_nodes(map_entry)
+        map_exit = graph.exit_nodes(map_entry)[0]
 
         # Retrieve transformation properties.
         dim_idx = self.dim_idx
@@ -180,6 +179,7 @@ class StripMining(pattern_matching.Transformation):
         new_map = nodes.Map(new_dim + '_' + map_entry.map.label, [new_dim],
                             subsets.Range([new_dim_range]))
         new_map_entry = nodes.MapEntry(new_map)
+        new_map_exit = nodes.MapExit(new_map)
 
         # Change the range of the selected dimension to iterate over a single
         # tile
@@ -208,113 +208,93 @@ class StripMining(pattern_matching.Transformation):
         # Make internal map's schedule to "not parallel"
         map_entry.map._schedule = dtypes.ScheduleType.Default
 
-        # Redirect/create edges.
-        new_in_edges = {}
-        for _src, conn, _dest, _, memlet in graph.out_edges(map_entry):
-            if not isinstance(sdfg.arrays[memlet.data], dace.data.Scalar):
+        # Redirect edges
+        new_map_entry.in_connectors = dcpy(map_entry.in_connectors)
+        nxutil.change_edge_dest(graph, map_entry, new_map_entry)
+        new_map_exit.out_connectors = dcpy(map_exit.out_connectors)
+        nxutil.change_edge_src(graph, map_exit, new_map_exit)
+
+        # Create new entry edges
+        new_in_edges = dict()
+        entry_in_conn = set()
+        entry_out_conn = set()
+        for _src, src_conn, _dst, _, memlet in graph.out_edges(map_entry):
+            if (src_conn[:4] == 'OUT_' and not
+                    isinstance(sdfg.arrays[memlet.data], dace.data.Scalar)):
                 new_subset = calc_set_image(
                     map_entry.map.params,
                     map_entry.map.range,
                     memlet.subset,
                 )
-                if memlet.data in new_in_edges:
-                    src, src_conn, dest, dest_conn, new_memlet, num = \
-                        new_in_edges[memlet.data]
-                    new_memlet.subset = calc_set_union(new_memlet.subset,
-                                                       new_subset)
-                    new_memlet.num_accesses = new_memlet.num_elements()
-                    new_in_edges.update({
-                        memlet.data: (src, src_conn, dest, dest_conn,
-                                      new_memlet, min(num, int(conn[4:])))
-                    })
+                conn = src_conn[4:]
+                key = (memlet.data, 'IN_' + conn, 'OUT_' + conn)
+                if key in new_in_edges.keys():
+                    old_subset = new_in_edges[key].subset
+                    new_in_edges[key].subset = calc_set_union(old_subset,
+                                                              new_subset)
                 else:
+                    entry_in_conn.add('IN_' + conn)
+                    entry_out_conn.add('OUT_' + conn)
                     new_memlet = dcpy(memlet)
                     new_memlet.subset = new_subset
                     new_memlet.num_accesses = new_memlet.num_elements()
-                    new_in_edges.update({
-                        memlet.data: (new_map_entry, None, map_entry, None,
-                                      new_memlet, int(conn[4:]))
-                    })
-        nxutil.change_edge_dest(graph, map_entry, new_map_entry)
+                    new_in_edges[key] = new_memlet
+            else:
+                if src_conn[:4] == 'OUT_':
+                    conn = src_conn[4:]
+                    in_conn = 'IN_' + conn
+                    out_conn = 'OUT_' + conn
+                else:
+                    in_conn = src_conn
+                    out_conn = src_conn
+                entry_in_conn.add(in_conn)
+                entry_out_conn.add(out_conn)
+                new_in_edges[(memlet.data, in_conn, out_conn)] = dcpy(memlet)
+        new_map_entry.out_connectors = entry_out_conn
+        map_entry.in_connectors = entry_in_conn
+        for (_, in_conn, out_conn), memlet in new_in_edges.items():
+            graph.add_edge(new_map_entry, out_conn, map_entry, in_conn, memlet)
 
-        new_out_edges = {}
-        new_exits = []
-        for map_exit in map_exits:
-            if isinstance(map_exit, nodes.MapExit):
-                new_exit = nodes.MapExit(new_map)
-                new_exits.append(new_exit)
-            for _src, conn, _dest, _, memlet in graph.in_edges(map_exit):
-                if not isinstance(sdfg.arrays[memlet.data], dace.data.Scalar):
-                    new_subset = calc_set_image(
-                        map_entry.map.params,
-                        map_entry.map.range,
-                        memlet.subset,
-                    )
-                    if memlet.data in new_out_edges:
-                        src, src_conn, dest, dest_conn, new_memlet, num = \
-                            new_out_edges[memlet.data]
-                        new_memlet.subset = calc_set_union(
-                            new_memlet.subset, new_subset)
-                        new_memlet.num_accesses = new_memlet.num_elements()
-                        new_out_edges.update({
-                            memlet.data: (src, src_conn, dest, dest_conn,
-                                          new_memlet, min(num, conn[4:]))
-                        })
-                    else:
-                        new_memlet = dcpy(memlet)
-                        new_memlet.subset = new_subset
-                        new_memlet.num_accesses = new_memlet.num_elements()
-                        new_out_edges.update({
-                            memlet.data: (map_exit, None, new_exit, None,
-                                          new_memlet, conn[4:])
-                        })
-            nxutil.change_edge_src(graph, map_exit, new_exit)
-
-        in_conn_nums = []
-        for _, e in new_in_edges.items():
-            _, _, _, _, _, num = e
-            in_conn_nums.append(num)
-        in_conn = {}
-        for i, num in enumerate(in_conn_nums):
-            in_conn.update({num: i + 1})
-
-        entry_in_connectors = set()
-        entry_out_connectors = set()
-        for i in range(len(in_conn_nums)):
-            entry_in_connectors.add('IN_' + str(i + 1))
-            entry_out_connectors.add('OUT_' + str(i + 1))
-        new_map_entry.in_connectors = entry_in_connectors
-        new_map_entry.out_connectors = entry_out_connectors
-
-        for _, e in new_in_edges.items():
-            src, _, dst, _, memlet, num = e
-            graph.add_edge(src, 'OUT_' + str(in_conn[num]), dst,
-                           'IN_' + str(in_conn[num]), memlet)
-
-        for new_exit in new_exits:
-
-            out_conn_nums = []
-            for _, e in new_out_edges.items():
-                _, _, dst, _, _, num = e
-                if dst is not new_exit:
-                    continue
-                out_conn_nums.append(num)
-            out_conn = {}
-            for i, num in enumerate(out_conn_nums):
-                out_conn.update({num: i + 1})
-
-            exit_in_connectors = set()
-            exit_out_connectors = set()
-            for i in range(len(out_conn_nums)):
-                exit_in_connectors.add('IN_' + str(i + 1))
-                exit_out_connectors.add('OUT_' + str(i + 1))
-            new_exit.in_connectors = exit_in_connectors
-            new_exit.out_connectors = exit_out_connectors
-
-            for _, e in new_out_edges.items():
-                src, _, dst, _, memlet, num = e
-                graph.add_edge(src, 'OUT_' + str(out_conn[num]), dst,
-                               'IN_' + str(out_conn[num]), memlet)
+        # Create new exit edges
+        new_out_edges = dict()
+        exit_in_conn = set()
+        exit_out_conn = set()
+        for _src, _, _dst, dst_conn, memlet in graph.in_edges(map_exit):
+            if (dst_conn[:3] == 'IN_' and not
+                    isinstance(sdfg.arrays[memlet.data], dace.data.Scalar)):
+                new_subset = calc_set_image(
+                    map_entry.map.params,
+                    map_entry.map.range,
+                    memlet.subset,
+                )
+                conn = dst_conn[3:]
+                key = (memlet.data, 'IN_' + conn, 'OUT_' + conn)
+                if key in new_out_edges.keys():
+                    old_subset = new_out_edges[key].subset
+                    new_out_edges[key].subset = calc_set_union(old_subset,
+                                                               new_subset)
+                else:
+                    exit_in_conn.add('IN_' + conn)
+                    exit_out_conn.add('OUT_' + conn)
+                    new_memlet = dcpy(memlet)
+                    new_memlet.subset = new_subset
+                    new_memlet.num_accesses = new_memlet.num_elements()
+                    new_out_edges[key] = new_memlet
+            else:
+                if dst_conn[:3] == 'IN_':
+                    conn = dst_conn[3:]
+                    in_conn = 'IN_' + conn
+                    out_conn = 'OUT_' + conn
+                else:
+                    in_conn = src_conn
+                    out_conn = src_conn
+                exit_in_conn.add(in_conn)
+                exit_out_conn.add(out_conn)
+                new_in_edges[(memlet.data, in_conn, out_conn)] = dcpy(memlet)
+        new_map_exit.in_connectors = exit_in_conn
+        map_exit.out_connectors = exit_out_conn
+        for (_, in_conn, out_conn), memlet in new_out_edges.items():
+            graph.add_edge(map_exit, out_conn, new_map_exit, in_conn, memlet)
 
         # Return strip-mined dimension.
         return target_dim, new_dim, new_map
