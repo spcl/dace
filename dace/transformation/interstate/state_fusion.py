e3a306e094853b4655aaedc4d58cef326172dadd
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2019-12-02 14:12:53 +0100

    yapf

--- dace/transformation/interstate/state_fusion.py
+++ dace/transformation/interstate/state_fusion.py
@@ -116,8 +116,9 @@ class StateFusion(pattern_matching.Transformation):
             check_strict = len(first_cc)
             for cc_output in first_cc_output:
                 for node in cc_output:
-                    if (next((x for x in second_input
-                              if x.label == node.label), None) is not None):
+                    if (next(
+                        (x for x in second_input if x.label == node.label),
+                            None) is not None):
                         check_strict -= 1
                         break
 
@@ -125,13 +126,15 @@ class StateFusion(pattern_matching.Transformation):
                 # Check strict conditions
                 # RW dependency
                 for node in first_input:
-                    if (next((x for x in second_output
-                              if x.label == node.label), None) is not None):
+                    if (next(
+                        (x for x in second_output if x.label == node.label),
+                            None) is not None):
                         return False
                 # WW dependency
                 for node in first_output:
-                    if (next((x for x in second_output
-                              if x.label == node.label), None) is not None):
+                    if (next(
+                        (x for x in second_output if x.label == node.label),
+                            None) is not None):
                         return False
 
         return True
@@ -141,8 +144,8 @@ class StateFusion(pattern_matching.Transformation):
         first_state = graph.nodes()[candidate[StateFusion._first_state]]
         second_state = graph.nodes()[candidate[StateFusion._second_state]]
 
-        return " -> ".join(
-            state.label for state in [first_state, second_state])
+        return " -> ".join(state.label
+                           for state in [first_state, second_state])
 
     def apply(self, sdfg):
         first_state = sdfg.nodes()[self.subgraph[StateFusion._first_state]]
@@ -194,7 +197,10 @@ class StateFusion(pattern_matching.Transformation):
 
         # Merge second state to first state
         # First keep a backup of the topological sorted order of the nodes
-        order = [x for x in reversed(list(nx.topological_sort(first_state._nx))) if isinstance(x, nodes.AccessNode)]
+        order = [
+            x for x in reversed(list(nx.topological_sort(first_state._nx)))
+            if isinstance(x, nodes.AccessNode)
+        ]
         for node in second_state.nodes():
             first_state.add_node(node)
         for src, src_conn, dst, dst_conn, data in second_state.edges():
@@ -203,8 +209,8 @@ class StateFusion(pattern_matching.Transformation):
         # Merge common (data) nodes
         for node in first_input:
             try:
-                old_node = next(
-                    x for x in second_input if x.label == node.label)
+                old_node = next(x for x in second_input
+                                if x.label == node.label)
             except StopIteration:
                 continue
             nxutil.change_edge_src(first_state, old_node, node)
@@ -212,8 +218,8 @@ class StateFusion(pattern_matching.Transformation):
             second_input.remove(old_node)
         for node in first_output:
             try:
-                new_node = next(
-                    x for x in second_input if x.label == node.label)
+                new_node = next(x for x in second_input
+                                if x.label == node.label)
             except StopIteration:
                 continue
             nxutil.change_edge_dest(first_state, node, new_node)
@@ -227,7 +233,6 @@ class StateFusion(pattern_matching.Transformation):
                 if n:
                     nxutil.change_edge_src(first_state, node, n)
                     first_state.remove_node(node)
-
 
         # Redirect edges and remove second state
         nxutil.change_edge_src(sdfg, second_state, first_state)
