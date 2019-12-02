13f7170b217d9974d2e98e7d5038e6e5fd72b854
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2019-12-02 21:56:24 +0100

    Improved the fix for reduction output to an array of size 1. The fix now triggers only for non-transient arrays.

--- dace/codegen/targets/cpu.py
+++ dace/codegen/targets/cpu.py
@@ -2088,10 +2088,10 @@ for (int {mapname}_iter = 0; {mapname}_iter < {mapname}_rng.size(); ++{mapname}_
                     if r != 0 and r != (0, 0, 1):
                         scalar_output = False
                         break
-                if scalar_output:
+                if scalar_output and not sdfg.arrays[output_memlet.data].transient:
                     out_var = output_memlet.data
                 else:
-                    cpp_array_expr(sdfg, output_memlet)
+                    out_var = cpp_array_expr(sdfg, output_memlet)
                 callsite_stream.write(
                     "%s = __tmpout;" % out_var,
                     sdfg,
