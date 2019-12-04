7c5513828074af1b1d78bfa7a685d43d54d1dd15
am-ivanov <am-ivanov@users.noreply.github.com>
2019-12-04 17:21:07 +0100

    Test for issue #51

---
+++ tests/numpy/subarray_assignment_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import dace
+
+
+@dace.program
+def foo123(a: dace.float32[2, 3], b: dace.float32[2, 3]):
+    b[0, :] = a[0, :]
+
+if __name__ == '__main__':
+
+    A = np.full((2, 3), 3, dtype=np.float32)
+    B = np.full((2, 3), 4, dtype=np.float32)
+
+    foo123(A, B)
+
+    if not np.allclose(B[0, :], A[0, :]):
+        exit(1)
