dd168219330f21514aa201685198129926353ea6
am-ivanov <am-ivanov@users.noreply.github.com>
2019-12-04 17:15:55 +0100

    Test for issue #50

---
+++ tests/numpy/element_assignment_test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import dace
+
+
+@dace.program
+def foo123(a: dace.float32[2], b: dace.float32[2]):
+    b[0] = a[0]
+
+if __name__ == '__main__':
+    A = np.array([1, 2], dtype=np.float32)
+    B = np.array([3, 4], dtype=np.float32)
+
+    foo123(A, B)
+
+    if A[0] != B[0]:
+        exit(1)
