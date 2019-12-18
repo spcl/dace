a0676d098537d194111239fa70c17f89e219f494
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2019-12-18 10:26:20 +0100

    changed speedtest

--- samples/tensorflow/speedtest.py
+++ samples/tensorflow/speedtest.py
@@ -19,26 +19,31 @@ config.gpu_options.allow_growth = True
 sess_dace = TFSession()
 sess_tf = tf.Session(config=config)
 
-sess_run = sess_dace.compile(output, gpu=False)
+'''sess_run = sess_dace.compile(output, gpu=False)
 start_dace_cpu = time.time()
-output_dace = sess_run(feed_dict={input: test_input, filter: test_filter})
-end_dace_cpu = time.time()
+for i in range(100):
+    output_dace = sess_run(feed_dict={input: test_input, filter: test_filter})
+end_dace_cpu = time.time()'''
 
 sess_run = sess_dace.compile(output, gpu=True, cudnn=False)
 start_dace_gpu = time.time()
-output_dace = sess_run(feed_dict={input: test_input, filter: test_filter})
+for i in range(1000):
+    output_dace = sess_run(feed_dict={input: test_input, filter: test_filter})
 end_dace_gpu = time.time()
+
 
 sess_run = sess_dace.compile(output, gpu=True, cudnn=True)
 start_dace_cudnn = time.time()
-output_dace = sess_run(feed_dict={input: test_input, filter: test_filter})
+for i in range(1000):
+    output_dace = sess_run(feed_dict={input: test_input, filter: test_filter})
 end_dace_cudnn = time.time()
 
 start_tf = time.time()
-output_tf = sess_tf.run(output, feed_dict={input: test_input, filter: test_filter})
+for i in range(1000):
+    output_tf = sess_tf.run(output, feed_dict={input: test_input, filter: test_filter})
 end_tf = time.time()
 
-print("time dace cpu: ", end_dace_cpu-start_dace_cpu)
+#print("time dace cpu: ", end_dace_cpu-start_dace_cpu)
 print("time dace gpu: ", end_dace_gpu-start_dace_gpu)
 print("time dace cudnn: ", end_dace_cudnn-start_dace_cudnn)
 print("time tf: ", end_tf-start_tf)
