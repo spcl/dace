aa46dfabce04c841607c2653592108dc7550920e
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2020-01-15 13:28:41 +0100

    Fix MKL environment

--- dacelibs/blas/environments/intel_mkl.py
+++ dacelibs/blas/environments/intel_mkl.py
@@ -12,6 +12,6 @@ class IntelMKL:
     cmake_link_flags = []
     cmake_files = []
 
-    headers = ["cblas.h", "mkl/mkl.h", "../include/dace_blas.h"]
+    headers = ["mkl.h", "../include/dace_blas.h"]
     init_code = ""
     finalize_code = ""
