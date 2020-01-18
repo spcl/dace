7d343c082d96d63d3c04a81248f264d1a3ee7c2e
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2020-01-18 23:19:21 +0100

    Fixed loop causing oob access on array b.

--- tests/numpy/rowwise_assign_test.py
+++ tests/numpy/rowwise_assign_test.py
@@ -14,7 +14,7 @@ def my_assign(X_in: dace.float32[N], X_out: dace.float32[N]):
 @dace.program
 def my_func(a: dace.float32[M, K], b: dace.float32[M, K]):
 
-    for j in dace.map[0:K]:
+    for j in dace.map[0:M]:
         my_assign(b[j], a[j])
 
 
