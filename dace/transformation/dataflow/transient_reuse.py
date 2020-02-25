79d2b98d7fb0722a2ae7ae4c3dd598364b9b338d
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2020-02-25 15:41:04 +0100

    First try

--- dace/transformation/dataflow/transient_reuse.py
+++ dace/transformation/dataflow/transient_reuse.py
@@ -1,8 +1,50 @@
-from dace import registry
+from dace import data, memlet, dtypes, registry, sdfg as sd
+from dace.graph import nodes, nxutil, edges as ed
 from dace.transformation import pattern_matching
-from dace.properties import make_properties
+from dace.properties import Property, make_properties, SubsetProperty
+from dace.memlet import Memlet
 
 @registry.autoregister
 @make_properties
 class TransientReuse(pattern_matching.Transformation):
 
+    _map_entry = nodes.MapEntry(nodes.Map("", [], []))
+
+    @staticmethod
+    def can_be_applied(graph, candidate, expr_index, sdfg, strict=False):
+        return True
+
+    @staticmethod
+    def match_to_str(graph, candidate):
+        return graph.label
+
+    @staticmethod
+    def expressions():
+        return [sd.SDFG('_')]
+
+    def expansion(node):
+        pass
+
+    def apply(self, sdfg):
+        list = []
+        for i, state in enumerate(sdfg.nodes()):
+            for j, node in enumerate(state.nodes()):
+                if isinstance(node,
+                              nodes.AccessNode) and node.desc(sdfg).transient:
+                    print(node, type(node))
+                    list.append(node)
+
+        temp1 = list[0]
+        temp2 = list[2]
+        for i, state in enumerate(sdfg.nodes()):
+            for e in state.edges():
+                if e._dst == temp2:
+                    print(e.data.subset)
+                    state.add_edge(e._src, e.src_conn, temp1, None, Memlet.from_array(temp1, temp2.desc(sdfg)))
+                    state.remove_edge(e)
+                if e._src == temp2:
+                    state.add_edge(temp1, None, e._dst, e.dst_conn, Memlet.from_array(temp1, temp2.desc(sdfg)))
+                    state.remove_edge(e)
+        sdfg.states()[0].remove_node(temp2)
+
+        #sdfg.states()[0].remove_edge(sdfg.states()[0].edges()[3])
--- samples/simple/transients.py
+++ samples/simple/transients.py
@@ -1,34 +1,28 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import argparse
 import dace
 import numpy as np
+from dace.transformation.dataflow.transient_reuse import TransientReuse
 
 M = dace.symbol('M')
 K = dace.symbol('K')
 N = dace.symbol('N')
 
 
-@dace.program(dace.float64[M, K], dace.float64[K, N], dace.float64[M, N])
-def gemm(A, B, C):
-    # Transient variable
-    tmp = dace.define_local([M, N, K], dtype=A.dtype)
-
-    @dace.map(_[0:M, 0:N, 0:K])
-    def multiplication(i, j, k):
-        in_A << A[i, k]
-        in_B << B[k, j]
-        out >> tmp[i, j, k]
-
-        out = in_A * in_B
-
-    dace.reduce(lambda a, b: a + b, tmp, C, axis=2, identity=0)
+@dace.program
+def operation(A: dace.float64[M, M], B: dace.float64[M, M], C: dace.float64[M, M]):
+    C[:] = (((A @ B) @ A) @ B) @ A
 
 
 if __name__ == "__main__":
     print("==== Program start ====")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("M", type=int, nargs="?", default=24)
-    parser.add_argument("K", type=int, nargs="?", default=24)
-    parser.add_argument("N", type=int, nargs="?", default=24)
+    parser.add_argument("M", type=int, nargs="?", default=5)
+    parser.add_argument("K", type=int, nargs="?", default=5)
+    parser.add_argument("N", type=int, nargs="?", default=5)
     args = vars(parser.parse_args())
 
     M.set(args["M"])
@@ -43,17 +37,16 @@ if __name__ == "__main__":
     C = np.zeros([M.get(), N.get()], dtype=np.float64)
     C_regression = np.zeros_like(C)
 
-    gemm(A, B, C)
-
+    sdfg = operation.to_sdfg()
+    sdfg.apply_transformations(TransientReuse)
+    sdfg(A=A, B=B, C=C, M=M)
     if dace.Config.get_bool('profiling'):
         dace.timethis('gemm', 'numpy', (2 * M.get() * K.get() * N.get()),
                       np.dot, A, B, C_regression)
     else:
-        np.dot(A, B, C_regression)
-
+        C_regression = np.dot(np.dot(np.dot(np.dot(A, B), A), B), A)
+    print(C)
     diff = np.linalg.norm(C_regression - C) / (M.get() * N.get())
     print("Difference:", diff)
     print("==== Program end ====")
-    exit(0 if diff <= 1e-5 else 1)
-
     exit(0 if diff <= 1e-5 else 1)
