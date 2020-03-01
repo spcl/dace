eefa50ec06db9358474ae835c158c0fe82763400
Tiziano De Matteis <tdematt@inf.ethz.ch>
2020-03-01 13:19:26 +0100

    Temporarly disable smi tests due to package conflicts between Quartus and CLang

--- tests/intel_fpga_smi_test.sh
+++ tests/intel_fpga_smi_test.sh
@@ -108,6 +108,7 @@ echo "====== Target: INTEL FPGA ======"
 
 DACE_compiler_use_cache=0
 DACE_compiler_fpga_vendor="intel_fpga"
+DACE_compiler_intel_fpga_board="p520_max_sg280l"
 
 TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $TEST_DIR
