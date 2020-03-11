78823597487f5167de99765256cb942208617571
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2020-03-11 13:04:11 +0100

    latest version

--- dace/transformation/interstate/transient_reuse.py
+++ dace/transformation/interstate/transient_reuse.py
@@ -7,6 +7,13 @@ from dace.sdfg import SDFG, SDFGState
 from sympy import Symbol, N
 import numpy as np
 
+def _atomic_counter_generator():
+    ctr = 0
+    while True:
+        ctr += 1
+        yield ctr
+_atomic_count = _atomic_counter_generator()
+
 @registry.autoregister
 @make_properties
 class TransientReuse(pattern_matching.Transformation):
@@ -27,79 +34,102 @@ class TransientReuse(pattern_matching.Transformation):
         pass
 
     def apply(self, sdfg):
-        state: SDFGState = sdfg.nodes()[self.state_id]
+        for state in sdfg.nodes():
 
-        # Step 1: Find all transients.
-        transients = []
-        memory_before = 0
-        for a in sdfg.arrays:
-            memory_before += sdfg.arrays[a].total_size
-            if sdfg.arrays[a].transient == True:
-                transients.append(a)
+            # Step 1: Find all transients.
+            transients = []
+            memory_before = 0
+            for a in sdfg.arrays:
+                memory_before += sdfg.arrays[a].total_size
+                if sdfg.arrays[a].transient == True:
+                    transients.append(a)
+            print(len(sdfg.arrays))
 
-        # Step 2: Determine all possible reuses and decide on a mapping.
-        #   a) for each transient construct set of nodes that use that transient.
-        live = {}
-        for t in transients:
-            live[t] = set()
-            for n in state.nodes():
-                if isinstance(n, nodes.AccessNode) and n.data == t:
-                    live[t].add(n)
-                    for e in state.all_edges(n):
-                        for edge in state.memlet_tree(e):
-                            live[t].add(edge.dst)
-                            live[t].add(edge.src)
+            # Step 2: Determine all possible reuses and decide on a mapping.
+            #   a) for each transient construct set of nodes that use that transient.
+            live = {}
+            for t in transients:
+                live[t] = set()
+                for n in state.nodes():
+                    if isinstance(n, nodes.AccessNode) and n.data == t:
+                        live[t].add(n)
+                        for e in state.all_edges(n):
+                            for edge in state.memlet_tree(e):
+                                live[t].add(edge.dst)
+                                live[t].add(edge.src)
 
-        #   b) Find all possible mappings. Intersect all sets with each other.
-        #      Only empty intersections are valid mappings. (Interference graph)
-        mappings = {}
-        for t in transients:
-            mappings[t] = set()
-        for t in live:
-            for s in live:
-                if not s is t and live[t].intersection(live[s]) == set():
-                    mappings[t].add(s)
-                    mappings[s].add(t)
+            #   b) Find all possible mappings. Intersect all sets with each other.
+            #      Only empty intersections are valid mappings. (Interference graph)
+            mappings = {}
+            for t in transients:
+                mappings[t] = set()
+            for t in live:
+                for s in live:
+                    if not s is t and live[t].intersection(live[s]) == set():
+                        mappings[t].add(s)
+                        mappings[s].add(t)
 
-        #   c) Construct valid mapping combination. Each bucket contains transients that can be merged. (graph coloring)
-        buckets = []
-        for i in range(len(transients)):
-            buckets.append(set())
+            #   c) Construct valid mapping combination. Each bucket contains transients that can be merged. (graph coloring)
+            buckets = []
+            for i in range(len(transients)):
+                buckets.append(set())
 
-        for t in mappings:
+            for t in mappings:
+                for i in range(len(buckets)):
+                    if buckets[i] == set():
+                        buckets[i].add(t)
+                        break
+                    elif buckets[i].issubset(mappings[t]) and len(sdfg.arrays[t].shape) == len(sdfg.arrays[list(buckets[i])[0]].shape):  # only same shape
+                        buckets[i].add(t)
+                        break
+
+            # Step 3 For each mapping reshape transients to fit data
+            bucket_list = []
             for i in range(len(buckets)):
-                if buckets[i] == set():
-                    buckets[i].add(t)
-                    break
-                elif buckets[i].issubset(mappings[t]) \
-                        and sdfg.arrays[t].shape == sdfg.arrays[list(buckets[i])[0]].shape:  # only same shape
-                    buckets[i].add(t)
-                    break
+                bucket_list.append(sorted(list(buckets[i])))
+                if len(buckets[i]) > 1:
+                    local_ctr = str(next(_atomic_count))
+                    shape = []
+                    shape_list = [list(sdfg.arrays[e].shape) for e in bucket_list[i]]
+                    for el in shape_list:
+                        for j in range(len(el)):
+                            if len(shape) == j:
+                                shape.append(el[j])
+                            else:
+                                shape[j] = max(shape[j],el[j])
+                    print("transient_reuse_" + local_ctr, shape)
+                    temp = sdfg.add_transient(name="transient_reuse_" + local_ctr, shape=shape, dtype=sdfg.arrays[bucket_list[i][0]].dtype)
+                    bucket_list[i].insert(0, "transient_reuse_" + local_ctr)
 
-        mapping = set()
-        for i in range(len(buckets)):
-            if len(buckets[i]) > 1:
-                bucket_list = sorted(list(buckets[i]))
-                for j in range(1, len(bucket_list)):
-                    mapping.add((bucket_list[0], bucket_list[j]))
+            mapping = set()
+            for i in range(len(bucket_list)):
+                for j in range(1, len(bucket_list[i])):
+                    mapping.add((bucket_list[i][0], bucket_list[i][j]))
 
-        # Step 3 For each mapping reshape transients to fit data
 
-        # Step 4: For each mapping redirect edges and rename memlets in the whole tree.
-        for (new, old) in list(mapping):
-            for n in state.nodes():
-                if isinstance(n, nodes.AccessNode) and n.data == old:
-                    n.data = new
-                    for e in state.all_edges(n):
-                        for edge in state.memlet_tree(e):
-                            if edge.data.data == old:
-                                edge.data.data = new
-            sdfg.remove_data(old)
+            # Step 4: For each mapping redirect edges and rename memlets in the whole tree.
+            for (new, old) in list(mapping):
+                sdfg.arrays[new].shape = max(sdfg.arrays[new].shape, sdfg.arrays[old].shape)
+                for n in state.nodes():
+                    if isinstance(n, nodes.AccessNode) and n.data == old:
+                        n.data = new
+                        for e in state.all_edges(n):
+                            for edge in state.memlet_tree(e):
+                                if edge.data.data == old:
+                                    edge.data.data = new
+                sdfg.remove_data(old)
 
-        # Analyze memory savings
-        memory_after = 0
-        for a in sdfg.arrays:
-            memory_after += sdfg.arrays[a].total_size
-        print('memory before: ', memory_before, 'B')
-        print('memory after: ', memory_after, 'B')
-        print('memory savings: ', memory_before- memory_after, 'B ,', 100-N((100/memory_before)*memory_after, 2), "%")
+            # Analyze memory savings
+            memory_after = 0
+            for a in sdfg.arrays:
+                memory_after += sdfg.arrays[a].total_size
+
+            print(len(sdfg.arrays))
+            print('memory before: ', memory_before, 'B')
+            print('memory after: ', memory_after, 'B')
+            print('memory savings: ', memory_before- memory_after, 'B ,', 100-N((100/memory_before)*memory_after, 2), "%")
+
+    def payoff(shapes):
+        numpy = np.array(shapes)
+        prod = np.prod(numpy)
+        return np.prod(numpy) < np.sum(numpy)
--- tests/transformations/transient_reuse_test.py
+++ tests/transformations/transient_reuse_test.py
@@ -7,11 +7,12 @@ import numpy as np
 from dace.transformation.interstate.transient_reuse import TransientReuse
 
 M = dace.symbol('M')
-
+N = dace.symbol('N')
 
 @dace.program
-def operation(A: dace.float64[M, M], B: dace.float64[M, M], C: dace.float64[M, M]):
-    C[:] = A @ (A @ B) @ (A @ B) @ B
+def operation(A: dace.float64[5, 5], B: dace.float64[5, 5], C: dace.float64[5, 100], D: dace.float64[5, 100]):
+    C[:] = A @ (A @ B) @ (A @ B) @ (B @ D)
+
 
 
 if __name__ == "__main__":
@@ -19,27 +20,30 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("M", type=int, nargs="?", default=5)
+    parser.add_argument("N", type=int, nargs="?", default=100)
     args = vars(parser.parse_args())
 
     M.set(args["M"])
+    N.set(args["N"])
 
     # Initialize arrays: Randomize A and B, zero C
-    A = np.random.rand(M.get(), M.get()).astype(np.float64)
-    B = np.random.rand(M.get(), M.get()).astype(np.float64)
-    C = np.zeros([M.get(), M.get()], dtype=np.float64)
+    A = np.random.rand(5, 5).astype(np.float64)
+    B = np.random.rand(5, 5).astype(np.float64)
+    D = np.random.rand(5, 100).astype(np.float64)
+    C = np.zeros([5, 100], dtype=np.float64)
     C_regression = np.zeros_like(C)
 
     sdfg = operation.to_sdfg()
     sdfg.apply_transformations(TransientReuse)
-    sdfg(A=A, B=B, C=C, M=M)
+    sdfg(A=A, B=B, C=C, D=D)
 
     if dace.Config.get_bool('profiling'):
         dace.timethis('gemm', 'numpy', (2 * M.get() * M.get() * M.get()),
                       np.dot, A, B, C_regression)
     else:
-        C_regression = np.dot(np.dot(A, np.dot(np.dot(A, B), np.dot(A, B))), B)
+        C_regression = np.dot(np.dot(A, np.dot(np.dot(A, B), np.dot(A, B))), np.dot(B,D))
 
-    diff = np.linalg.norm(C_regression - C) / (M.get() * M.get())
+    diff = np.linalg.norm(C_regression - C) / (M.get() * N.get())
     print("Difference:", diff)
     print("==== Program end ====")
     exit(0 if diff <= 1e-5 else 1)
