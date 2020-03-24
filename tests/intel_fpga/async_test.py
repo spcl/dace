5875644ee2301612eb57b43a08e9bf1d2dadd61e
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-03-24 13:02:26 +0100

    Default to not use std::async, as this is causing problems with the Intel runtime

---
+++ tests/intel_fpga/async_test.py
@@ -0,0 +1,36 @@
+import os
+import subprocess as sp
+
+from simple_systolic_array import P, N, make_sdfg
+from dace.config import Config
+import dace.dtypes
+
+import numpy as np
+
+if __name__ == "__main__":
+
+    N.set(128)
+    P.set(4)
+
+    Config.set("compiler", "fpga_vendor", value="intel_fpga")
+    Config.set("compiler", "intel_fpga", "mode", value="emulator")
+
+    A = np.empty((N.get()), dtype=np.int32)
+
+    for v in [False, True]:
+
+        Config.set("compiler", "intel_fpga", "launch_async", value=v)
+
+        name = "async_test_{}".format(v)
+        sdfg = make_sdfg(name)
+        sdfg.specialize({"P": P.get(), "N": N.get()})
+        sdfg.compile()
+
+        # Compile the emulation kernel
+        build_folder = os.path.join(".dacecache", name, "build")
+        sp.run(["make", "intelfpga_compile_" + name + "_emulator"],
+               cwd=build_folder,
+               check=True)
+
+        # We don't care about the result, as long as it compiles and runs
+        sdfg(A=A)
