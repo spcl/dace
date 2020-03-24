828df9ad7d421d685cd64bd146f879e236d59e32
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2020-03-24 16:54:34 +0100

    Code refactoring and commenting

--- dace/transformation/interstate/transient_reuse.py
+++ dace/transformation/interstate/transient_reuse.py
@@ -37,13 +37,11 @@ class TransientReuse(pattern_matching.Transformation):
     def apply(self, sdfg):
         for state in sdfg.nodes():
 
-            #manage memory usage before transformation
             memory_before = 0
             for a in sdfg.arrays:
                 memory_before += sdfg.arrays[a].total_size
-            scope_dict = state.scope_dict(node_to_children=True)
 
-            #copy the whole graph
+            # Copy the whole graph
             G = nx.DiGraph()
             for n in state.nodes():
                 G.add_node(n)
@@ -51,15 +49,16 @@ class TransientReuse(pattern_matching.Transformation):
                 for e in state.all_edges(n):
                     G.add_edge(e.src, e.dst)
 
-            #collapse all mappings and their scopes into one node
+            # Collapse all mappings and their scopes into one node
+            scope_dict = state.scope_dict(node_to_children=True)
             for n in state.nodes():
-                if isinstance(n, nodes.MapEntry):
+                if isinstance(n, nodes.EntryNode):
                     for m in scope_dict[n]:
-                        if isinstance(m, nodes.MapExit):
+                        if isinstance(m, nodes.ExitNode):
                             G.add_edges_from([(n, x) for (y, x) in G.out_edges(m)])
                         G.remove_node(m)
 
-            #remove all nodes that are not AccessNodes and reconnect the corresponding edges
+            # Remove all nodes that are not AccessNodes and connect their predecessors and successors
             for n in state.nodes():
                 if not isinstance(n, nodes.AccessNode) and n in G.nodes():
                     for p in G.predecessors(n):
@@ -67,35 +66,32 @@ class TransientReuse(pattern_matching.Transformation):
                             G.add_edge(p,c)
                     G.remove_node(n)
 
-            #plot the resulting proxy graph
-            #import matplotlib.pyplot as plt
-            #nx.draw_networkx(G, with_labels=True)
-            #plt.show()
-
-            #setup the ancestors and successors arrays
+            # Setup the ancestors and successors arrays
             ancestors = {}
             successors = {}
             for n in G.nodes():
                 successors[n] = set(G.successors(n))
                 ancestors[n] = set(nx.ancestors(G, n))
 
-            #find all transients and set up the mappings dict
-            transients = []
+            # Find all transients and set up the mappings dict
+            transients = set()
             mappings = {}
             for n in G.nodes():
-                if sdfg.arrays[n.data].transient == True:
-                    transients.append(n.data)
+                if sdfg.arrays[n.data].transient:
+                    transients.add(n.data)
                     mappings[n.data] = set()
 
-
-            # Find valid mappings
-            for n in successors.keys():
-                for m in ancestors.keys():
+            # Find valid mappings. A mapping (n, m) is only valid if the successors of n
+            # are a subset of the ancestors of m.
+            for n in successors:
+                for m in ancestors:
                     if isinstance(m, nodes.AccessNode) and successors[n].issubset(ancestors[m]) and \
                             sdfg.arrays[n.data].transient and sdfg.arrays[m.data].transient:
                         mappings[n.data].add(m.data)
 
-            # find a final mapping
+            # Find a final mapping, greedy coloring algorithm to find a mapping.
+            # Only add a transient to a bucket if either there is a mapping from it to all other elements
+            # or there is a mapping from all nodes to it.
             buckets = []
             for i in range(len(transients)):
                 buckets.append([])
@@ -105,26 +101,27 @@ class TransientReuse(pattern_matching.Transformation):
                     if buckets[i] == []:
                         buckets[i].append(n)
                         break
+
                     temp = True
                     for j in range(len(buckets[i])):
                         temp = (temp and n in mappings[buckets[i][j]]
                                 and sdfg.arrays[n].shape == sdfg.arrays[buckets[i][j]].shape
                                 and sdfg.arrays[n].strides == sdfg.arrays[buckets[i][j]].strides
-                                and sdfg.arrays[n].dtype == sdfg.arrays[buckets[i][j]].dtype
-                                and sdfg.arrays[n].offset == sdfg.arrays[buckets[i][j]].offset
-                                and sdfg.arrays[n].allow_conflicts == sdfg.arrays[buckets[i][j]].allow_conflicts
                                 )
-                        print(sdfg.arrays[n].strides,sdfg.arrays[buckets[i][j]].strides)
                     if temp:
                         buckets[i].append(n)
                         break
+
                     temp2 = True
                     for j in range(len(buckets[i])):
-                        temp2 = temp2 and buckets[i][j] in mappings[n] and sdfg.arrays[n].shape == sdfg.arrays[buckets[i][j]].shape and sdfg.arrays[n].strides == sdfg.arrays[buckets[i][j]].strides
+                        temp2 = (temp2 and buckets[i][j] in mappings[n]
+                                 and sdfg.arrays[n].shape == sdfg.arrays[buckets[i][j]].shape
+                                 and sdfg.arrays[n].strides == sdfg.arrays[buckets[i][j]].strides)
                     if temp2:
                         buckets[i].insert(0, n)
                         break
 
+            # Build new custom transient to replace the other transients
             for i in range(len(transients)):
                 if len(buckets[i]) > 1:
                     local_ctr = str(next(_atomic_count))
@@ -146,14 +143,14 @@ class TransientReuse(pattern_matching.Transformation):
                     )
                     buckets[i].insert(0, name)
 
+            # Construct final mapping (transient_reuse_i, some_transient)
             mapping = set()
             for i in range(len(buckets)):
                 for j in range(1, len(buckets[i])):
                     mapping.add((buckets[i][0], buckets[i][j]))
 
-            # Step 5: For each mapping redirect edges and rename memlets in the whole tree.
-            for (new, old) in list(mapping):
-                sdfg.arrays[new].shape = max(sdfg.arrays[new].shape, sdfg.arrays[old].shape)
+            # For each mapping redirect edges, rename memlets in the whole tree and remove the old array
+            for (new, old) in sorted(list(mapping)):
                 for n in state.nodes():
                     if isinstance(n, nodes.AccessNode) and n.data == old:
                         n.data = new
