a3677499d2acedb3ea1f3a4670a0f3b69c23716a
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-03-30 15:03:12 +0200

    Run test in multiprocessing environment

--- tests/intel_fpga/async.py
+++ tests/intel_fpga/async.py
@@ -1,11 +1,22 @@
 import os
-import subprocess as sp
+import multiprocessing as mp
 
 from simple_systolic_array import P, N, make_sdfg
 from dace.config import Config
 import dace.dtypes
 
 import numpy as np
+
+def run_test(do_async):
+
+    Config.set("compiler", "intel_fpga", "launch_async", value=do_async)
+
+    name = "async_test"
+    sdfg = make_sdfg(name)
+    sdfg.specialize({"P": P.get(), "N": N.get()})
+    # We don't care about the result, as long as it compiles and runs
+    sdfg(A=A)
+
 
 if __name__ == "__main__":
 
@@ -18,12 +29,8 @@ if __name__ == "__main__":
     A = np.empty((N.get()), dtype=np.int32)
 
     for v in [False, True]:
-
-        Config.set("compiler", "intel_fpga", "launch_async", value=v)
-
-        name = "async_test"
-        sdfg = make_sdfg(name)
-        sdfg.specialize({"P": P.get(), "N": N.get()})
-        # We don't care about the result, as long as it compiles and runs
-        sdfg(A=A)
-
+        # Has to be a separate process, as the Intel FPGA runtime cannot be
+        # initialized twice in the same executable
+        p = mp.Process(target=run_test, args=(v,))
+        p.start()
+        p.join()
