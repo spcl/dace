82cb5565844635899e2bb887a692c78ebec2766d
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-04-29 13:36:54 +0200

    Add degenerate loop test to both Intel and Xilinx

--- tests/intel_fpga_test.sh
+++ tests/intel_fpga_test.sh
@@ -52,14 +52,15 @@ run_sample() {
 
 run_all() {
 
-    run_sample remove_degenerate_loop remove_degenerate_loop_test "\n" 
-
     #### VECTORIZATION ####
     # Vectorization 1: first vectorize and then transform for FPGA
     run_sample intel_fpga/vec_sum vec_sum "Vectorization\$0(propagate_parent=True)\nFPGATransformSDFG\$0\n"
     # Vectorization 2: first transform for FPGA then vectorize
     run_sample intel_fpga/vec_sum vec_sum "FPGATransformSDFG\$0\nVectorization\$0(propagate_parent=True)\n"
     # Vectorization 3: TODO non vectorizable N
+
+    # Test removing degenerate loops that only have a single iteration
+    run_sample remove_degenerate_loop remove_degenerate_loop_test "\n" 
 
     # ### MAP TILING ####
     # First tile then transform
