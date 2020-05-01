51b6bc43939cd6b09a7133af4bfa86c11f9e6b16
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-05-01 13:45:17 +0200

    Shift register storage type

--- dace/dtypes.py
+++ dace/dtypes.py
@@ -26,6 +26,7 @@ class StorageType(aenum.AutoNumberEnum):
     FPGA_Global = ()  # Off-chip global memory (DRAM)
     FPGA_Local = ()  # On-chip memory (bulk storage)
     FPGA_Registers = ()  # On-chip memory (fully partitioned registers)
+    ShiftRegister = ()  # Only accessible at constant indices
 
 
 @extensible_enum
