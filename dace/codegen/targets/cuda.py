da34e31797a307344b7c9be99dd7847db3a04774
Jan <test@bla.com>
2020-05-04 10:02:13 +0200

    Dynamic thread-block scheduler works

--- dace/codegen/targets/cuda.py
+++ dace/codegen/targets/cuda.py
@@ -1041,7 +1041,7 @@ void __dace_alloc_{location}(uint32_t size, dace::GPUStream<{type}, {is_pow2}>& 
             symbol_sigs.append('cub::GridBarrier __gbar')
 
         # Comprehend grid/block dimensions from scopes
-        grid_dims, block_dims, tbmap = self.get_kernel_dimensions(dfg_scope)
+        grid_dims, block_dims, tbmap, dtbmap = self.get_kernel_dimensions(dfg_scope)
 
         kernel_args = [
             sdfg.arrays[p].signature(False, name=p) for p in sorted(params)
@@ -1068,7 +1068,7 @@ void __dace_alloc_{location}(uint32_t size, dace::GPUStream<{type}, {is_pow2}>& 
         kernel_stream = CodeIOStream()
         self.generate_kernel_scope(sdfg, dfg_scope, state_id, scope_entry.map,
                                    kernel_name, grid_dims, block_dims, tbmap,
-                                   kernel_args_typed, self._globalcode,
+                                   dtbmap, kernel_args_typed, self._globalcode,
                                    kernel_stream)
 
         # Write kernel prototype
@@ -1238,7 +1238,11 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
                     and n.schedule in (dtypes.ScheduleType.GPU_ThreadBlock, dtypes.ScheduleType.GPU_ThreadBlock_Dynamic)
                 ])
 
-        print("JAN DEBUG 2: %s" % tb_maps)
+        has_dtbmap = len([tbmap for tbmap in tb_maps
+                          if tbmap.schedule == dtypes.ScheduleType.GPU_ThreadBlock_Dynamic
+                          ]) > 0
+
+        tb_maps = [tbmap for tbmap in tb_maps if tbmap.schedule == dtypes.ScheduleType.GPU_ThreadBlock]
 
         # Case (1): no thread-block maps
         if len(tb_maps) == 0:
@@ -1246,10 +1250,16 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
             warnings.warn('Thread-block maps not found in kernel, assuming ' +
                           'block size of (%s)' %
                           Config.get('compiler', 'cuda', 'default_block_size'))
-            block_size = [
-                int(b) for b in Config.get('compiler', 'cuda',
-                                           'default_block_size').split(',')
-            ]
+            if has_dtbmap:
+                block_size = [
+                    int(b) for b in Config.get('compiler', 'cuda',
+                                               'dynamic_map_block_size').split(',')
+                ]
+            else:
+                block_size = [
+                    int(b) for b in Config.get('compiler', 'cuda',
+                                               'default_block_size').split(',')
+                ]
             assert (len(block_size) >= 1 and len(block_size) <= 3)
 
             int_ceil = sympy.Function('int_ceil')
@@ -1259,9 +1269,7 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
                 int_ceil(gs, bs) for gs, bs in zip(grid_size, block_size)
             ]
 
-            return grid_size, block_size, False
-
-        print("JAN DEBUG 2: %s" % tb_maps)
+            return grid_size, block_size, False, has_dtbmap
 
         # Find all thread-block maps to determine overall block size
         block_size = [1, 1, 1]
@@ -1272,10 +1280,9 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
             # Over-approximate block size (e.g. min(N,(i+1)*32)-i*32 --> 32)
             # The partial trailing thread-block is emitted as an if-condition
             # that returns on some of the participating threads
-            if tbmap.schedule == dtypes.ScheduleType.GPU_ThreadBlock_Dynamic:
-                tbsize = [128] # TODO don't hard-code block-size!!!
-            else:
-                tbsize = [symbolic.overapproximate(s) for s in tbsize]
+            tbsize = [symbolic.overapproximate(s) for s in tbsize]
+
+            print("JAN DEBUG: %s" % tbsize)
 
             # Linearize (flatten) rest of dimensions to third
             if len(tbsize) > 3:
@@ -1295,12 +1302,12 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
         #       kernel, raise an invalid SDFG exception and recommend
         #       overapproximation.
 
-        return grid_size, block_size, True
+        return grid_size, block_size, True, has_dtbmap
 
     def generate_kernel_scope(
         self, sdfg: SDFG, dfg_scope: ScopeSubgraphView, state_id: int,
         kernel_map: nodes.Map, kernel_name: str, grid_dims: list,
-        block_dims: list, has_tbmap: bool, kernel_params: list,
+        block_dims: list, has_tbmap: bool, has_dtbmap: bool, kernel_params: list,
         function_stream: CodeIOStream, kernel_stream: CodeIOStream):
         node = dfg_scope.source_nodes()[0]
 
@@ -1332,8 +1339,7 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
             else:
                 block_expr = 'blockIdx.%s' % _named_idx(i)
                 # If we defaulted to 32 threads per block, offset by thread ID
-                if not has_tbmap:
-                    print("WHYYYYYY?")
+                if not has_tbmap or has_dtbmap:
                     block_expr = '(%s * %s + threadIdx.%s)' % (
                         block_expr, _topy(block_dims[i]), _named_idx(i))
 
@@ -1383,7 +1389,7 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
 
         # Generate conditions for this block's execution using min and max
         # element, e.g., skipping out-of-bounds threads in trailing block
-        if has_tbmap == False:
+        if has_tbmap == False and has_dtbmap == False:
             dsym_end = [d + bs - 1 for d, bs in zip(dsym, self._block_dims)]
             minels = krange.min_element()
             maxels = krange.max_element()
@@ -1414,7 +1420,7 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
                                            kernel_stream,
                                            skip_entry_node=True)
 
-        if has_tbmap == False:
+        if has_tbmap == False and has_dtbmap == False:
             for _ in kernel_map.params:
                 kernel_stream.write('}\n', sdfg, state_id, node)
 
@@ -1509,7 +1515,7 @@ cudaLaunchKernel((void*){kname}, dim3({gdims}), dim3({bdims}), {kname}_args, {dy
                 sdfg, state_id, scope_entry)
 
             callsite_stream.write(
-                'dace::DynamicMap<{bsize}>::template '
+                'dace::DynamicMap<true, true, {bsize}>::template '
                 'schedule(__dace_dynmap_begin, __dace_dynmap_end, {kmapIdx}, [&](auto {kmapIdx}, '
                 'auto {param}) {{'.format(bsize=total_block_size,
                                           param=scope_map.params[0],
