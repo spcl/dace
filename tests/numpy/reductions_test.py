75e5215cc29969711be998b8e422231d3b258a00
Oliver Rausch <oliverrausch99@gmail.com>
2020-05-06 17:45:45 +0200

    Update compare_numpy_output to take parameters

--- tests/numpy/reductions_test.py
+++ tests/numpy/reductions_test.py
@@ -4,82 +4,82 @@ from copy import deepcopy as dc
 from common import compare_numpy_output
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_sum(A: dace.float64[10, 5, 3]):
     return np.sum(A)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_sum_1(A: dace.float64[10, 5, 3]):
     return np.sum(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_min(A: dace.float64[10, 5, 3]):
     return np.min(A)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_max(A: dace.float64[10, 5, 3]):
     return np.max(A)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_min_1(A: dace.float64[10, 5, 3]):
     return np.min(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_min_int32(A: dace.int32[10, 5, 3]):
     return np.min(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_min_int64(A: dace.int64[10, 5, 3]):
     return np.min(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_max_int32(A: dace.int32[10, 5, 3]):
     return np.max(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_max_int64(A: dace.int64[10, 5, 3]):
     return np.max(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_max_1(A: dace.float64[10, 5, 3]):
     return np.max(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_argmax_1(A: dace.float64[10, 5, 3]):
     return np.argmax(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_argmin_1(A: dace.float64[10, 5, 3]):
     return np.argmin(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_argmin_1_int32(A: dace.int32[10, 5, 3]):
     return np.argmin(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_argmin_1_int64(A: dace.int64[10, 5, 3]):
     return np.argmin(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_argmax_1_int32(A: dace.int32[10, 5, 3]):
     return np.argmax(A, axis=1)
 
 
-@compare_numpy_output
+@compare_numpy_output()
 def test_argmax_1_int64(A: dace.int64[10, 5, 3]):
     return np.argmax(A, axis=1)
 
