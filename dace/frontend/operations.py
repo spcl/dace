c2b27b5298d74457be7a84a343797856c6294def
JanKleine <jkleine@student.ethz.ch>
2020-06-25 08:25:27 +0200

    Add optional tqdm status bar to dace profiler

--- dace/frontend/operations.py
+++ dace/frontend/operations.py
@@ -30,7 +30,16 @@ def timethis(program, title, flop_count, f, *args, **kwargs):
     REPS = int(Config.get('treps'))
     times = [start] * (REPS + 1)
     ret = None
-    for i in range(REPS):
+    print('\nProfiling...')
+    iterator = range(REPS)
+    try:
+        from tqdm import tqdm
+        iterator = tqdm(iterator)
+    except ImportError:
+        print('WARNING: Cannot show profiling progress, missing optional '
+              'dependency tqdm...\n\tTo see a live progress bar please install '
+              'tqdm (`pip install tqdm`)')
+    for i in iterator:
         # Call function
         ret = f(*args, **kwargs)
         times[i + 1] = timer()
