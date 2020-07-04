c1a3b5df6c37123cc7746693baf18637da4cdd5a
Tal Ben-Nun <tbennun@gmail.com>
2020-07-04 20:39:57 +0200

    Fix test

--- tests/transformations/vectorization_test.py
+++ tests/transformations/vectorization_test.py
@@ -38,6 +38,7 @@ def test_vectorization_uneven():
     sdfg(A=A, N=20)
     assert np.allclose(A, result)
 
+    sdfg.apply_strict_transformations()
     assert sdfg.apply_transformations(Vectorization, options={'vector_len':
                                                               2}) == 1
     assert 'vec<double, 2>' in sdfg.generate_code()[0].code
