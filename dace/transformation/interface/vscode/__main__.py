8dbd76b6439f0ce9feb6bfb4c09bcf25128ed767
Philipp Schaad <schaad.phil@gmail.com>
2020-07-09 17:24:10 +0200

    VC-INTERFACE: Added option to launch as deamon or direct mode

--- dace/transformation/interface/vscode/__main__.py
+++ dace/transformation/interface/vscode/__main__.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from argparse import ArgumentParser
 
-deamon = Flask('dace.transformation.interface.vscode')
-deamon.config['DEBUG'] = True
+def get_transformations(sdfg):
+    print("ok")
 
-@deamon.route('/', methods=['GET'])
-def root():
-    return 'success!'
+def run_daemon():
+    from flask import Flask
+
+    deamon = Flask('dace.transformation.interface.vscode')
+    deamon.config['DEBUG'] = False
+
+    @deamon.route('/', methods=['GET'])
+    def root():
+        return 'success!'
+
+    deamon.run()
 
 if __name__ == '__main__':
-    deamon.run()
+    parser = ArgumentParser()
+
+    '''
+    parser.add_argument('-d',
+                        '--daemon',
+                        action='store_true',
+                        help='Run as a daemon')
+                        '''
+
+    parser.add_argument('-t',
+                        '--transformations',
+                        action='store_true',
+                        help='Get applicable transformations for an SDFG')
+
+    args = parser.parse_args()
+
+    if (args.transformations):
+        get_transformations(None)
+    else:
+        run_daemon()
