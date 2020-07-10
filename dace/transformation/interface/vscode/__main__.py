6eb8fbfc8d86c68b04d77384dfd75850099774d5
Philipp Schaad <schaad.phil@gmail.com>
2020-07-10 15:48:19 +0200

    VC-INTERFACE: Global pattern matching to list transformations

--- dace/transformation/interface/vscode/__main__.py
+++ dace/transformation/interface/vscode/__main__.py
@@ -1,19 +1,58 @@
+import json
 from argparse import ArgumentParser
 
 def get_transformations(sdfg):
-    print("ok")
+    # We lazy import DaCe, not to break cyclic imports, but to avoid any large
+    # delays when booting in daemon mode.
+    from dace.transformation.optimizer import SDFGOptimizer
+    from dace.sdfg import SDFG
+
+    sdfg_json = json.loads(sdfg)
+    sdfg_object = SDFG.from_json(sdfg_json)
+    optimizer = SDFGOptimizer(sdfg_object)
+    matches = optimizer.get_pattern_matches()
+
+    transformations = []
+    for transformation in matches:
+        parameters = []
+        node_ids = []
+        if transformation is not None:
+            sdfg_id = transformation.sdfg_id
+            state_id = transformation.state_id
+            nodes = list(transformation.subgraph.values())
+            for node in nodes:
+                node_ids.append([sdfg_id, state_id, node])
+
+        transformations.append({
+            'label': transformation.__str__(),
+            'parameters': parameters,
+            'affected_nodes': node_ids,
+            'children': [],
+        })
+        # if set(transformation.subgraph.values()) & viewed_nodes:
+        #     pass
+
+    return {
+        'transformations': transformations,
+    }
 
 def run_daemon():
-    from flask import Flask
+    from flask import Flask, request
 
-    deamon = Flask('dace.transformation.interface.vscode')
-    deamon.config['DEBUG'] = False
+    daemon = Flask('dace.transformation.interface.vscode')
+    daemon.config['DEBUG'] = False
 
-    @deamon.route('/', methods=['GET'])
+    @daemon.route('/', methods=['GET'])
     def root():
         return 'success!'
 
-    deamon.run()
+    @daemon.route('/transformations', methods=['POST'])
+    def transformations():
+        sdfg = request.get_json()
+        transformation_json = get_transformations(sdfg)
+        return transformation_json
+
+    daemon.run()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -23,6 +62,14 @@ if __name__ == '__main__':
                         '--daemon',
                         action='store_true',
                         help='Run as a daemon')
+                        '''
+
+    '''
+    parser.add_argument('-p',
+                        '--port',
+                        action='store',
+                        type='int',
+                        help='The port to listen on')
                         '''
 
     parser.add_argument('-t',
