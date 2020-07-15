41e920a31e7bb3765c1badf3c8cebda9c212a96a
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2020-07-15 13:41:09 +0200

    cuda cmake include

--- dace/libraries/cudnn/cudnn.py
+++ dace/libraries/cudnn/cudnn.py
@@ -1,5 +1,11 @@
 import dace.library
+import os
 
+def _find_cuda_include():
+    if 'CUDADIR' in os.environ:
+        return [os.path.join(os.environ['CUDADIR'], 'include')]
+    else:
+        return []
 
 @dace.library.environment
 class cuDNN:
@@ -7,7 +13,7 @@ class cuDNN:
     cmake_minimum_version = None
     cmake_packages = []
     cmake_variables = {}
-    cmake_includes = []
+    cmake_includes = _find_cuda_include()
     cmake_libraries = ["cudnn"]
     cmake_compile_flags = []
     cmake_link_flags = []
