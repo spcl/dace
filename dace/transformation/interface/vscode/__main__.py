d9cd1284e8c7b05a8ff93f0e4d03004ae518d8a6
Philipp Schaad <schaad.phil@gmail.com>
2020-07-16 14:54:05 +0200

    Added transformation (de)serialization to/from JSON

--- dace/transformation/interface/vscode/__main__.py
+++ dace/transformation/interface/vscode/__main__.py
@@ -4,20 +4,14 @@ from argparse import ArgumentParser
 def apply_transformation(sdfg, transformation):
     # We lazy import DaCe, not to break cyclic imports, but to avoid any large
     # delays when booting in daemon mode.
-    from dace.transformation.optimizer import SDFGOptimizer
     from dace.transformation.pattern_matching import Transformation
     from dace.sdfg import SDFG
 
     sdfg_json = json.loads(sdfg)
     sdfg_object = SDFG.from_json(sdfg_json)
-    optimizer = SDFGOptimizer(sdfg_object)
 
-    transformation_obj = next(t for t in Transformation.extensions().keys()
-                              if t.__name__ == transformation['label'])
-    matching = optimizer.get_pattern_matches(patterns=[transformation_obj])
-
-    for pattern in matching:
-        pattern.apply_pattern(sdfg_object)
+    revived_transformation = Transformation.from_json(transformation)
+    revived_transformation.apply_pattern(sdfg_object)
 
     new_sdfg = sdfg_object.to_json()
     return {
@@ -37,24 +31,7 @@ def get_transformations(sdfg):
 
     transformations = []
     for transformation in matches:
-        parameters = []
-        node_ids = []
-        if transformation is not None:
-            sdfg_id = transformation.sdfg_id
-            state_id = transformation.state_id
-            nodes = list(transformation.subgraph.values())
-            for node in nodes:
-                node_ids.append([sdfg_id, state_id, node])
-            #print(transformation.to_json())
-
-        transformations.append({
-            'label': transformation.__str__(),
-            'parameters': parameters,
-            'affected_nodes': node_ids,
-            'children': [],
-        })
-        # if set(transformation.subgraph.values()) & viewed_nodes:
-        #     pass
+        transformations.append(transformation.to_json())
 
     return {
         'transformations': transformations,
