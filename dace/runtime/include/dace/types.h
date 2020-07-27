bf7b16aa40b48c2d5a0cc7ea2630757a0c9ff95b
Tal Ben-Nun <tbennun@gmail.com>
2020-07-27 11:48:00 +0200

    Better support for pi, constexpr, and other constants in modules

--- dace/runtime/include/dace/types.h
+++ dace/runtime/include/dace/types.h
@@ -19,7 +19,7 @@
 #endif
 
 // Visual Studio (<=2017) + CUDA support
-#if defined(_MSC_VER) && (_MSC_VER <= 1999 || defined(__CUDACC__)) || defined(DACE_XILINX)
+#if defined(_MSC_VER) && _MSC_VER <= 1999
 #define DACE_CONSTEXPR
 #else
 #define DACE_CONSTEXPR constexpr
