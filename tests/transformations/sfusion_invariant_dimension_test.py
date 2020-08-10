57a5b9668e0a55ca9e9ec94f31aa7b224b5960f4
luca <sancierra@gmail.com>
2020-08-10 17:44:41 +0200

    avoid bug in unittest

--- tests/transformations/sfusion_invariant_dimension_test.py
+++ tests/transformations/sfusion_invariant_dimension_test.py
@@ -6,7 +6,10 @@ from dace.transformation.subgraph.helpers import *
 import dace.sdfg.nodes as nodes
 import numpy as np
 
+import itertools
+
 from dace.sdfg.graph import SubgraphView
+from dace.sdfg.utils import *
 
 
 from dace.transformation.subgraph.pipeline import expand_reduce, expand_maps, fusion
@@ -45,10 +48,65 @@ def TEST(A: dace.float64[N,M,O], B: dace.float64[N,M,O], C: dace.float64[N,M,O])
 
                 out = 2*in1 + 2*in2 + in3
 
+@dace.program
+def FIX(AA: dace.float64[O], BB: dace.float64[O], CC: dace.float64[O]):
+    for z in range(1,0):
+        with dace.tasklet:
+            in1 << AA[z]
+            in2 << BB[z]
+            in3 << CC[0]
+            out >> CC[z]
+
+            out = 2*in1 + 2*in2 + in3
+
+
+
+
+
 def test_qualitatively(sdfg, graph):
     fusion(sdfg, graph)
     sdfg.validate()
     print("PASS")
+
+def fix_sdfg(sdfg, graph):
+    for node in graph.nodes():
+        if isinstance(node, dace.sdfg.nodes.NestedSDFG):
+            nested_original = node
+            for edge in itertools.chain(graph.in_edges(node), graph.out_edges(node)):
+                for e in graph.memlet_tree(edge):
+                    if 'z' in e.data.subset.free_symbols:
+                        new_subset = str(e.data.subset)
+                        new_subset = new_subset.replace('z','0:O')
+                        e.data.subset = subsets.Range.from_string(new_subset)
+
+    # next up replace sdfg
+    inner_sdfg = FIX.to_sdfg()
+    nnode = graph.add_nested_sdfg(inner_sdfg, sdfg, {'AA','BB','CC'}, {'CC'})
+    # redirect edges
+    for e in graph.in_edges(nested_original):
+        if e.dst_conn == '__tmp_44_23':
+            graph.add_edge(e.src, e.src_conn,
+                           e.dst, 'AA', e.data)
+            graph.remove_edge(e)
+        if e.dst_conn == '__tmp_45_23':
+            graph.add_edge(e.src, e.src_conn,
+                           e.dst, 'BB', e.data)
+            graph.remove_edge(e)
+        if e.dst_conn == '__tmp_46_23':
+            graph.add_edge(e.src, e.src_conn,
+                           e.dst, 'CC', e.data)
+            graph.remove_edge(e)
+    e = graph.out_edges(nested_original)[0]
+    graph.add_edge(e.src, 'CC', e.dst, e.dst_conn, e.data)
+    graph.remove_edge(e)
+
+
+    change_edge_dest(graph, nested_original, nnode)
+    change_edge_src(graph, nested_original, nnode)
+    graph.remove_node(nested_original)
+    sdfg.validate()
+
+
 
 def test_quantitatively(sdfg, graph):
     A = np.random.rand(N.get(), M.get(), O.get()).astype(np.float64)
@@ -65,10 +123,11 @@ def test_quantitatively(sdfg, graph):
     csdfg(A=A,B=B,C=C2,N=N,M=M,O=O)
 
     assert np.allclose(C1,C2)
-
+    print('PASS')
 
 if __name__ == '__main__':
     sdfg = TEST.to_sdfg()
     sdfg.apply_strict_transformations()
     graph = sdfg.nodes()[0]
+    fix_sdfg(sdfg, graph)
     test_quantitatively(sdfg, graph)
