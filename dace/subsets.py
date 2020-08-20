017675fb6d73fd5eba6e94b251e3610eea9983c5
luca <sancierra@gmail.com>
2020-08-20 20:54:07 +0200

    subsets min_element_approx

--- dace/subsets.py
+++ dace/subsets.py
@@ -227,6 +227,9 @@ class Range(Subset):
     def max_element_approx(self):
         return [_approx(x[1]) for x in self.ranges]
 
+    def min_element_approx(self):
+        return [_approx(x[0]) for x in self.ranges]
+
     def coord_at(self, i):
         """ Returns the offseted coordinates of this subset at
             the given index tuple.
@@ -672,6 +675,9 @@ class Indices(Subset):
     def max_element_approx(self):
         return [_approx(ind) for ind in self.indices]
 
+    def min_element_approx(self):
+        return [_approx(ind) for ind in self.indices]
+
     def data_dims(self):
         return 0
 
@@ -791,6 +797,12 @@ class Indices(Subset):
         for i, ind in enumerate(self.indices):
             self.indices[i] = (ind.subs(repl_dict)
                                if symbolic.issymbolic(ind) else ind)
+    def pop(self, dimensions):
+        new_indices = []
+        for i in range(len(self.indices)):
+            if i not in dimensions:
+                new_indices.append(self.indices[i])
+        self.indices = new_indices
 
 
 def bounding_box_union(subset_a: Subset, subset_b: Subset) -> Range:
@@ -798,10 +810,38 @@ def bounding_box_union(subset_a: Subset, subset_b: Subset) -> Range:
     if subset_a.dims() != subset_b.dims():
         raise ValueError('Dimension mismatch between %s and %s' %
                          (str(subset_a), str(subset_b)))
-
+    '''
     result = [(min(arb, brb), max(are, bre), 1) for arb, brb, are, bre in zip(
         subset_a.min_element(), subset_b.min_element(), subset_a.max_element(),
         subset_b.max_element())]
+    return Range(result)
+    '''
+
+    result = []
+    for arb, brb, are, bre in zip(subset_a.min_element(), subset_b.min_element(),
+                                  subset_a.max_element(), subset_b.max_element()):
+        try:
+            minrb = min(arb, brb)
+        except TypeError:
+            if len(arb.free_symbols) == 0:
+                minrb = arb
+            elif len(brb.free_symbols) == 0:
+                minrb = brb
+            else:
+                raise
+
+        try:
+            maxre = max(are, bre)
+        except TypeError:
+            if len(are.free_symbols) == 0:
+                maxre = bre
+            elif len(bre.free_symbols) == 0:
+                maxre = are
+            else:
+                raise
+        result.append((minrb, maxre, 1))
+
+
     return Range(result)
 
 
