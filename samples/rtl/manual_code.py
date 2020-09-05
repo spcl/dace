cac2d06335ad7c964b920a3f9e1d3d9a8284f8c0
andreaskuster <mail@andreaskuster.ch>
2020-09-05 20:45:37 +0200

    Remove unused features. First working integration.

--- samples/rtl/manual_code.py
+++ samples/rtl/manual_code.py
@@ -1,13 +1,12 @@
 ''' example rtl tasklet '''
 import dace
 import os
-import subprocess
 
 import numpy as np
 
 # add symbol
-N = dace.symbol('N')
-BIT_WIDTH = dace.symbol('BIT_WIDTH')
+#N = dace.symbol('N')
+#BIT_WIDTH = dace.symbol('BIT_WIDTH')
 
 # add sdfg
 sdfg = dace.SDFG('rtl_tasklet_demo')
@@ -16,13 +15,18 @@ sdfg = dace.SDFG('rtl_tasklet_demo')
 state = sdfg.add_state()
 
 # add arrays
-sdfg.add_array('A', [N], dtype=dace.int32)
-sdfg.add_array('B', [N], dtype=dace.int32)
+sdfg.add_array('A', [1], dtype=dace.int32)
+sdfg.add_array('B', [1], dtype=dace.int32)
 
 # add header includes (verilator & verilated rtl design)
 sdfg.append_global_code(cpp_code='''
         // include common routines
         #include <verilated.h>
+        
+        
+
+#include <iostream>
+
         
         // include model header, generated from verilating "top.v"
         #include "Vtop.h"
@@ -41,14 +45,13 @@ tasklet = state.add_tasklet(
     inputs={'a'},
     outputs={'b'},
     code='''
-   
         // Construct the Verilated model, from Vtop.h generated from Verilating "top.v"
         Vtop* top = new Vtop;  // Or use a const unique_ptr, or the VL_UNIQUE_PTR wrapper
     
         // apply initial input values
         top->rst_i = 0;
         top->clk_i = 0;
-        top->a = *a;
+        top->a = a;
         top->eval();
     
         // apply reset signal and simulate
@@ -78,8 +81,8 @@ tasklet = state.add_tasklet(
             top->eval();
     
             // read outputs
-            VL_PRINTF("[%x] clk_i=%x rst_i=%x a=%x b=%x\\n", main_time, top->clk_i, top->rst_i, top->a, top->b);
-    
+            VL_PRINTF("[%lx] clk_i=%x rst_i=%x a=%x b=%x\\n", main_time, top->clk_i, top->rst_i, top->a, top->b);
+
             // negative clock edge
             top->clk_i = !top->clk_i;
             top->eval();
@@ -87,7 +90,8 @@ tasklet = state.add_tasklet(
     
         // write result
         int b_local = (int)top->b;
-        b = &b_local;
+        std::cout << b_local << std::endl;
+        b = b_local;
     
         // final model cleanup
         top->final();
@@ -98,14 +102,13 @@ tasklet = state.add_tasklet(
         ''',
     language=dace.Language.CPP)
 
-
 # add input/output array
 A = state.add_read('A')
 B = state.add_write('B')
 
 # connect input/output array with the tasklet
-state.add_edge(A, None, tasklet, 'a', dace.Memlet.simple('A', '0:N-1'))
-state.add_edge(tasklet, 'b', B, None, dace.Memlet.simple('B', '0:N-1'))
+state.add_edge(A, None, tasklet, 'a', dace.Memlet.simple('A', '0'))
+state.add_edge(tasklet, 'b', B, None, dace.Memlet.simple('B', '0'))
 
 # validate sdfg
 sdfg.validate()
@@ -125,7 +128,7 @@ module top
   , output reg [WIDTH-1:0] b
   );
 
-  always_ff @(posedge clk_i) begin
+  always@(posedge clk_i) begin
     if (rst_i)
       b <= a;
     else
@@ -133,8 +136,8 @@ module top
   end    
 
 
-  always@ (*) begin
-      if (b >= 10) begin
+  always@(*) begin
+      if (b >= 100) begin
         $finish;
       end
    end
@@ -144,30 +147,25 @@ endmodule
 
 # write verilog to file
 import shutil
+
 if os.path.isdir(absolut_path):
     shutil.rmtree(absolut_path)
 os.mkdir(absolut_path)
 with open(os.path.join(absolut_path, "top.v"), "w") as file:
     file.writelines(code)
 
-# call verilator
-subprocess.call(['verilator', '-Wall', '-cc', 'top.v'], cwd=absolut_path)
-
-print()
-
 ######################################################################
 
 if __name__ == '__main__':
     # init data structures
-    n = 1
-    a = np.random.randint(0, 100, n).astype(np.int32)
-    b = np.random.randint(0, 100, n).astype(np.int32)
+    a = np.random.randint(0, 100, 1).astype(np.int32)
+    b = np.random.randint(0, 100, 1).astype(np.int32)
 
     # show initial values
     print("a={}, b={}".format(a, b))
 
     # call program
-    sdfg(A=a, B=b, N=n) #, BIT_WIDTH=32)
+    sdfg(A=a, B=b)
 
     # show result
     print("a={}, b={}".format(a, b))
