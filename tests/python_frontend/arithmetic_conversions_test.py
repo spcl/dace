b27cbc103131eafc19ce1b8b74d3c66d14b1c7fd
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2020-09-16 17:28:58 +0200

    Updated arithmetic conversions test.

--- tests/python_frontend/arithmetic_conversions_test.py
+++ tests/python_frontend/arithmetic_conversions_test.py
@@ -8,22 +8,17 @@ def add(A: dace.complex64[5, 5], B: dace.float64[5, 5]):
 
 def test_add():
     A = np.random.randint(0, high=10, size=(5, 5), dtype=np.uint64).astype(np.complex64)
-    B = np.random.randint(-10, high=0, size=(5, 5), dtype=np.int32).astype(np.float64pty)
+    B = np.random.randint(-10, high=0, size=(5, 5), dtype=np.int32).astype(np.float64)
     C = add(A, B)
-    print(A)
-    print(B)
-    print(C)
-    print(A + B)
     assert(np.linalg.norm(C - A - B) / np.linalg.norm(A + B) < 1e-12)
 
 
 @dace.program
-def complex_conversion(a: dace.complex128[1], b: int):
+def complex_conversion(a: dace.complex128[1], b: dace.int32):
     return a[0] + b
 
 
 def test_complex_conversion():
-    # a = 5 + 6j
     a = np.zeros((1,), dtype=np.complex128)
     a[0] = 5 + 6j
     b = 7
@@ -32,15 +27,15 @@ def test_complex_conversion():
 
 
 @dace.program
-def float_conversion(a: float, b: int):
+def float_conversion(a: dace.float32, b: dace.int64):
     return a + b
 
 
 def test_float_conversion():
-    a = 5.2
-    b = 7
+    a = np.float32(5.2)
+    b = np.int64(7)
     c = float_conversion(a=a, b=b)
-    assert(c[0] == 12.2)
+    assert(c[0] == a + b)
 
 
 if __name__ == "__main__":
