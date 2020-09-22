b658c5f0e4afd73547ddbfe060836bda1076552d
thobauma <thobauma@ethz.ch>
2020-09-22 10:41:35 +0200

    added copying of header file to optional path

--- dace/codegen/sdfgcc.py
+++ dace/codegen/sdfgcc.py
@@ -5,6 +5,7 @@ import dace
 import os
 import sys
 import argparse
+import shutil
 
 
 def main():
@@ -12,23 +13,30 @@ def main():
     parser = argparse.ArgumentParser(description='Simple SDFG command-line compiler.')
 
     # Required argument for SDGF file path
-    parser.add_argument('SDFGfile', help='<PATH TO SDFG FILE>', type=str)
+    parser.add_argument('SDFGfilepath', help='<PATH TO SDFG FILE>', type=str)
 
     # Optional argument for output location
-    parser.add_argument('-o','--out', type=str, help='If provided, save output to path or filename.\nDirectories in path already need to exist.')
+    parser.add_argument('-o','--out', type=str, help='If provided, saves lib and header file to path. Directories in path need to exist beforehand.')
 
     args = parser.parse_args()
 
-    filename = args.SDFGfile
-    if not os.path.isfile(filename):
-        print('SDFG file', filename, 'not found')
+    filepath = args.SDFGfilepath
+    if not os.path.isfile(filepath):
+        print('SDFG file', filepath, 'not found')
         exit(1)
 
     # Load SDFG
-    sdfg = dace.SDFG.from_file(filename)
+    sdfg = dace.SDFG.from_file(filepath)
 
     # Compile SDFG
     sdfg.compile(args.out)
+
+    # Copying header file to optional path
+    if args.out:
+        source = os.path.join(sdfg.build_folder, 'src/cpu', sdfg.name+'.h')
+        destination = os.path.join(args.out,sdfg.name+'.h')
+        shutil.copyfile(source, destination)
+
 
 if __name__ == '__main__':
     main()
