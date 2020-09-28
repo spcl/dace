e6e19b329551e221377b7f52eb118d3b0fa77d92
Tal Ben-Nun <tbennun@gmail.com>
2020-09-28 09:09:11 +0200

    Copyright line

--- tests/transformations/apply_to_test.py
+++ tests/transformations/apply_to_test.py
@@ -1,3 +1,4 @@
+# Copyright 2019-2020 ETH Zurich and the DaCe authors. All rights reserved.
 """ Tests the `apply_to` transformation API. """
 import dace
 from dace.transformation.dataflow import MapFusion
