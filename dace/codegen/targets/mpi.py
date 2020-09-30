fe04991a0c074e9a84f7a6f912d99c8e953505fe
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2020-09-30 15:02:38 +0200

    WIP: MPI code generator, various edits for MPI maps and copies of data to/from Tasklets.

--- dace/codegen/targets/mpi.py
+++ dace/codegen/targets/mpi.py
@@ -2,7 +2,7 @@
 import warnings
 
 import dace
-from dace import registry, symbolic, dtypes, data
+from dace import registry, symbolic, dtypes, data, subsets
 from dace.codegen.prettycode import CodeIOStream
 from dace.codegen.codeobject import CodeObject
 from dace.codegen.targets import cpp
@@ -74,6 +74,13 @@ void __dace_exit_mpi({params}) {{
         # Register dispatchers
         dispatcher.register_map_dispatcher(dtypes.ScheduleType.MPI, self)
         dispatcher.register_array_dispatcher(dtypes.StorageType.Distributed, self)
+        for schedule in dtypes.ScheduleType:
+            dispatcher.register_copy_dispatcher(
+                dtypes.StorageType.Distributed, dtypes.StorageType.Register,
+                schedule, self)
+            dispatcher.register_copy_dispatcher(
+                dtypes.StorageType.Register, dtypes.StorageType.Distributed,
+                schedule, self)
 
     def get_generated_codeobjects(self):
         return [self._codeobj]
@@ -165,24 +172,40 @@ void __dace_exit_mpi({params}) {{
         # generator)
         callsite_stream.write('{', sdfg, state_id, map_header)
 
-        if len(map_header.map.params) > 1:
-            raise NotImplementedError(
-                'Multi-dimensional MPI maps are not supported')
+        # if len(map_header.map.params) > 1:
+        #     raise NotImplementedError(
+        #         'Multi-dimensional MPI maps are not supported')
 
+        ndims = len(map_header.map.params)
+        sdims = [e for _, e, _ in map_header.map.range]
+        callsite_stream.write("MPI_Comm comm;\n"
+                              "int dims[{n}];\n"
+                              "int coords[{n}];\n"
+                              "int periods[{n};\n"
+                              "int reorder = 0;\n".format(n=ndims),
+                              sdfg, state_id, map_header)  # TODO: Unique names?
+        for i, s in enumerate(sdims):
+            callsite_stream.write("dims[{i}] = {s};\n"
+                                  "periods[{i}] = 0\n".format(i=i, s=s),
+                                  sdfg, state_id, map_header)  # TODO: Assume non-periodic for now
+        callsite_stream.write("MPI_Card_create(MPI_COMM_WORLD, {n}, dims, "
+                              "periods, reorder, &comm);\n"
+                              "MPI_Cart_coords(comm, __dace_comm_rank, "
+                              "{n}, coords);\n".format(n=ndims),
+                              sdfg, state_id, map_header)
+        
         state = sdfg.node(state_id)
         symtypes = map_header.new_symbols(sdfg, state,
                                           state.symbols_defined_at(map_header))
 
-        for var, r in zip(map_header.map.params, map_header.map.range):
+        for i, (var, r) in enumerate(zip(map_header.map.params,
+                                         map_header.map.range)):
             begin, end, skip = r
 
             callsite_stream.write('{\n', sdfg, state_id, map_header)
             callsite_stream.write(
-                '%s %s = %s + __dace_comm_rank * (%s);\n' %
-                (symtypes[var], var,
-                 cppunparse.pyexpr2cpp(symbolic.symstr(begin)),
-                 cppunparse.pyexpr2cpp(symbolic.symstr(skip))), sdfg, state_id,
-                map_header)
+                "{t} {v} = coords[{i}];\n".format(t=symtypes[var], v=var, i=i),
+                sdfg, state_id, map_header)  # TODO: Bound checking
 
         to_allocate = dace.sdfg.local_transients(sdfg, dfg_scope, map_header)
         allocated = set()
@@ -201,3 +224,142 @@ void __dace_exit_mpi({params}) {{
                                            function_stream,
                                            callsite_stream,
                                            skip_entry_node=True)
+
+    def copy_memory(
+            self,
+            sdfg,
+            dfg,
+            state_id,
+            src_node,
+            dst_node,
+            edge,
+            function_stream,
+            callsite_stream,
+    ):
+        u, uconn, v, vconn, memlet = edge
+
+        # Determine memlet directionality
+        if isinstance(src_node,
+                      nodes.AccessNode) and memlet.data == src_node.data:
+            write = True
+            parent = dst_node
+        elif isinstance(dst_node,
+                        nodes.AccessNode) and memlet.data == dst_node.data:
+            write = False
+            parent = src_node
+        elif isinstance(src_node, nodes.CodeNode) and isinstance(
+                dst_node, nodes.CodeNode):
+            # Code->Code copy (not read nor write)
+            raise RuntimeError(
+                "Copying between code nodes is only supported as"
+                " part of the participating nodes")
+        else:
+            raise LookupError("Memlet does not point to any of the nodes")
+
+        # Find the map/map entry of the MPI scope we are in.
+        while (not isinstance(parent, nodes.MapEntry)
+                or parent.map.schedule != dtypes.ScheduleType.MPI):
+            parent = dfg.scope_dict()[parent]
+            if parent is None:
+                raise Exception("Distributed copy outside MPI map")
+
+        data = sdfg.arrays[memlet.data]
+
+        # Extract the distributed subset index
+        # This is needed because we may either have subsets.Indices
+        # or subsets.Range in the distributed subset.
+        # Currently we only support Indices and
+        # Range of length 1 in all dimensions.
+        if isinstance(edge.data.dist_subset, subsets.Indices):
+            index = edge.data.dist_subset
+        else:
+            index = []
+            for r in edge.data.dist_subset:
+                if len(r) == 3:
+                    begin, end, _ = r
+                else:
+                    begin, end, _, _ = r
+                if begin != end:
+                    raise NotImplementedError(
+                        "Only distributed indices are currently supported")
+                index.append(begin)
+
+        # # Parse distributed location lambda method to a sympy expression.
+        # # Replace the arguments of the lambda with the parameters
+        # # of the MPI map, in order to find the rank-owner of the source data.
+        # dist_location = LambdaProperty.from_string(data.dist_location)
+        # body = pystr_to_symbolic(unparse(dist_location.body))
+        # args = [pystr_to_symbolic(a.arg) for a in dist_location.args.args]
+        # if len(args) != len(index):
+        #     raise ValueError(
+        #         "The number of arguments of the distributed location lambda "
+        #         "method does not match the length of the memlet subset")
+        # repl = {arg: idx for arg, idx in zip(args, index)}
+        # other_rank = body.subs(repl)
+        # callsite_stream.write("int other_rank = %s;\n" % str(other_rank),
+        #                       sdfg, state_id, [src_node, dst_node])
+
+        # # Use the index matching of the pair (data, map) to create a system
+        # # of equations. Solve the system to find the expressions that describe
+        # # the coordinates of the ranks that request the source data.
+        # eqs = []
+        # for k, v in data.dist_shape_map.items():
+        #     eqs.append(pystr_to_symbolic(
+        #         '{i} - my_{r}'.format(i=index[k], r=parent.map.params[v])))
+        # symbols = [pystr_to_symbolic(p) for p in parent.map.params]
+        # solution = sympy.solve(eqs, *symbols)
+        # repl = {pystr_to_symbolic('my_{r}'.format(r=r)): r
+        #         for r in symbols}
+        # fixed = {}
+        # ranges = {}
+        # for i, (var, r) in enumerate(zip(symbols, parent.map.range)):
+        #     if var in solution.keys():
+        #         fixed[i] = solution[var].subs(repl)
+        #     else:
+        #         ranges[i] = r
+
+        # # Parse the distributed location of the MPI map to sympy expression.
+        # # We will use this expression to compute the actual ranks of the
+        # # processes that request the source data.
+        # dist_location = LambdaProperty.from_string(parent.map.dist_location)
+        # body = pystr_to_symbolic(unparse(dist_location.body))
+        # args = [pystr_to_symbolic(a.arg) for a in dist_location.args.args]
+
+        if isinstance(dst_node, nodes.Tasklet):
+            # Copy into tasklet
+            # int MPI_Get(
+            #     void *origin_addr,
+            #     int origin_count,
+            #     MPI_Datatype origin_datatype, 
+            #     int target_rank,
+            #     MPI_Aint target_disp,
+            #     int target_count,
+            #     MPI_Datatype target_datatype,
+            #     MPI_Win win
+            #     );
+            callsite_stream.write(
+                "MPI_Get Data:{d} Subset:{s} Target:{t}".format(
+                    d=memlet.data, s=memlet.subset, t=index
+                ), sdfg, state_id, [src_node, dst_node])
+                # TODO: We also need some kind of flush here
+        elif isinstance(src_node, nodes.Tasklet):
+            # Copy out of tasklet
+            if memlet.wcr:
+                callsite_stream.write(
+                    "MPI_Accumulate Data:{d} Subset:{s} Target:{t}".format(
+                    d=memlet.data, s=memlet.subset, t=index
+                ), sdfg, state_id, [src_node, dst_node])
+            else:
+                callsite_stream.write(
+                    "MPI_Put Data:{d} Subset:{s} Target:{t}".format(
+                    d=memlet.data, s=memlet.subset, t=index
+                ), sdfg, state_id, [src_node, dst_node])
+        else:  # Copy array-to-array
+            # src_nodedesc = src_node.desc(sdfg)
+            # dst_nodedesc = dst_node.desc(sdfg)
+
+            # if write:
+            #     vconn = dst_node.data
+            # ctype = "dace::vec<%s, %d>" % (dst_nodedesc.dtype.ctype,
+            #                                memlet.veclen)
+            raise NotImplementedError
