41efebdbc5dc0360401e5179763decdea3880362
luca <sancierra@gmail.com>
2020-10-12 17:13:09 +0200

    estimator works

--- dace/transformation/estimator/enumerator.py
+++ dace/transformation/estimator/enumerator.py
@@ -21,22 +21,22 @@ class TreeNode:
         else:
             self.children[map] = TreeNode(map)
 
-
+@make_properties
 class Enumerator:
     mode = Property(desc = "What the Iterator should return",
                     default = "subgraph",
                     choices = ["subgraph", "map_entries"])
 
     def __init__(self, sdfg, graph, subgraph):
-        self.sdfg = sdfg
-        self.graph = graph
-        self.subgraph = subgraph
+        self._sdfg = sdfg
+        self._graph = graph
+        self._scope_dict = graph.scope_dict(node_to_children = True)
 
         # get hightest scope maps
         map_entries = helpers.get_highest_scope_maps(sdfg, graph, subgraph)
 
         # create adjacency list
-        self.adjacency_list = {m: set() for m in map_entries}
+        self._adjacency_list = {m: set() for m in map_entries}
         for map_entry in map_entries:
             map_exit = graph.exit_node(map_entry)
             for edge in graph.out_edges(map_exit):
@@ -45,16 +45,20 @@ class Enumerator:
                     continue
                 for dst_edge in graph.out_edges(current_node):
                     if dst_edge.dst in map_entries:
-                        self.adjacency_list[map_entry].add(dst_edge.dst)
-                        self.adjacency_list[dst_edge.dst].add(map_entry)
+                        self._adjacency_list[map_entry].add(dst_edge.dst)
+                        self._adjacency_list[dst_edge.dst].add(map_entry)
 
 
     def traverse(self, current: List, forbidden: Set, prune = False):
         if len(current) > 0:
-            yield current.copy()
-            go_next = set(m for c in current for m in self.adjacency_list[c] if m not in current and m not in forbidden)
+            if self.mode == 'map_entries':
+                yield current.copy()
+            else:
+                yield helpers.subgraph_from_maps(self._sdfg, self._graph, current, self._scope_dict)
+
+            go_next = set(m for c in current for m in self._adjacency_list[c] if m not in current and m not in forbidden)
         else:
-            go_next = set(m for m in self.adjacency_list.keys())
+            go_next = set(m for m in self._adjacency_list.keys())
         if len(go_next) > 0:
             # we can explore
             forbidden_current = set()
@@ -72,3 +76,17 @@ class Enumerator:
 
     def __iter__(self):
         yield from self.iterator()
+
+    def histogram(self, visual = True):
+        old_mode = self.mode
+        self.mode = 'map_entries'
+        lst = self.list()
+        print("Subgraph Statistics")
+        print("-------------------")
+        for i in range(1, 1 + len(self._adjacency_list)):
+            no_elements = sum([len(sg) == i for sg in lst])
+            if visual:
+                print(i, no_elements, "*" * no_elements)
+            else:
+                print("Subgraphs with", i, "elements:", no_elements)
+        self.mode = old_mode
