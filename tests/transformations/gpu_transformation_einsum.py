fea8b6bc21468f56b105edd47b555950ca74216d
Shigang Li <shigangli.cs@gmail.com>
2020-10-15 09:47:50 +0200

    gpu trans test update

--- tests/transformations/gpu_transformation_einsum.py
+++ tests/transformations/gpu_transformation_einsum.py
@@ -12,13 +12,18 @@ def test_batch_matmul():
         C[:] = np.einsum('bik,bkj->bij', A, B)
 
     sdfg = einsumtest.to_sdfg()
-    sdfg.apply_gpu_transformations()
 
     A = np.random.rand(4, 10, 20)
     B = np.random.rand(4, 20, 10)
     C = np.random.rand(4, 10, 10)
     sdfg(A=A, B=B, C=C, M=10, N=20)
     assert np.allclose(C, A @ B)
+    print("CPU testing passed")
+
+    sdfg.apply_gpu_transformations()
+    sdfg(A=A, B=B, C=C, M=10, N=20)
+    assert np.allclose(C, A @ B)
+    print("GPU testing passed")
 
 if __name__ == '__main__':
     test_batch_matmul()
