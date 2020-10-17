0b2c461dee11650529fda985cb0a0d4606d9b47f
andreaskuster <mail@andreaskuster.ch>
2020-10-17 02:10:31 +0200

    Change from flat vector to 2D input/output (i.e. vec4(int32): [127:0]  -> [3:0][31:0])

--- dace/codegen/targets/rtl.py
+++ dace/codegen/targets/rtl.py
@@ -24,7 +24,6 @@ class RTLCodeGen(TargetCodeGenerator):
         self.codegen = self._dispatcher.get_generic_node_dispatcher()
         self.sdfg = sdfg
 
-
     # define cpp code templates
     header_template = """
                             // generic includes
@@ -124,8 +123,9 @@ endmodule
 """
 
     @staticmethod
-    def unparse_rtl_tasklet(sdfg, state_id, dfg, node, function_stream, callsite_stream: dace.codegen.prettycode.CodeIOStream,
-                    locals, ldepth, toplevel_schedule, codegen):
+    def unparse_rtl_tasklet(sdfg, state_id, dfg, node, function_stream,
+                            callsite_stream: dace.codegen.prettycode.CodeIOStream,
+                            locals, ldepth, toplevel_schedule, codegen):
 
         # extract data
         state = sdfg.nodes()[state_id]
@@ -143,20 +143,37 @@ endmodule
             parameter_string = """\
 #(
 {}
-)""".format("\n".join(["{} parameter {} = {}".format("," if i > 0 else "", key, sdfg.constants[key]) for i, key in enumerate(sdfg.constants)]))
+)""".format("\n".join(["{} parameter {} = {}".format("," if i > 0 else "", key, sdfg.constants[key]) for i, key in
+                       enumerate(sdfg.constants)]))
 
         # construct input / output module header
         MAX_PADDING = 17
-        inputs = [", input{padding}[{}:0] {}".format(tasklet.in_connectors[inp].bytes*8-1,
-                                                     inp,
-                                                     padding=" "*(MAX_PADDING-len("[{}:0]".format(tasklet.in_connectors[inp].bytes*8-1))))
-                  for inp in tasklet.in_connectors]
-        MAX_PADDING = 12
-        outputs = [", output reg{padding}[{}:0] {}".format(tasklet.out_connectors[inp].bytes*8-1,
-                                                  inp,
-                                                  padding=" "*(MAX_PADDING-len("[{}:0]".format(tasklet.out_connectors[inp].bytes*8-1))))
-                   for inp in tasklet.out_connectors]
+        inputs = list()
+        for inp in tasklet.in_connectors:
+            # add vector index
+            idx_str = ""
+            if tasklet.in_connectors[inp].veclen > 1:
+                idx_str = "[{}:0]".format(tasklet.in_connectors[inp].veclen-1)
+            # add element index
+            idx_str += "[{}:0]".format(int(tasklet.in_connectors[inp].bytes / tasklet.in_connectors[inp].veclen) * 8 - 1)
+            # generate padded string and add to list
+            inputs.append(", input{padding}{idx_str} {name}".format(padding=" " * (MAX_PADDING-len(idx_str)),
+                                                                    idx_str=idx_str,
+                                                                    name=inp))
 
+        MAX_PADDING = 12
+        outputs = list()
+        for inp in tasklet.out_connectors:
+            # add vector index
+            idx_str = ""
+            if tasklet.out_connectors[inp].veclen > 1:
+                idx_str = "[{}:0]".format(tasklet.out_connectors[inp].veclen-1)
+            # add element index
+            idx_str += "[{}:0]".format(int(tasklet.out_connectors[inp].bytes / tasklet.out_connectors[inp].veclen) * 8 - 1)
+            # generate padded string and add to list
+            outputs.append(", output reg{padding}{idx_str} {name}".format(padding=" " * (MAX_PADDING-len(idx_str)),
+                                                                    idx_str=idx_str,
+                                                                    name=inp))
         # generate cpp input reading/output writing code
         """
         input:
@@ -185,36 +202,42 @@ endmodule
                                        for var_name in tasklet.in_connectors])
 
         output_read_string = "\n".join(["{name} = (int)model->{name};".format(name=var_name)
-                                       if tasklet.out_connectors[var_name].veclen == 1 else
-                                       """\
-                                       for(int i = 0; i < {veclen}; i++){{
-                                         {name}[i] = (int)model->{name}[i];
-                                       }}\
-                                       """.format(veclen=tasklet.out_connectors[var_name].veclen, name=var_name)
-                                       for var_name in tasklet.out_connectors])
+                                        if tasklet.out_connectors[var_name].veclen == 1 else
+                                        """\
+                                        for(int i = 0; i < {veclen}; i++){{
+                                          {name}[i] = (int)model->{name}[i];
+                                        }}\
+                                        """.format(veclen=tasklet.out_connectors[var_name].veclen, name=var_name)
+                                        for var_name in tasklet.out_connectors])
         # write verilog to file
         if os.path.isdir(absolut_path):
             shutil.rmtree(absolut_path)
         os.makedirs(absolut_path)
         with open(os.path.join(absolut_path, "{}.sv".format(unique_name)), "w") as file:
-            file.writelines(RTLCodeGen.rtl_header.format(name=unique_name, parameters=parameter_string, inputs="\n".join(inputs), outputs="\n".join(outputs)))
+            file.writelines(
+                RTLCodeGen.rtl_header.format(name=unique_name, parameters=parameter_string, inputs="\n".join(inputs),
+                                             outputs="\n".join(outputs)))
             file.writelines(tasklet.code.code)
             file.writelines(RTLCodeGen.rtl_footer)
 
-
         sdfg.append_global_code(cpp_code=RTLCodeGen.header_template.format(name=unique_name,
                                                                            debug="// enable/disable debug log\n" +
-                                                                           "bool DEBUG = false;" if "DEBUG" not in sdfg.constants else ""))
+                                                                                 "bool DEBUG = false;" if "DEBUG" not in sdfg.constants else ""))
 
         callsite_stream.write(contents=RTLCodeGen.main_template.format(name=unique_name,
                                                                        inputs=input_read_string,
                                                                        outputs=output_read_string,
-                                                                       internal_state_str=" ".join(["{}=0x%x".format(var_name) for var_name in {**tasklet.in_connectors, **tasklet.out_connectors}]),
-                                                                       internal_state_var=", ".join(["model->{}".format(var_name) for var_name in {**tasklet.in_connectors, **tasklet.out_connectors}])),
+                                                                       internal_state_str=" ".join(
+                                                                           ["{}=0x%x".format(var_name) for var_name in
+                                                                            {**tasklet.in_connectors,
+                                                                             **tasklet.out_connectors}]),
+                                                                       internal_state_var=", ".join(
+                                                                           ["model->{}".format(var_name) for var_name in
+                                                                            {**tasklet.in_connectors,
+                                                                             **tasklet.out_connectors}])),
                               sdfg=sdfg,
                               state_id=state_id,
                               node_id=node)
-
 
     @staticmethod
     def cmake_options():
