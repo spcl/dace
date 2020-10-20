d7b3366e852ca802705bc28f6083353a994e71e6
andreaskuster <mail@andreaskuster.ch>
2020-10-20 14:04:32 +0200

    Add rtl tasklet vector testcase.

--- tests/rtl/simulation_test.py
+++ tests/rtl/simulation_test.py
@@ -9,7 +9,7 @@ def test_tasklet_scalar():
     """
 
     # add sdfg
-    sdfg = dace.SDFG('rtl_tasklet_demo')
+    sdfg = dace.SDFG('rtl_tasklet_scalar')
 
     # add state
     state = sdfg.add_state()
@@ -94,7 +94,7 @@ def test_tasklet_parameter():
     """
 
     # add sdfg
-    sdfg = dace.SDFG('rtl_tasklet_demo')
+    sdfg = dace.SDFG('rtl_tasklet_parameter')
 
     # add state
     state = sdfg.add_state()
@@ -176,6 +176,98 @@ def test_tasklet_parameter():
     assert b == sdfg.constants["MAX_VAL"]
 
 
+def test_tasklet_vector():
+    """
+        Test rtl tasklet vector support.
+    """
+
+    # add symbol
+    N = dace.symbol('N')
+
+    # add sdfg
+    sdfg = dace.SDFG('rtl_tasklet_demo')
+
+    # define compile-time constant
+    sdfg.specialize(dict(N=4))
+
+    # add state
+    state = sdfg.add_state()
+
+    # add arrays
+    sdfg.add_array('A', [N], dtype=dace.int32)
+    sdfg.add_array('B', [1], dtype=dace.int32)
+
+    # enable debugging output
+    sdfg.add_constant("DEBUG", 1)
+
+    # add custom cpp tasklet
+    tasklet = state.add_tasklet(
+        name='rtl_tasklet',
+        inputs={'a': dace.vector(dace.int32, N)},
+        outputs={'b'},
+        code='''
+        /*
+            Convention:
+               |----------------------------------------------------|
+            -->| clk_i (clock input)                                |
+            -->| rst_i (reset input, rst on high)                   |
+               |                                                    |
+            -->| {inputs}                             reg {outputs} |-->
+               |                                                    |
+            <--| ready_o (ready for data)       (data avail) valid_o|-->
+            -->| valid_i (new data avail)    (data consumed) ready_i|<--
+               |----------------------------------------------------|
+        */
+
+        always@(posedge clk_i) begin
+            if (rst_i) begin // case: reset
+                b <= 0;
+                ready_o <= 1'b1;
+            end else if (valid_i) begin // case: load a 
+                b <= a[0];
+                ready_o <= 1'b0;
+            end else if (b < a[1]) // case: increment counter b
+                b <= b + 1;
+            else
+                b <= b; 
+        end    
+
+        assign valid_o = (b >= a[1]) ? 1'b1:1'b0; 
+        ''',
+        language=dace.Language.RTL)
+
+    # add input/output array
+    A = state.add_read('A')
+    B = state.add_write('B')
+
+    # connect input/output array with the tasklet
+    state.add_edge(A, None, tasklet, 'a', dace.Memlet.simple('A', '0:N-1'))
+    state.add_edge(tasklet, 'b', B, None, dace.Memlet.simple('B', '0'))
+
+    # validate sdfg
+    sdfg.validate()
+
+    """
+        Execute    
+    """
+    # init data structures
+    a = np.random.randint(0, 100, dace.symbolic.evaluate(N, sdfg.constants)).astype(np.int32)
+    b = np.random.randint(0, 100, 1).astype(np.int32)
+
+    # show initial values
+    print("a={}, b={}".format(a, b))
+
+    # call program
+    sdfg(A=a, B=b)
+
+    # show result
+    print("a={}, b={}".format(a, b))
+
+    # check result
+    assert b == a[1]
+
+
 if __name__ == '__main__':
     test_tasklet_scalar()
     test_tasklet_parameter()
+    test_tasklet_vector()
