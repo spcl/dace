4a0d230fa63a9d56a35becb15ca78f7827cc092d
andreaskuster <mail@andreaskuster.ch>
2020-10-20 14:01:02 +0200

    Add sv tasklet parameter support testcase.

--- tests/rtl/simulation_test.py
+++ tests/rtl/simulation_test.py
@@ -1,4 +1,3 @@
-
 import dace
 
 import numpy as np
@@ -89,5 +88,94 @@ def test_tasklet_scalar():
     assert b == 100
 
 
+def test_tasklet_parameter():
+    """
+        Test the sv parameter support.
+    """
+
+    # add sdfg
+    sdfg = dace.SDFG('rtl_tasklet_demo')
+
+    # add state
+    state = sdfg.add_state()
+
+    # add arrays
+    sdfg.add_array('A', [1], dtype=dace.int32)
+    sdfg.add_array('B', [1], dtype=dace.int32)
+
+    # enable debugging output
+    sdfg.add_constant("DEBUG", 1)
+
+    # add parameter(s)
+    sdfg.add_constant("MAX_VAL", 42)
+
+    # add custom cpp tasklet
+    tasklet = state.add_tasklet(
+        name='rtl_tasklet',
+        inputs={'a'},
+        outputs={'b'},
+        code='''
+        /*
+            Convention:
+               |----------------------------------------------------|
+            -->| clk_i (clock input)                                |
+            -->| rst_i (reset input, rst on high)                   |
+               |                                                    |
+            -->| {inputs}                             reg {outputs} |-->
+               |                                                    |
+            <--| ready_o (ready for data)       (data avail) valid_o|-->
+            -->| valid_i (new data avail)    (data consumed) ready_i|<--
+               |----------------------------------------------------|
+        */
+
+        always@(posedge clk_i) begin
+            if (rst_i) begin // case: reset
+                b <= 0;
+                ready_o <= 1'b1;
+            end else if (valid_i) begin // case: load a 
+                b <= a;
+                ready_o <= 1'b0;
+            end else if (b < MAX_VAL) // case: increment counter b
+                b <= b + 1;
+            else
+                b <= b; 
+        end    
+
+        assign valid_o = (b >= MAX_VAL) ? 1'b1:1'b0; 
+        ''',
+        language=dace.Language.RTL)
+
+    # add input/output array
+    A = state.add_read('A')
+    B = state.add_write('B')
+
+    # connect input/output array with the tasklet
+    state.add_edge(A, None, tasklet, 'a', dace.Memlet.simple('A', '0'))
+    state.add_edge(tasklet, 'b', B, None, dace.Memlet.simple('B', '0'))
+
+    # validate sdfg
+    sdfg.validate()
+
+    """
+        Execute    
+    """
+    # init data structures
+    a = np.random.randint(0, 100, 1).astype(np.int32)
+    b = np.random.randint(0, 100, 1).astype(np.int32)
+
+    # show initial values
+    print("a={}, b={}".format(a, b))
+
+    # call program
+    sdfg(A=a, B=b)
+
+    # show result
+    print("a={}, b={}".format(a, b))
+
+    # check result
+    assert b == sdfg.constants["MAX_VAL"]
+
+
 if __name__ == '__main__':
     test_tasklet_scalar()
+    test_tasklet_parameter()
