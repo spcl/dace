c3d224f0077f01c0e67b10190b2aef3d4381ab1b
Tiziano De Matteis <tiziano.dematteis@inf.ethz.ch>
2020-10-20 16:15:27 +0200

    Change CMake rules to first compile and then link .xo files

--- tests/xilinx_test.sh
+++ tests/xilinx_test.sh
@@ -62,7 +62,7 @@ run_all() {
     run_sample fpga/veclen_copy_conversion veclen_copy_conversion 1
     run_sample ../samples/fpga/axpy_transformed axpy_fpga_24 0 24
     run_sample ../samples/fpga/spmv_fpga_stream spmv_fpga_stream 0 64 64 640
-    run_sample ../samples/fpga/gemm_fpga_systolic gemm_fpga_systolic_4_64x64x64 1 64 64 64 4 -specialize 
+    run_sample ../samples/fpga/gemm_fpga_systolic gemm_fpga_systolic_4_64x64x64 1 64 64 64 4 -specialize
     run_sample ../samples/fpga/filter_fpga_vectorized filter_fpga_vectorized_4 1 8192 4 0.25
     # run_sample jacobi_fpga_systolic jacobi_fpga_systolic_4_Hx128xT 1 128 128 8 4
     # TODO: this doesn't pipeline. Should it? Why doesn't it?
@@ -75,6 +75,8 @@ run_all() {
       run_sample ../samples/fpga/filter_fpga filter_fpga 1 8192 0.5
       run_sample ../samples/fpga/jacobi_fpga_stream jacobi_fpga_stream_Hx128xT 1 128 128 8
     fi
+
+    run_sample fpga/multiple_kernels multiple_kernels 0
 }
 
 # Check if xocc is vailable
