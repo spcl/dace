b726818efae13ab962ab4d71c69825b494f444ad
Tal Ben-Nun <tbennun@gmail.com>
2020-10-23 18:24:11 +0200

    Initial implementation of scal2sym without indirection cleanup and more tests

--- dace/sdfg/analysis/scalar_to_symbol.py
+++ dace/sdfg/analysis/scalar_to_symbol.py
@@ -2,9 +2,15 @@
 """ Scalar to symbol promotion functionality. """
 
 import ast
-from dace import dtypes, nodes, sdfg as sd, data as dt, properties as props
+from dace.sdfg.sdfg import InterstateEdge
+from dace.frontend.python.astutils import ASTFindReplace
+from dace import (dtypes, nodes, sdfg as sd, data as dt, properties as props,
+                  memlet as mm)
+from dace.sdfg import graph as gr
+from dace.frontend.python import astutils
+from dace.transformation import helpers as xfh
 import re
-from typing import Dict, Set
+from typing import Any, Dict, Set
 
 
 def find_promotable_scalars(sdfg: sd.SDFG) -> Set[str]:
@@ -79,6 +85,11 @@ def find_promotable_scalars(sdfg: sd.SDFG) -> Set[str]:
                 # If input is array, ensure it is not a stream
                 if isinstance(sdfg.arrays[edge.src.data], dt.Stream):
                     candidates.remove(candidate)
+                    continue
+                # Ensure no inputs exist to the array
+                if state.in_degree(edge.src) > 0:
+                    candidates.remove(candidate)
+                    continue
             elif isinstance(edge.src, nodes.Tasklet):
                 # If input tasklet has more than one output, skip
                 if state.out_degree(edge.src) > 1:
@@ -99,17 +110,24 @@ def find_promotable_scalars(sdfg: sd.SDFG) -> Set[str]:
                         if (len(cb.code) > 1
                                 or not isinstance(cb.code[0], ast.Assign)):
                             candidates.remove(candidate)
-                            break
+                            continue
+                        # Ensure the candidate is assigned to
+                        if (len(cb.code[0].targets) != 1 or astutils.rname(
+                                cb.code[0].targets[0]) != edge.src_conn):
+                            candidates.remove(candidate)
+                            continue
                     elif cb.language is dtypes.Language.CPP:
                         # Try to match a single C assignment
                         cstr = cb.as_string.strip()
-                        if re.match(r'^[a-zA-Z_][a-zA-Z_0-9]* = .*;$',
+                        # Since we cannot remove subscripts from C++ tasklets,
+                        # if the type of the data is an array we will also skip
+                        if re.match(r'^[a-zA-Z_][a-zA-Z_0-9]*\s*=.*;$',
                                     cstr) is None:
                             candidates.remove(candidate)
-                            break
+                            continue
                     else:  # Other languages are currently unsupported
                         candidates.remove(candidate)
-                        break
+                        continue
             else:  # If input is not an acceptable node type, skip
                 candidates.remove(candidate)
         candidates_seen |= candidates_in_state
@@ -118,6 +136,36 @@ def find_promotable_scalars(sdfg: sd.SDFG) -> Set[str]:
     candidates &= candidates_seen
 
     return candidates
+
+
+class TaskletPromoter(ast.NodeTransformer):
+    """
+    Promotes scalars to symbols in Tasklets.
+    If connector name is used in tasklet as subscript, modifies to symbol name.
+    If connector is used as a standard name, modify tasklet code to use symbol.
+    """
+    def __init__(self, connector: str, symbol: str) -> None:
+        """
+        Initializes AST transformer.
+        :param connector: Connector name (replacement source).
+        :param symbol: Symbol name (replacement target).
+        """
+        self.conn = connector
+        self.symbol = symbol
+
+    def visit_Name(self, node: ast.Name) -> Any:
+        # Convert connector to symbol
+        if node.id == self.conn:
+            node.id = self.symbol
+        return self.generic_visit(node)
+
+    def visit_Subscript(self, node: ast.Subscript) -> Any:
+        # Convert subscript to symbol name
+        node_name = astutils.rname(node)
+        if node_name == self.conn:
+            return ast.copy_location(ast.Name(id=self.symbol, ctx=ast.Load()),
+                                     node)
+        return self.generic_visit(node)
 
 
 def promote_scalars_to_symbols(sdfg: sd.SDFG):
@@ -131,4 +179,141 @@ def promote_scalars_to_symbols(sdfg: sd.SDFG):
     :param sdfg: The SDFG to run the pass on.
     :note: Operates in-place.
     """
+    # Process:
+    # 1. Find scalars to promote
+    # 2. For every assignment tasklet/access:
+    #    2.1. Fission state to isolate assignment
+    #    2.2. Replace assignment with inter-state edge assignment
+    # 3. For every read of the scalar:
+    #    3.1. If destination is tasklet, remove node, edges, and connectors
+    #    3.2. If used in tasklet as subscript or connector, modify tasklet code
+    #    3.3. If destination is array, change to tasklet that copies symbol data
+    # 4. Remove newly-isolated access nodes
+    # 5. Remove data descriptors and add symbols to SDFG
+    # 6. Replace subscripts in all interstate conditions and assignments
+    # 7. Make indirections with symbols a single memlet
+
     to_promote = find_promotable_scalars(sdfg)
+
+    for state in sdfg.nodes():
+        scalar_nodes = [
+            n for n in state.nodes()
+            if isinstance(n, nodes.AccessNode) and n.data in to_promote
+        ]
+        # Step 2: Assignment tasklets
+        for node in scalar_nodes:
+            # There is only zero or one incoming edges by definition
+            if state.in_degree(node) == 0:
+                continue
+            in_edge = state.in_edges(node)[0]
+            input = in_edge.src
+            tasklet_inputs = [e.src for e in state.in_edges(input)]
+            # Step 2.1
+            new_state = xfh.state_fission(
+                sdfg, gr.SubgraphView(state, [input, node] + tasklet_inputs))
+            new_isedge: sd.InterstateEdge = sdfg.out_edges(new_state)[0]
+            # Step 2.2
+            node: nodes.AccessNode = new_state.sink_nodes()[0]
+            input = new_state.in_edges(node)[0].src
+            if isinstance(input, nodes.Tasklet):
+                # Convert tasklet to interstate edge
+                newcode: str = ''
+                if input.language is dtypes.Language.Python:
+                    newcode = astutils.unparse(input.code.code[0].value)
+                elif input.language is dtypes.Language.CPP:
+                    newcode = re.findall(r'.*=\s*(.*);',
+                                         input.code.as_string.strip())[0]
+                # Replace tasklet inputs with incoming edges
+                for e in new_state.in_edges(input):
+                    memlet_str: str = e.data.data
+                    if e.data.subset is not None:
+                        memlet_str += '[%s]' % e.data.subset
+                    newcode = re.sub(r'\b%s\b' % re.escape(e.dst_conn),
+                                     memlet_str, newcode)
+                # Add interstate edge assignment
+                new_isedge.data.assignments[node.data] = newcode
+            elif isinstance(input, nodes.AccessNode):
+                memlet: mm.Memlet = in_edge.data
+                if memlet.src_subset:
+                    new_isedge.data.assignments[
+                        node.data] = '%s[%s]' % (input.data, memlet.src_subset)
+                else:
+                    new_isedge.data.assignments[node.data] = input.data
+
+            # Clean up all nodes after assignment was transferred
+            new_state.remove_nodes_from(new_state.nodes())
+
+        scalar_nodes = [
+            n for n in state.nodes()
+            if isinstance(n, nodes.AccessNode) and n.data in to_promote
+        ]
+
+        # Step 3: Scalar reads
+        for node in scalar_nodes:
+            for out_edge in state.out_edges(node):
+                for e in state.memlet_tree(out_edge):
+                    # Step 3.1
+                    dst = e.dst
+                    state.remove_edge_and_connectors(e)
+                    if isinstance(dst, nodes.Tasklet):
+                        # Step 3.2
+                        if dst.language is dtypes.Language.Python:
+                            promo = TaskletPromoter(e.dst_conn, node.data)
+                            for stmt in dst.code.code:
+                                promo.visit(stmt)
+                        elif dst.language is dtypes.Language.CPP:
+                            # Replace whole-word matches (identifiers) in code
+                            dst.code = re.sub(r'\b%s\b' % re.escape(e.dst_conn),
+                                              node.data, dst.code.as_string)
+                    elif isinstance(dst, nodes.AccessNode):
+                        # Step 3.3
+                        t = state.add_tasklet('symassign', {}, {'__out'},
+                                              '__out = %s' % node.data)
+                        state.add_edge(
+                            t, '__out', dst, e.dst_conn,
+                            mm.Memlet(data=dst.data,
+                                      subset=e.data.dst_subset,
+                                      volume=1))
+                        # Reassign destination for check below
+                        dst = t
+
+                    # If nodes were disconnected, reconnect with empty memlet
+                    if (isinstance(e.src, nodes.EntryNode)
+                            and len(state.edges_between(e.src, dst)) == 0):
+                        state.add_nedge(e.src, dst, mm.Memlet())
+
+    # Step 4: Isolated nodes
+    for state in sdfg.nodes():
+        scalar_nodes = [
+            n for n in state.nodes()
+            if isinstance(n, nodes.AccessNode) and n.data in to_promote
+        ]
+        state.remove_nodes_from(
+            [n for n in scalar_nodes if len(state.all_edges(n)) == 0])
+
+    # Step 5: Data descriptor management
+    for scalar in to_promote:
+        desc = sdfg.arrays[scalar]
+        sdfg.remove_data(scalar, validate=False)
+        sdfg.add_symbol(scalar, desc.dtype)
+
+    # Step 6: Inter-state edge cleanup
+    for edge in sdfg.edges():
+        ise: InterstateEdge = edge.data
+        for scalar in to_promote:
+            # Condition
+            if ise.condition.language is dtypes.Language.Python:
+                promo = TaskletPromoter(scalar, scalar)
+                for stmt in ise.condition.code:
+                    promo.visit(stmt)
+            elif ise.condition.language is dtypes.Language.CPP:
+                ise.condition = re.sub(r'\b%s\[.*\]' % re.escape(scalar),
+                                       scalar, ise.condition.as_string)
+            # Assignments
+            for aname, assignment in ise.assignments.items():
+                ise.assignments[aname] = re.sub(
+                    r'\b%s\[.*\]' % re.escape(scalar), scalar,
+                    assignment.strip())
+
+    # Step 7: Indirection
+    # TODO
--- tests/sdfg/scalar_to_symbol_test.py
+++ tests/sdfg/scalar_to_symbol_test.py
@@ -2,6 +2,12 @@
 """ Tests the scalar to symbol promotion functionality. """
 import dace
 from dace.sdfg.analysis import scalar_to_symbol
+from dace.transformation import interstate as isxf
+from dace.transformation.interstate import loop_detection as ld
+from dace import registry
+from dace.transformation import helpers as xfh
+
+import numpy as np
 
 
 def test_find_promotable():
@@ -31,21 +37,314 @@ def test_find_promotable():
 
 def test_promote_simple():
     """ Simple promotion with Python tasklets. """
-    pass
+    @dace.program
+    def testprog(A: dace.float64[20, 20]):
+        j = 5
+        A[:] += j
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'j'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_transformations_repeated(isxf.StateFusion)
+
+    # There should be two states:
+    # [empty] --j=5--> [A->MapEntry->Tasklet->MapExit->A]
+    assert sdfg.number_of_nodes() == 2
+    assert sdfg.source_nodes()[0].number_of_nodes() == 0
+    assert sdfg.sink_nodes()[0].number_of_nodes() == 5
+    tasklet = next(n for n in sdfg.sink_nodes()[0]
+                   if isinstance(n, dace.nodes.Tasklet))
+    assert '+ j' in tasklet.code.as_string
+
+    # Program should produce correct result
+    A = np.random.rand(20, 20)
+    expected = A + 5
+    sdfg(A=A)
+    assert np.allclose(A, expected)
+
+
+def test_promote_simple_c():
+    """ Simple promotion with C++ tasklets. """
+    @dace.program
+    def testprog(A: dace.float32[20, 20]):
+        i = 0
+        j = 0
+        k = dace.ndarray([1], dtype=dace.int32)
+        with dace.tasklet(dace.Language.CPP):
+            jj << j
+            """
+            ii = jj + 1;
+            """
+            ii >> i
+        with dace.tasklet(dace.Language.CPP):
+            jin << j
+            """
+            int something = (int)jin;
+            jout = something + 1;
+            """
+            jout >> j
+        with dace.tasklet(dace.Language.CPP):
+            """
+            kout[0] = 0;
+            """
+            kout >> k
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    scalars = scalar_to_symbol.find_promotable_scalars(sdfg)
+    assert scalars == {'i'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_transformations_repeated(isxf.StateFusion)
+
+    # SDFG: [empty] --i=0--> [Tasklet->j] --i=j+1--> [j->Tasklet->j, Tasklet->k]
+    assert sdfg.number_of_nodes() == 3
+    src_state = sdfg.source_nodes()[0]
+    sink_state = sdfg.sink_nodes()[0]
+    middle_state = next(s for s in sdfg.nodes()
+                        if s not in [src_state, sink_state])
+    assert src_state.number_of_nodes() == 0
+    assert middle_state.number_of_nodes() == 2
+    assert sink_state.number_of_nodes() == 5
+
+
+def test_promote_disconnect():
+    """ Promotion that disconnects tasklet from map. """
+    @dace.program
+    def testprog(A: dace.float64[20, 20]):
+        j = 5
+        A[:] = j
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'j'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_transformations_repeated(isxf.StateFusion)
+
+    # There should be two states:
+    # [empty] --j=5--> [MapEntry->Tasklet->MapExit->A]
+    assert sdfg.number_of_nodes() == 2
+    assert sdfg.source_nodes()[0].number_of_nodes() == 0
+    assert sdfg.sink_nodes()[0].number_of_nodes() == 4
+
+    # Program should produce correct result
+    A = np.random.rand(20, 20)
+    expected = np.zeros_like(A)
+    expected[:] = 5
+    sdfg(A=A)
+    assert np.allclose(A, expected)
+
+
+def test_promote_copy():
+    """ Promotion that has a connection to an array and to another symbol. """
+    # Create SDFG
+    sdfg = dace.SDFG('testprog')
+    sdfg.add_array('A', [20, 20], dace.float64)
+    sdfg.add_transient('i', [1], dace.int32)
+    sdfg.add_transient('j', [1], dace.int32)
+    state = sdfg.add_state()
+    state.add_edge(state.add_tasklet('seti', {}, {'out'}, 'out = 0'), 'out',
+                   state.add_write('i'), None, dace.Memlet('i'))
+    state = sdfg.add_state_after(state)
+    state.add_edge(state.add_tasklet('setj', {}, {'out'}, 'out = 5'), 'out',
+                   state.add_write('j'), None, dace.Memlet('j'))
+    state = sdfg.add_state_after(state)
+    state.add_nedge(state.add_read('j'), state.add_write('i'), dace.Memlet('i'))
+    state = sdfg.add_state_after(state)
+    state.add_nedge(state.add_read('i'), state.add_write('A'),
+                    dace.Memlet('A[5, 5]'))
+
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'i', 'j'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_transformations_repeated(isxf.StateFusion)
+
+    # There should be two states:
+    # [empty] --i=0,j=5--> [empty] --j=i--> [Tasklet->A]
+    assert sdfg.number_of_nodes() == 3
+    src_state = sdfg.source_nodes()[0]
+    sink_state = sdfg.sink_nodes()[0]
+    middle_state = next(s for s in sdfg.nodes()
+                        if s not in [src_state, sink_state])
+    assert src_state.number_of_nodes() == 0
+    assert middle_state.number_of_nodes() == 0
+    assert sink_state.number_of_nodes() == 2
+
+    # Program should produce correct result
+    A = np.random.rand(20, 20)
+    expected = np.copy(A)
+    expected[5, 5] = 5.0
+    sdfg(A=A)
+    assert np.allclose(A, expected)
+
+
+def test_promote_array_assignment():
+    """ Simple promotion with array assignment. """
+    @dace.program
+    def testprog(A: dace.float64[20, 20]):
+        j = A[1, 1]
+        A[:] += j
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'j'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_transformations_repeated(isxf.StateFusion)
+
+    # There should be two states:
+    # [empty] --j=A[1, 1]--> [A->MapEntry->Tasklet->MapExit->A]
+    assert sdfg.number_of_nodes() == 2
+    assert sdfg.source_nodes()[0].number_of_nodes() == 0
+    assert sdfg.sink_nodes()[0].number_of_nodes() == 5
+
+    # Program should produce correct result
+    A = np.random.rand(20, 20)
+    expected = A + A[1, 1]
+    sdfg(A=A)
+    assert np.allclose(A, expected)
+
+
+def test_promote_array_assignment_tasklet():
+    """ Simple promotion with array assignment. """
+    @dace.program
+    def testprog(A: dace.float64[20, 20]):
+        j = dace.define_local_scalar(dace.float64)
+        with dace.tasklet:
+            inp << A[1, 1]
+            out >> j
+            out = inp
+        A[:] += j
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'j'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_transformations_repeated(isxf.StateFusion)
+
+    # There should be two states:
+    # [empty] --j=A[1, 1]--> [A->MapEntry->Tasklet->MapExit->A]
+    assert sdfg.number_of_nodes() == 2
+    assert sdfg.source_nodes()[0].number_of_nodes() == 0
+    assert sdfg.sink_nodes()[0].number_of_nodes() == 5
+
+    # Program should produce correct result
+    A = np.random.rand(20, 20)
+    expected = A + A[1, 1]
+    sdfg(A=A)
+    assert np.allclose(A, expected)
+
+
+@registry.autoregister
+class LoopTester(ld.DetectLoop):
+    """ Tester method that sets loop index on a guard state. """
+    @staticmethod
+    def can_be_applied(graph, candidate, expr_index, sdfg, strict):
+        if not ld.DetectLoop.can_be_applied(graph, candidate, expr_index, sdfg,
+                                            strict):
+            return False
+        guard = graph.node(candidate[ld.DetectLoop._loop_guard])
+        if hasattr(guard, '_LOOPINDEX'):
+            return False
+        return True
+
+    def apply(self, sdfg: dace.SDFG):
+        guard = sdfg.node(self.subgraph[ld.DetectLoop._loop_guard])
+        edge = sdfg.in_edges(guard)[0]
+        loopindex = next(iter(edge.data.assignments.keys()))
+        guard._LOOPINDEX = loopindex
+
+
+def test_promote_loop():
+    """ Loop promotion. """
+    N = dace.symbol('N')
+
+    @dace.program
+    def testprog(A: dace.float32[20, 20]):
+        i = dace.ndarray([1], dtype=dace.int32)
+        i = 0
+        while i[0] < N:
+            A += i
+            i += 2
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'i'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_strict_transformations()
+    assert sdfg.apply_transformations_repeated(LoopTester) == 1
 
 
 def test_promote_loops():
     """ Nested loops. """
-    pass
+    N = dace.symbol('N')
+
+    @dace.program
+    def testprog(A: dace.float32[20, 20]):
+        i = 0
+        while i < N:
+            A += i
+            for j in range(5):
+                k = 0
+                while k < N / 2:
+                    A += k
+                    k += 1
+            i += 2
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'i', 'k'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_strict_transformations()
+    xfh.split_interstate_edges(sdfg)
+    assert sdfg.apply_transformations_repeated(LoopTester) == 3
 
 
 def test_promote_indirection():
     """ Indirect access in promotion. """
-    pass
+    @dace.program
+    def testprog(A: dace.float64[2, 3, 4, 5], B: dace.float64[4]):
+        i = 2
+        j = 1
+        k = 0
+
+        # Complex numpy expression with double indirection
+        B[:] = A[:, i, :, j][k, :]
+
+        # Include tasklet with more than one statement (one with indirection
+        # and one without) and two outputs.
+        for m in dace.map[0:2]:
+            with dace.tasklet:
+                a << A(1)[:]
+                ii << i
+                b1in << B[m]
+                b2in << B[2 * m]
+
+                c = a[0, 0, 0, ii]
+                b1 = c + 1
+                b2 = b2in + 1
+
+                b1 >> B[m]
+                b2 >> B[2 * m]
+
+    sdfg: dace.SDFG = testprog.to_sdfg()
+    assert scalar_to_symbol.find_promotable_scalars(sdfg) == {'i', 'j', 'k'}
+    scalar_to_symbol.promote_scalars_to_symbols(sdfg)
+    sdfg.apply_strict_transformations()
+
+    # Assertions on SDFG
+
+    # Check result
+    A = np.random.rand(2, 3, 4, 5)
+    B = np.random.rand(4)
+    expected = A[0, 2, :, 1]
+    expected[0:2] += A[0, 0, 0, 2]
+    expected[2:4] += 1
+
+    sdfg(A=A, B=B)
+
+    assert np.allclose(B, expected)
 
 
 if __name__ == '__main__':
     test_find_promotable()
     test_promote_simple()
+    test_promote_simple_c()
+    test_promote_disconnect()
+    test_promote_copy()
+    test_promote_array_assignment()
+    test_promote_array_assignment_tasklet()
+    test_promote_loop()
     test_promote_loops()
-    test_promote_indirection()
+    # test_promote_indirection()
