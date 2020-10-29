d40a32c6d33273e7c8bdb01f220deced21a2ac50
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-10-29 18:08:15 +0100

    Refactor

--- dace/transformation/dataflow/prune_connectors.py
+++ dace/transformation/dataflow/prune_connectors.py
@@ -6,6 +6,18 @@ from dace import registry, SDFG, SDFGState
 from dace.transformation import transformation as pm, helpers
 from dace.sdfg import nodes, utils
 
+def connectors_to_prune(nsdfg):
+
+        read_set = set()
+        write_set = set()
+
+        for state in nsdfg.sdfg.states():
+            rs, ws = helpers.read_and_write_set(state)
+            read_set |= rs
+            write_set |= ws
+
+        return (nsdfg.in_connectors.keys() - read_set, nsdfg.out_connectors.keys() - write_set)
+
 @registry.autoregister_params(singlestate=True, strict=True)
 class PruneConnectors(pm.Transformation):
     """ Removes unused connectors from nested SDFGs, as well as their memlets
@@ -27,21 +39,18 @@ class PruneConnectors(pm.Transformation):
 
         nsdfg = graph.node(candidate[PruneConnectors.nsdfg])
 
-        read_set = set()
-        write_set = set()
+        prune_in, prune_out = connectors_to_prune(nsdfg)
 
-        for state in nsdfg.sdfg.states():
-            rs, ws = helpers.read_and_write_set(state)
-            read_set |= rs
-            write_set |= ws
-
-        if len(nsdfg.in_connectors.keys() - read_set) > 0:
-            return True
-
-        if len(nsdfg.out_connectors.keys() - write_set) > 0:
+        if len(prune_in) > 0 or len(prune_out) > 0:
             return True
 
         return False
 
     def apply(self, sdfg: SDFG) -> Union[Any, None]:
+
+        graph = sdfg.node(self.state_id)
+        nsdfg = self.nsdfg(sdfg)
+
+        prune_in, prune_out = connectors_to_prune(nsdfg)
+
         raise NotImplementedError
