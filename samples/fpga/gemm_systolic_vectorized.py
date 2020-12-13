87d0c7aacbf08cc5fc9c2670bc7343b2814fede6
Tiziano De Matteis <tiziano.dematteis@inf.ethz.ch>
2020-12-13 01:43:41 +0100

    Use vector types instead of unrolling

--- samples/fpga/gemm_systolic_vectorized.py
+++ samples/fpga/gemm_systolic_vectorized.py
@@ -1,8 +1,8 @@
 # Copyright 2019-2020 ETH Zurich and the DaCe authors. All rights reserved.
 # Computes C = AB + C
 # This sample build on matrix_multiplication_systolic by adding
-# vectorization. Input/output matrices use plain data types (e.g. float) , while the systolic arrays
-# used data type depends on the used vectorization width (e.g., float16 for vec_width = 16)
+# vectorization. The systolic arrays used data type depends on the used vectorization width
+# (e.g., float16 for vec_width = 16)
 
 import argparse
 import dace
@@ -17,31 +17,35 @@ M = dace.symbol("M")
 P = dace.symbol("P")
 
 
-def make_copy_to_fpga_state(sdfg):
+def make_copy_to_fpga_state(sdfg, vec_width = 1):
 
     ###########################################################################
-    # Copy data to FPGA
+    # Copy data to FPGA, from plain to vectorized data type if needed
 
     state = sdfg.add_state("copy_to_device")
+    vec_type = dace.vector(dace.float32, vec_width)
 
+    #host data has plain data types
     sdfg.add_array("A", [N, K], dtype=dace.float32)
-    sdfg.add_array("B", [K, M], dtype=dace.float32)
-    sdfg.add_array("C", [N, M], dtype=dace.float32)
+    sdfg.add_array("B", [K,  M/vec_width], dtype=vec_type)
+    sdfg.add_array("C", [N,  M/vec_width], dtype=vec_type)
     A_host = state.add_read("A")
     B_host = state.add_read("B")
     C_host = state.add_read("C")
 
-    # Matrices are vectorized along rows
+    # On the device, vector B and C will be vectorized along rows.
+    # Matrix A has plain data type
+
     sdfg.add_array("A_device", [N, K],
                    dtype=dace.float32,
                    transient=True,
                    storage=dace.dtypes.StorageType.FPGA_Global)
-    sdfg.add_array("B_device", [K, M],
-                   dtype=dace.float32,
+    sdfg.add_array("B_device", [K, M/vec_width],
+                   dtype=vec_type,
                    transient=True,
                    storage=dace.dtypes.StorageType.FPGA_Global)
-    sdfg.add_array("C_device", [N, M],
-                   dtype=dace.float32,
+    sdfg.add_array("C_device", [N, M/vec_width],
+                   dtype=vec_type,
                    transient=True,
                    storage=dace.dtypes.StorageType.FPGA_Global)
     A_device = state.add_write("A_device")
@@ -53,15 +57,15 @@ def make_copy_to_fpga_state(sdfg):
                           memlet=dace.Memlet("A_device[0:N, 0:K]"))
     state.add_memlet_path(B_host,
                           B_device,
-                          memlet=dace.Memlet("B_device[0:K, 0:M]"))
+                          memlet=dace.Memlet("B_device[0:K, 0:M/{}]".format(vec_width)))
     state.add_memlet_path(C_host,
                           C_device,
-                          memlet=dace.Memlet("C_device[0:N, 0:M]"))
+                          memlet=dace.Memlet("C_device[0:N, 0:M/{}]".format(vec_width)))
 
     return state
 
 
-def make_copy_to_host_state(sdfg):
+def make_copy_to_host_state(sdfg, vec_width=1):
 
     ###########################################################################
     # Copy data to FPGA
@@ -71,14 +75,14 @@ def make_copy_to_host_state(sdfg):
     C_device = state.add_read("C_device")
     C_host = state.add_write("C")
 
-    state.add_memlet_path(C_device, C_host, memlet=dace.Memlet("C[0:N, 0:M]"))
+    state.add_memlet_path(C_device, C_host, memlet=dace.Memlet("C[0:N, 0:M/{}]".format(vec_width)))
 
     return state
 
 
 def make_read_A(state):
 
-    # TODO: vectorize also this, by reading more than one element at a time
+    # A is stored with plain data type
     entry, exit = state.add_map("read_A", {
         "n0": "0:N/P",
         "k": "0:K",
@@ -104,69 +108,34 @@ def make_read_A(state):
 
 
 def make_read_B(state, sdfg, vec_width=1):
-    # B is accessed by row
-    # gear boxing: we read plain data types, we stream vector data types
-    # Therefore we have two maps, the innermost is unrolled
+    # B is vectorized along rows
     entry, exit = state.add_map("read_B", {
         "n": "0:N/P",
         "k": "0:K",
-        "m0": "0:M/{}".format(vec_width)
+        "m": "0:M/{}".format(vec_width)
     },
                                 schedule=dace.ScheduleType.FPGA_Device)
 
-    read_map_entry, read_map_exit = state.add_map(
-        "unrolled_reads_B", {"m1": "0:{}".format(vec_width)},
-        schedule=dace.ScheduleType.FPGA_Device,
-        unroll=True)
-
-    # local storage to accumulate data
-    sdfg.add_array('vec_data_B',
-                   shape=[vec_width],
-                   dtype=dace.float32,
-                   transient=True,
-                   storage=dace.dtypes.StorageType.FPGA_Registers)
     mem = state.add_read("B_device")
     pipe = state.add_write("B_pipe")
-    vect_data = state.add_access("vec_data_B")
     tasklet = state.add_tasklet("read_B", {"from_memory"}, {"to_kernel"},
                                 "to_kernel = from_memory")
 
-    # In the innermost map we read W=vec_width data elements and we store them into `vec_data`
     state.add_memlet_path(mem,
                           entry,
-                          read_map_entry,
                           tasklet,
                           dst_conn="from_memory",
-                          memlet=dace.Memlet(
-                              "B_device[k, m0*{}+m1]".format(vec_width)))
-
+                          memlet=dace.Memlet("B_device[k, m]"))
     state.add_memlet_path(tasklet,
-                          read_map_exit,
-                          vect_data,
-                          src_conn="to_kernel",
-                          memlet=dace.Memlet("vec_data_B[m1]"))
-
-    # then we transfer them to the output stream
-    copy_out_tasklet = state.add_tasklet('pack_and_copy_to_stream_B',
-                                         {'in_con'}, {'out_con'},
-                                         'out_con = in_con')
-    state.add_memlet_path(vect_data,
-                          copy_out_tasklet,
-                          dst_conn="in_con",
-                          memlet=dace.Memlet("vec_data_B"))
-
-    state.add_memlet_path(copy_out_tasklet,
                           exit,
                           pipe,
-                          src_conn="out_con",
+                          src_conn="to_kernel",
                           memlet=dace.Memlet("B_pipe[0]"))
 
 
-def make_write_C(state, sdfg, vec_width):
+def make_write_C(state, sdfg, vec_width=1):
 
-    # C data arrives as expressed in vect. data type. Needs to be unpacked
-    # For doing so we first store it into a local buffer and then we write it in memory
-    # as gear boxing works on local data only (not global memory)
+    # Receives the results and adds it to C
 
     pipe = state.add_read("C_pipe")
     mem_read = state.add_read("C_device")
@@ -174,69 +143,33 @@ def make_write_C(state, sdfg, vec_width):
 
     entry_map, exit_map = state.add_map("write_C", {
         "n": "0:N",
-        "m0": "0:M/{}".format(vec_width)
+        "m": "0:M/{}".format(vec_width)
     },
                                         schedule=dace.ScheduleType.FPGA_Device)
 
-    write_map_entry, write_map_exit = state.add_map(
-        "unrolled_write_C", {"m1": "0:{}".format(vec_width)},
-        schedule=dace.ScheduleType.FPGA_Device,
-        unroll=True)
 
-    # local storage to accumulate data
-    sdfg.add_array('vec_data_C',
-                   shape=[vec_width],
-                   dtype=dace.float32,
-                   transient=True,
-                   storage=dace.dtypes.StorageType.FPGA_Registers)
-
-    vect_data = state.add_access("vec_data_C")
-
-    # then we transfer them to the output stream
-    copy_in_tasklet = state.add_tasklet('copy_from_stream_C', {'in_con'},
-                                        {'out_con'}, 'out_con = in_con')
-
-    state.add_memlet_path(pipe,
-                          entry_map,
-                          copy_in_tasklet,
-                          dst_conn="in_con",
-                          memlet=dace.Memlet("C_pipe[P-1]"))
-    # this will trigger gear boxing
-    state.add_memlet_path(copy_in_tasklet,
-                          vect_data,
-                          src_conn="out_con",
-                          memlet=dace.Memlet("vec_data_C"))
-
-    #then we copy that to memory
+    # write in memory by adding itthen we copy that to memory
     tasklet = state.add_tasklet("write_C", {"from_kernel", "prev_c"}, {"to_memory"},
                                 "to_memory = from_kernel + prev_c")
-    state.add_memlet_path(vect_data,
-                          write_map_entry,
+    state.add_memlet_path(pipe,
+                          entry_map,
                           tasklet,
                           dst_conn="from_kernel",
-                          memlet=dace.Memlet("vec_data_C[m1]"))
+                          memlet=dace.Memlet("C_pipe[P-1]"))
 
     state.add_memlet_path(mem_read,
                           entry_map,
-                          write_map_entry,
                           tasklet,
                           dst_conn="prev_c",
-                          memlet=dace.Memlet("C_device[n, m0*{}+m1]".format(vec_width)))
+                          memlet=dace.Memlet("C_device[n, m]".format(vec_width)))
 
     state.add_memlet_path(tasklet,
-                          write_map_exit,
                           exit_map,
                           mem,
                           src_conn="to_memory",
                           memlet=dace.Memlet(
-                              "C_device[n, m0*{}+m1]".format(vec_width)))
+                              "C_device[n, m]"))
 
-    # previous versio, direct copy
-
-    # state.add_memlet_path(pipe,
-    #                       mem,
-    #                       memlet=dace.Memlet("C_device[0:N, 0:M]",
-    #                                          other_subset="P"))
 
 
 def make_compute(sdfg, state, vec_width=1):
@@ -351,30 +284,6 @@ if p < P - 1:
                           src_conn="c_out")
     state.add_memlet_path(C_buffer_out, exit_n0, memlet=dace.Memlet())
 
-    # Write back
-    #     write_c_tasklet = state.add_tasklet(
-    #         "write_c", {"buffer_in", "forward_in"}, {"c_out"}, """\
-    # if n1 == 0:
-    #     c_out = buffer_in
-    # elif n1 <= p:
-    #     c_out = forward_in""")
-    #     state.add_memlet_path(C_buffer_out,
-    #                           entry_c,
-    #                           write_c_tasklet,
-    #                           memlet=dace.Memlet("C_buffer[m]", dynamic=True),
-    #                           dst_conn="buffer_in")
-    #     state.add_memlet_path(C_pipe_in,
-    #                           entry_n0,
-    #                           entry_c,
-    #                           write_c_tasklet,
-    #                           memlet=dace.Memlet("C_pipe[p]", dynamic=True),
-    #                           dst_conn="forward_in")
-    #     state.add_memlet_path(write_c_tasklet,
-    #                           exit_c,
-    #                           exit_n0,
-    #                           C_pipe_out,
-    #                           memlet=dace.Memlet("C_pipe[p+1]", dynamic=True),
-    #                           src_conn="c_out")
 
     write_c_tasklet = state.add_tasklet(
         "write_c", {"buffer_in", "forward_in"}, {"c_out"}, """\
@@ -443,9 +352,7 @@ def make_fpga_state(sdfg, vec_width=1):
     return state
 
 
-def make_sdfg(specialized):
-
-    vec_width = 16
+def make_sdfg(specialized, vec_width):
 
     if specialized:
         sdfg = dace.SDFG("gemm_fpga_systolic_vectorized_{}_{}x{}x{}".format(
@@ -454,13 +361,12 @@ def make_sdfg(specialized):
         sdfg = dace.SDFG("gemm_fpga_systolic_vectorized_{}_NxKx{}".format(
             P.get(), M.get()))
 
-    pre_state = make_copy_to_fpga_state(sdfg)
+    pre_state = make_copy_to_fpga_state(sdfg, vec_width)
     compute_state = make_fpga_state(sdfg, vec_width)
-    post_state = make_copy_to_host_state(sdfg)
+    post_state = make_copy_to_host_state(sdfg, vec_width)
 
     sdfg.add_edge(pre_state, compute_state, dace.sdfg.InterstateEdge())
     sdfg.add_edge(compute_state, post_state, dace.sdfg.InterstateEdge())
-    sdfg.save('/tmp/out.sdfg')
     return sdfg
 
 
@@ -468,21 +374,22 @@ if __name__ == "__main__":
     print("==== Program start ====")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("M", type=int)
     parser.add_argument("N", type=int)
+    parser.add_argument("M", type=int)
     parser.add_argument("K", type=int)
-    parser.add_argument("P", type=int)
+    parser.add_argument("P", type=int, help="Number of processing elements")
+    parser.add_argument("W", type=int, help="Vectorization width")
     parser.add_argument("-specialize",
                         default=False,
                         action="store_true",
                         help="Fix all loop bounds at compile time/in hardware")
     args = vars(parser.parse_args())
-
+    vec_width = args["W"]
     if not args["specialize"]:
         P.set(args["P"])
         M.set(args["M"])
         # M must always be specialized, as it's used for the static buffer size
-        sdfg = make_sdfg(False)
+        sdfg = make_sdfg(False, vec_width)
         sdfg.specialize(dict(P=P, M=M))
         N.set(args["N"])
         K.set(args["K"])
@@ -491,7 +398,7 @@ if __name__ == "__main__":
         M.set(args["M"])
         N.set(args["N"])
         K.set(args["K"])
-        sdfg = make_sdfg(True)
+        sdfg = make_sdfg(True, vec_width)
         sdfg.specialize(dict(P=P, M=M, N=N, K=K))
 
     print("Matrix multiplication {}x{}x{} with {} PEs ({}specialized)".format(
@@ -504,7 +411,7 @@ if __name__ == "__main__":
     C = np.ndarray([N.get(), M.get()], dtype=dace.float32.type)
     A[:] = np.random.rand(N.get(), K.get()).astype(dace.float32.type)
     B[:] = np.random.rand(K.get(), M.get()).astype(dace.float32.type)
-    C[:] = np.random.rand(K.get(), M.get()).astype(dace.float32.type)
+    C[:] = np.random.rand(N.get(), M.get()).astype(dace.float32.type)
 
     C_regression = np.ndarray([N.get(), M.get()], dtype=np.float32)
 
