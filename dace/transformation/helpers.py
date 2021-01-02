d9a8d390b074d24168a85e436720c90f8020e93e
luca <lucalav@student.ethz.ch>
2021-01-02 12:08:09 +0100

    deduplicate fix

--- dace/transformation/helpers.py
+++ dace/transformation/helpers.py
@@ -495,8 +495,7 @@ def are_subsets_contiguous(subset_a: subsets.Subset,
     # General case
     bbunion = subsets.bounding_box_union(subset_a, subset_b)
     try:
-        # NOTE: Changed here!!
-        if bbunion.num_elements() <= (subset_a.num_elements() +
+        if bbunion.num_elements() == (subset_a.num_elements() +
                                         subset_b.num_elements()):
             return True
     except TypeError:
