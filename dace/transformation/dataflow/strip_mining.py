575c5e0d1103572f1e2072e9251dec212435a1a4
thobauma <43672505+thobauma@users.noreply.github.com>
2021-01-06 10:41:43 +0100

    Update strip_mining.py
    small change

--- dace/transformation/dataflow/strip_mining.py
+++ dace/transformation/dataflow/strip_mining.py
@@ -407,10 +407,10 @@ class StripMining(transformation.Transformation):
         dim_idx = self.dim_idx
         target_dim = map_entry.map.params[dim_idx]
 
-        if self.tiling_type is 'ceilrange':
+        if self.tiling_type == 'ceilrange':
             new_dim, new_map, td_rng = self._create_ceil_range(
                 sdfg, graph, map_entry)
-        elif self.tiling_type is 'number_of_tiles':
+        elif self.tiling_type == 'number_of_tiles':
             new_dim, new_map, td_rng = self._create_from_tile_numbers(
                 sdfg, graph, map_entry)
         else:
