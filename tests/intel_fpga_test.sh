baa51624c41a58a62ceefbb6f2532ebc90023ce1
Johannes de Fine Licht <johannes@musicmedia.dk>
2021-01-22 17:49:21 +0100

    Whoops

--- tests/intel_fpga_test.sh
+++ tests/intel_fpga_test.sh
@@ -54,89 +54,89 @@ run_sample() {
 
 run_all() {
 
-    # #### Vectorization ####
-    # # TODO: These tests require getting access to how types are generated in
-    # # process_out_memlets on a fine-grained level, which is not yet possible.
-    # # This will be implement as part of the new codegen.
-    # # run_sample intel_fpga/vec_sum vec_sum "\n" true
-    # # run_sample intel_fpga/vec_sum vec_sum "\n" false
-    # # run_sample fpga/veclen_conversion_connector "\n"
-    # run_sample fpga/veclen_conversion "\n"
-    # run_sample fpga/veclen_copy_conversion "\n"
-    #
-    # # Test removing degenerate loops that only have a single iteration
-    # run_sample fpga/remove_degenerate_loop remove_degenerate_loop_test "\n" 
-    #
-    # # Test pipeline scopes 
-    # run_sample fpga/pipeline_scope pipeline_scope "\n" 
-    #
-    # # Test shift register abstraction with stencil code
-    # run_sample fpga/fpga_stencil fpga_stencil_test "\n"
-    #
-    # ### MAP TILING ####
-    # # First tile then transform
-    # run_sample intel_fpga/dot dot "MapTiling\$0\nFPGATransformSDFG\$0\n"
-    # # Other way around
-    # run_sample intel_fpga/dot dot "FPGATransformSDFG\$0\nMapTiling\$0\n"
-    #
-    # #### WCR ####
-    # # simple WCR (accumulates on scalar)
-    # run_sample intel_fpga/dot dot "FPGATransformSDFG\$0\n"
-    #
-    # #### REDUCE ####
-    # # Simple reduce
-    # run_sample intel_fpga/vector_reduce vector_reduce "FPGATransformSDFG\$0\n"
-    #
-    # # Matrix multiplication sample
-    # run_sample ../samples/simple/matmul matmul "FPGATransformSDFG\$0\n"
-    #
-    # #### TYPE INFERENCE ####
-    # run_sample ../samples/simple/mandelbrot mandelbrot "FPGATransformSDFG\$0\n"
-    #
-    # # type inference for statements with annotation
-    # run_sample intel_fpga/type_inference type_inference "FPGATransformSDFG\$0\n"
-    #
-    # #### SYSTOLIC ARRAY ###
-    # run_sample intel_fpga/simple_systolic_array simple_systolic_array_4 "\n" 128 4
-    # run_sample ../samples/fpga/matrix_multiplication_systolic mm_fpga_systolic_4_NxKx256 "\n" 256 256 256 4
-    # run_sample ../samples/fpga/jacobi_fpga_systolic jacobi_fpga_systolic_8_Hx8192xT "\n"
-    #
-    # #### MISCELLANEOUS ####
-    # # Execute some of the compatible tests in samples/fpga (some of them have C++ code in tasklet)
-    # # They contain streams
-    # run_sample intel_fpga/async async_test "\n" 
-    # run_sample ../samples/fpga/filter_fpga filter_fpga "\n" 1000 0.2
-    # run_sample ../samples/fpga/matrix_multiplication_stream mm_fpga_stream_NxKx128 "\n" 128 128 128
-    # run_sample ../samples/fpga/spmv_fpga_stream spmv_fpga_stream "\n" 128 128 64
-    # run_sample ../samples/fpga/axpy_transformed axpy_fpga_24 "\n" 24
-    #
-    # ## Multiple kernels
-    # run_sample fpga/multiple_kernels multiple_kernels "\n"
-    #
-    # #Unique nested sdfg
-    # run_sample fpga/unique_nested_sdfg_fpga two_vecAdd "\n"
-    #
-    # ## BLAS
+    #### Vectorization ####
+    # TODO: These tests require getting access to how types are generated in
+    # process_out_memlets on a fine-grained level, which is not yet possible.
+    # This will be implement as part of the new codegen.
+    # run_sample intel_fpga/vec_sum vec_sum "\n" true
+    # run_sample intel_fpga/vec_sum vec_sum "\n" false
+    # run_sample fpga/veclen_conversion_connector "\n"
+    run_sample fpga/veclen_conversion "\n"
+    run_sample fpga/veclen_copy_conversion "\n"
+
+    # Test removing degenerate loops that only have a single iteration
+    run_sample fpga/remove_degenerate_loop remove_degenerate_loop_test "\n" 
+
+    # Test pipeline scopes 
+    run_sample fpga/pipeline_scope pipeline_scope "\n" 
+
+    # Test shift register abstraction with stencil code
+    run_sample fpga/fpga_stencil fpga_stencil_test "\n"
+
+    ### MAP TILING ####
+    # First tile then transform
+    run_sample intel_fpga/dot dot "MapTiling\$0\nFPGATransformSDFG\$0\n"
+    # Other way around
+    run_sample intel_fpga/dot dot "FPGATransformSDFG\$0\nMapTiling\$0\n"
+
+    #### WCR ####
+    # simple WCR (accumulates on scalar)
+    run_sample intel_fpga/dot dot "FPGATransformSDFG\$0\n"
+
+    #### REDUCE ####
+    # Simple reduce
+    run_sample intel_fpga/vector_reduce vector_reduce "FPGATransformSDFG\$0\n"
+
+    # Matrix multiplication sample
+    run_sample ../samples/simple/matmul matmul "FPGATransformSDFG\$0\n"
+
+    #### TYPE INFERENCE ####
+    run_sample ../samples/simple/mandelbrot mandelbrot "FPGATransformSDFG\$0\n"
+
+    # type inference for statements with annotation
+    run_sample intel_fpga/type_inference type_inference "FPGATransformSDFG\$0\n"
+
+    #### SYSTOLIC ARRAY ###
+    run_sample intel_fpga/simple_systolic_array simple_systolic_array_4 "\n" 128 4
+    run_sample ../samples/fpga/matrix_multiplication_systolic mm_fpga_systolic_4_NxKx256 "\n" 256 256 256 4
+    run_sample ../samples/fpga/jacobi_fpga_systolic jacobi_fpga_systolic_8_Hx8192xT "\n"
+
+    #### MISCELLANEOUS ####
+    # Execute some of the compatible tests in samples/fpga (some of them have C++ code in tasklet)
+    # They contain streams
+    run_sample intel_fpga/async async_test "\n" 
+    run_sample ../samples/fpga/filter_fpga filter_fpga "\n" 1000 0.2
+    run_sample ../samples/fpga/matrix_multiplication_stream mm_fpga_stream_NxKx128 "\n" 128 128 128
+    run_sample ../samples/fpga/spmv_fpga_stream spmv_fpga_stream "\n" 128 128 64
+    run_sample ../samples/fpga/axpy_transformed axpy_fpga_24 "\n" 24
+
+    ## Multiple kernels
+    run_sample fpga/multiple_kernels multiple_kernels "\n"
+
+    #Unique nested sdfg
+    run_sample fpga/unique_nested_sdfg_fpga two_vecAdd "\n"
+
+    ## BLAS
     run_sample blas/nodes/axpy_test axpy_test_fpga_4_w4_1 "\n" --target fpga
-    # run_sample blas/nodes/dot_test dot_FPGA_Accumulate_float_w16_1 "\n" --target intel_fpga
-    # run_sample blas/nodes/gemv_test gemv_fpga_test "\n" --target tiles_by_column
-    # run_sample blas/nodes/gemv_test gemv_fpga_test "\n" --target accumulate --vectorize 4 
-    # run_sample blas/nodes/ger_test ger_test_w8_x16_y32 "\n" --target fpga 
-    #
-    # # Nested SDFGs generated as FPGA kernels
-    # run_sample fpga/nested_sdfg_as_kernel nested_sdfg_kernels "\n"
-    #
-    # # Generating autorun kernels
-    # run_sample intel_fpga/autorun autorun_test "\n"
-    #
-    # # Multiple gearboxing
-    # run_sample fpga/multiple_veclen_conversions multiple_veclen_conversions "\n"
-    #
-    # # Channels mangling
-    # run_sample intel_fpga/channels_mangling channels_mangling "\n"
-    #
-    # # Constant Type inference
-    # run_sample intel_fpga/constant_type_inference constant_type_inference "\n"
+    run_sample blas/nodes/dot_test dot_FPGA_Accumulate_float_w16_1 "\n" --target intel_fpga
+    run_sample blas/nodes/gemv_test gemv_fpga_test "\n" --target tiles_by_column
+    run_sample blas/nodes/gemv_test gemv_fpga_test "\n" --target accumulate --vectorize 4 
+    run_sample blas/nodes/ger_test ger_test_w8_x16_y32 "\n" --target fpga 
+
+    # Nested SDFGs generated as FPGA kernels
+    run_sample fpga/nested_sdfg_as_kernel nested_sdfg_kernels "\n"
+
+    # Generating autorun kernels
+    run_sample intel_fpga/autorun autorun_test "\n"
+
+    # Multiple gearboxing
+    run_sample fpga/multiple_veclen_conversions multiple_veclen_conversions "\n"
+
+    # Channels mangling
+    run_sample intel_fpga/channels_mangling channels_mangling "\n"
+
+    # Constant Type inference
+    run_sample intel_fpga/constant_type_inference constant_type_inference "\n"
 
 }
 
