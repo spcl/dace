4f47e7d9131f9427249cfb458c9ae0c845054e0f
Tal Ben-Nun <tbennun@gmail.com>
2021-02-01 10:36:31 +0100

    GEMV test

---
+++ tests/blas/nodes/blas_nodes_test.py
@@ -0,0 +1,39 @@
+# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+import dace
+import dace.libraries.blas as blas
+from dace.transformation.dataflow import RedundantSecondArray
+import numpy as np
+import pytest
+
+M = dace.symbol('M')
+N = dace.symbol('N')
+
+
+@pytest.mark.parametrize(('implementation', ),
+                         [('pure', ), ('MKL', ),
+                          pytest.param('cuBLAS', marks=pytest.mark.gpu)])
+def test_gemv_strided(implementation):
+    @dace.program
+    def gemv(A: dace.float64[M, N], x: dace.float64[N, N]):
+        return A @ x[:, 1]
+
+    A = np.random.rand(20, 30)
+    x = np.random.rand(30, 30)
+    reference = A @ x[:, 1]
+    sdfg = gemv.to_sdfg()
+    sdfg.name = f'{sdfg.name}_{implementation}'
+    if implementation == 'cuBLAS':
+        sdfg.apply_gpu_transformations()
+        sdfg.apply_transformations_repeated(RedundantSecondArray)
+
+    blas.default_implementation = implementation
+
+    daceres = sdfg(A=A, x=x, M=20, N=30)
+    assert np.allclose(daceres, reference)
+
+
+
+if __name__ == '__main__':
+    test_gemv_strided('pure')
+    test_gemv_strided('MKL')
+    test_gemv_strided('cuBLAS')
