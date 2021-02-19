641f8bc3f03f976b49fc74e5304dd6beab5abc00
luca <sancierra@gmail.com>
2021-02-19 14:28:17 +0100

    __init__.py

--- dace/transformation/estimator/__init__.py
+++ dace/transformation/estimator/__init__.py
@@ -1,3 +1,3 @@
 from .scoring import ScoringFunction, ExecutionScore, MemletScore, RegisterScore
 from .enumeration import Enumerator
-from .enumeration import BruteForceEnumerator, ConnectedEnumerator
+from .enumeration import BruteForceEnumerator, ConnectedEnumerator, GreedyEnumerator
