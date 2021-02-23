b98c9ef2242dd7ae9de2d2793a42b514e0c7c12f
Timo Schneider <timos@inf.ethz.ch>
2021-02-23 18:02:26 +0100

    add getrf library node

---
+++ dace/libraries/lapack/nodes/getrf.py
@@ -0,0 +1,136 @@
+# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+import copy
+import dace.library
+import dace.properties
+import dace.sdfg.nodes
+from dace.symbolic import symstr
+from dace.transformation.transformation import ExpandTransformation
+from .. import environments
+from dace import data as dt, dtypes, memlet as mm, SDFG, SDFGState, symbolic
+from dace.frontend.common import op_repository as oprepo
+
+
+@dace.library.expansion
+class ExpandDotPure(ExpandTransformation):
+    """
+    Naive backend-agnostic expansion of LAPACK GETRF.
+    """
+
+    environments = []
+
+    @staticmethod
+    def expansion(node, parent_state, parent_sdfg, n=None, **kwargs):
+        raise(NotImplementedError)
+  
+@dace.library.expansion
+class ExpandDotOpenBLAS(ExpandTransformation):
+
+    environments = [environments.openblas.OpenBLAS]
+
+    @staticmethod
+    def expansion(node, parent_state, parent_sdfg, n=None, **kwargs):
+        (desc_x, stride_x, rows_x, cols_x),  desc_ipiv, desc_result = node.validate(
+            parent_sdfg, parent_state)
+        dtype = desc_x.dtype.base_type
+        lapack_dtype = "X"
+        if dtype == dace.dtypes.float32:
+            lapack_dtype = "s"
+        elif dtype == dace.dtypes.float64:
+            lapack_dtype = "d" 
+        elif dtype == dace.dtypes.complex64:
+            lapack_dtype = "c"
+        elif dtype == dace.dtypes.complex128:
+            lapack_dtype = "z"
+        else:
+            print("The datatype "+str(dtype)+" is not supported!")
+            raise(NotImplementedError) 
+        if desc_x.dtype.veclen > 1:
+            raise(NotImplementedError)
+
+
+        n = n or node.n
+        code = f"_res[0] = LAPACKE_{lapack_dtype}getrf(LAPACK_ROW_MAJOR, {rows_x}, {cols_x}, _xin, {stride_x}, _ipiv);"
+        tasklet = dace.sdfg.nodes.Tasklet(node.name,
+                                          node.in_connectors,
+                                          node.out_connectors,
+                                          code,
+                                          language=dace.dtypes.Language.CPP)
+        return tasklet
+
+
+@dace.library.expansion
+class ExpandDotMKL(ExpandTransformation):
+
+    environments = [environments.intel_mkl.IntelMKL]
+
+    @staticmethod
+    def expansion(*args, **kwargs):
+        return ExpandDotOpenBLAS.expansion(*args, **kwargs)
+
+
+@dace.library.node
+class Getrf(dace.sdfg.nodes.LibraryNode):
+
+    # Global properties
+    implementations = {
+        "OpenBLAS": ExpandDotOpenBLAS,
+        "MKL": ExpandDotMKL,
+    }
+    default_implementation = ExpandDotOpenBLAS
+
+    # Object fields
+    n = dace.properties.SymbolicProperty(allow_none=True, default=None)
+
+    def __init__(self, name, n=None, *args, **kwargs):
+        super().__init__(name,
+                         *args,
+                         inputs={"_xin"},
+                         outputs={"_xout", "_ipiv", "_res"},
+                         **kwargs)
+
+    def validate(self, sdfg, state):
+        """
+        :return: A three-tuple (x, ipiv, res) of the three data descriptors in the
+                 parent SDFG.
+        """
+        in_edges = state.in_edges(self)
+        if len(in_edges) != 1:
+            raise ValueError("Expected exactly one input to getrf")
+        in_memlets = [in_edges[0].data]
+        out_edges = state.out_edges(self)
+        if len(out_edges) != 3:
+            raise ValueError("Expected exactly three outputs from dot product")
+        out_memlet = out_edges[0].data
+
+        # Squeeze input memlets
+        squeezed1 = copy.deepcopy(in_memlets[0].subset)
+        sqdims1 = squeezed1.squeeze()
+        
+        desc_xin, desc_xout, desc_ipiv, desc_res = None, None, None, None
+        for e in state.in_edges(self):
+            if e.dst_conn == "_xin":
+                desc_xin = sdfg.arrays[e.data.data]
+        for e in state.out_edges(self):
+            if e.src_conn == "_xout":
+                desc_xout = sdfg.arrays[e.data.data]
+            if e.src_conn == "_ipiv":
+                desc_ipiv = sdfg.arrays[e.data.data]
+            if e.src_conn == "_result":
+                desc_res = sdfg.arrays[e.data.data]
+        
+        if desc_xin.dtype.base_type != desc_xout.dtype.base_type:
+            raise ValueError("Basetype of input and output must be equal!")
+        if desc_ipiv.dtype.base_type != dace.dtypes.int32:
+            raise ValueError("Pivot output must be an integer array!")
+
+        stride_x = desc_xin.strides[sqdims1[0]]
+        rows_x = desc_xin.shape[0]
+        cols_x = desc_xin.shape[1]
+
+        if len(squeezed1.size()) != 2:
+            print(str(squeezed1))
+            raise ValueError(
+                "getrf only supported on 2-dimensional arrays")
+
+        return (desc_xin, stride_x, rows_x, cols_x), desc_ipiv, desc_res
+
---
+++ tests/library/lapack_getrf_test.py
@@ -0,0 +1,98 @@
+# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+import dace
+from dace.memlet import Memlet
+from dace.codegen.exceptions import CompilerConfigurationError, CompilationError
+import dace.libraries.lapack as lapack
+import numpy as np
+import sys
+import warnings
+
+###############################################################################
+
+
+def make_sdfg(implementation, dtype, storage=dace.StorageType.Default):
+
+    n = dace.symbol("n")
+
+    suffix = "_device" if storage != dace.StorageType.Default else ""
+    transient = storage != dace.StorageType.Default
+
+    sdfg = dace.SDFG("dot_product_{}_{}".format(implementation, dtype))
+    state = sdfg.add_state("dataflow")
+
+    sdfg.add_array("x" + suffix, [n,n],
+                   dtype,
+                   storage=storage,
+                   transient=transient)
+    sdfg.add_array("pivots" + suffix, [n],
+                   dace.dtypes.int32,
+                   storage=storage,
+                   transient=transient)
+    sdfg.add_array("result" + suffix, [1],
+                   dace.dtypes.int32,
+                   storage=storage,
+                   transient=transient)
+
+    xin = state.add_read("x" + suffix)
+    xout = state.add_read("x" + suffix)
+    pivots = state.add_read("pivots" + suffix)
+    result = state.add_write("result" + suffix)
+
+    getrf_node = lapack.nodes.getrf.Getrf("getrf")
+    getrf_node.implementation = implementation
+
+    state.add_memlet_path(xin,
+                          getrf_node,
+                          dst_conn="_xin",
+                          memlet=Memlet.simple(xin, "0:n, 0:n", num_accesses=n*n))
+    # TODO: remove -1 once this no longer triggers a write in the codegen.
+    state.add_memlet_path(getrf_node,
+                          result,
+                          src_conn="_res",
+                          memlet=Memlet.simple(result, "0", num_accesses=-1))
+    state.add_memlet_path(getrf_node,
+                          pivots,
+                          src_conn="_ipiv",
+                          memlet=Memlet.simple(pivots, "0:n", num_accesses=-1))
+    state.add_memlet_path(getrf_node,
+                          xout,
+                          src_conn="_xout",
+                          memlet=Memlet.simple(xout, "0:n, 0:n", num_accesses=-1))
+
+
+
+    return sdfg
+
+
+###############################################################################
+
+
+def _test_getrf(implementation, dtype, sdfg):
+    getrf_sdfg = sdfg.compile()
+    
+    from scipy.linalg import lu_factor
+    size = 4
+    lapack_status = np.array([-1], dtype=np.int32)
+    A = np.array([[2, 5, 8, 7], [5, 2, 2, 8], [7, 5, 6, 6], [5, 4, 4, 8]], dtype=dtype)
+    lu_ref, piv_ref = lu_factor(A)
+  
+    # the x is input AND output, the "result" argument gives the lapack status!
+    getrf_sdfg(x=A, result=lapack_status, pivots=np.ndarray([0,0,0,0], dtype=np.int32), n=size)
+
+    comparison = A == lu_ref
+    
+    if comparison.all():
+        print("Test ran successfully for {}.".format(implementation))
+    else:
+        raise ValueError("Validation error!")
+
+
+def test_getrf():
+    _test_getrf("32-bit MKL", np.float32, make_sdfg("MKL", dace.float32))
+    _test_getrf("64-bit MKL", np.float64, make_sdfg("MKL", dace.float64))
+
+###############################################################################
+
+if __name__ == "__main__":
+    test_getrf()
+###############################################################################
