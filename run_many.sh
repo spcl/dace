f3375beb8ea5fc7072e6787d8f97d1f400d2f6e4
Oliver Rausch <oliverrausch99@gmail.com>
2021-02-26 00:03:02 +0100

    Bisector

---
+++ run_many.sh
@@ -0,0 +1,13 @@
+set -e
+pytest tests/library/gemm_test.py || false
+pytest tests/reloadable_lib_test.py || false
+pytest tests/library/gemm_test.py || false
+pytest tests/reloadable_lib_test.py || false
+pytest tests/library/gemm_test.py || false
+pytest tests/reloadable_lib_test.py || false
+pytest tests/library/gemm_test.py || false
+pytest tests/reloadable_lib_test.py || false
+pytest tests/library/gemm_test.py || false
+pytest tests/reloadable_lib_test.py || false
+pytest tests/library/gemm_test.py || false
+pytest tests/reloadable_lib_test.py || false
