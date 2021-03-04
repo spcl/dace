7c3dcedc835d0e6add0af7e31ba5c54572d955e1
Timo Schneider <timos@inf.ethz.ch>
2021-03-04 18:02:20 +0100

    linalg libs need both cblas and lapack

--- dace/libraries/linalg/environments/openblas.py
+++ dace/libraries/linalg/environments/openblas.py
@@ -6,15 +6,15 @@ import dace.library
 class OpenBLAS:
 
     cmake_minimum_version = "3.6"
-    cmake_packages = ["LAPACK"]
+    cmake_packages = ["LAPACK", "BLAS"]
     cmake_variables = {"BLA_VENDOR": "OpenBLAS"}
     cmake_includes = []  
-    cmake_libraries = ["${LAPACK_LIBRARIES}"]
+    cmake_libraries = ["${LAPACK_LIBRARIES}" "${BLAS_LIBRARIES}"]
     cmake_compile_flags = []
-    cmake_link_flags = ["${LAPACK_LINKER_FLAGS}"]
+    cmake_link_flags = ["${LAPACK_LINKER_FLAGS}", "${BLAS_LINKER_FLAGS}"]
     cmake_files = []
 
-    headers = ["lapacke.h"]
+    headers = ["cblas.h", "lapacke.h"]
     state_fields = []
     init_code = ""
     finalize_code = ""
