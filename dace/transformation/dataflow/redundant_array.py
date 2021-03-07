d8e950ea356de66cc5888afef742617f9a51ad0b
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-03-07 19:11:48 +0100

    Added extra check in `_validate_subsets`.

--- dace/transformation/dataflow/redundant_array.py
+++ dace/transformation/dataflow/redundant_array.py
@@ -48,6 +48,10 @@ def _validate_subsets(edge: graph.MultiConnectorEdge,
             desc = arrays[src_name]
             if isinstance(desc, data.View) or edge.data.data == dst_name:
                 src_subset = subsets.Range.from_array(desc)
+                if src_subset.num_elements() != dst_subset.num_elements():
+                    raise ValueError(
+                        "Source subset is missing (dst_subset: {}, "
+                        "src_shape: {}".format(dst_subset, desc.shape))
             else:
                 src_subset = copy.deepcopy(dst_subset)
                 padding = len(desc.shape) - len(src_subset)
@@ -72,6 +76,10 @@ def _validate_subsets(edge: graph.MultiConnectorEdge,
             desc = arrays[dst_name]
             if isinstance(desc, data.View) or edge.data.data == src_name:
                 dst_subset = subsets.Range.from_array(desc)
+                if src_subset.num_elements() != dst_subset.num_elements():
+                    raise ValueError(
+                        "Destination subset is missing (src_subset: {}, "
+                        "dst_shape: {}".format(src_subset, desc.shape))
             else:
                 dst_subset = copy.deepcopy(src_subset)
                 padding = len(desc.shape) - len(dst_subset)
@@ -417,7 +425,7 @@ class RedundantSecondArray(pm.Transformation):
             # We may proceed only in the case of View -> Access
             if not isinstance(in_desc, data.View):
                 return False
-            # Check that the VIew's immediate ancestors are Accesses
+            # Check that the View's immediate ancestors are Accesses.
             # Otherwise, the application of the transformation will result in an
             # ambiguous View.
             view_ancestors_desc = [
