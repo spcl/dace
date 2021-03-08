231bc378430857908447aa46e7ed59ab07596886
Tiziano De Matteis <tdematt@inf.ethz.ch>
2021-03-08 17:37:45 +0100

    Declare and define view in the same line

--- dace/codegen/targets/cpu.py
+++ dace/codegen/targets/cpu.py
@@ -197,10 +197,13 @@ class CPUCodeGen(TargetCodeGenerator):
                                                         dtypes.pointer(
                                                             nodedesc.dtype),
                                                         ancestor=0)
-
-        declaration_stream.write(f'{atype} {aname};', sdfg, state_id, node)
-        # Casting is already done in emit_memlet_reference
-        allocation_stream.write(f'{aname} = {value};', sdfg, state_id, node)
+        if declaration_stream == allocation_stream:
+            # Declare and define to guarantee compatibility with OpenCL
+            declaration_stream.write(f'{atype} {aname} = {value};', sdfg, state_id, node)
+        else:
+            declaration_stream.write(f'{atype} {aname};', sdfg, state_id, node)
+            # Casting is already done in emit_memlet_reference
+            allocation_stream.write(f'{aname} = {value};', sdfg, state_id, node)
 
     def allocate_array(self, sdfg, dfg, state_id, node, function_stream,
                        declaration_stream, allocation_stream):
