f0729ce031b1f5f7ec0ffe3343b057b4f56c897e
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-03-12 17:45:42 +0100

    Enhanced the lapack getrs test to test the cuSolver implementation too.

--- tests/library/lapack_getrs_test.py
+++ tests/library/lapack_getrs_test.py
@@ -2,6 +2,7 @@
 import dace
 from dace.memlet import Memlet
 from dace.codegen.exceptions import CompilerConfigurationError, CompilationError
+import dace.libraries.blas as blas
 import dace.libraries.lapack as lapack
 import numpy as np
 import sys
@@ -21,31 +22,69 @@ def make_sdfg(implementation, dtype, storage=dace.StorageType.Default):
     sdfg = dace.SDFG("matrix_solve_getrf_getrs_{}_{}".format(implementation, dtype))
     state = sdfg.add_state("dataflow")
 
-    sdfg.add_array("A" + suffix, [n,n],
-                   dtype,
-                   storage=storage,
-                   transient=False)
-    sdfg.add_array("B" + suffix, [n],
-                   dtype,
-                   storage=storage,
-                   transient=False)
+    Ahost_arr = sdfg.add_array("A", [n, n],
+                               dtype,
+                               storage=dace.StorageType.Default)
+    Bhost_arr = sdfg.add_array("B", [n],
+                               dtype,
+                               storage=dace.StorageType.Default)
+
+    if transient:
+        A_arr = sdfg.add_array("A" + suffix, [n, n],
+                               dtype,
+                               storage=storage,
+                               transient=transient)
+        AT_arr = sdfg.add_array('AT' + suffix, [n, n],
+                                dtype,
+                                storage=storage,
+                                transient=transient)
+        B_arr = sdfg.add_array("B" + suffix, [n],
+                               dtype,
+                               storage=storage,
+                               transient=transient)
     sdfg.add_array("pivots" + suffix, [n],
                    dace.dtypes.int32,
                    storage=storage,
-                   transient=True)
+                   transient=transient)
     sdfg.add_array("result_getrf" + suffix, [1],
                    dace.dtypes.int32,
                    storage=storage,
-                   transient=False)
+                   transient=transient)
     sdfg.add_array("result_getrs" + suffix, [1],
                    dace.dtypes.int32,
                    storage=storage,
-                   transient=False)
-    
-    ain = state.add_access("A" + suffix)
-    aout_getrf = state.add_access("A" + suffix)
-    rhs_in_getrs = state.add_access("B" + suffix)
-    rhs_out_getrs = state.add_access("B" + suffix)
+                   transient=transient)
+
+    if transient:
+        Ahi = state.add_read("A")
+        # Aho = state.add_write("A")
+        Ai = state.add_access("A" + suffix)
+        # Ao = state.add_access("A" + suffix)
+        Ain = state.add_access("AT" + suffix)
+        Aout = state.add_access("AT" + suffix)
+        Bhi = state.add_read("B")
+        Bho = state.add_read("B")
+        Bin = state.add_access("B" + suffix)
+        Bout = state.add_access("B" + suffix)
+        transpose_in = blas.nodes.transpose.Transpose("transpose_in",
+                                                      dtype=dtype)
+        transpose_in.implementation = "cuBLAS"
+        state.add_nedge(Ahi, Ai, Memlet.from_array(*Ahost_arr))
+        state.add_nedge(Bhi, Bin, Memlet.from_array(*Bhost_arr))
+        state.add_nedge(Bout, Bho, Memlet.from_array(*Bhost_arr))
+        state.add_memlet_path(Ai,
+                              transpose_in,
+                              dst_conn='_inp',
+                              memlet=Memlet.from_array(*A_arr))
+        state.add_memlet_path(transpose_in,
+                              Ain,
+                              src_conn='_out',
+                              memlet=Memlet.from_array(*AT_arr))
+    else:
+        Ain = state.add_access("A" + suffix)
+        Aout = state.add_access("A" + suffix)
+        Bin = state.add_access("B" + suffix)
+        Bout = state.add_access("B" + suffix)
     pivots = state.add_access("pivots" + suffix)
     res_getrf = state.add_access("result_getrf" + suffix)
     res_getrs = state.add_access("result_getrs" + suffix)
@@ -56,10 +95,10 @@ def make_sdfg(implementation, dtype, storage=dace.StorageType.Default):
     getrs_node.implementation = implementation
 
 
-    state.add_memlet_path(ain,
+    state.add_memlet_path(Ain,
                           getrf_node,
                           dst_conn="_xin",
-                          memlet=Memlet.simple(ain, "0:n, 0:n", num_accesses=n*n))
+                          memlet=Memlet.simple(Ain, "0:n, 0:n", num_accesses=n*n))
     state.add_memlet_path(getrf_node,
                           res_getrf,
                           src_conn="_res",
@@ -77,21 +116,21 @@ def make_sdfg(implementation, dtype, storage=dace.StorageType.Default):
                           dst_conn="_ipiv",
                           memlet=Memlet.simple(pivots, "0:n", num_accesses=n))
     state.add_memlet_path(getrf_node,
-                          aout_getrf,
+                          Aout,
                           src_conn="_xout",
-                          memlet=Memlet.simple(aout_getrf, "0:n, 0:n", num_accesses=n*n))
-    state.add_memlet_path(aout_getrf,
+                          memlet=Memlet.simple(Aout, "0:n, 0:n", num_accesses=n*n))
+    state.add_memlet_path(Aout,
                           getrs_node,
                           dst_conn="_a",
-                          memlet=Memlet.simple(aout_getrf, "0:n, 0:n", num_accesses=n*n))
-    state.add_memlet_path(rhs_in_getrs,
+                          memlet=Memlet.simple(Aout, "0:n, 0:n", num_accesses=n*n))
+    state.add_memlet_path(Bin,
                           getrs_node,
                           dst_conn="_rhs_in",
-                          memlet=Memlet.simple(rhs_in_getrs, "0:n", num_accesses=n))
+                          memlet=Memlet.simple(Bin, "0:n", num_accesses=n))
     state.add_memlet_path(getrs_node,
-                          rhs_out_getrs,
+                          Bout,
                           src_conn="_rhs_out",
-                          memlet=Memlet.simple(rhs_out_getrs, "0:n", num_accesses=n))
+                          memlet=Memlet.simple(Bout, "0:n", num_accesses=n))
 
     return sdfg
 
@@ -100,10 +139,12 @@ def make_sdfg(implementation, dtype, storage=dace.StorageType.Default):
 
 @pytest.mark.parametrize("implementation, dtype", [
     pytest.param("MKL", dace.float32, marks=pytest.mark.mkl),
-    pytest.param("MKL", dace.float64, marks=pytest.mark.mkl)
+    pytest.param("MKL", dace.float64, marks=pytest.mark.mkl),
+    pytest.param("cuSolverDn", dace.float32, marks=pytest.mark.gpu),
+    pytest.param("cuSolverDn", dace.float64, marks=pytest.mark.gpu),
 ])
-def test_getrs(implementation, dtype):
-    sdfg = make_sdfg(implementation, dtype)
+def test_getrs(implementation, dtype, storage=dace.StorageType.Default):
+    sdfg = make_sdfg(implementation, dtype, storage)
     solve_sdfg = sdfg.compile()
     np_dtype = getattr(np, dtype.to_string())
     
@@ -116,8 +157,8 @@ def test_getrs(implementation, dtype):
     if not np.allclose(np.dot(a1, x), b1):
         raise ValueError("NumPy solve returned wrong result o_O")
   
-    lapack_status1 = np.array([-1], dtype=np_dtype)
-    lapack_status2 = np.array([-1], dtype=np_dtype)
+    lapack_status1 = np.array([-1], dtype=np.int32)
+    lapack_status2 = np.array([-1], dtype=np.int32)
     a2 = np.copy(a1)  # a input will be overwritten by its lu factorization (by getrf) 
     b2 = np.copy(b1)  # rhs input will be overwritten by the solution (by getrs)
     solve_sdfg(A=a2, B=b2, result_getrf=lapack_status1, result_getrs=lapack_status2, pivots=np.ndarray([0,0], dtype=np.int32), n=2)
@@ -125,7 +166,8 @@ def test_getrs(implementation, dtype):
     if np.allclose(np.dot(a1, b2), b1):
         print("Test ran successfully for {}.".format(implementation))
     else:
-        print(A1-A3)
+        print(b2)
+        print(np.dot(a1, b2) - b1)
         raise ValueError("Validation error!")
 
 
@@ -134,4 +176,6 @@ def test_getrs(implementation, dtype):
 if __name__ == "__main__":
     test_getrs("MKL", dace.float32)
     test_getrs("MKL", dace.float64)
+    test_getrs("cuSolverDn", dace.float32, dace.StorageType.GPU_Global)
+    test_getrs("cuSolverDn", dace.float64, dace.StorageType.GPU_Global)
 ###############################################################################
