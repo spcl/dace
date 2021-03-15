f55ad4adebc9e411ba8635549b9d4ae2085cb9ce
Tiziano De Matteis <tdematt@inf.ethz.ch>
2021-03-15 11:21:38 +0100

    Address PR comments

--- tests/xilinx_test.sh
+++ tests/xilinx_test.sh
@@ -120,7 +120,7 @@ run_all() {
     run_sample blas/nodes/ger_test ger_test_1 1 --target fpga
 
     ## STD LibNodes
-    run_multi_sample fpga/reduce_fpga 0 "reduction_sum_one_axis" "reduction_sum_all_axis" "reduction_sum_4D" "reduction_max"
+    run_multi_sample fpga/reduce_fpga 1 "reduction_sum_one_axis" "reduction_sum_all_axis" "reduction_sum_4D" "reduction_max"
 
     # Multiple gearboxing
     run_sample fpga/multiple_veclen_conversions multiple_veclen_conversions 0
