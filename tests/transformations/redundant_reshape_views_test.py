83d3cc7723b68b69882ad611277e022770de3428
Oliver Rausch <oliverrausch99@gmail.com>
2021-03-22 15:54:52 +0100

    Remove regression test of other issue

--- tests/transformations/redundant_reshape_views_test.py
+++ tests/transformations/redundant_reshape_views_test.py
@@ -42,27 +42,6 @@ def test_inline_reshape_views_work():
     assert(views == 3)
 
 
-def test_regression_reshape_unsqueeze():
-    nsdfg = dace.SDFG("nested_reshape_node")
-    nstate = nsdfg.add_state()
-    nsdfg.add_array("input", [9], dace.float64)
-    nsdfg.add_view("view", [3, 3], dace.float64)
-    nsdfg.add_array("output", [3, 3], dace.float64)
-
-    R = nstate.add_read("input")
-    A = nstate.add_access("view")
-    W = nstate.add_write("output")
-
-    nstate.add_edge(R, None, A, None, dace.Memlet("input[0:3, 0:3]"))
-    nstate.add_edge(A, None, W, None, dace.Memlet("view[0:3, 0:3]"))
-
-    @dace.program
-    def test_reshape_unsqueeze(A: dace.float64[9], B: dace.float64[3, 3]):
-        nsdfg(input=A, output=B)
-
-    test_reshape_unsqueeze.to_sdfg()
-
-
 def test_views_between_maps_work():
     @dace.program
     def test_inline_reshape_views_work(A: dace.float64[3, 3], B: dace.float64[9]):
