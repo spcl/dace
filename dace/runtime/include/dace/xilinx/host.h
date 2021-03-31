291251ea8c03cecdeb028ae458cd773e3b7afbe1
definelicht <definelicht@inf.ethz.ch>
2021-03-31 16:23:34 +0200

    Update host.h

--- dace/runtime/include/dace/xilinx/host.h
+++ dace/runtime/include/dace/xilinx/host.h
@@ -5,6 +5,6 @@
 
 #include "hlslib/xilinx/OpenCL.h"
 
-#include <dace/fpga_host.h>  // Must be included after hlslib/xilinx/SDAccel.h
+#include <dace/fpga_host.h>  // Must be included after hlslib/xilinx/OpenCL.h
 #include <dace/os.h>
 #include <dace/types.h>
