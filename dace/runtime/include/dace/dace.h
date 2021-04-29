abe71ec6f08bf36ad47d53a187568177c348dc3c
Oliver Rausch <oliverrausch99@gmail.com>
2021-04-29 11:13:57 +0200

    hack in vec ops

--- dace/runtime/include/dace/dace.h
+++ dace/runtime/include/dace/dace.h
@@ -41,4 +41,39 @@
 
 #include "fpga_common.h"
 
+DACE_HDFI float4 exp(float4 v) {
+    float4 result;
+    result.x = exp(v.x);
+    result.y = exp(v.y);
+    result.z = exp(v.z);
+    result.w = exp(v.w);
+    return result;
+}
+
+DACE_HDFI float4 operator+(float f, float4 v) {
+    return make_float4(v.x + f, v.y + f, v.z + f, v.w + f);
+}
+
+DACE_HDFI float4 operator*(float4 u, float4 v) {
+    return make_float4(u.x * v.x, u.y * v.y, u.z * v.z, u.w * v.w);
+}
+
+DACE_HDFI float4 log(float4 v) {
+    float4 result;
+    result.x = log(v.x);
+    result.y = log(v.y);
+    result.z = log(v.z);
+    result.w = log(v.w);
+    return result;
+}
+
+DACE_HDFI float4 tanh(float4 v) {
+    float4 result;
+    result.x = tanh(v.x);
+    result.y = tanh(v.y);
+    result.z = tanh(v.z);
+    result.w = tanh(v.w);
+    return result;
+}
+
 #endif  // __DACE_RUNTIME_H
