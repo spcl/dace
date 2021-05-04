92974a6f824a72f7ce78a28a73b684fa5eee4535
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-05-04 13:35:40 +0200

    Minor update to script.

--- sc21.sh
+++ sc21.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for (( i=1; i<=32; i*=2 ))
+for (( i=1; i<=8; i*=2 ))
 do
     echo "Running with $i processes ..."
     OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 DACE_compiler_use_cache=1 timeout 60s mpirun -n $i python samples/distributed/explicit/sc21_bench.py
