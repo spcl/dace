3602874741e1475376cb26576d74333a59619f1f
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-05-06 00:27:59 +0200

    Explicitly linking to liblapacke, liblapack, and libblas.

--- dace/libraries/blas/environments/openblas.py
+++ dace/libraries/blas/environments/openblas.py
@@ -6,11 +6,12 @@ import ctypes.util
 class OpenBLAS:
 
     cmake_minimum_version = "3.6"
-    cmake_packages = ["BLAS", "LAPACK"]
+    cmake_packages = ["LAPACK", "BLAS"]
     cmake_variables = {"BLA_VENDOR": "OpenBLAS"}
     cmake_includes = []  # For some reason, FindBLAS does not find includes
     cmake_compile_flags = []
-    cmake_link_flags = ["${BLAS_LINKER_FLAGS} ${LAPACK_LINKER_FLAGS}"]
+    # cmake_link_flags = ["${BLAS_LINKER_FLAGS} ${LAPACK_LINKER_FLAGS}"]
+    cmake_link_flags = ["-llapacke -llapack -lblas -lgfortran"]
     cmake_files = []
 
     headers = ["cblas.h", "lapacke.h", "../include/dace_blas.h"]
@@ -22,8 +23,11 @@ class OpenBLAS:
     @staticmethod
     def cmake_libraries():
         path = ctypes.util.find_library('openblas')
+        print(path)
         if path:
-            return ["${BLAS_LIBRARIES} ${LAPACK_LIBRARIES}"]
+            return ['liblapacke.so', 'liblapack.so', 'libblas.so']
+            # return ["${BLAS_LIBRARIES} ${LAPACK_LIBRARIES}"]
+            # return ["${BLAS_openblas_LIBRARY}", "${LAPACK_openblas_LIBRARY}"]
 
         return []
 
