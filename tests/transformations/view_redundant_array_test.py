072a32d9b8f0f3819d0d687390f34b2ef192190b
Oliver Rausch <oliverrausch99@gmail.com>
2021-05-13 13:40:56 +0200

    Add failing test cases for RedundantArray

--- tests/transformations/view_redundant_array_test.py
+++ tests/transformations/view_redundant_array_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytest
 
 import dace
+from dace import nodes, data
 
 
 def test_redundant_array_removal():
@@ -23,8 +24,8 @@ def test_redundant_array_removal():
 
     data_accesses = {
         n.data
-        for n, _ in
-        test_redundant_array_removal.to_sdfg(strict=True).all_nodes_recursive()
+        for n, _ in test_redundant_array_removal.to_sdfg(
+            strict=True).all_nodes_recursive()
         if isinstance(n, dace.nodes.AccessNode)
     }
     assert "A_reshaped" not in data_accesses
@@ -46,3 +47,70 @@ def test_libnode_expansion():
     C = test_broken_matmul(A.copy(), B.copy())
 
     assert np.allclose(A @ B, C)
+
+
+@pytest.mark.parametrize("copy_subset", ["O", "T"])
+def test_redundant_array_1_into_2_dims(copy_subset):
+    sdfg = dace.SDFG("testing")
+    state = sdfg.add_state()
+
+    sdfg.add_array("I", [9], dtype=dace.float32, transient=False)
+    sdfg.add_array("T", [9], dtype=dace.float32, transient=True)
+    sdfg.add_array("O", [3, 3], dtype=dace.float32, transient=False)
+
+    state.add_mapped_tasklet("add_one",
+                             dict(i="0:9"),
+                             dict(inp=dace.Memlet("I[i]")),
+                             "out = inp + 1",
+                             dict(out=dace.Memlet("T[i]")),
+                             external_edges=True)
+    copy_state = sdfg.add_state_after(state)
+    copy_state.add_edge(copy_state.add_read("T"), None,
+                        copy_state.add_write("O"), None,
+                        sdfg.make_array_memlet(copy_subset))
+
+    sdfg.apply_strict_transformations()
+    sdfg.view()
+
+    I = np.ones((3, 3), dtype=np.float32)
+    O = np.zeros_like(I)
+    sdfg(I=I, O=O)
+    assert np.allclose(O, I + 1)
+    assert len([
+        n for n in sdfg.node(0).nodes()
+        if isinstance(n, nodes.AccessNode) and type(n.desc(sdfg)) is data.Array
+    ]) == 2
+
+
+@pytest.mark.parametrize("copy_subset", ["O", "T"])
+def test_redundant_array_2_into_1_dim(copy_subset):
+
+    sdfg = dace.SDFG("testing")
+    state = sdfg.add_state()
+
+    sdfg.add_array("I", [3, 3], dtype=dace.float32, transient=False)
+    sdfg.add_array("T", [3, 3], dtype=dace.float32, transient=True)
+    sdfg.add_array("O", [9], dtype=dace.float32, transient=False)
+
+    state.add_mapped_tasklet("add_one",
+                             dict(i="0:3", j="0:3"),
+                             dict(inp=dace.Memlet("I[i, j]")),
+                             "out = inp + 1",
+                             dict(out=dace.Memlet("T[i, j]")),
+                             external_edges=True)
+    copy_state = sdfg.add_state_after(state)
+    copy_state.add_edge(copy_state.add_read("T"), None,
+                        copy_state.add_write("O"), None,
+                        sdfg.make_array_memlet(copy_subset))
+
+    sdfg.apply_strict_transformations()
+    sdfg.view()
+
+    I = np.ones((3, 3), dtype=np.float32)
+    O = np.zeros_like(I)
+    sdfg(I=I, O=O)
+    assert np.allclose(O, I + 1)
+    assert len([
+        n for n in sdfg.node(0).nodes()
+        if isinstance(n, nodes.AccessNode) and type(n.desc(sdfg)) is data.Array
+    ]) == 2
