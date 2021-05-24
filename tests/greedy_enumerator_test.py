563bc20118ddce5a33579e9f40475325b2835f9e
luca <lucalav@student.ethz.ch>
2021-05-24 19:27:23 +0200

    Autoopt: Extend greedy enum test to connected / brute force enum

--- tests/greedy_enumerator_test.py
+++ tests/greedy_enumerator_test.py
@@ -1,4 +1,6 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+from dace.transformation.estimator.enumeration.brute_force_enumerator import BruteForceEnumerator
+from dace.transformation.estimator.enumeration.connected_enumerator import ConnectedEnumerator
 import dace
 import numpy as np
 import pytest
@@ -46,7 +48,7 @@ def p1(in1: dace.float32[W, H, B], in2: dace.float32[W, H],
 
 
 @pytest.mark.parametrize(["map_splits"], [[True], [False]])
-def test(map_splits):
+def test_greedy(map_splits):
     # Test diamond graph structure and ensure topologically correct enumeration
     w = 30
     h = 30
@@ -73,6 +75,70 @@ def test(map_splits):
         assert len(result) == 2
 
 
+
+@pytest.mark.parametrize(["map_splits"], [[True], [False]])
+def test_connected(map_splits):
+    # Test diamond graph structure and ensure topologically correct enumeration
+    w = 30
+    h = 30
+    b = 20
+    A1 = np.random.rand(w, h, b).astype(np.float32)
+    A2 = np.random.rand(w, h).astype(np.float32)
+    ret = np.zeros([w, h], dtype=np.float32)
+
+    sdfg = p1.to_sdfg()
+    sdfg.apply_strict_transformations()
+    graph = sdfg.nodes()[0]
+
+    sdfg.apply_transformations_repeated(ReduceExpansion)
+
+    subgraph = SubgraphView(graph, graph.nodes())
+    composite = CompositeFusion(subgraph)
+    composite.expansion_split = map_splits
+    cf = lambda sdfg, subgraph: composite.can_be_applied(sdfg, subgraph)
+    enum = ConnectedEnumerator(sdfg, graph, subgraph, cf)
+    result = enum.list()
+
+    if map_splits:
+        assert len(result) == 14
+    else:
+        assert len(result) == 4
+
+
+
+@pytest.mark.parametrize(["map_splits"], [[True], [False]])
+def test_brute_force(map_splits):
+    # Test diamond graph structure and ensure topologically correct enumeration
+    w = 30
+    h = 30
+    b = 20
+    A1 = np.random.rand(w, h, b).astype(np.float32)
+    A2 = np.random.rand(w, h).astype(np.float32)
+    ret = np.zeros([w, h], dtype=np.float32)
+
+    sdfg = p1.to_sdfg()
+    sdfg.apply_strict_transformations()
+    graph = sdfg.nodes()[0]
+
+    sdfg.apply_transformations_repeated(ReduceExpansion)
+
+    subgraph = SubgraphView(graph, graph.nodes())
+    composite = CompositeFusion(subgraph)
+    composite.expansion_split = map_splits
+    cf = lambda sdfg, subgraph: composite.can_be_applied(sdfg, subgraph)
+    enum = BruteForceEnumerator(sdfg, graph, subgraph, cf)
+    result = enum.list()
+    if map_splits:
+        assert len(result) == 15
+    else:
+        assert len(result) == 5
+
 if __name__ == "__main__":
-    test(True)
-    test(False)
+    test_greedy(True)
+    test_greedy(False)
+
+    test_connected(True) 
+    test_connected(False)
+
+    test_brute_force(True)
+    test_brute_force(False)
