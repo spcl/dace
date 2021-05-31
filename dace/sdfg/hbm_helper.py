b03ed250565eb2bce7056dfe9f74cc75fa2f241c
Jannis Widmer <widmerja@student.ethz.ch>
2021-05-31 18:51:15 +0200

    Added hbm_helper for general functions in the context of hbm

---
+++ dace/sdfg/hbm_helper.py
@@ -0,0 +1,40 @@
+from dace import data, memlet, subsets, symbolic, dtypes, sdfg as sd
+from typing import Iterable, Union, Any
+
+def parseHBMArray(arrayname : str, array : data.Array) -> "dict[str, Any]":
+    """
+    parses HBM properties of an array (hbmbank and hbmalignment). 
+    Returns none if hbmbank is not present as property
+
+    :return: A mapping from (arrayname, sdfg of the array) to a mapping
+    from string that contains collected information.
+    'ndim': contains the dimension of the array
+    'splitcount': contains how many times this array is split
+        on each of the axes along which it is split 
+        (ie splitcount==2 on an 2d-array which is split along axis 0
+        and axis 1 => There are 4 parts)
+    'splitaxes': List that contains axes along which the array is split
+    'lowbank': The lowest bank index this array is placed on
+    'shape': The shape of the whole array
+    'numbanks': The number of banks across which this array spans
+    """
+    if("hbmbank" not in array.location):
+        return None
+    hbmbankrange : subsets.Range = array.location["hbmbank"]
+    if(not isinstance(hbmbankrange, subsets.Range)):
+        raise TypeError(f"Locationproperty 'hbmbank' must be of type subsets.Range for {arrayname}")
+    low, high, stride = hbmbankrange[0]
+    if(stride != 1):
+        raise NotImplementedError(f"Locationproperty 'hbmbank' does not support stride != 1 for {arrayname}")
+    numbank = high - low + 1
+    shape = array.shape
+    ndim = len(shape)
+    if(low == high):
+        return {"ndim" : ndim, "shape" : array.shape,
+            "lowbank" : low, "numbank": 1}
+    if(shape[0] != numbank):
+        raise ValueError("The size of the first dimension for an array divided "
+            "accross k HBM-Banks must equal k. This does not hold for "
+            f"{arrayname}")
+    return {"ndim" : ndim, "shape" : array.shape, "lowbank" : low,
+            "numbank": numbank}
