9ddeb54e648a51ef0d46b90be0d29cabf2047a0c
sscholbe <sscholbe@student.ethz.ch>
2021-06-01 17:27:42 +0200

    Modified the SDFG name to not include the invalid dace:: prefix

--- tests/library/lapack_getrs_test.py
+++ tests/library/lapack_getrs_test.py
@@ -17,7 +17,7 @@ def make_sdfg(implementation, dtype, storage=dace.StorageType.Default):
     transient = storage != dace.StorageType.Default
 
     sdfg = dace.SDFG("matrix_solve_getrf_getrs_{}_{}".format(
-        implementation, dtype))
+        implementation, dtype.ocltype))
     state = sdfg.add_state("dataflow")
 
     Ahost_arr = sdfg.add_array("A", [n, n],
