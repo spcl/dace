185bf2817fd0eab5f8cb42739818d7be694e7e08
Jannis Widmer <widmerja@student.ethz.ch>
2021-06-05 01:04:32 +0200

    ptr modified

--- dace/codegen/targets/cpp.py
+++ dace/codegen/targets/cpp.py
@@ -216,7 +216,7 @@ def memlet_copy_to_absolute_strides(dispatcher,
     return copy_shape, src_strides, dst_strides, src_expr, dst_expr
 
 
-def ptr(name: str, desc: data.Data, subset_info : Any = None) -> str:
+def ptr(name: str, desc: data.Data, subset_info : "Union[subsets.Subset, int]" = None) -> str:
     """
     Returns a string that points to the data based on its name and descriptor.
     :param name: Data name.
@@ -231,6 +231,13 @@ def ptr(name: str, desc: data.Data, subset_info : Any = None) -> str:
         from dace.codegen.targets.cuda import CUDACodeGen  # Avoid import loop
         if not CUDACodeGen._in_device_code:  # GPU kernels cannot access state
             return f'__state->{name}'
+    if("hbmbank" in desc.location):
+        if(subset_info == None):
+            raise ValueError("Cannot generate name for hbmbank without subset info")
+        elif(isinstance(subset_info, int)):
+            return f"{name}_hbm{subset_info}" #for hbm banks this is an int
+        elif(isinstance(subset_info, subsets.Subset)):
+            return f"{name}_hbm{subset_info[0][0]}"
 
     return name
 
