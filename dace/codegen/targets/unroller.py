98df826c5b088caa18c22dd37ac56dcea63bbe55
Jannis Widmer <widmerja@student.ethz.ch>
2021-06-07 17:46:19 +0200

    Cleanup again + yapf

--- dace/codegen/targets/unroller.py
+++ dace/codegen/targets/unroller.py
@@ -1,5 +1,5 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
-from copy import deepcopy
+import copy
 from typing import Any, Dict, Tuple
 
 import dace
@@ -18,7 +18,9 @@ from dace.sdfg.replace import deepreplace
 from dace.sdfg import nodes as nd
 from dace import Config
 
-def backup_statescope_fields(subgraph: 'dace.sdfg.state.StateGraphView') -> Tuple:
+
+def backup_statescope_fields(
+        subgraph: 'dace.sdfg.state.StateGraphView') -> Tuple:
     """
     Creates mappings from all nodes/edges to all the fields that are affected 
     by replace, before overwritting all those fields with deepcopies of themselves.
@@ -34,18 +36,19 @@ def backup_statescope_fields(subgraph: 'dace.sdfg.state.StateGraphView') -> Tupl
         for propclass, propval in node.properties():
             pname = propclass.attr_name
             oldproperties[node] = (pname, propval)
-            setattr(node, pname, deepcopy(propval))
+            setattr(node, pname, copy.deepcopy(propval))
     for edge in subgraph.edges():
         oldedgedata[edge] = edge.data.data
-        edge.data.data = deepcopy(edge.data.data)
+        edge.data.data = copy.deepcopy(edge.data.data)
         oldedgesubsets[edge] = (edge.data.subset, edge.data.other_subset)
-        edge.data.subset = deepcopy(edge.data.subset)
-        edge.data.other_subset = deepcopy(edge.data.other_subset)
+        edge.data.subset = copy.deepcopy(edge.data.subset)
+        edge.data.other_subset = copy.deepcopy(edge.data.other_subset)
         oldedgevolumes[edge] = edge.data.volume
-        edge.data.volume = deepcopy(edge.data.volume)
+        edge.data.volume = copy.deepcopy(edge.data.volume)
     return (oldproperties, oldedgedata, oldedgesubsets, oldedgevolumes)
 
-def use_statescope_fields_backup(backup : "Tuple"):
+
+def use_statescope_fields_backup(backup: "Tuple"):
     """
     Restores values saved using backup_replacement_fields. Will apply
     the changes to the nodes/edges objects it saw during backup, in other
@@ -64,38 +67,40 @@ def use_statescope_fields_backup(backup : "Tuple"):
     for edge, volume in oldedgevolumes.items():
         edge.data.volume = volume
 
-def backup_sdfg_fields(sdfg : dace.SDFG) -> Tuple:
+
+def backup_sdfg_fields(sdfg: dace.SDFG) -> Tuple:
     """
     Backup all the fields of an sdfg that are changed on call to replace.
     Can be restored using use_sdfg_field_backup
     """
     arrback = sdfg._arrays
-    sdfg._arrays = deepcopy(sdfg._arrays)
+    sdfg._arrays = copy.deepcopy(sdfg._arrays)
     symback = sdfg.symbols
-    sdfg.symbols = deepcopy(sdfg.symbols)
+    sdfg.symbols = copy.deepcopy(sdfg.symbols)
 
     arrpropstore = {}
     for array in sdfg.arrays.values():
         for propclass, propval in array.properties():
             propname = propclass.attr_name
             arrpropstore[array] = (propname, propval)
-            setattr(array, propname, deepcopy(propval))
+            setattr(array, propname, copy.deepcopy(propval))
 
     edgefields = []
     for edge in sdfg.edges():
         edgeAssign = edge.data.assignments
-        edge.data.assignments = deepcopy(edge.data.assignments)
+        edge.data.assignments = copy.deepcopy(edge.data.assignments)
         edgeCond = edge.data.condition
-        edge.data.condition = deepcopy(edge.data.condition)
+        edge.data.condition = copy.deepcopy(edge.data.condition)
         edgefields.append((edge, edgeAssign, edgeCond))
-    
+
     statefields = []
     for state in sdfg.nodes():
         statefields.append(backup_statescope_fields(state))
-    
+
     return (sdfg, arrback, symback, arrpropstore, edgefields, statefields)
 
-def use_sdfg_field_backup(backup : Tuple):
+
+def use_sdfg_field_backup(backup: Tuple):
     """
     Counterpart to backup_sdfg_fields. Restores the fields with the values
     from the backup
@@ -115,7 +120,9 @@ def use_sdfg_field_backup(backup : Tuple):
     for stateback in statefields:
         use_statescope_fields_backup(stateback)
 
-def deep_replacement_field_backup(subgraph: 'dace.sdfg.state.StateGraphView') -> "list[Tuple]":
+
+def deep_replacement_field_backup(
+        subgraph: 'dace.sdfg.state.StateGraphView') -> "list[Tuple]":
     """
     Stores all occurences of a symbol in a scope and in the nestedSDFGs inside of it. 
     The backup can be applied using use_deep_replacement_fields_backup
@@ -123,7 +130,7 @@ def deep_replacement_field_backup(subgraph: 'dace.sdfg.state.StateGraphView') ->
     def _backupNSDFG(sg):
         backups = []
         for node in sg.nodes():
-            if(isinstance(node, nd.NestedSDFG)):
+            if (isinstance(node, nd.NestedSDFG)):
                 backups.append(backup_sdfg_fields(node.sdfg))
                 for nsdfg_state in node.sdfg.nodes():
                     backups.extend(_backupNSDFG(nsdfg_state))
@@ -133,6 +140,7 @@ def deep_replacement_field_backup(subgraph: 'dace.sdfg.state.StateGraphView') ->
     backups.extend(_backupNSDFG(subgraph))
     return backups
 
+
 def use_deep_replacement_fields_backup(backups: "list[Tuple]"):
     """
     Counterpart to deep_replacement_field_backup,restores all values saved by it.
@@ -140,7 +148,8 @@ def use_deep_replacement_fields_backup(backups: "list[Tuple]"):
     use_statescope_fields_backup(backups[0])
     for i in range(1, len(backups)):
         use_sdfg_field_backup(backups[i])
-        
+
+
 @registry.autoregister_params(name='unroll')
 class UnrollCodeGen(TargetCodeGenerator):
     """ A constant-range map unroller code generator. """
@@ -154,7 +163,8 @@ class UnrollCodeGen(TargetCodeGenerator):
         dispatcher = self._dispatcher
 
         # Register dispatchers
-        self._dispatcher.register_map_dispatcher(dace.ScheduleType.Unrolled, self)
+        self._dispatcher.register_map_dispatcher(dace.ScheduleType.Unrolled,
+                                                 self)
 
     def get_generated_codeobjects(self):
         return []
@@ -162,7 +172,7 @@ class UnrollCodeGen(TargetCodeGenerator):
     def generate_scope(self, sdfg: dace.SDFG, scope: ScopeSubgraphView,
                        state_id: int, function_stream: CodeIOStream,
                        callsite_stream: CodeIOStream):
-        
+
         entry_node = scope.source_nodes()[0]
         index_list = []
 
@@ -187,15 +197,20 @@ class UnrollCodeGen(TargetCodeGenerator):
                 )
             backup = []
             for node in scope.nodes():
-                if(isinstance(node, nd.NestedSDFG)):
+                if (isinstance(node, nd.NestedSDFG)):
                     if unique_functions and not unique_functions_hash and node.unique_name != "":
-                        backup.append((node, True, deepcopy(node.unique_name), deepcopy(node.symbol_mapping)))
+                        backup.append(
+                            (node, True, copy.deepcopy(node.unique_name),
+                             copy.deepcopy(node.symbol_mapping)))
                         node.unique_name = f"{node.unique_name}_{param}{paramval}"
                     else:
-                        backup.append((node, False, deepcopy(node.sdfg.name), deepcopy(node.symbol_mapping)))
+                        backup.append(
+                            (node, False, copy.deepcopy(node.sdfg.name),
+                             copy.deepcopy(node.symbol_mapping)))
                         node.sdfg.name = f"{node.sdfg.name}_{param}{paramval}"
                     for nstate in node.sdfg.nodes():
-                        backup.extend(nsdfg_prepare_unroll(nstate, paramname, paramval))
+                        backup.extend(
+                            nsdfg_prepare_unroll(nstate, paramname, paramval))
                     if param in node.symbol_mapping:
                         node.symbol_mapping.pop(param)
             return backup
@@ -207,7 +222,6 @@ class UnrollCodeGen(TargetCodeGenerator):
                 else:
                     node.sdfg.name = name
                 node.symbol_mapping = symbols
-                
 
         for begin, end, stride in entry_node.map.range:
             l = []
@@ -223,13 +237,17 @@ class UnrollCodeGen(TargetCodeGenerator):
             for param, index in zip(entry_node.map.params, indices):
                 deepreplace(scope, str(param), str(index))
                 if nsdfg_unroll_info is None:
-                    nsdfg_unroll_info = nsdfg_prepare_unroll(scope, str(param), str(index))
+                    nsdfg_unroll_info = nsdfg_prepare_unroll(
+                        scope, str(param), str(index))
                 else:
                     nsdfg_prepare_unroll(scope, str(param), str(index))
-            self._dispatcher.dispatch_subgraph(sdfg, scope, state_id,
-                                            function_stream, callsite_stream,
-                                            skip_entry_node=True,
-                                            skip_exit_node=True)
+            self._dispatcher.dispatch_subgraph(sdfg,
+                                               scope,
+                                               state_id,
+                                               function_stream,
+                                               callsite_stream,
+                                               skip_entry_node=True,
+                                               skip_exit_node=True)
             callsite_stream.write('}')
             nsdfg_after_unroll(nsdfg_unroll_info)
             use_deep_replacement_fields_backup(backups)
--- dace/sdfg/replace.py
+++ dace/sdfg/replace.py
@@ -25,17 +25,22 @@ def _replsym(symlist, symrepl):
                           if symbolic.issymbolic(dim) else dim)
     return symlist
 
+
 def deepreplace(subgraph: 'dace.sdfg.state.StateGraphView', name: str,
                 new_name: str):
     """
     Deeply replaces all occurences of name using replace, also in NestedSDFGs.
+    :param subgraph: The given graph or subgraph to replace in.
+    :param name: Name to find.
+    :param new_name: Name to replace.
     """
     replace(subgraph, name, new_name)
     for node in subgraph.nodes():
-        if(isinstance(node, nd.NestedSDFG)):
+        if isinstance(node, nd.NestedSDFG):
             node.sdfg.replace(name, new_name)
             for nstate in node.sdfg.nodes():
                 deepreplace(nstate, name, new_name)
+
 
 def replace(subgraph: 'dace.sdfg.state.StateGraphView', name: str,
             new_name: str):
