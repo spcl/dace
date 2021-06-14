d0dd540f1cc0a380d1edbbbaaed4bdf31275ca77
Jannis Widmer <widmerja@student.ethz.ch>
2021-06-14 10:46:55 +0200

    Expanded deeply nested hbm test

--- tests/fpga/hbm_deeply_nested_fpga.py
+++ tests/fpga/hbm_deeply_nested_fpga.py
@@ -6,30 +6,46 @@ import numpy as np
 def create_deeply_nested_sdfg():
     sdfg = dace.SDFG("deepnest_test")
     state : dace.SDFGState = sdfg.add_state("init")
-    xarr = state.add_array("x", [4, 100, 100], dace.float32)
-    sdfg.arrays["x"].location["hbmbank"] = "0:4"
-    yarr = state.add_array("y", [4, 100, 100], dace.float32)
-    sdfg.arrays["y"].location["hbmbank"] = "4:8"
+    xarr = state.add_array("x", [4, 10], dace.float32)
+    sdfg.arrays["x"].location["hbmbank"] = sbs.Range.from_string("0:4")
+    yarr = state.add_array("y", [4, 10], dace.float32)
+    sdfg.arrays["y"].location["hbmbank"] = sbs.Range.from_string("4:8")
 
     topMapEntry, topMapExit = state.add_map("topmap", dict(k="0:2"))
     
     nsdfg = dace.SDFG("nest")
     nstate = nsdfg.add_state("nested_state")
-    xRead = nstate.add_array("xin", [4, 100, 100], dace.float32)
-    xWrite = nstate.add_array("xout", [4, 100, 100], dace.float32)
+    xRead = nstate.add_array("xin", [4, 10], dace.float32, dtypes.StorageType.FPGA_Global)
+    xWrite = nstate.add_array("xout", [4, 10], dace.float32, dtypes.StorageType.FPGA_Global)
+    nsdfg.arrays["xin"].location["hbmbank"] = sbs.Range.from_string("0:4")
+    nsdfg.arrays["xout"].location["hbmbank"] = sbs.Range.from_string("4:8")
     mapEntry, mapExit = nstate.add_map("map1", dict(w="0:2"))
+    imapEntry, imapExit = nstate.add_map("map2", dict(i="0:10"))
     nope = nstate.add_tasklet("nop", dict(_in=None), dict(_out=None),
                                 "_out = _in")
-    inputMem = mem.Memlet("xin[2*k+w]")
-    outputMem = mem.Memlet("xout[2*k+w]")
-    nstate.add_memlet_path(xRead, mapEntry, nope, memlet=inputMem, dst_conn="_in")
-    nstate.add_memlet_path(nope, xWrite, mapExit, memlet=outputMem, src_conn="_out")
+    inputMem = mem.Memlet("xin[2*k+w, i]")
+    outputMem = mem.Memlet("xout[2*k+w, i]")
+    nstate.add_memlet_path(xRead, mapEntry, imapEntry, nope, memlet=inputMem, dst_conn="_in")
+    nstate.add_memlet_path(nope, imapExit, mapExit, xWrite, memlet=outputMem, src_conn="_out")
     nsdfg_node = state.add_nested_sdfg(nsdfg, state, set(["xin"]), set(['xout']))
 
     state.add_memlet_path(xarr, topMapEntry, nsdfg_node, 
                         memlet=mem.Memlet.from_array("x", sdfg.arrays["x"]), dst_conn="xin")
     state.add_memlet_path(nsdfg_node, topMapExit, yarr, 
                         memlet=mem.Memlet.from_array("y", sdfg.arrays["y"]), src_conn="xout")
-    sdfg.apply_fpga_transformations(validate=False)
+    sdfg.apply_fpga_transformations()
 
     return sdfg
+
+def exec_deeply_nested_test():
+    sdfg = create_deeply_nested_sdfg()
+    #sdfg.view()
+    a = np.zeros((4, 10), np.float32)
+    a[2, 4:9] += 1
+    a[3, 3:8] += 2
+    a[0, 7] += 3
+    c = np.ones((4, 10), np.float32)
+    sdfg(a=a, c=c)
+    assert np.allclose(a, c, 10e-6)
+
+exec_deeply_nested_test()
