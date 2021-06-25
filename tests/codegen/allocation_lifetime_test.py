b20d1b020843521e96cc85d385bb35c48c55419b
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-06-25 16:21:53 +0200

    Updated test to unpack the correct number of variables.

--- tests/codegen/allocation_lifetime_test.py
+++ tests/codegen/allocation_lifetime_test.py
@@ -70,7 +70,7 @@ def _check_alloc(id, name, codegen, scope):
     # for sdfg_id, _, node in codegen.to_allocate[scope]:
     #     if id == sdfg_id and name == node.data:
     #         return True
-    for sdfg, _, node in codegen.to_allocate[scope]:
+    for sdfg, _, node, _, _, _ in codegen.to_allocate[scope]:
         if sdfg.sdfg_id == id and name == node.data:
             return True
     return False
