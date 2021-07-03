1e051d22cef8db763c73a5df862cfb4ecb3797fd
Marc Widmer <mwidmer@protonmail.com>
2021-07-03 11:44:59 +0200

    ensuring fpga streams are allocated scope globally

--- dace/codegen/targets/fpga.py
+++ dace/codegen/targets/fpga.py
@@ -451,7 +451,7 @@ class FPGACodeGen(TargetCodeGenerator):
                     # Global data is passed from outside the kernel
                     global_data_parameters.add(
                         (is_output, dataname, desc, interface_id))
-                elif dataname in shared_data:
+                elif dataname in shared_data or isinstance(desc, dt.Stream):
                     # Add the data as a parameter to this PE
                     subgraph_parameters[subgraph].add(
                         (is_output, dataname, desc, interface_id))
