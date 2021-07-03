8615cbe740d2c7d3af480cc4585d1e3d1fcd6fe6
Marc Widmer <mwidmer@protonmail.com>
2021-07-03 11:45:16 +0200

    fpga stream allocation test

--- tests/stream_allocation_scope_test.py
+++ tests/stream_allocation_scope_test.py
@@ -3,6 +3,102 @@
 """ Test for an issue where streams would be allocated globally (to a state) and locally.
     Expected behavior is to never allocate streams locally to a scope. """
 import dace
+import numpy as np
+
+def test_stream_only_used_in_one_scope():
+    sdfg = dace.SDFG("stream_only_in_one_scope")
+    N = dace.symbol("N")
+    sdfg.add_array("A", [N], dace.float32)
+    sdfg.add_array("B", [N], dace.float32)
+    sdfg.add_stream("accumulator_stream",
+                    dace.float64,
+                    transient=True,
+                    storage=dace.dtypes.StorageType.FPGA_Local
+                    )
+    sdfg.add_array("A_device", [N],
+                   dtype=dace.float32,
+                   transient=True,
+                   storage=dace.dtypes.StorageType.FPGA_Global)
+    sdfg.add_array("B_device", [N],
+                   dtype=dace.float32,
+                   transient=True,
+                   storage=dace.dtypes.StorageType.FPGA_Global)
+    prep_state = sdfg.add_state()
+    A_host = prep_state.add_read("A")
+    A_device = prep_state.add_write("A_device")
+
+    prep_state.add_memlet_path(A_host,
+                          A_device,
+                          memlet=dace.Memlet("A_device[0:N]"))
+    state = sdfg.add_state()
+    a = state.add_read("A_device")
+    b = state.add_write("B_device")
+    read_accumulator_stream = state.add_read("accumulator_stream")
+    write_accumulator_stream = state.add_write("accumulator_stream")
+
+    entry, exit = state.add_map(
+        "map", {"n": "0:N"},
+        schedule=dace.ScheduleType.FPGA_Device)
+
+    acc_tasklet = state.add_tasklet(
+        "accumulate", {"in_data", "acc"}, {"out_data", "acc_out"}, """\
+out_data = in_data if (n == 0) else (in_data + acc)
+acc_out=out_data""")
+
+
+    state.add_memlet_path(
+        a,
+        entry,
+        acc_tasklet,
+        dst_conn="in_data",
+        memlet=dace.Memlet("A_device[n]")
+    )
+    state.add_memlet_path(
+        read_accumulator_stream,
+        entry,
+        acc_tasklet,
+        dst_conn="acc",
+        memlet=dace.Memlet("accumulator_stream[0]", dynamic=True)
+    )
+
+    state.add_memlet_path(
+        acc_tasklet,
+        exit,
+        b,
+        src_conn="out_data",
+        memlet=dace.Memlet("B_device[n]")
+    )
+
+    state.add_memlet_path(
+        acc_tasklet,
+        exit,
+        write_accumulator_stream,
+        src_conn="acc_out",
+        memlet=dace.Memlet("accumulator_stream[0]", dynamic=True)
+    )
+
+    post_state = sdfg.add_state()
+    B_host = post_state.add_write("B")
+    B_device = post_state.add_read("B_device")
+    post_state.add_memlet_path(B_device,
+                          B_host,
+                          memlet=dace.Memlet("B[0:N]"))
+
+    sdfg.add_edge(prep_state, state, dace.sdfg.InterstateEdge())
+    sdfg.add_edge(state, post_state, dace.sdfg.InterstateEdge())
+
+    N.set(5)
+
+    A = np.ndarray([N.get()], dtype=dace.float32.type)
+    B = np.ndarray([N.get()], dtype=dace.float32.type)
+    A[:] = [dace.float32(0),dace.float32(1),dace.float32(2),dace.float32(3),dace.float32(4),]
+
+    sdfg(A, B)
+
+    print(A)
+    print(B)
+
+    #assert sdfg.generate_code()[2].clean_code.count('dace::FIFO<double, 1, 1> accumulator_stream("accumulator_stream");') == 1
 
 def test_stream_allocation_scope():
     sdfg = dace.SDFG("stream_allocation")
@@ -95,4 +191,5 @@ out_data = in_data""")
     # by searching for the substring used in the assertion
 
 if __name__ == '__main__':
+    test_stream_only_used_in_one_scope()
     test_stream_allocation_scope()
