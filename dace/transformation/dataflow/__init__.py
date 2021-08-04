cb3e2a8bc06cc48f33d6173a6c8e3c6d6c2fd4fa
Jannis Widmer <widmerja@student.ethz.ch>
2021-08-04 13:22:46 +0200

    Added HbmBankSplit to __init__

--- dace/transformation/dataflow/__init__.py
+++ dace/transformation/dataflow/__init__.py
@@ -45,6 +45,7 @@ from .gpu_transform import GPUTransformMap
 from .gpu_transform_local_storage import GPUTransformLocalStorage
 from .mpi import MPITransformMap
 from .warp_tiling import WarpTiling
+from .hbm_bank_split import HbmBankSplit
 
 # Algorithmic
 from .matrix_product_transpose import MatrixProductTranspose
