969ed0c022c81b081b3f487afc3a1dad4428488f
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-08-16 14:56:19 +0200

    Various fixes and added tests.

--- dace/transformation/dataflow/tasklet_fusion.py
+++ dace/transformation/dataflow/tasklet_fusion.py
@@ -1,21 +1,14 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
 """ Contains classes that fuse Tasklets """
 
-from dace.sdfg.utils import consolidate_edges
-from copy import deepcopy
-from numbers import Number
-from typing import Any, Dict, List
+from typing import Any, Dict, Set
 import ast
 import dace
 import re
-import sympy
-from dace import data, dtypes, registry, subsets, symbolic
+from dace import dtypes, registry
 from dace.sdfg import nodes
 from dace.sdfg import utils as sdutil
-from dace.sdfg.graph import OrderedMultiDiConnectorGraph
 from dace.transformation import transformation as pm
-from dace.transformation.subgraph.helpers import subgraph_from_maps
-from functools import reduce
 
 
 class ConnectorRenamer(ast.NodeTransformer):
@@ -39,11 +32,11 @@ class PythonRHSExtractor(ast.NodeVisitor):
     """
     def __init__(self):
         self.assignments = set()
-    
+
     def visit_Assign(self, node: ast.Assign):
         if isinstance(node.targets[0], ast.Name):
             self.assignments.add(node.targets[0].id)
-    
+
     def visit_AugAssign(self, node: ast.AugAssign):
         if isinstance(node.targets[0], ast.Name):
             self.assignments.add(node.targets[0].id)
@@ -59,7 +52,10 @@ class SimpleTaskletFusion(pm.Transformation):
 
     @staticmethod
     def expressions():
-        return [sdutil.node_path_graph(SimpleTaskletFusion._t1, SimpleTaskletFusion._t2)]
+        return [
+            sdutil.node_path_graph(SimpleTaskletFusion._t1,
+                                   SimpleTaskletFusion._t2)
+        ]
 
     @staticmethod
     def can_be_applied(graph: dace.SDFGState,
@@ -67,10 +63,25 @@ class SimpleTaskletFusion(pm.Transformation):
                        expr_index: int,
                        sdfg: dace.SDFG,
                        strict: bool = False):
-    
+
         t1 = graph.node(candidate[SimpleTaskletFusion._t1])
         t2 = graph.node(candidate[SimpleTaskletFusion._t2])
-        return t1.language == t2.language
+
+        # Tasklets must be of the same language
+        if t1.language != t2.language:
+            return False
+
+        # Avoid circles
+        t1_dst = set()
+        for e in graph.out_edges(t1):
+            t1_dst.add(e.dst)
+        t2_src = set()
+        for e in graph.in_edges(t2):
+            t2_src.add(e.src)
+        if len(t1_dst.intersection(t2_src)):
+            return False
+
+        return True
 
     @staticmethod
     def match_to_str(graph: dace.SDFGState, candidate: Dict[pm.PatternNode,
@@ -78,7 +89,6 @@ class SimpleTaskletFusion(pm.Transformation):
         t1 = graph.node(candidate[SimpleTaskletFusion._t1])
         t2 = graph.node(candidate[SimpleTaskletFusion._t2])
         return f'fuse({t1.label}, {t2.label})'
-
 
     def apply(self, sdfg: dace.SDFG):
         graph = sdfg.nodes()[self.state_id]
@@ -92,6 +102,8 @@ class SimpleTaskletFusion(pm.Transformation):
             return conn + '_0'
 
         def replace(tasklet, repl_dict):
+            """ Renames connectors based on the input replacement dictionary.
+            """
             if tasklet.language is dtypes.Language.Python:
                 repl = ConnectorRenamer(repl_dict)
                 for stmt in tasklet.code.code:
@@ -101,26 +113,44 @@ class SimpleTaskletFusion(pm.Transformation):
                     tasklet.code.code = re.sub(r'\b%s\b' % re.escape(old), new,
                                                tasklet.code.as_string)
 
+        def replace_rhs(tasklet, repl_dict):
+            """ Replaces assignments' RHS based on the input replacement
+                dictionary. This is used only on CPP tasklets.
+            """
+            if tasklet.language is dtypes.Language.Python:
+                raise ValueError(
+                    "This method should only be used with CPP Tasklets")
+            elif tasklet.language is dtypes.Language.CPP:
+                for old, new in repl_dict.items():
+                    tasklet.code.code = re.sub(
+                        r'(?<!auto\s)%s[\s\t]*=' % re.escape(old), new,
+                        tasklet.code.as_string)
+
+        def extract_rhs(tasklet) -> Set[str]:
+            """ Returns the RHS of assignments in Tasklet code.
+            """
+            if tasklet.language is dtypes.Language.Python:
+                extr = PythonRHSExtractor()
+                for stmt in tasklet.code.code:
+                    extr.visit(stmt)
+                return extr.assignments
+            elif tasklet.language is dtypes.Language.CPP:
+                rhs = set()
+                for match in re.findall('[\s\t\n\r]*([\w]*)[\s\t]*=',
+                                        tasklet.code.code):
+                    rhs.add(match)
+                return rhs
+
         t1_dict = dict()
         rdict = dict()
         rdict_inout = dict()
 
-        # print("**********")
-        # print(f"t1: {t1.in_connectors} -> {t1.out_connectors}")
-        # print(f"{t1.code.as_string}")
-        # print(f"t2: {t2.in_connectors} -> {t2.out_connectors}")
-        # print(f"{t2.code.as_string}")
-
         cnames = t1.in_connectors.keys() | t1.out_connectors.keys()
+        rhs = extract_rhs(t1)
+        if rhs:
+            cnames |= rhs
 
-        extr = PythonRHSExtractor()
-        for stmt in t1.code.code:
-            extr.visit(stmt)
-        cnames = cnames | extr.assignments
-
-        # print(cnames)
-
-        # Handle input edges. Rename t2 input connectors
+        # Handle input edges.
         inconn = {}
         for e in graph.in_edges(t1):
             inconn[e.dst_conn] = t1.in_connectors[e.dst_conn]
@@ -135,9 +165,9 @@ class SimpleTaskletFusion(pm.Transformation):
                 rdict[e.dst_conn] = nconn
                 cnames.add(nconn)
                 inconn[nconn] = t2.in_connectors[e.dst_conn]
-                graph.add_edge(e.src, e.src_conn, t1, nconn, e.data) 
+                graph.add_edge(e.src, e.src_conn, t1, nconn, e.data)
 
-        # Handle output edges. Rename t2 output connectors      
+        # Handle output edges.
         outconn = {}
         for e in graph.out_edges(t1):
             outconn[e.src_conn] = t1.out_connectors[e.src_conn]
@@ -150,19 +180,15 @@ class SimpleTaskletFusion(pm.Transformation):
             cnames.add(nconn)
             outconn[nconn] = t2.out_connectors[e.src_conn]
             graph.add_edge(t1, nconn, e.dst, e.dst_conn, e.data)
-        
-        extr = PythonRHSExtractor()
-        for stmt in t2.code.code:
-            extr.visit(stmt)
-        for name in extr.assignments:
+
+        # Handle "lost" connectors, i.e. assignment RHS that are not connectors.
+        t2_rhs = extract_rhs(t2)
+        for name in t2_rhs:
             if name in cnames and name not in t2.out_connectors:
                 newname = name
                 while newname in cnames:
                     newname = rename_conn(newname)
                 rdict[name] = newname
-
-        # print(rdict)
-        # print(rdict_inout)
 
         if t1_dict:
             replace(t1, t1_dict)
@@ -170,193 +196,21 @@ class SimpleTaskletFusion(pm.Transformation):
             replace(t2, rdict)
         if rdict_inout:
             replace(t2, rdict_inout)
-        
-        # print(t2.code.as_string)
 
         t1.in_connectors = inconn
         t1.out_connectors = outconn
         if t1.language is dtypes.Language.Python:
             t1.code.code.extend(t2.code.code)
-        elif t1.language is dtypes.Lanaguage.CPP:
+        elif t1.language is dtypes.Language.CPP:
             t1.code.code += f'\n{t2.code.code}'
         graph.remove_node(t2)
 
-        # print(f"t: {t1.in_connectors} -> {t1.out_connectors}")
-        # print(t1.code.as_string)
-        # print("**********")
-
-
-# import ast
-# import re
-# from typing import Dict, Optional, Set
-
-# import astunparse
-# from dace import registry, nodes as nd, SDFGState, SDFG, dtypes
-# from dace.sdfg.utils import node_path_graph
-# from dace.transformation.transformation import Transformation, PatternNode
-
-# # from daceml.util import find_str_not_in_set
-
-# def find_str_not_in_set(existing: Set[str], target_str: Optional[str]) -> str:
-#     """ Try to find a new str that is not in the set.
-#         :param existing: the existing strs.
-#         :param target_str: (optional) a target_str that should be used as a base for the new str.
-#         :return: a new str that is not in `existing`.
-#     """
-#     base_name = target_str or "temp"
-
-#     if base_name not in existing:
-#         return base_name
-
-#     i = 0
-#     while (base_name + "_" + str(i)) in existing:
-#         i += 1
-#     return base_name + "_" + str(i)
-
-
-# class Renamer(ast.NodeTransformer):
-#     def __init__(self, repldict: Dict[str, str]):
-#         self.repldict = repldict
-
-#     def visit_Name(self, node):
-#         if node.id in self.repldict:
-#             node.id = self.repldict[node.id]
-#         return self.generic_visit(node)
-
-
-# class Inliner(ast.NodeTransformer):
-#     def __init__(self, target_id, target_ast):
-#         self.target_id = target_id
-#         self.target_ast = target_ast
-
-#     def visit_Name(self, node):
-#         if node.id == self.target_id:
-#             return ast.copy_location(self.target_ast, node)
-#         else:
-#             return self.generic_visit(node)
-
-
-# @registry.autoregister_params(singlestate=True, strict=True)
-# class TaskletFusion(Transformation):
-#     """ Fuse a constant pad into a convolution.
-#     """
-
-#     tsk1 = PatternNode(nd.Tasklet)
-#     data = PatternNode(nd.AccessNode)
-#     tsk2 = PatternNode(nd.Tasklet)
-
-#     @classmethod
-#     def expressions(cls):
-#         return [
-#             node_path_graph(cls.tsk1, cls.data, cls.tsk2),
-#             node_path_graph(cls.tsk1, cls.tsk2)
-#         ]
-
-#     def can_be_applied(self, graph: SDFGState, candidate: Dict[PatternNode,
-#                                                                int],
-#                        expr_index: int, sdfg: SDFG, strict: bool) -> bool:
-#         tsk1: nd.Tasklet = self.tsk1(sdfg)
-#         data: nd.AccessNode = self.data(sdfg) if self.expr_index == 0 else None
-#         tsk2: nd.Tasklet = self.tsk2(sdfg)
-
-#         if tsk1.language is not dtypes.Language.Python or tsk2.language is not dtypes.Language.Python:
-#             return False
-
-#         if data is not None and data.desc(sdfg).total_size != 1:
-#             return False
-
-#         # tsk1 is not used anywhere else
-#         if graph.out_degree(tsk1) != 1 or (data is not None
-#                                            and graph.out_degree(data) != 1):
-#             return False
-
-#         # tsk2 should have one out connector only
-#         if graph.out_degree(tsk2) != 1:
-#             return False
-
-#         # try to parse the tasklet
-#         try:
-#             if len(tsk1.code.code) != 1 or len(tsk2.code.code) != 1:
-#                 return False
-#             if len(tsk1.code.code[0].targets) != 1:
-#                 return False
-#         except:
-#             return False
-#         return True
-
-#     def apply(self, sdfg: SDFG) -> nd.Tasklet:
-#         state: SDFGState = sdfg.node(self.state_id)
-#         tsk1: nd.Tasklet = self.tsk1(sdfg)
-#         data: nd.AccessNode = self.data(sdfg) if self.expr_index == 0 else None
-#         tsk2: nd.Tasklet = self.tsk2(sdfg)
-
-#         tsk2_in_edge = state.out_edges(data if data is not None else tsk1)[0]
-
-#         # remove the connector from tsk2
-#         inputs = {
-#             k: v
-#             for k, v in tsk2.in_connectors.items()
-#             if k != tsk2_in_edge.dst_conn
-#         }
-
-#         def rename_conn(conn):
-#             match = re.match('(.*?)([0-9]+)$', conn)
-#             if match:
-#                 return match.group(1) + str(int(match.group(2)) + 1)
-#             return conn + '_0'
-
-#         # copy tsk1's in connectors
-#         repldict = {}
-#         for in_edge in state.in_edges(tsk1):
-#             old_value = in_edge.dst_conn
-#             # check if there's a conflict
-#             if in_edge.dst_conn in inputs:
-#                 # conflicts are ok if the memlets are the same
-#                 tsk2edge = list(
-#                     state.in_edges_by_connector(tsk2, in_edge.dst_conn))[0]
-#                 if (in_edge.data != tsk2edge.data
-#                         or in_edge.data.data != tsk2edge.data.data):
-#                     nconn = in_edge.dst_conn
-#                     while nconn in set(inputs):
-#                         nconn = rename_conn(nconn)
-#                     in_edge.dst_conn = nconn
-#                     # in_edge.dst_conn = find_str_not_in_set(
-#                     #     set(inputs), in_edge.dst_conn)
-#                     repldict[old_value] = in_edge.dst_conn
-
-#             inputs[in_edge.dst_conn] = tsk1.in_connectors[old_value]
-
-#         assigned_value = tsk1.code.code[0].value
-#         if repldict:
-#             assigned_value = Renamer(repldict).visit(assigned_value)
-#         new_code = Inliner(tsk2_in_edge.dst_conn,
-#                            assigned_value).visit(tsk2.code.code[0])
-#         new_code_str = astunparse.unparse(new_code)
-
-#         new_tasklet = state.add_tasklet(tsk1.label + "_fused_" + tsk2.label,
-#                                         inputs, tsk2.out_connectors,
-#                                         new_code_str)
-
-#         for in_edge in state.in_edges(tsk1):
-#             state.add_edge(in_edge.src, in_edge.src_conn, new_tasklet,
-#                            in_edge.dst_conn, in_edge.data)
-
-#         for in_edge in state.in_edges(tsk2):
-#             # only connect if there is no edge connected to that connector yet
-#             if len(
-#                     list(
-#                         state.in_edges_by_connector(new_tasklet,
-#                                                     in_edge.dst_conn))) == 0:
-#                 state.add_edge(in_edge.src, in_edge.src_conn, new_tasklet,
-#                                in_edge.dst_conn, in_edge.data)
-#             else:
-#                 state.remove_memlet_path(in_edge)
-
-#         for out_edge in state.out_edges(tsk2):
-#             state.add_edge(new_tasklet, out_edge.src_conn, out_edge.dst,
-#                            out_edge.dst_conn, out_edge.data)
-
-#         state.remove_node(tsk1)
-#         if data is not None:
-#             state.remove_node(data)
-#         state.remove_node(tsk2)
+        # Fix CPP assignemnt RHS that are not connectors.
+        if t1.language is dtypes.Language.CPP:
+            rhs = extract_rhs(t1)
+            repl_dict = dict()
+            for name in rhs:
+                if name not in inconn and name not in outconn:
+                    repl_dict[name] = f'auto {name} ='
+            if repl_dict:
+                replace_rhs(t1, repl_dict)
