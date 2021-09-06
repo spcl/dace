9505bf98254b4f79f7f038c373d16d43ae65d89c
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-09-06 15:31:42 +0200

    Added new tests.

--- tests/transformations/stencil_detection_test.py
+++ tests/transformations/stencil_detection_test.py
@@ -91,7 +91,84 @@ def test_jacobi2d():
     assert (np.allclose(B, refB))
 
 
+@dace.program
+def jacobi1d_with_scalar(TMAX: dace.int32, one_third: dace.float32,
+                         A: dace.float32[12], B: dace.float32[12]):
+    for _ in range(TMAX):
+        B[1:-1] = one_third * (A[:-2] + A[1:-1] + A[2:])
+        A[1:-1] = one_third * (B[:-2] + B[1:-1] + B[2:])
+
+
+def test_jacobi1d_with_scalar():
+    rng = np.random.default_rng(42)
+    A = rng.random(12, dtype=np.float32)
+    B = rng.random(12, dtype=np.float32)
+    one_third = np.float32(1/3)
+    refA = A.copy()
+    refB = B.copy()
+
+    jacobi1d_with_scalar.f(100, one_third, refA, refB)
+
+    sdfg = jacobi1d_with_scalar.to_sdfg(strict=True)
+    sdfg.apply_transformations_repeated(MapFusion)
+    sdfg.apply_transformations_repeated(SimpleTaskletFusion)
+    num = sdfg.apply_transformations_repeated(StencilDetection)
+    assert (num == 2)
+    sdfg(TMAX=100, one_third=one_third, A=A, B=B)
+
+    assert (np.allclose(A, refA))
+    assert (np.allclose(B, refB))
+
+
+
+nx, ny, nit = (dace.symbol(s) for s in ('nx', 'ny', 'nit'))
+
+
+@dace.program
+def build_up_b(b: dace.float64[ny, nx], rho: dace.float64, dt: dace.float64,
+               u: dace.float64[ny, nx], v: dace.float64[ny, nx],
+               dx: dace.float64, dy: dace.float64):
+
+    b[1:-1,
+      1:-1] = (rho * (1 / dt * ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx) +
+                                (v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy)) -
+                      ((u[1:-1, 2:] - u[1:-1, 0:-2]) / (2 * dx))**2 - 2 *
+                      ((u[2:, 1:-1] - u[0:-2, 1:-1]) / (2 * dy) *
+                       (v[1:-1, 2:] - v[1:-1, 0:-2]) / (2 * dx)) -
+                      ((v[2:, 1:-1] - v[0:-2, 1:-1]) / (2 * dy))**2))
+
+
+def test_CFD_build_up_b():
+
+    nx = 41
+    ny = 41
+    dx = 2 / (nx - 1)
+    dy = 2 / (ny - 1)
+    rho = 1
+    dt = .001
+
+    rng = np.random.default_rng(42)
+    u = rng.random((ny, nx))
+    v = rng.random((ny, nx))
+
+    b = np.zeros((ny, nx))
+    ref = np.zeros((ny, nx))
+
+    build_up_b.f(ref, rho, dt, u, v, dx, dy)
+
+    sdfg = build_up_b.to_sdfg(strict=True)
+    sdfg.apply_transformations_repeated(MapFusion)
+    sdfg.apply_transformations_repeated(SimpleTaskletFusion)
+    num = sdfg.apply_transformations_repeated(StencilDetection)
+    assert (num == 1)
+    sdfg(b=b, rho=rho, dt=dt, u=u, v=v, dx=dx, dy=dy, nx=nx, ny=ny)
+
+    assert (np.allclose(b, ref))
+
+
 if __name__ == '__main__':
     test_stencil1d()
     test_jacobi1d()
     test_jacobi2d()
+    test_jacobi1d_with_scalar()
+    test_CFD_build_up_b()
