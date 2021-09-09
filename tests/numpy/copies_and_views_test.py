4927859f9fd0c161b6f3dad534ce7daf73a4959c
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-09-09 17:52:12 +0200

    Added tests for views and copies.

---
+++ tests/numpy/copies_and_views_test.py
@@ -0,0 +1,82 @@
+import dace
+import numpy as np
+
+from dace.frontend.python import parser
+
+
+@dace.program
+def set_by_view(A: dace.int64[10]):
+    v = A
+    v += 1
+
+
+def test_set_by_view():
+    val = np.arange(10)
+    set_by_view(val)
+    ref = np.arange(10)
+    set_by_view.f(ref)
+    assert (np.allclose(val, ref))
+
+
+@dace.program
+def set_by_view_1(A: dace.int64[10]):
+    v = A[:]
+    v += 1
+
+
+def test_set_by_view_1():
+    val = np.arange(10)
+    set_by_view_1(val)
+    ref = np.arange(10)
+    set_by_view_1.f(ref)
+    assert (np.allclose(val, ref))
+
+
+@dace.program
+def set_by_view_2(A: dace.int64[10]):
+    v = A[1:-1]
+    v += 1
+
+
+def test_set_by_view_2():
+    val = np.arange(10)
+    set_by_view_2(val)
+    ref = np.arange(10)
+    set_by_view_2.f(ref)
+    assert (np.allclose(val, ref))
+
+
+@dace.program
+def set_by_view_3(A: dace.int64[10]):
+    v = A[4:5]
+    v += 1
+
+
+def test_set_by_view_3():
+    val = np.arange(10)
+    set_by_view_3(val)
+    ref = np.arange(10)
+    set_by_view_3.f(ref)
+    assert (np.allclose(val, ref))
+
+
+@dace.program
+def is_a_copy(A: dace.int64[10]):
+    v = A[4]
+    v += 1
+
+
+def test_is_a_copy():
+    val = np.arange(10)
+    is_a_copy(val)
+    ref = np.arange(10)
+    is_a_copy.f(ref)
+    assert (np.allclose(val, ref))
+
+
+if __name__ == '__main__':
+    test_set_by_view()
+    test_set_by_view_1()
+    test_set_by_view_2()
+    test_set_by_view_3()
+    test_is_a_copy()
