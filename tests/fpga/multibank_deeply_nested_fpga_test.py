4c6695daaa43df22548b987024d1b681e92c7983
Neville Walo <43504521+Walon1998@users.noreply.github.com>
2021-10-16 09:04:12 +0200

    [FPGA] Extended codegen and added tests to support multibank DDR memory (#863)
    * extended multiple interfaces test to ddr
    
    * changed sdfg name
    
    * Cleaner names
    
    * Added ddr copy tests
    
    * Added correct naming
    
    * changed DDR and HBM to lowercase
    
    * added ddr tests to deepy nested
    
    * fixed
    
    * added ddr to dynamic memlet test
    
    * changed sdfg name
    
    * added default arg
    
    * add ddr to reduce test
    
    * renamed file
    
    * added ddr test to vadd
    
    * added ddr tests to validation
    
    * fixed import
    
    * removed comment
    
    * added Neville Walo to contributers
    
    * Update dace/sdfg/validation.py
    
    Co-authored-by: definelicht <johannes@musicmedia.dk>
    
    * Update tests/fpga/hbm_ddr_deeply_nested_fpga_test.py
    
    Co-authored-by: definelicht <johannes@musicmedia.dk>
    
    * removed 'test' from methods such that pytest works correctly
    
    * changed name to multibank in tests
    
    * cleanup validation
    
    * cleanup targets/xilinx
    
    * cleanup and renamed hbm -> multibank
    
    * removed print

--- tests/fpga/multibank_deeply_nested_fpga_test.py
+++ tests/fpga/multibank_deeply_nested_fpga_test.py
@@ -5,17 +5,19 @@ from dace import subsets as sbs, dtypes, memlet as mem
 from dace import subsets
 import numpy as np
 
-# A test checking HBM in the context of nested maps and nested sdfgs
+# A test checking Multibank HBM/DDR in the context of nested maps and nested sdfgs
+# Note, usually there are only 4 ddr banks but much more hmb banks.
+# Since the tests run in simulation mode, this should not be an issue.
 
 
-def create_deeply_nested_sdfg():
-    sdfg = dace.SDFG("deepnest_test")
+def create_deeply_nested_sdfg(mem_type):
+    sdfg = dace.SDFG("deepnest_test_" + mem_type)
     state: dace.SDFGState = sdfg.add_state("init")
     xarr = state.add_array("x", [4, 10], dace.float32)
-    sdfg.arrays["x"].location["memorytype"] = "hbm"
+    sdfg.arrays["x"].location["memorytype"] = mem_type
     sdfg.arrays["x"].location["bank"] = "0:4"
     yarr = state.add_array("y", [4, 10], dace.float32)
-    sdfg.arrays["y"].location["memorytype"] = "hbm"
+    sdfg.arrays["y"].location["memorytype"] = mem_type
     sdfg.arrays["y"].location["bank"] = "4:8"
 
     top_map_entry, top_map_exit = state.add_map("topmap", dict(k="0:2"))
@@ -27,9 +29,9 @@ def create_deeply_nested_sdfg():
                               dtypes.StorageType.FPGA_Global)
     x_write = nstate.add_array("xout", [4, 10], dace.float32,
                                dtypes.StorageType.FPGA_Global)
-    nsdfg.arrays["xin"].location["memorytype"] = "hbm"
+    nsdfg.arrays["xin"].location["memorytype"] = mem_type
     nsdfg.arrays["xin"].location["bank"] = "0:4"
-    nsdfg.arrays["xout"].location["memorytype"] = "hbm"
+    nsdfg.arrays["xout"].location["memorytype"] = mem_type
     nsdfg.arrays["xout"].location["bank"] = "4:8"
     map_entry, map_exit = nstate.add_map("map1", dict(w="0:2"))
     map_entry.schedule = dtypes.ScheduleType.Unrolled
@@ -68,9 +70,8 @@ def create_deeply_nested_sdfg():
     return sdfg
 
 
-@xilinx_test()
-def test_hbm_deeply_nested_sdfg():
-    sdfg = create_deeply_nested_sdfg()
+def deeply_nested_sdfg(mem_type):
+    sdfg = create_deeply_nested_sdfg(mem_type)
     a = np.zeros((4, 10), np.float32)
     a[2, 4:9] += 1
     a[3, 3:8] += 2
@@ -81,5 +82,16 @@ def test_hbm_deeply_nested_sdfg():
     return sdfg
 
 
+@xilinx_test()
+def test_hbm_deeply_nested_sdfg():
+    return deeply_nested_sdfg(mem_type="hbm")
+
+
+@xilinx_test()
+def test_ddr_deeply_nested_sdfg():
+    return deeply_nested_sdfg(mem_type="ddr")
+
+
 if __name__ == "__main__":
     test_hbm_deeply_nested_sdfg(None)
+    test_ddr_deeply_nested_sdfg(None)
