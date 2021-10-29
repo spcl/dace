5fbcaa9e489de469ea1eb638f4d2137801178c42
Neville Walo <neville.walo@gmail.com>
2021-10-29 21:09:26 +0200

    added test cases

--- tests/fpga/streaming_memory_test.py
+++ tests/fpga/streaming_memory_test.py
@@ -31,10 +31,17 @@ def matadd_streaming(A: dace.float32[M, N], B: dace.float32[M, N],
 
 
 @dace.program
+def tensoradd_streaming(A: dace.float32[M, N, K], B: dace.float32[M, N, K],
+                        C: dace.float32[M, N, K]):
+    C[:] = A + B
+
+
+@dace.program
 def matadd_multistream(A: dace.float32[M, N], B: dace.float32[M, N],
                        C: dace.float32[M, N], D: dace.float32[M, N]):
     C[:] = A + B
     D[:] = A - B
+
 
 @dace.program
 def matmul_streaming(A: dace.float32[M, K], B: dace.float32[K, N],
@@ -52,7 +59,6 @@ def matmul_streaming(A: dace.float32[M, K], B: dace.float32[K, N],
 
     # Sum last dimension of temporary array to obtain resulting matrix
     dace.reduce(lambda a, b: a + b, tmp, C, axis=2, identity=0)
-
 
 
 @dace.program
@@ -278,12 +284,12 @@ def test_mem_buffer_vec_add_1():
     ])
 
     assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
-                                        options=[{
-                                            'use_memory_buffering':
-                                            True,
-                                            "storage":
-                                            dace.StorageType.FPGA_Local
-                                        }]) == 2
+                                               options=[{
+                                                   'use_memory_buffering':
+                                                   True,
+                                                   "storage":
+                                                   dace.StorageType.FPGA_Local
+                                               }]) == 2
 
     # Run verification
     A = np.random.rand(N).astype(np.float32)
@@ -308,12 +314,12 @@ def test_mem_buffer_vec_add():
     ])
 
     assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
-                                        options=[{
-                                            'use_memory_buffering':
-                                            True,
-                                            "storage":
-                                            dace.StorageType.FPGA_Local
-                                        }]) == 3
+                                               options=[{
+                                                   'use_memory_buffering':
+                                                   True,
+                                                   "storage":
+                                                   dace.StorageType.FPGA_Local
+                                               }]) == 3
 
     # Run verification
     A = np.random.rand(N).astype(np.float32)
@@ -336,12 +342,12 @@ def test_mem_buffer_mat_add():
     sdfg.apply_transformations([FPGATransformSDFG, InlineSDFG])
 
     assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
-                                        options=[{
-                                            'use_memory_buffering':
-                                            True,
-                                            "storage":
-                                            dace.StorageType.FPGA_Local
-                                        }]) == 3
+                                               options=[{
+                                                   'use_memory_buffering':
+                                                   True,
+                                                   "storage":
+                                                   dace.StorageType.FPGA_Local
+                                               }]) == 3
 
     # Run verification
     A = np.random.rand(M, N).astype(np.float32)
@@ -355,6 +361,35 @@ def test_mem_buffer_mat_add():
     assert diff <= 1e-5
 
     return sdfg
+
+@xilinx_test()
+def test_mem_buffer_tensor_add():
+    # Make SDFG
+    sdfg: dace.SDFG = tensoradd_streaming.to_sdfg()
+    # Transform
+    sdfg.apply_transformations([FPGATransformSDFG, InlineSDFG])
+
+    assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
+                                               options=[{
+                                                   'use_memory_buffering':
+                                                   True,
+                                                   "storage":
+                                                   dace.StorageType.FPGA_Local
+                                               }]) == 3
+
+    # Run verification
+    A = np.random.rand(M, N, K).astype(np.float32)
+    B = np.random.rand(M, N, K).astype(np.float32)
+    C = np.random.rand(M, N, K).astype(np.float32)
+
+    sdfg(A=A, B=B, C=C)
+
+    diff = np.linalg.norm(C - (A + B))
+
+    assert diff <= 1e-5
+
+    return sdfg
+
 
 
 @xilinx_test()
@@ -365,12 +400,12 @@ def test_mem_buffer_mapnests():
     sdfg.apply_transformations([FPGATransformSDFG, InlineSDFG, MapExpansion])
 
     assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
-                                        options=[{
-                                            'use_memory_buffering':
-                                            True,
-                                            "storage":
-                                            dace.StorageType.FPGA_Local
-                                        }]) == 3
+                                               options=[{
+                                                   'use_memory_buffering':
+                                                   True,
+                                                   "storage":
+                                                   dace.StorageType.FPGA_Local
+                                               }]) == 3
 
     # Run verification
     A = np.random.rand(M, N).astype(np.float32)
@@ -385,52 +420,91 @@ def test_mem_buffer_mapnests():
     return sdfg
 
 @xilinx_test()
-def test_mem_buffer_mat_mul():
+def test_mem_buffer_multistream():
     # Make SDFG
-    sdfg: dace.SDFG = matmul_streaming.to_sdfg()
+    sdfg: dace.SDFG = matadd_multistream.to_sdfg()
     # Transform
     sdfg.apply_transformations([FPGATransformSDFG, InlineSDFG])
 
-    # sdfg.apply_transformations(sm.StreamingMemory,
-    #                                     options=[{
-    #                                         'use_memory_buffering':
-    #                                         True,
-    #                                         "storage":
-    #                                         dace.StorageType.FPGA_Local
-    #                                     }])
+    assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
+                                               options=[{
+                                                   'use_memory_buffering':
+                                                   True,
+                                                   "storage":
+                                                   dace.StorageType.FPGA_Local
+                                               }]) == 4
 
-    # assert sdfg.apply_transformations_repeated(
-    #     mb.MemoryBuffering, dict(storage=dace.StorageType.FPGA_Local)) == 3
+    # TODO
+    # Ensure only 4 connected components exist
+    # mainstate = next(s for s in sdfg.nodes() if 'copy' not in s.label)
+    # assert len(list(nx.weakly_connected_components(mainstate.nx))) == 6
 
     # Run verification
-    A = np.random.rand(M, K).astype(np.float32)
-    B = np.random.rand(K, N).astype(np.float32)
+    A = np.random.rand(M, N).astype(np.float32)
+    B = np.random.rand(M, N).astype(np.float32)
     C = np.random.rand(M, N).astype(np.float32)
+    D = np.random.rand(M, N).astype(np.float32)
 
-    sdfg(A=A, B=B, C=C)
+    sdfg(A=A, B=B, C=C, D=D)
 
-    diff = np.linalg.norm(C - (A @ B))
+    diff1 = np.linalg.norm(C - (A + B))
+    diff2 = np.linalg.norm(D - (A - B))
+    assert diff1 <= 1e-5 and diff2 <= 1e-5
+
+    return sdfg
+
+@xilinx_test()
+def test_mem_buffer_multistream_with_deps():
+    # Make SDFG
+    sdfg: dace.SDFG = streamingcomp.to_sdfg()
+    # Transform
+    sdfg.apply_transformations([FPGATransformSDFG, InlineSDFG])
+
+    assert sdfg.apply_transformations_repeated(sm.StreamingMemory,
+                                        options=[{
+                                            'use_memory_buffering':
+                                            True,
+                                            "storage":
+                                            dace.StorageType.FPGA_Local
+                                        }]) == 3
+
+    # TODO:
+    # Ensure only 4 connected components exist
+    # mainstate = next(s for s in sdfg.nodes() if 'copy' not in s.label)
+    # assert len(list(nx.weakly_connected_components(mainstate.nx))) == 4
+
+    # Run verification
+    A = np.random.rand(M, N).astype(np.float32)
+    B = np.random.rand(M, N).astype(np.float32)
+
+    C = sdfg(A=A, B=B)
+
+    diff = np.linalg.norm(C - ((A + B) * B)) / (M * N)
     assert diff <= 1e-5
 
     return sdfg
 
 
 
+
 if __name__ == "__main__":
-    # test_streaming_mem(None)
-    # test_streaming_mem_mapnests(None)
-    # test_multistream(None)
-    # test_multistream_with_deps(None)
-    # test_streaming_composition_matching(None)
-    # test_streaming_composition(None)
-    # test_streaming_composition_mapnests(None)
-    # test_streaming_and_composition(None)
+    test_streaming_mem(None)
+    test_streaming_mem_mapnests(None)
+    test_multistream(None)
+    test_multistream_with_deps(None)
+    test_streaming_composition_matching(None)
+    test_streaming_composition(None)
+    test_streaming_composition_mapnests(None)
+    test_streaming_and_composition(None)
 
     test_mem_buffer_vec_add_1(None)
     test_mem_buffer_vec_add(None)
     test_mem_buffer_mat_add(None)
+    test_mem_buffer_tensor_add(None)
     test_mem_buffer_mapnests(None)
-    # test_mem_buffer_mat_mul(None)
+    test_mem_buffer_multistream(None) 
+    test_mem_buffer_multistream_with_deps(None)
+
 
     # TODO: Write more test cases
     # Symbol
