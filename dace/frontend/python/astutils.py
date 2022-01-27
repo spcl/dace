40a50d0fb5569e735fa03c87d3637b4e7d05a880
Tal Ben-Nun <tbennun@gmail.com>
2022-01-27 20:10:33 +0100

    Minor fix for copy_tree

--- dace/frontend/python/astutils.py
+++ dace/frontend/python/astutils.py
@@ -379,6 +379,29 @@ def copy_tree(node: ast.AST) -> ast.AST:
                 return visitor(node)
             newnode = copy.copy(node)
             return self.generic_visit(newnode)
+        
+        def generic_visit(self, node):
+            for field, old_value in ast.iter_fields(node):
+                if isinstance(old_value, list):
+                    new_values = []
+                    for value in old_value:
+                        if isinstance(value, ast.AST):
+                            value = self.visit(value)
+                            if value is None:
+                                continue
+                            elif not isinstance(value, ast.AST):
+                                new_values.extend(value)
+                                continue
+                        new_values.append(value)
+                    # Copy list instead of modifying it in-place
+                    setattr(node, field, new_values)
+                elif isinstance(old_value, ast.AST):
+                    new_node = self.visit(old_value)
+                    if new_node is None:
+                        delattr(node, field)
+                    else:
+                        setattr(node, field, new_node)
+            return node
 
     return Copier().visit(node)
 
