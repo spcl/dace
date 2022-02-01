d670ec9ce9de439680cfff3212c1a32aa8900cbb
Grzegorz Kwasniewski <grzegkwas@gmail.com>
2022-02-01 23:50:44 +0100

    working for most of npbench, einsums are correct

--- tests/transformations/soap/einsum_io_test.py
+++ tests/transformations/soap/einsum_io_test.py
@@ -11,9 +11,9 @@ def test_mttkrp_io():
     Test MTTKRP I/O lower bound Q >= 3*N^4/S**(2/3)
     """
     # get MTTKRP sdfg with auto-generated default tensor sizes  
-    einsum_str = 'ijk,jl,kl->il' 
-    #einsum_str = 'ik,kj->ij'
-    decomp_params=[("p", 17), ("Ss", 1024), ("S0", 256), ("S1", 256), ("S2", 256), ("S3", 256)]
+    einsum_str = 'ijk,jl,kl->il'
+    # einsum_str = 'ik,kj,jl->il'  # ik,kj -> ij.....    ij, jl -> il
+    decomp_params=[("p", 17), ("Ss", 1024), ("S0", 256), ("S1", 256), ("S2", 256), ("S3", 25600)]
     soap_result = perform_soap_analysis_einsum(einsum_str, decomp_params, generate_schedule=True)
     # test MTTKRP I/O bound
     assert d2sp(soap_result.Q) == sp.sympify('3*S0*S1*S2*S3/Ss**(2/3)')
@@ -32,3 +32,27 @@ def test_opt_einsum_example_io():
 if __name__ == "__main__":
     test_mttkrp_io()
     test_opt_einsum_example_io()
+
+
+
+# for i in range:
+#     for j in range:
+#         for k in range:
+#             C[i,j] += A[i,k] * B[k,j]
+
+# # Intermediate memory size: N^2
+# # FLOP count: 4N^3
+
+# for i in range:
+#     for j in range:
+#         for k in range:
+#             D[i,j] += C[i,k] * E[k,j]
+
+
+# # Intermediate memory size: 0
+# # FLOP count: 3N^4
+# for i in range:
+#     for j in range:
+#         for k in range:
+#             for l in range:
+#                 D[i,l] += A[i,k] * B[k,j] * E[j, l]
