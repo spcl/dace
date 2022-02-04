915a316fc5ab914d573b28fede1212c0890d25ce
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-02-04 15:16:35 +0100

    Added test for CPU codegen.

--- tests/codegen/constant_arrays_test.py
+++ tests/codegen/constant_arrays_test.py
@@ -7,13 +7,26 @@ def test_nsdfg_input():
     """ Tests constexpr array passed as input argument to a NestedSDFG. """
 
     @dace.program
-    def foo():
+    def constexpr_nsdfg():
         a = np.array([1.,2.,3.])
         b = np.max(a)
 
     with dace.config.set_temporary('compiler', 'inline_sdfgs', value=False):
-        foo()
+        constexpr_nsdfg()
+
+
+def test_tasklet_input_cpu():
+    """ Tests constexpr array passed as input argument to a Tasklet (CPU)."""
+
+    @dace.program
+    def constexpr_tasklet_cpu():
+        a = np.array([1.,2.,3.])
+        b = np.max(a)
+
+    with dace.config.set_temporary('optimizer', 'autooptimize', value=True):
+        constexpr_tasklet_cpu()
 
 
 if __name__ == "__main__":
     test_nsdfg_input()
+    test_tasklet_input_cpu()
