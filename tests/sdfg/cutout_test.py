e1fd1f5806501792076a94faebb1eb262f723198
Tal Ben-Nun <tbennun@users.noreply.github.com>
2022-02-23 13:42:35 +0100

    Remove unnecessary import

--- tests/sdfg/cutout_test.py
+++ tests/sdfg/cutout_test.py
@@ -1,6 +1,5 @@
 # Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
 import numpy as np
-from pandas import cut
 import dace
 from dace.sdfg.analysis import cutout
 import pytest
