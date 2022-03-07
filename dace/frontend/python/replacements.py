cd3dc01b80ddac33da2f5d09ea8a0504e50aff04
Tal Ben-Nun <tbennun@gmail.com>
2022-03-07 02:28:24 +0100

    CuPy replacements

--- dace/frontend/python/replacements.py
+++ dace/frontend/python/replacements.py
@@ -417,7 +417,7 @@ def _numpy_flip(pv: 'ProgramVisitor', sdfg: SDFG, state: SDFGState, arr: str, ax
     # anode = state.add_read(acpy)
     # state.add_edge(vnode, None, anode, None, Memlet(f'{view}[{sset}] -> {dset}'))
 
-    arr_copy, _ = sdfg.add_temp_transient(desc.shape, desc.dtype, desc.storage)
+    arr_copy, _ = sdfg.add_temp_transient_like(desc)
     inpidx = ','.join([f'__i{i}' for i in range(ndim)])
     outidx = ','.join([f'{s} - __i{i} - 1' if a else f'__i{i}' for i, (a, s) in enumerate(zip(axis, desc.shape))])
     state.add_mapped_tasklet(name="_numpy_flip_",
@@ -4359,3 +4359,83 @@ def _inv(pv: 'ProgramVisitor', sdfg: SDFG, state: SDFGState, inp_op: str):
     state.add_memlet_path(chlsky_node, out, src_conn="_b", memlet=Memlet.from_array(*out_arr))
 
     return out_arr[0]
+
+
+# CuPy replacements
+
+
+@oprepo.replaces("cupy._core.core.ndarray")
+@oprepo.replaces("cupy.ndarray")
+def _define_cupy_local(
+    pv: "ProgramVisitor",
+    sdfg: SDFG,
+    state: SDFGState,
+    shape: Shape,
+    dtype: typeclass,
+):
+    """Defines a local array in a DaCe program."""
+    if not isinstance(shape, (list, tuple)):
+        shape = [shape]
+    name, _ = sdfg.add_temp_transient(shape, dtype, storage=dtypes.StorageType.GPU_Global)
+    return name
+
+
+@oprepo.replaces('cupy.full')
+def _cupy_full(pv: 'ProgramVisitor',
+               sdfg: SDFG,
+               state: SDFGState,
+               shape: Shape,
+               fill_value: Union[sp.Expr, Number],
+               dtype: dace.typeclass = None):
+    """ Creates and array of the specified shape and initializes it with
+        the fill value.
+    """
+    if isinstance(fill_value, (Number, np.bool_)):
+        vtype = dtypes.DTYPE_TO_TYPECLASS[type(fill_value)]
+    elif isinstance(fill_value, sp.Expr):
+        vtype = _sym_type(fill_value)
+    else:
+        raise mem_parser.DaceSyntaxError(pv, None, "Fill value {f} must be a number!".format(f=fill_value))
+    dtype = dtype or vtype
+    name, _ = sdfg.add_temp_transient(shape, dtype, storage=dtypes.StorageType.GPU_Global)
+
+    state.add_mapped_tasklet(
+        '_cupy_full_', {"__i{}".format(i): "0: {}".format(s)
+                        for i, s in enumerate(shape)}, {},
+        "__out = {}".format(fill_value),
+        dict(__out=dace.Memlet.simple(name, ",".join(["__i{}".format(i) for i in range(len(shape))]))),
+        external_edges=True)
+
+    return name
+
+
+@oprepo.replaces('cupy.zeros')
+def _cupy_zeros(pv: 'ProgramVisitor', sdfg: SDFG, state: SDFGState, shape: Shape, dtype: dace.typeclass = dace.float64):
+    """ Creates and array of the specified shape and initializes it with zeros.
+    """
+    return _cupy_full(pv, sdfg, state, shape, 0.0, dtype)
+
+
+@oprepo.replaces('cupy.empty_like')
+def _cupy_empty_like(pv: 'ProgramVisitor',
+                     sdfg: SDFG,
+                     state: SDFGState,
+                     prototype: str,
+                     dtype: dace.typeclass = None,
+                     shape: Shape = None):
+    if prototype not in sdfg.arrays.keys():
+        raise mem_parser.DaceSyntaxError(pv, None, "Prototype argument {a} is not SDFG data!".format(a=prototype))
+    desc = sdfg.arrays[prototype]
+    name, newdesc = sdfg.add_temp_transient_like(desc)
+    if dtype is not None:
+        newdesc.dtype = dtype
+    if shape is not None:
+        newdesc.shape = shape
+    return name
+
+
+@oprepo.replaces('cupy.empty')
+@oprepo.replaces('cupy_empty')
+def _cupy_empty(pv: 'ProgramVisitor', sdfg: SDFG, state: SDFGState, shape: Shape, dtype: dace.typeclass):
+    """ Creates an unitialized array of the specificied shape and dtype. """
+    return _define_cupy_local(pv, sdfg, state, shape, dtype)
