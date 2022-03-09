08bfc14f35621199d66df0591a32ce08f1234720
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-03-09 22:52:34 +0100

    Added HPTT expansion.

--- dace/libraries/ttranspose/environments/hptt.py
+++ dace/libraries/ttranspose/environments/hptt.py
@@ -1,5 +1,7 @@
 # Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
-from dace import library
+import ctypes
+import os
+from dace import config, library
 
 
 @library.environment
@@ -8,14 +10,31 @@ class HPTT:
     cmake_minimum_version = None
     cmake_packages = []
     cmake_variables = {}
-    cmake_includes = []
     cmake_libraries = []
-    cmake_compile_flags = ["-I${HPTT_ROOT}/include"]
-    cmake_link_flags = ["-L${HPTT_ROOT}/lib -lhptt"]
+    cmake_compile_flags = []
+    cmake_link_flags = []
     cmake_files = []
 
-    headers = ["http.h"]
+    headers = ["hptt.h"]
     state_fields = []
     init_code = ""
     finalize_code = ""
     dependencies = []
+
+    @staticmethod
+    def cmake_includes():
+        if 'HPTT_ROOT' in os.environ:
+            return [os.path.join(os.environ['HPTT_ROOT'], 'include')]
+        else:
+            return []
+    
+    @staticmethod
+    def cmake_libraries():
+        if 'HPTT_ROOT' in os.environ:
+            prefix = config.Config.get('compiler', 'library_prefix')
+            suffix = config.Config.get('compiler', 'library_extension')
+            libfile = os.path.join(os.environ['HPTT_ROOT'], 'lib', prefix + 'hptt.' + suffix)
+            if os.path.isfile(libfile):
+                return [libfile]
+        
+        return ['hptt']
