1ca95d3c271c98e98f78faa45a1a37b923fb27c7
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-03-10 13:36:39 +0100

    Added second tensordot test.

--- tests/numpy/linalg_test.py
+++ tests/numpy/linalg_test.py
@@ -78,8 +78,21 @@ def test_tensordot_0():
         assert(np.allclose(tensordot_0(A.copy(), B.copy()), tensordot_0.f(A, B)))
 
 
+def test_tensordot_1():
+
+    @dace.program
+    def tensordot_1(A: dace.float32[3, 3, 3, 3, 3, 3], B: dace.float32[3, 3, 3, 3, 3, 3]):
+        return np.tensordot(A, B, axes=([0, 3], [4, 2]))
+    
+    A = np.arange(3**6, dtype=np.float32).reshape(3, 3, 3, 3, 3, 3)
+    B = np.arange(3**6, dtype=np.float32).reshape(3, 3, 3, 3, 3, 3)
+    with dace.config.set_temporary('library', 'linalg', 'default_implementation', value='pure'):
+        assert(np.allclose(tensordot_1(A.copy(), B.copy()), tensordot_1.f(A, B)))
+
+
 if __name__ == "__main__":
     test_linalg_inv()
     test_linalg_solve()
     test_linalg_cholesky()
     test_tensordot_0()
+    test_tensordot_1()
