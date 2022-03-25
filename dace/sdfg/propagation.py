5b65ebb39b0be4b4a4cc27591eb03da27054be77
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-03-25 11:01:31 +0100

    Fixed propagation with new desc input in unsqueeze memlet.

--- dace/sdfg/propagation.py
+++ dace/sdfg/propagation.py
@@ -1079,7 +1079,8 @@ def propagate_memlets_nested_sdfg(parent_sdfg, parent_state, nsdfg_node):
             if internal_memlet is None:
                 continue
             try:
-                iedge.data = unsqueeze_memlet(internal_memlet, iedge.data, True, desc=sdfg.arrays[iedge.data.data])
+                # iedge.data = unsqueeze_memlet(internal_memlet, iedge.data, True, desc=sdfg.arrays[iedge.data.data])
+                iedge.data = unsqueeze_memlet(internal_memlet, iedge.data, True, desc=parent_state.parent.arrays[iedge.data.data])
                 # If no appropriate memlet found, use array dimension
                 for i, (rng, s) in enumerate(zip(internal_memlet.subset, parent_sdfg.arrays[iedge.data.data].shape)):
                     if rng[1] + 1 == s:
@@ -1099,7 +1100,8 @@ def propagate_memlets_nested_sdfg(parent_sdfg, parent_state, nsdfg_node):
             if internal_memlet is None:
                 continue
             try:
-                oedge.data = unsqueeze_memlet(internal_memlet, oedge.data, True, desc=sdfg.arrays[oedge.data.data])
+                # oedge.data = unsqueeze_memlet(internal_memlet, oedge.data, True, desc=sdfg.arrays[oedge.data.data])
+                oedge.data = unsqueeze_memlet(internal_memlet, oedge.data, True, desc=parent_state.parent.arrays[oedge.data.data])
                 # If no appropriate memlet found, use array dimension
                 for i, (rng, s) in enumerate(zip(internal_memlet.subset, parent_sdfg.arrays[oedge.data.data].shape)):
                     if rng[1] + 1 == s:
