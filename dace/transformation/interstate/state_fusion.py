d3926ff89f6f75a3e2993ff6a0a8c0bfe1757910
Tiziano De Matteis <tdematt@inf.ethz.ch>
2022-04-21 11:23:25 +0200

    State fusion: move state type check at the end of can_be_applied

--- dace/transformation/interstate/state_fusion.py
+++ dace/transformation/interstate/state_fusion.py
@@ -169,11 +169,6 @@ class StateFusion(transformation.MultiStateTransformation, transformation.Simpli
         first_state: SDFGState = self.first_state
         second_state: SDFGState = self.second_state
 
-        from dace.codegen.targets.fpga import is_fpga_kernel # avoid circular import
-        # Do not fuse FPGA and NON-FPGA states
-        if is_fpga_kernel(sdfg, first_state) != is_fpga_kernel(sdfg, second_state):
-            return False
-
         out_edges = graph.out_edges(first_state)
         in_edges = graph.in_edges(first_state)
 
@@ -448,6 +443,11 @@ class StateFusion(transformation.MultiStateTransformation, transformation.Simpli
                                         return False
                                 found = outnode
 
+        from dace.codegen.targets.fpga import is_fpga_kernel  # avoid circular import
+        # Do not fuse FPGA and NON-FPGA states
+        if is_fpga_kernel(sdfg, first_state) != is_fpga_kernel(sdfg, second_state):
+            return False
+        
         return True
 
     def apply(self, _, sdfg):
