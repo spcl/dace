e017df0c3a05f8fe5b8f6eb67a04d0f8a4e17616
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-05-11 12:09:18 +0200

    Improvements for programs that use both CUDA and MPI.

---
+++ dace/libraries/mpi/include/cuda_mpi_interop.h
@@ -0,0 +1,8 @@
+// Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+#pragma once
+
+typedef int MPI_Comm;
+typedef int MPI_Group;
+typedef int MPI_Datatype;
+typedef int MPI_Request;
+typedef int MPI_Status;
