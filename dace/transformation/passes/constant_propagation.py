009e13510d9a4e5526ee997017c0ce336351fc22
Tal Ben-Nun <talbn@inf.ethz.ch>
2022-05-28 03:32:44 +0200

    First version of constant propagation for symbol assignments

---
+++ dace/transformation/passes/constant_propagation.py
@@ -0,0 +1,166 @@
+# Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
+
+from dace.sdfg.sdfg import InterstateEdge
+from dace.sdfg import utils as sdutil
+from dace.transformation import pass_pipeline as ppl
+from dace import SDFG, SDFGState, symbolic
+from typing import Any, Dict, Set, Optional
+
+from dace.transformation.passes.scalar_to_symbol import ScalarToSymbolPromotion
+from dace.transformation.passes.analysis import StateReachability
+
+
+class _UnknownValue:
+    """ A helper class that indicates a symbol value is ambiguous. """
+    pass
+
+
+class ConstantPropagation(ppl.Pass):
+    """
+    Propagates constants and symbols that were assigned to one value forward through the SDFG, reducing
+    the number of overall symbols.
+    """
+    def modifies(self) -> ppl.Modifies:
+        return ppl.Modifies.Symbols | ppl.Modifies.Edges | ppl.Modifies.Nodes
+
+    def should_reapply(self, modified: ppl.Modifies) -> bool:
+        # If anything was modified, reapply
+        return modified != ppl.Modifies.Nothing
+
+    def apply_pass(self, sdfg: SDFG, _) -> Optional[Set[str]]:
+        """
+        Propagates constants throughout the SDFG.
+        :param sdfg: The SDFG to modify.
+        :param pipeline_results: If in the context of a ``Pipeline``, a dictionary that is populated with prior Pass
+                                 results as ``{Pass subclass name: returned object from pass}``. If not run in a
+                                 pipeline, an empty dictionary is expected.
+        :return: A set of propagated constants, or None if nothing was changed.
+        """
+        # TODO(later): Early exit if no constants can be propagated?
+
+        # Trace all constants and symbols through states
+        per_state_constants: Dict[SDFGState, Dict[str, Any]] = self.collect_constants(sdfg)
+
+        # Keep track of replaced and ambiguous symbols
+        symbols_replaced: Set[str] = set()
+        remaining_unknowns: Set[str] = set()
+
+        # Replace constants per state
+        for state, mapping in per_state_constants.items():
+            remaining_unknowns.update({k for k, v in mapping.items() if v is _UnknownValue})
+            mapping = {k: v for k, v in mapping.items() if v is not _UnknownValue}
+            symbols_replaced.update(mapping.keys())
+
+            # TODO: Remove symbol assignments in interstate edges
+
+            # Replace in state contents
+            state.replace_dict(mapping)
+            # Replace in outgoing edges as well
+            for e in sdfg.out_edges(state):
+                e.data.replace_dict(mapping)
+
+        # If symbols are never unknown any longer, remove from SDFG
+        result = (symbols_replaced - remaining_unknowns)
+        for sym in result:
+            sdfg.remove_symbol(sym)
+
+        # Return result
+        if not result:
+            return None
+        return result
+
+    def collect_constants(self, sdfg: SDFG) -> Dict[SDFGState, Dict[str, Any]]:
+        """
+        Finds all constants and constant-assigned symbols in the SDFG for each state.
+        :param sdfg: The SDFG to traverse.
+        :return: A dictionary mapping an SDFG state to a mapping of constants and their corresponding values.
+        """
+        arrays: Set[str] = set(sdfg.arrays.keys() | sdfg.constants_prop.keys())
+        result: Dict[SDFGState, Dict[str, Any]] = {}
+
+        # Process:
+        # * Collect constants in topologically ordered states
+        # * If unvisited state has one incoming edge - propagate symbols forward and edge assignments
+        # * If unvisited state has more than one incoming edge, consider all paths (use reverse DFS on unvisited paths)
+        #   * If value is ambiguous (not the same), set value to UNKNOWN
+
+        # Traverse SDFG topologically
+        for state in sdfg.topological_sort(sdfg.start_state):
+            if state in result:
+                continue
+            result[state] = {}
+
+            # Get predecessors
+            in_edges = sdfg.in_edges(state)
+            if len(in_edges) == 1:  # Special case, propagate as-is
+                # First the prior state
+                result[state].update(result[in_edges[0].src])
+
+                # Then assignments on the incoming edge
+                result[state].update(self._data_independent_assignments(in_edges[0].data, arrays))
+                continue
+
+            # More than one incoming edge: may require reversed traversal
+            assignments = {}
+            for edge in in_edges:
+                # If source was already visited, use its propagated constants
+                constants: Dict[str, Any]
+                if edge.src in result:
+                    constants = result[edge.src]
+                else:  # Otherwise, reverse DFS to find constants until a visited state
+                    constants = self._constants_from_unvisited_state(sdfg, edge.src, arrays, result)
+
+                # Update constants with incoming edge
+                constants.update(self._data_independent_assignments(edge.data, arrays))
+
+                for aname, aval in constants.items():
+                    # If something was assigned more than once (to a different value), it's not a constant
+                    if aname in assignments and aval != assignments[aname]:
+                        assignments[aname] = _UnknownValue
+                    else:
+                        assignments[aname] = aval
+
+            result[state].update(assignments)
+
+            # TODO: Update results with values of other propagated symbols
+
+        return result
+
+    def _data_independent_assignments(self, edge: InterstateEdge, arrays: Set[str]) -> Dict[str, Any]:
+        """
+        Return symbol assignments that only depend on other symbols and constants, rather than data descriptors.
+        """
+        return {k: v for k, v in edge.assignments.items() if not symbolic.free_symbols_and_functions(v) & arrays}
+
+    def _constants_from_unvisited_state(self, sdfg: SDFG, state: SDFGState, arrays: Set[str],
+                                        existing_constants: Dict[SDFGState, Dict[str, Any]]) -> Dict[str, Any]:
+        """
+        Collects constants from an unvisited state, traversing backwards until reaching states that do have
+        collected constants.
+        """
+        result: Dict[str, Any] = {}
+
+        def _update_if_new(existing: Dict[Any, Any], new: Dict[Any, Any]) -> None:
+            for k, v in new.items():
+                if k not in existing:
+                    existing[k] = v
+
+        for parent, node in sdutil.dfs_conditional(sdfg,
+                                                   sources=[state],
+                                                   reverse=True,
+                                                   condition=lambda p, c: c not in existing_constants,
+                                                   yield_parent=True):
+            # Skip first node
+            if parent is None:
+                continue
+
+            # Get connecting edge (reversed)
+            edge = sdfg.edges_between(node, parent)[0]
+
+            # If node already has propagated constants, update dictionary and stop traversal
+            if node in existing_constants:
+                self._data_independent_assignments(edge.data, arrays)
+                _update_if_new(result, existing_constants[node])
+                continue
+
+        return result
---
+++ tests/passes/constant_propagation_test.py
@@ -0,0 +1,192 @@
+# Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
+
+import dace
+from dace.transformation.pass_pipeline import Pipeline
+from dace.transformation.passes.analysis import StateReachability
+from dace.transformation.passes.constant_propagation import ConstantPropagation
+from dace.transformation.passes.scalar_to_symbol import ScalarToSymbolPromotion
+import numpy as np
+
+
+def test_simple_constants():
+    @dace.program
+    def program(A: dace.float64[20]):
+        val = 5
+        cval = val % 4
+        if cval + 1 == 1:
+            A[:] = cval
+        else:
+            A[:] = cval + 4
+
+    sdfg = program.to_sdfg()
+    ScalarToSymbolPromotion().apply_pass(sdfg, {})
+    res = Pipeline([ConstantPropagation()]).apply_pass(sdfg, {})
+
+    assert len(sdfg.symbols) == 0
+    for e in sdfg.edges():
+        assert len(e.data.assignments) == 0
+    a = np.random.rand(20)
+    program(a)
+    assert np.allclose(a, 5)
+
+
+def test_nested_constants():
+    @dace.program
+    def program(A: dace.int32[20]):
+        i = A[0]
+        j = i + 1
+        k = j + 1  # Should become i + 2
+        l = i + k  # 2*i + 2
+        A[l] = k
+
+    sdfg = program.to_sdfg()
+    Pipeline([ConstantPropagation()]).apply_pass(sdfg, {})
+
+    assert set(sdfg.symbols.keys()) == {'i'}
+
+    # Test memlet
+    sdfg.simplify()
+    assert sdfg.number_of_nodes() == 2
+    last_state = sdfg.sink_nodes()[0]
+    sink = last_state.sink_nodes()[0]
+    memlet = last_state.in_edges(sink)[0].data
+    assert memlet.data == 'A'
+    assert str(memlet.subset) == '2*i + 2'
+
+
+def test_simple_loop():
+    @dace.program
+    def program(a: dace.float64[20]):
+        for i in range(3):
+            a[i] = 5
+        i = 5
+        a[0] = i  # Use i - should be const
+
+    sdfg = program.to_sdfg()
+    Pipeline([ConstantPropagation()]).apply_pass(sdfg, {})
+
+    assert set(sdfg.symbols.keys()) == {'i'}
+    # Test tasklets
+    for node, _ in sdfg.all_nodes_recursive():
+        if isinstance(node, dace.nodes.Tasklet):
+            code = node.code.as_string
+            assert '5' in code and 'i' not in code
+
+
+def test_cprop_inside_loop():
+    @dace.program
+    def program(a: dace.float64[20]):
+        for i in range(5):
+            a[i] = i  # Use i - not const
+            i = 8
+            a[i] = i  # Use i - const
+        a[i] = i  # Use i - not const
+
+    sdfg = program.to_sdfg()
+    Pipeline([ConstantPropagation()]).apply_pass(sdfg, {})
+
+    assert set(sdfg.symbols.keys()) == {'i'}
+
+    # Test tasklets
+    i_found = 0
+    for node, _ in sdfg.all_nodes_recursive():
+        if isinstance(node, dace.nodes.Tasklet):
+            if 'i' in node.code.as_string:
+                i_found += 1
+    assert i_found == 2
+
+
+def test_cprop_outside_loop():
+    @dace.program
+    def program(a: dace.float64[20, 20]):
+        k = 5
+        j = 2
+        for i in range(2):
+            j = 5
+        # Use j - not const
+        # Use k - const
+        a[j, k] = 1
+
+    sdfg = program.to_sdfg()
+    Pipeline([ConstantPropagation()]).apply_pass(sdfg, {})
+
+    assert set(sdfg.symbols.keys()) == {'i', 'j'}
+
+    # Test memlet
+    last_state = sdfg.sink_nodes()[0]
+    sink = last_state.sink_nodes()[0]
+    memlet = last_state.in_edges(sink)[0].data
+    assert memlet.data == 'a'
+    assert str(memlet.subset) == 'j, 5'
+
+
+def test_cond():
+    @dace.program
+    def program(a: dace.float64[20, 20], scal: dace.int32):
+        if scal > 0:
+            i = 0
+            j = 2
+        else:
+            i = 1
+            j = 2
+
+        # After condition, i is not constant and j is
+        a[i, j] = 3
+
+    sdfg = program.to_sdfg()
+    Pipeline([ConstantPropagation()]).apply_pass(sdfg, {})
+
+    assert set(sdfg.symbols.keys()) == {'i'}
+
+    # Test memlet
+    last_state = sdfg.sink_nodes()[0]
+    sink = last_state.sink_nodes()[0]
+    memlet = last_state.in_edges(sink)[0].data
+    assert memlet.data == 'a'
+    assert str(memlet.subset) == 'i, 2'
+
+
+def test_complex_case():
+    """ Tests a complex control flow case. """
+    sdfg = dace.SDFG('program')
+    sdfg.add_scalar('a', dace.float64)
+    init = sdfg.add_state()
+    guard = sdfg.add_state()
+    branch2 = sdfg.add_state()
+    branch2_1 = sdfg.add_state()
+    afterloop = sdfg.add_state()  # uses i, should not be constant
+    inside_loop1 = sdfg.add_state()
+    inside_loop2 = sdfg.add_state()
+    merge = sdfg.add_state()
+    usei = sdfg.add_state()  # uses i, should be constant
+    loop2 = sdfg.add_state()
+    last = sdfg.add_state()
+    sdfg.add_edge(init, guard, dace.InterstateEdge('a > 0', {'i': 5}))
+    sdfg.add_edge(init, branch2, dace.InterstateEdge('a <= 0', {'i': 6}))
+    sdfg.add_edge(branch2, branch2_1, dace.InterstateEdge())
+    sdfg.add_edge(guard, inside_loop1, dace.InterstateEdge('i < 6'))
+    sdfg.add_edge(guard, afterloop, dace.InterstateEdge('i >= 6'))
+    sdfg.add_edge(inside_loop1, inside_loop2, dace.InterstateEdge(assignments={'i': 6}))
+    sdfg.add_edge(inside_loop2, guard, dace.InterstateEdge(assignments={'i': 'i+1'}))
+
+    sdfg.add_edge(afterloop, merge, dace.InterstateEdge(assignments={'i': 6, 'j': 1}))
+    sdfg.add_edge(branch2_1, merge, dace.InterstateEdge(assignments={'j': 1}))
+
+    sdfg.add_edge(merge, loop2, dace.InterstateEdge('j < 2'))
+    sdfg.add_edge(loop2, usei, dace.InterstateEdge())
+    sdfg.add_edge(usei, merge, dace.InterstateEdge(assignments={'j': 'j+1'}))
+    sdfg.add_edge(merge, last, dace.InterstateEdge('j >= 2'))
+
+    propagated = ConstantPropagation().collect_constants(sdfg)  #, reachability
+    assert 'i' in propagated[usei] and propagated[usei]['i'] == 6
+    assert 'i' not in propagated[afterloop]
+
+
+if __name__ == '__main__':
+    test_simple_constants()
+    test_nested_constants()
+    test_simple_loop()
+    test_cprop_inside_loop()
+    test_cprop_outside_loop()
+    test_cond()
+    test_complex_case()
