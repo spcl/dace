54d2247703a0d1cb4d4d10d53958df97e4263625
Oliver Rausch <oliverrausch99@gmail.com>
2022-06-26 15:19:14 +0200

    Add CITATION.cff
    This adds a nice button to the repo metadata (with bibtex export)

---
+++ citation_format_authors.py
@@ -0,0 +1,21 @@
+"""
+Parse the authors file and print for CITATION.cff
+"""
+with open("AUTHORS", "r") as f:
+  content = f.readlines()
+
+for i, l in enumerate(content[4:]):
+    if l == "\n":
+        end_idx = i + 4
+        break
+else:
+    raise ValueError()
+
+for author in content[4:end_idx]:
+    names = author.strip().split()
+    first_name = names[0]
+    last_names = ' '.join(names[1:])
+    text = f"- family-names: {last_names}\n  given-names: {first_name}"
+    print(text)
+
+
