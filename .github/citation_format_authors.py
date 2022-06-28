a0aed3921a4a85698c88200f06097fa485231cd4
Tal Ben-Nun <tbennun@users.noreply.github.com>
2022-06-28 15:40:26 +0200

    Update citation_format_authors.py

--- .github/citation_format_authors.py
+++ .github/citation_format_authors.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+# Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
 """
 Parse the authors file and print for CITATION.cff
 """
