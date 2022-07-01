65e481a0c949d3afc93db56f5f5f144f2c2c8f5b
Tal Ben-Nun <tbennun@gmail.com>
2022-07-01 20:17:09 +0200

    Optional array inference pass

---
+++ dace/transformation/passes/optional_arrays.py
@@ -0,0 +1,108 @@
+# Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
+
+from typing import Dict, Iterator, Optional, Set, Tuple
+
+from dace import SDFG, SDFGState, data
+from dace.sdfg import nodes
+from dace.sdfg import utils as sdutil
+from dace.transformation import pass_pipeline as ppl
+
+
+class OptionalArrayInference(ppl.Pass):
+    """
+    Infers the ``optional`` property of arrays, i.e., if they can be given None, throughout the SDFG and all nested
+    SDFGs.
+
+    An array will be modified to optional if its previous ``optional`` property was set to ``None`` and:
+    * it is transient;
+    * it is in a nested SDFG and its parent array was transient; or
+    * it is definitely (unconditionally) read or written in the SDFG.
+    """
+    def modifies(self) -> ppl.Modifies:
+        return ppl.Modifies.States
+
+    def should_reapply(self, modified: ppl.Modifies) -> bool:
+        # If connectivity or any edges were changed, some more states might be dead
+        return modified & (ppl.Modifies.InterstateEdges | ppl.Modifies.States)
+
+    def apply_pass(self,
+                   sdfg: SDFG,
+                   _,
+                   parent_arrays: Optional[Dict[str, bool]] = None) -> Optional[Set[Tuple[int, str]]]:
+        """
+        Infers the ``optional`` property of arrays in the SDFG and its nested SDFGs.
+        :param sdfg: The SDFG to modify.
+        :param pipeline_results: If in the context of a ``Pipeline``, a dictionary that is populated with prior Pass
+                                 results as ``{Pass subclass name: returned object from pass}``. If not run in a
+                                 pipeline, an empty dictionary is expected.
+        :param parent_arrays: If not None, contains values of determined arrays from the parent SDFG.
+        :return: A set of the modified array names as a 2-tuple (SDFG ID, name), or None if nothing was changed.
+        """
+        result: Set[Tuple[int, str]] = set()
+        parent_arrays = parent_arrays or {}
+
+        sdfg_id = sdfg.sdfg_id
+
+        # Set information of arrays based on their transient and parent status
+        for aname, arr in sdfg.arrays.items():
+            if arr.transient:
+                if arr.optional is not False:
+                    result.add((sdfg_id, aname))
+                arr.optional = False
+            if aname in parent_arrays:
+                if arr.optional is not parent_arrays[aname]:
+                    result.add((sdfg_id, aname))
+                arr.optional = parent_arrays[aname]
+
+        # Change unconditionally-accessed arrays to non-optional
+        for state in self.traverse_unconditional_states(sdfg):
+            for anode in state.data_nodes():
+                desc = anode.desc(sdfg)
+                if isinstance(desc, data.Array) and desc.optional is None:
+                    desc.optional = False
+                    result.add((sdfg_id, anode.data))
+
+        # Propagate information to nested SDFGs
+        for state in sdfg.nodes():
+            for node in state.nodes():
+                if isinstance(node, nodes.NestedSDFG):
+                    # Create information about parent arrays
+                    pinfo: Dict[str, bool] = {}
+                    for e in state.in_edges(node):
+                        if e.data.is_empty():
+                            continue
+                        opt = sdfg.arrays[e.data.data].optional
+                        if opt is not None:
+                            pinfo[e.dst_conn] = opt
+                    for e in state.out_edges(node):
+                        if e.data.is_empty():
+                            continue
+                        opt = sdfg.arrays[e.data.data].optional
+                        if opt is not None:
+                            pinfo[e.src_conn] = opt
+                    
+                    # Apply pass recursively
+                    rec_result = self.apply_pass(node.sdfg, _, pinfo)
+                    if rec_result:
+                        result.update(rec_result)
+
+        return result or None
+
+    def traverse_unconditional_states(self, sdfg: SDFG) -> Iterator[SDFGState]:
+        """
+        Traverse SDFG and keep track of whether the state is executed unconditionally.
+        """
+        ipostdom = sdutil.postdominators(sdfg)
+        curstate = sdfg.start_state
+        out_degree = sdfg.out_degree(curstate)
+        while out_degree > 0:
+            yield curstate
+            if out_degree == 1:  # Unconditional, continue to next state
+                curstate = sdfg.successors(curstate)[0]
+            elif out_degree > 1:  # Conditional branch
+                # Conditional code follows, use immediate post-dominator for next unconditional state
+                curstate = ipostdom[curstate]
+            # Compute new out degree
+            out_degree = sdfg.out_degree(curstate)
+        # Yield final state
+        yield curstate
--- tests/python_frontend/optional_array_test.py
+++ tests/python_frontend/optional_array_test.py
@@ -1,10 +1,14 @@
 # Copyright 2019-2022 ETH Zurich and the DaCe authors. All rights reserved.
 """ Tests optional (could be None) arrays and arguments. """
+from typing import Optional, Union
+
+import numpy as np
+import pytest
+
 import dace
 from dace.sdfg.utils import inline_sdfgs
 from dace.transformation.passes.dead_state_elimination import DeadStateElimination
-from typing import Optional, Union
-import pytest
+from dace.transformation.passes.optional_arrays import OptionalArrayInference
 
 
 def test_type_hint():
@@ -72,9 +76,60 @@ def test_optional_dead_state(isnone):
     assert all('b' not in str(e.data.condition.as_string) for e in sdfg.edges())
 
 
+def test_optional_array_inference():
+    @dace.program
+    def nested(b):
+        tmp = np.zeros_like(b)
+        b[:] += 1 + tmp
+
+    NotOptional = dace.float64[20]
+    NotOptional.optional = False
+
+    @dace.program
+    def outer(yes: Optional[dace.float64[20]], no: NotOptional, maybe: dace.float64[20], always_read: dace.float64[20],
+              cond: dace.int32):
+        # Add loop to challenge unconditional traversal
+        for _ in range(10):
+            pass
+
+        nested(always_read)
+        if cond == 0:
+            nested(yes)
+        else:
+            nested(no)
+
+        if cond == 1:
+            nested(maybe)
+
+    sdfg = outer.to_sdfg(simplify=False)
+    sdfg.validate()
+
+    # Before pass, `maybe` and `always_read` are unknown
+    assert sdfg.arrays['maybe'].optional is None
+    assert sdfg.arrays['always_read'].optional is None
+
+    result = OptionalArrayInference().apply_pass(sdfg, {})
+    assert (0, 'always_read') in result
+
+    # Check top-level SDFG
+    assert sdfg.arrays['yes'].optional is True
+    assert sdfg.arrays['no'].optional is False
+    assert sdfg.arrays['maybe'].optional is None
+    assert sdfg.arrays['always_read'].optional is False
+
+    # Test nested SDFGs
+    for state in sdfg.nodes():
+        for node in state.nodes():
+            if isinstance(node, dace.nodes.NestedSDFG):
+                inputnode: dace.nodes.AccessNode = state.predecessors(node)[0]
+                assert node.sdfg.arrays['b'].optional == inputnode.desc(sdfg).optional
+                assert node.sdfg.arrays['tmp'].optional is False
+
+
 if __name__ == '__main__':
     test_type_hint()
     test_optional_arg_hint()
     test_optional_argcheck()
     test_optional_dead_state(False)
     test_optional_dead_state(True)
+    test_optional_array_inference()
