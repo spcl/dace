5f4326f7ead24fe1e318d84122e9fdbc823b4ee2
Tal Ben-Nun <tbennun@gmail.com>
2022-07-08 00:01:05 +0200

    DDE: More conditions

--- dace/transformation/passes/dead_dataflow_elimination.py
+++ dace/transformation/passes/dead_dataflow_elimination.py
@@ -12,6 +12,8 @@ from dace.sdfg import infer_types
 from dace.transformation import pass_pipeline as ppl
 from dace.transformation.passes import analysis as ap
 
+PROTECTED_NAMES = {'__pystate'}  #: A set of names that are not allowed to be erased
+
 
 @dataclass(unsafe_hash=True)
 class DeadDataflowElimination(ppl.Pass):
@@ -116,30 +118,33 @@ class DeadDataflowElimination(ppl.Pass):
             predecessor_nsdfgs: Dict[nodes.NestedSDFG, Set[str]] = defaultdict(set)
             for node in dead_nodes:
                 # Remove memlet paths and connectors pertaining to dead nodes
-                for e in state.in_edges(node):
-                    mtree = state.memlet_tree(e)
-                    for leaf in mtree.leaves():
-                        # Keep track of predecessors of removed nodes for connector pruning
-                        if isinstance(leaf.src, nodes.NestedSDFG):
-                            if not leaf.data.is_empty():
-                                predecessor_nsdfgs[leaf.src].add(leaf.src_conn)
+                try:
+                    for e in state.in_edges(node):
+                        mtree = state.memlet_tree(e)
+                        for leaf in mtree.leaves():
+                            # Keep track of predecessors of removed nodes for connector pruning
+                            if isinstance(leaf.src, nodes.NestedSDFG):
+                                if not leaf.data.is_empty():
+                                    predecessor_nsdfgs[leaf.src].add(leaf.src_conn)
 
-                        # Pruning connectors on tasklets sometimes needs to change their code
-                        elif (isinstance(leaf.src, nodes.Tasklet) and leaf.src.code.language != dtypes.Language.Python):
-                            if leaf.src.code.language == dtypes.Language.CPP:
-                                ctype = infer_types.infer_out_connector_type(sdfg, state, leaf.src, leaf.src_conn)
-                                if ctype is None:
+                            # Pruning connectors on tasklets sometimes needs to change their code
+                            elif (isinstance(leaf.src, nodes.Tasklet) and leaf.src.code.language != dtypes.Language.Python):
+                                if leaf.src.code.language == dtypes.Language.CPP:
+                                    ctype = infer_types.infer_out_connector_type(sdfg, state, leaf.src, leaf.src_conn)
+                                    if ctype is None:
+                                        raise NotImplementedError(f'Cannot eliminate dead connector "{leaf.src_conn}" on '
+                                                                'tasklet due to connector type inference failure.')
+                                    # Add definition
+                                    leaf.src.code.code = f'{ctype.as_arg(leaf.src_conn)};\n' + leaf.src.code.code
+                                else:
                                     raise NotImplementedError(f'Cannot eliminate dead connector "{leaf.src_conn}" on '
-                                                              'tasklet due to connector type inference failure.')
-                                # Add definition
-                                leaf.src.code.code = f'{ctype.as_arg(leaf.src_conn)};\n' + leaf.src.code.code
-                            else:
-                                raise NotImplementedError(f'Cannot eliminate dead connector "{leaf.src_conn}" on '
-                                                          'tasklet due to its code language.')
-                        state.remove_memlet_path(leaf)
+                                                            'tasklet due to its code language.')
+                            state.remove_memlet_path(leaf)
 
-                # Remove the node itself as necessary
-                state.remove_node(node)
+                    # Remove the node itself as necessary
+                    state.remove_node(node)
+                except KeyError:  # Node already removed
+                    continue
 
             result[state].update(dead_nodes)
 
@@ -167,7 +172,7 @@ class DeadDataflowElimination(ppl.Pass):
 
     def report(self, pass_retval: Dict[SDFGState, Set[str]]) -> str:
         n = sum(len(v) for v in pass_retval.values())
-        return f'Eliminated {n} nodes in {len(pass_retval)} states.'
+        return f'Eliminated {n} nodes in {len(pass_retval)} states: {pass_retval}'
 
     def _is_node_dead(self, node: nodes.Node, sdfg: SDFG, state: SDFGState, dead_nodes: Set[nodes.Node],
                       no_longer_used: Set[str], access_set: Tuple[Set[str], Set[str]]) -> bool:
@@ -193,6 +198,10 @@ class DeadDataflowElimination(ppl.Pass):
             return not node.has_side_effects(sdfg)
 
         elif isinstance(node, nodes.AccessNode):
+            # Search for names that are disallowed to remove
+            if node.data in PROTECTED_NAMES:
+                return False
+
             desc = sdfg.arrays[node.data]
 
             # If data descriptor is global, it cannot be removed
@@ -208,10 +217,17 @@ class DeadDataflowElimination(ppl.Pass):
             if node.data not in no_longer_used:
                 return False
 
-            # If data is connected to a side-effect tasklet/library node, cannot remove
+            # Check incoming edges
             for e in state.in_edges(node):
-                if any(_has_side_effects(l.src, sdfg) for l in state.memlet_tree(e).leaves()):
-                    return False
+                for l in state.memlet_tree(e).leaves():
+                    # If data is connected to a side-effect tasklet/library node, cannot remove
+                    if _has_side_effects(l.src, sdfg):
+                        return False
+
+                    # If data is connected to a nested SDFG as an input/output, do not remove
+                    if (isinstance(l.src, nodes.NestedSDFG)
+                            and any(ie.data.data == node.data for ie in state.in_edges(l.src))):
+                        return False
 
             # If it is a stream and is read somewhere in the state, it may be popped after pushing
             if isinstance(desc, data.Stream) and node.data in access_set[0]:
