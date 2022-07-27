f3fb00b8ff5f973dc2b15baaf71a732ec18adcb6
Linus Groner <linus.groner@cscs.ch>
2022-07-27 17:03:55 +0200

    yapf

--- tests/array_interface_test.py
+++ tests/array_interface_test.py
@@ -1,13 +1,13 @@
+# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
 import numpy as np
 
 import dace
 
-
 import dace
 import numpy as np
+
 
 class ArrayWrapper:
-
     def __init__(self, array, **kwargs):
         self.array = array
 
@@ -18,13 +18,14 @@ class ArrayWrapper:
 
 def test_array_interface_input():
     @dace.program
-    def simple_program(A: dace.float64[3,3,3]):
+    def simple_program(A: dace.float64[3, 3, 3]):
         A += 1
+
     simple_program.compile()
-    
-    A = np.ones((3,3,3))
-    Awrap=ArrayWrapper(A)
-    
+
+    A = np.ones((3, 3, 3))
+    Awrap = ArrayWrapper(A)
+
     simple_program(A=Awrap)
 
     np.testing.assert_equal(A, 2)
