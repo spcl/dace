2d41b62be74be5b5449b72ecc85998fbe2460e3e
Jiela <rotkehlchen789@gmail.com>
2022-07-30 14:12:33 +0200

    Added a draft for a warp Reduction library node

--- dace/libraries/standard/nodes/reduce.py
+++ dace/libraries/standard/nodes/reduce.py
@@ -1037,6 +1037,97 @@ reduce_out = {reduction_expr}''')
         return nsdfg
 
 
+@dace.library.expansion
+class WarpReductionExpansion(pm.ExpandTransformation):
+    
+    
+    
+    environments = [CUDA]
+    
+    @staticmethod
+    def expansion(node: SDFG.node, parent_state: SDFGState, parent_sdfg: SDFG, local_sum: SDFG.node, local_Result: SDFG.node):
+        
+        # ToDo: fix the data descriptors!
+            # The data we recieve:
+                # FROM PARENT
+                # mySum
+                # SRes
+                # FROM PARENT'S PARENT
+                # warpSize      this should be a free symbol for now I am just using a hardcoded 32 to see if warpSize was the thing causing the segfault
+        
+        # ToDo: implement checks (once this thing works when used appropriately)
+        
+        ###################
+        # Grabbing the Data:
+        
+        input_edge = parent_state.in_edges(node)[0]
+        output_edge = parent_state.out_edges(node)[0]
+        
+        input_data = parent_sdfg.arrays[input_edge.data]      # this I believe to be the location of sA, sB and mySum (the data) for now, this can probably be fixed by the way the library node is called (since sA and sB aren't needed)
+        output_data = parent_sdfg.arrays[output_edge.data]     # and this the location of sRes
+        
+        in_ptr = dace.pointer(input_data.dtype)
+        out_ptr = dace.pointer(output_data.dtype)
+        
+        
+        ###################
+        
+        ###################
+        # Notes to self:
+        # 
+        # I do not need an entry state
+        # Writeback writes into sRes
+        # Use pointers in Writeback
+        # Can I access an access node of the parent sdfg (gpu_sdfg) in this state?
+        # you need to do some connector renaming and stuff to actually get that data connected properly
+        
+        ###################
+        
+        def WriteBackState(state, mS, r):
+    
+            src_node = state.add_read(mS)
+            dst_node = state.add_write(r)
+            
+            state.add_nedge(src_node, dst_node, dace.Memlet(f"{r}", wcr="lambda x, y: x + y"))      
+
+        
+        subSDFG = dace.SDFG('WarpReduction_SDFG')
+        
+        wwr_state= subSDFG.add_state('WarpWise_Reduction')
+
+        mSum = wwr_state.add_access(local_sum)
+        wtasklet = wwr_state.add_tasklet('warpwise_Reduction',
+                                        {}, {'__out'},
+                                        f"__out = __shfl_down_sync(0xFFFFFFFF, {mSum.data}, offset);",
+                                        dace.Language.CPP)
+        wwr_state.add_edge(wtasklet, '__out', local_sum, None, dace.Memlet(f"{local_sum.data}", wcr="lambda x, y: x + y"))
+
+        _, _, after_state = subSDFG.add_loop(parent_state, wwr_state, None, 'offset', '32 / 2', 'offset > 0', 'offset / 2')
+
+        write_back_state = subSDFG.add_state('Write_Back')
+        WriteBackState(write_back_state, local_sum, local_Result)
+
+        random_end_state = subSDFG.add_state('RandomEndState')
+
+        subSDFG.add_edge(after_state, write_back_state, dace.InterstateEdge('j % 32 == 0'))
+        subSDFG.add_edge(after_state, random_end_state, dace.InterstateEdge('j % 32 != 0'))
+        subSDFG.add_edge(write_back_state, random_end_state, dace.InterstateEdge())
+        
+        ###################
+        # Redefining outer connectors and add to node
+        
+        
+        
+        
+        
+        ###################
+        
+        return subSDFG
+
+
+
+
+
 @dace.library.node
 class Reduce(dace.sdfg.nodes.LibraryNode):
     """ An SDFG node that reduces an N-dimensional array to an
@@ -1051,12 +1142,13 @@ class Reduce(dace.sdfg.nodes.LibraryNode):
         'CUDA (device)': ExpandReduceCUDADevice,
         'CUDA (block)': ExpandReduceCUDABlock,
         'CUDA (block allreduce)': ExpandReduceCUDABlockAll,
-        'FPGAPartialReduction': ExpandReduceFPGAPartialReduction
+        'FPGAPartialReduction': ExpandReduceFPGAPartialReduction,
+        'CUDA(shuffle)' : WarpReductionExpansion
         # 'CUDA (warp)': ExpandReduceCUDAWarp,
         # 'CUDA (warp allreduce)': ExpandReduceCUDAWarpAll
     }
 
-    default_implementation = 'pure'
+    default_implementation = 'CUDA(shuffle)'
 
     # Properties
     axes = ListProperty(element_type=int, allow_none=True)
