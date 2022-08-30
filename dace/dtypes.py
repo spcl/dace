a7705920b10b250a5c33295eceb8dc7b3a57ec0b
Tal Ben-Nun <tbennun@gmail.com>
2022-08-30 21:48:13 +0200

    Fix serialization name of boolean types

--- dace/dtypes.py
+++ dace/dtypes.py
@@ -341,7 +341,7 @@ class typeclass(object):
             3. Enabling extensions such as `dace.struct` and `dace.vector`
     """
 
-    def __init__(self, wrapped_type):
+    def __init__(self, wrapped_type, typename=None):
         # Convert python basic types
         if isinstance(wrapped_type, str):
             try:
@@ -377,13 +377,14 @@ class typeclass(object):
         self.ctype_unaligned = self.ctype  # Type in C (without alignment)
         self.dtype = self  # For compatibility support with numpy
         self.bytes = _BYTES[wrapped_type]  # Number of bytes for this type
+        self.typename = typename
 
     def __hash__(self):
         return hash((self.type, self.ctype))
 
     def to_string(self):
         """ A Numpy-like string-representation of the underlying data type. """
-        return self.type.__name__
+        return self.typename or self.type.__name__
 
     def as_ctypes(self):
         """ Returns the ctypes version of the typeclass. """
@@ -400,7 +401,7 @@ class typeclass(object):
     def to_json(self):
         if self.type is None:
             return None
-        return self.type.__name__
+        return self.typename or self.type.__name__
 
     @staticmethod
     def from_json(json_obj, context=None):
@@ -1107,7 +1108,7 @@ def isconstant(var):
     return type(var) in _CONSTANT_TYPES
 
 
-bool_ = typeclass(numpy.bool_)
+bool_ = typeclass(numpy.bool_, 'bool')
 int8 = typeclass(numpy.int8)
 int16 = typeclass(numpy.int16)
 int32 = typeclass(numpy.int32)
@@ -1171,7 +1172,7 @@ DTYPE_TO_TYPECLASS = {
 
 # Since this overrides the builtin bool, this should be after the
 # DTYPE_TO_TYPECLASS dictionary
-bool = typeclass(numpy.bool_)
+bool = bool_
 
 TYPECLASS_TO_STRING = {
     bool: "dace::bool",
