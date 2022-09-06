5d4f746f69449a5d36b2114bc7e92049034acdbb
Lukas Trümper <lukas.truemper@outlook.de>
2022-09-06 12:10:38 +0200

    LIKWID_GPU instrumentation

--- samples/instrumentation/matmul_likwid_gpu.py
+++ samples/instrumentation/matmul_likwid_gpu.py
@@ -39,6 +39,7 @@ def matmul(A: dace.float32[M, K], B: dace.float32[K, N], C: dace.float32[M, N]):
 sdfg = matmul.to_sdfg()
 sdfg.expand_library_nodes()
 sdfg.simplify()
+sdfg.apply_gpu_transformations()
 
 # Specialize SDFG for input sizes
 m = 512
@@ -56,17 +57,19 @@ C = np.zeros((m, n), dtype=np.float32)
 # Non-top-level map entries are currently not supported!
 for nsdfg in sdfg.all_sdfgs_recursive():
     for state in nsdfg.nodes():
-        state.instrument = dace.InstrumentationType.LIKWID_CPU
-        for node in state.nodes():
-            if isinstance(node, dace.nodes.MapEntry) and xfh.get_parent_map(state, node) is None:
-                node.instrument = dace.InstrumentationType.LIKWID_CPU
+        state.instrument = dace.InstrumentationType.LIKWID_GPU
 
 ## 3. Compile and execute
 # During execution, the counters for different parts of the SDFG and different
 # threads are measured by likwid and written into a performance report
 # in form of events. This report is saved at .dacecache/matmul/perf.
-csdfg = sdfg.compile()
-csdfg(A=A, B=B, C=C, K=k, M=m, N=n)
+with dace.config.set_temporary('instrumentation', 'report_each_invocation', value=False):
+    csdfg = sdfg.compile()
+
+    for _ in range(10):
+        csdfg(A=A, B=B, C=C, K=k, M=m, N=n)
+
+    csdfg.finalize()
 
 ## 4. Report
 # We can now parse the performance report into a python-object
@@ -77,22 +80,3 @@ report = sdfg.get_latest_report()
 # Print human-readable table
 # Tip: Try this feature with only a 1-2 on instrumented states/nodes.
 print(report)
-
-# Access counters
-# We will now demonstrate how to access the raw values from the report
-# on the example of number of SP FLOPS. Those are measured
-# when executing the sample with LIKWID_EVENTS="FLOPS_SP".
-#
-# Counter values are grouped by the SDFG element which defines the scope
-# of the intrumentation. Those elements are described as the triplet
-# (sdfg_id, state_id, node_id).
-
-measured_flops = 0
-flops_report = report.counters[(0, 0, -1)]["state_0_0_-1"]["RETIRED_SSE_AVX_FLOPS_SINGLE_ALL"]
-for tid in flops_report:
-    measured_flops += flops_report[tid][0]
-
-# ~ expected FLOPS
-expected_flops = m * k * (n * 2)
-
-print(f"Expected {expected_flops} FLOPS, measured {measured_flops} FLOPS, diff: {measured_flops - expected_flops}")
