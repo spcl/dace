bdd81068d94b153814d7d81039987334a75d23b6
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-09-26 14:27:56 +0200

    Added missing auto_optimize

--- tests/npbench/polybench/lu_test.py
+++ tests/npbench/polybench/lu_test.py
@@ -66,6 +66,7 @@ def run_lu(device_type: dace.dtypes.DeviceType):
     if device_type in {dace.dtypes.DeviceType.CPU, dace.dtypes.DeviceType.GPU}:
         # Parse the SDFG and apply autopot
         sdfg = lu_kernel.to_sdfg()
+        sdfg = auto_optimize(sdfg, device_type)
         dace_res = sdfg(A=A, N=N)
 
     elif device_type == dace.dtypes.DeviceType.FPGA:
