17d63196287a577462091f2e33a2d9c818c874ca
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-01-13 16:53:58 +0100

    Added pass to fission scopes.

--- dace/sdfg/analysis/schedule_tree/passes.py
+++ dace/sdfg/analysis/schedule_tree/passes.py
@@ -3,9 +3,9 @@
 Assortment of passes for schedule trees.
 """
 
-import copy
 from dace import data as dt, Memlet, subsets as sbs, symbolic as sym
 from dace.sdfg.analysis.schedule_tree import treenodes as tn
+from dataclasses import dataclass
 from typing import Set
 
 
@@ -136,53 +136,69 @@ def canonicalize_if(tree: tn.ScheduleTreeScope):
     """
 
     from dace.sdfg.nodes import CodeBlock
-    from dace.sdfg.analysis.schedule_tree.transformations import if_fission
 
     class CanonicalizeIf(tn.ScheduleNodeTransformer):
 
-
         def visit(self, node: tn.ScheduleTreeNode):
-            if not isinstance(node, (tn.ElifScope, tn.ElseScope)):
-                return super().visit(node)
-            
-            parent = node.parent
-            node_idx = parent.children.index(node)
 
-            conditions = []
-            for curr_node in reversed(parent.children[:node_idx]):
-                conditions.append(curr_node.condition)
-                if isinstance(curr_node, tn.IfScope):
-                    break
-            condition = f"not ({' or '.join([f'({c.as_string})' for c in conditions])})"
-            if isinstance(node, tn.ElifScope):
-                condition = f"{condition} and {node.condition.as_string}"
-            new_node = tn.IfScope(node.sdfg, node.top_level, node.children, CodeBlock(condition))
-            new_node.parent = node.parent
+            if isinstance(node, (tn.ElifScope, tn.ElseScope)):         
+                parent = node.parent
+                assert node in parent.children
+                node_idx = parent.children.index(node)
+
+                conditions = []
+                for curr_node in reversed(parent.children[:node_idx]):
+                    conditions.append(curr_node.condition)
+                    if isinstance(curr_node, tn.IfScope):
+                        break
+                condition = f"not ({' or '.join([f'({c.as_string})' for c in conditions])})"
+                if isinstance(node, tn.ElifScope):
+                    condition = f"{condition} and {node.condition.as_string}"
+                new_node = tn.IfScope(node.sdfg, node.top_level, node.children, CodeBlock(condition))
+                new_node.parent = parent
+            else:
+                new_node = node
 
             return self.generic_visit(new_node)
 
-    CanonicalizeIf().visit(tree)
-    print(tree.as_string())
-    print()
+    return CanonicalizeIf().visit(tree)
 
-    frontier = list(tree.children)
-    while frontier:
-        node = frontier.pop()
-        if isinstance(node, tn.IfScope):
-            parent = node.parent
-            node_idx = parent.children.index(node)
-            next_node = None
-            if len(parent.children) > node_idx + 1:
-                next_node = parent.children[node_idx + 1]
-            if_fission(node, distribute=True)
-            end = len(parent.children)
-            if next_node:
-                end = parent.children.index(next_node)
-            for child in parent.children[node_idx: end]:
-                if len(child.children) > 1:
-                    frontier.append(child)
-            # frontier.extend(parent.children[node_idx: end])
-        elif isinstance(node, tn.ScheduleTreeScope):
-            frontier.extend(node.children)
-    
-    return tree
+
+def fission_scopes(node: tn.ScheduleTreeScope):
+
+    from dace.sdfg.analysis.schedule_tree.transformations import loop_fission, if_fission
+
+    @dataclass
+    class FissionScopes(tn.ScheduleNodeTransformer):
+
+        tree: tn.ScheduleTreeScope
+
+        def visit_IfScope(self, node: tn.IfScope):
+            return if_fission(node, assume_canonical=True, distribute=True)
+
+        def visit_ForScope(self, node: tn.ForScope):
+            return loop_fission(node, self.tree)
+
+        def visit_MapScope(self, node: tn.MapScope):
+            return loop_fission(node, self.tree)
+        
+        def visit(self, node: tn.ScheduleTreeNode):
+            node = self.generic_visit(node)
+            if isinstance(node, (tn.IfScope, tn.ForScope, tn.MapScope)):
+                return super().visit(node)
+            return node
+
+    return FissionScopes(node).visit(node)
+
+
+def validate(node: tn.ScheduleTreeNode) -> bool:
+
+    if isinstance(node, tn.ScheduleTreeScope):
+        if any(child.parent is not node for child in node.children):
+            return False
+        if all(validate(child) for child in node.children):
+            return True
+        else:
+            return False
+        
+    return True
