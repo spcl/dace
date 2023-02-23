fe08d831b498edfb62a5348688b8e565ed4402c6
Marcin Copik <mcopik@gmail.com>
2023-02-23 13:50:49 +0100

    Remove codegen hack added to support CPU tasklets writing to GPU arrays

--- dace/codegen/targets/cpu.py
+++ dace/codegen/targets/cpu.py
@@ -969,31 +969,15 @@ class CPUCodeGen(TargetCodeGenerator):
                             write_expr = f"*({ptr_str} + {array_expr}) = {in_local_name};"
                         else:
 
-                            # FIXME: should we also support scalars for writing into GPU?
-                            from dace.codegen.targets.cuda import CUDACodeGen  # Avoid import loop
-
-                            if not CUDACodeGen._in_device_code and dst_is_gpu:
-                                dispatcher.dispatch_copy(
-                                    node,
-                                    dst_node,
-                                    edge,
-                                    sdfg,
-                                    dfg,
-                                    state_id,
-                                    function_stream,
-                                    result,
-                                )
-                            else:
-                                # FIXME: why are generating here the array expression for GPU kernel?
-                                # this method is also called from cuda.py through the dispatch_subgraph method.
-                                # should this not be within CUDA?
-                                desc_dtype = desc.dtype
-                                expr = cpp.cpp_array_expr(sdfg, memlet, codegen=self._frame)
-                                write_expr = codegen.make_ptr_assignment(in_local_name, conntype, expr, desc_dtype)
+                            # FIXME: why are generating here the array expression for GPU kernel?
+                            # this method is also called from cuda.py through the dispatch_subgraph method.
+                            # should this not be within CUDA?
+                            desc_dtype = desc.dtype
+                            expr = cpp.cpp_array_expr(sdfg, memlet, codegen=self._frame)
+                            write_expr = codegen.make_ptr_assignment(in_local_name, conntype, expr, desc_dtype)
 
                     # Write out
-                    if write_expr is not None:
-                        result.write(write_expr, sdfg, state_id, node)
+                    result.write(write_expr, sdfg, state_id, node)
 
             # Dispatch array-to-array outgoing copies here
             elif isinstance(node, nodes.AccessNode):
@@ -1190,56 +1174,37 @@ class CPUCodeGen(TargetCodeGenerator):
             # that dispatches to the appropriate backend.
             from dace.codegen.targets import cuda
             # Handle CPU code reading data from GPU memory.
-            if cuda.is_gpu_data(desc) and not cuda.CUDACodeGen._in_device_code:
+            # FIXME: what is output in this context? Unclear, missing documentation.
+            if output:
+                if is_pointer and var_type == DefinedType.ArrayInterface:
+                    result += "{} {} = {};".format(memlet_type, local_name, expr)
+                elif not memlet.dynamic or (memlet.dynamic and memlet.wcr is not None):
+                    # Dynamic WCR memlets start uninitialized
+                    result += "{} {};".format(memlet_type, local_name)
+                    defined = DefinedType.Scalar
 
-                # Dynamic memlet does not play a role here - we always need to read from the GPU.
-
-                # First define the scalar.
-                if not is_scalar:
-                    raise NotImplementedError('Cannot load a non-scalar from GPU array')
-                result += f'{memlet_type} {local_name};\n'
-
-                # FIXME: this should be in the CUDA codegen
-                # FIXME: add support for hip (different backend)
-                # FIXME: we should synchronize here, but we don't know the stream
-                result += f'cudaMemcpy(&{local_name},'\
-                    f'&{expr},'\
-                    f'sizeof({desc.dtype}),'\
-                    'cudaMemcpyDeviceToHost);'
-
-            # Handle CPU code reading data from CPU memory and GPU code reading data from GPU memory.
             else:
-                # FIXME: what is output in this context? Unclear, missing documentation.
-                if output:
-                    if is_pointer and var_type == DefinedType.ArrayInterface:
+                if not memlet.dynamic:
+                    if is_scalar:
+                        # We can pre-read the value
                         result += "{} {} = {};".format(memlet_type, local_name, expr)
-                    elif not memlet.dynamic or (memlet.dynamic and memlet.wcr is not None):
-                        # Dynamic WCR memlets start uninitialized
-                        result += "{} {};".format(memlet_type, local_name)
-                        defined = DefinedType.Scalar
-
-                else:
-                    if not memlet.dynamic:
-                        if is_scalar:
-                            # We can pre-read the value
-                            result += "{} {} = {};".format(memlet_type, local_name, expr)
-                        else:
-                            # constexpr arrays
-                            if memlet.data in self._frame.symbols_and_constants(sdfg):
-                                result += "const {} {} = {};".format(memlet_type, local_name, expr)
-                            else:
-                                # Pointer reference
-                                result += "{} {} = {};".format(ctypedef, local_name, expr)
                     else:
-                        # Variable number of reads: get a const reference that can
-                        # be read if necessary
-                        memlet_type = 'const %s' % memlet_type
-                        if is_pointer:
-                            # This is done to make the reference constant, otherwise
-                            # compilers error out with initial reference value.
-                            memlet_type += ' const'
-                        result += "{} &{} = {};".format(memlet_type, local_name, expr)
-                    defined = (DefinedType.Scalar if is_scalar else DefinedType.Pointer)
+                        # constexpr arrays
+                        if memlet.data in self._frame.symbols_and_constants(sdfg):
+                            result += "const {} {} = {};".format(memlet_type, local_name, expr)
+                        else:
+                            # Pointer reference
+                            result += "{} {} = {};".format(ctypedef, local_name, expr)
+                else:
+                    # Variable number of reads: get a const reference that can
+                    # be read if necessary
+                    memlet_type = 'const %s' % memlet_type
+                    if is_pointer:
+                        # This is done to make the reference constant, otherwise
+                        # compilers error out with initial reference value.
+                        memlet_type += ' const'
+                    result += "{} &{} = {};".format(memlet_type, local_name, expr)
+                defined = (DefinedType.Scalar if is_scalar else DefinedType.Pointer)
         elif var_type in [DefinedType.Stream, DefinedType.StreamArray]:
             if not memlet.dynamic and memlet.num_accesses == 1:
                 if not output:
