37d8398b891bfae919264fdac177fa3613b91ea5
Philipp Schaad <schaad.phil@gmail.com>
2023-03-02 11:51:10 +0100

    Add test

--- tests/passes/scalar_write_shadow_scopes_analysis_test.py
+++ tests/passes/scalar_write_shadow_scopes_analysis_test.py
@@ -101,6 +101,7 @@ def test_scalar_write_shadow_split():
     assert results[0]['B'][None] == set([(loop_1_1, b1_read), (loop_1_2, loop1_read_b), (loop_2_1, b2_read),
                                          (loop_2_2, loop2_read_b)])
 
+
 def test_scalar_write_shadow_fused():
     """
     Test the scalar write shadow scopes pass with writes dominating reads in the same state.
@@ -180,9 +181,10 @@ def test_scalar_write_shadow_fused():
     assert results[0]['A'][None] == set([(loop_1, a1_read), (loop_2, a2_read)])
     assert results[0]['B'][None] == set([(loop_1, b1_read), (loop_2, b2_read)])
 
-def test_scalar_write_shadow_interstate():
+
+def test_scalar_write_shadow_interstate_self():
     """
-    Tests the scalar write shadow pass with interstate edge reads being shadowed.
+    Tests the scalar write shadow pass with interstate edge reads being shadowed by the state they're originating from.
     """
     # Construct the SDFG.
     sdfg = dace.SDFG('scalar_isedge')
@@ -275,7 +277,109 @@ def test_scalar_write_shadow_interstate():
     assert results[0]['B'][None] == set([(loop_1_1, b1_read), (loop_1_2, loop1_read_b), (loop_2_1, b2_read),
                                          (loop_2_2, loop2_read_b)])
 
+
+def test_scalar_write_shadow_interstate_pred():
+    """
+    Tests the scalar write shadow pass with interstate edge reads being shadowed by a predecessor state.
+    """
+    # Construct the SDFG.
+    sdfg = dace.SDFG('scalar_isedge')
+
+    N = dace.symbol('N')
+    sdfg.add_array('A', [N], dace.int32)
+    sdfg.add_array('B', [N], dace.int32)
+    sdfg.add_array('tmp', [1], dace.int32, transient=True)
+
+    init_state = sdfg.add_state('init')
+    guard_1 = sdfg.add_state('guard_1')
+    loop_1_1 = sdfg.add_state('loop_1_1')
+    loop_1_2 = sdfg.add_state('loop_1_2')
+    loop_1_3 = sdfg.add_state('loop_1_3')
+    intermediate = sdfg.add_state('intermediate')
+    guard_2 = sdfg.add_state('guard_2')
+    loop_2_1 = sdfg.add_state('loop_2_1')
+    loop_2_2 = sdfg.add_state('loop_2_2')
+    loop_2_3 = sdfg.add_state('loop_2_3')
+    end_state = sdfg.add_state('end')
+
+    init_tasklet = init_state.add_tasklet('init', {}, {'out'}, 'out = 0')
+    init_write = init_state.add_write('tmp')
+    init_state.add_edge(init_tasklet, 'out', init_write, None, dace.Memlet('tmp[0]'))
+
+    tmp1_tasklet = loop_1_1.add_tasklet('tmp1', {'a', 'b'}, {'out'}, 'out = a * b')
+    tmp1_write = loop_1_1.add_write('tmp')
+    a1_read = loop_1_1.add_read('A')
+    b1_read = loop_1_1.add_read('B')
+    loop_1_1.add_edge(a1_read, None, tmp1_tasklet, 'a', dace.Memlet('A[i]'))
+    loop_1_1.add_edge(b1_read, None, tmp1_tasklet, 'b', dace.Memlet('B[i]'))
+    loop_1_1.add_edge(tmp1_tasklet, 'out', tmp1_write, None, dace.Memlet('tmp[0]'))
+
+    loop1_tasklet_1 = loop_1_3.add_tasklet('loop1_1', {'ap', 't'}, {'a'}, 'a = ap + 2 * t')
+    loop1_tasklet_2 = loop_1_3.add_tasklet('loop1_2', {'bp', 't'}, {'b'}, 'b = bp - 2 * t')
+    loop1_read_tmp = loop_1_3.add_read('tmp')
+    loop1_read_a = loop_1_3.add_read('A')
+    loop1_read_b = loop_1_3.add_read('B')
+    loop1_write_a = loop_1_3.add_write('A')
+    loop1_write_b = loop_1_3.add_write('B')
+    loop_1_3.add_edge(loop1_read_tmp, None, loop1_tasklet_1, 't', dace.Memlet('tmp[0]'))
+    loop_1_3.add_edge(loop1_read_tmp, None, loop1_tasklet_2, 't', dace.Memlet('tmp[0]'))
+    loop_1_3.add_edge(loop1_read_a, None, loop1_tasklet_1, 'ap', dace.Memlet('A[i + 1]'))
+    loop_1_3.add_edge(loop1_read_b, None, loop1_tasklet_2, 'bp', dace.Memlet('B[i + 1]'))
+    loop_1_3.add_edge(loop1_tasklet_1, 'a', loop1_write_a, None, dace.Memlet('A[i]'))
+    loop_1_3.add_edge(loop1_tasklet_2, 'b', loop1_write_b, None, dace.Memlet('B[i]'))
+
+    tmp2_tasklet = loop_2_1.add_tasklet('tmp2', {'a', 'b'}, {'out'}, 'out = a / b')
+    tmp2_write = loop_2_1.add_write('tmp')
+    a2_read = loop_2_1.add_read('A')
+    b2_read = loop_2_1.add_read('B')
+    loop_2_1.add_edge(a2_read, None, tmp2_tasklet, 'a', dace.Memlet('A[i + 1]'))
+    loop_2_1.add_edge(b2_read, None, tmp2_tasklet, 'b', dace.Memlet('B[i + 1]'))
+    loop_2_1.add_edge(tmp2_tasklet, 'out', tmp2_write, None, dace.Memlet('tmp[0]'))
+
+    loop2_tasklet_1 = loop_2_3.add_tasklet('loop2_1', {'ap', 't'}, {'a'}, 'a = ap + t * t')
+    loop2_tasklet_2 = loop_2_3.add_tasklet('loop2_2', {'bp', 't'}, {'b'}, 'b = bp - t * t')
+    loop2_read_tmp = loop_2_3.add_read('tmp')
+    loop2_read_a = loop_2_3.add_read('A')
+    loop2_read_b = loop_2_3.add_read('B')
+    loop2_write_a = loop_2_3.add_write('A')
+    loop2_write_b = loop_2_3.add_write('B')
+    loop_2_3.add_edge(loop2_read_tmp, None, loop2_tasklet_1, 't', dace.Memlet('tmp[0]'))
+    loop_2_3.add_edge(loop2_read_tmp, None, loop2_tasklet_2, 't', dace.Memlet('tmp[0]'))
+    loop_2_3.add_edge(loop2_read_a, None, loop2_tasklet_1, 'ap', dace.Memlet('A[i]'))
+    loop_2_3.add_edge(loop2_read_b, None, loop2_tasklet_2, 'bp', dace.Memlet('B[i]'))
+    loop_2_3.add_edge(loop2_tasklet_1, 'a', loop2_write_a, None, dace.Memlet('A[i + 1]'))
+    loop_2_3.add_edge(loop2_tasklet_2, 'b', loop2_write_b, None, dace.Memlet('B[i + 1]'))
+
+    sdfg.add_edge(init_state, guard_1, dace.InterstateEdge(assignments={'i': 0}))
+    sdfg.add_edge(guard_1, loop_1_1, dace.InterstateEdge(condition='i < (N - 1)'))
+    sdfg.add_edge(loop_1_1, loop_1_2, dace.InterstateEdge())
+    tmp1_edge = dace.InterstateEdge(assignments={'j': 'tmp'})
+    sdfg.add_edge(loop_1_2, loop_1_3, tmp1_edge)
+    sdfg.add_edge(loop_1_3, guard_1, dace.InterstateEdge(assignments={'i': 'i + 1'}))
+    sdfg.add_edge(guard_1, intermediate, dace.InterstateEdge(condition='i >= (N - 1)'))
+
+    sdfg.add_edge(intermediate, guard_2, dace.InterstateEdge(assignments={'i': 0}))
+    sdfg.add_edge(guard_2, loop_2_1, dace.InterstateEdge(condition='i < (N - 1)'))
+    sdfg.add_edge(loop_2_1, loop_2_2, dace.InterstateEdge())
+    tmp2_edge = dace.InterstateEdge(assignments={'j': 'tmp'})
+    sdfg.add_edge(loop_2_2, loop_2_3, tmp2_edge)
+    sdfg.add_edge(loop_2_3, guard_2, dace.InterstateEdge(assignments={'i': 'i + 1'}))
+    sdfg.add_edge(guard_2, end_state, dace.InterstateEdge(condition='i >= (N - 1)'))
+
+    # Test the pass.
+    pipeline = Pipeline([ScalarWriteShadowScopes()])
+    results = pipeline.apply_pass(sdfg, {})[ScalarWriteShadowScopes.__name__]
+
+    assert results[0]['tmp'][(loop_1_1, tmp1_write)] == set([(loop_1_3, loop1_read_tmp), (loop_1_2, tmp1_edge)])
+    assert results[0]['tmp'][(loop_2_1, tmp2_write)] == set([(loop_2_3, loop2_read_tmp), (loop_2_2, tmp2_edge)])
+    assert results[0]['A'][None] == set([(loop_1_1, a1_read), (loop_1_3, loop1_read_a), (loop_2_1, a2_read),
+                                         (loop_2_3, loop2_read_a)])
+    assert results[0]['B'][None] == set([(loop_1_1, b1_read), (loop_1_3, loop1_read_b), (loop_2_1, b2_read),
+                                         (loop_2_3, loop2_read_b)])
+
+
 if __name__ == '__main__':
     test_scalar_write_shadow_split()
     test_scalar_write_shadow_fused()
-    test_scalar_write_shadow_interstate()
+    test_scalar_write_shadow_interstate_self()
+    test_scalar_write_shadow_interstate_pred()
