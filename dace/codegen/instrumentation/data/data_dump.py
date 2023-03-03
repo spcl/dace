ba25b2b1379d5b0aa2112de4528bbc62e2a8f5cb
Philipp Schaad <schaad.phil@gmail.com>
2023-03-03 12:03:52 +0100

    Conditional Data Instrumentation (#1202)

--- dace/codegen/instrumentation/data/data_dump.py
+++ dace/codegen/instrumentation/data/data_dump.py
@@ -6,9 +6,12 @@ from dace.codegen.instrumentation.provider import InstrumentationProvider
 from dace.sdfg.scope import is_devicelevel_fpga
 from dace.sdfg.state import SDFGState
 from dace.codegen import common
+from dace.codegen import cppunparse
 from dace.codegen.targets import cpp
+from dace.properties import CodeBlock
 import os
-from typing import Tuple, TYPE_CHECKING
+import warnings
+from typing import Tuple, TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from dace.codegen.targets.framecode import DaCeCodeGenerator
@@ -106,6 +109,20 @@ class SaveProvider(InstrumentationProvider, DataInstrumentationProviderMixin):
             # Only run on host code
             return
 
+        condition_preamble, condition_postamble = '', ''
+        condition: Optional[CodeBlock] = node.instrument_condition
+        if condition is not None and not condition.as_string == '1':
+            cond_string = None
+            if condition.language == dtypes.Language.CPP:
+                cond_string = condition.as_string
+            elif condition.language == dtypes.Language.Python:
+                cond_string = cppunparse.py2cpp(condition.code[0], expr_semicolon=False)
+            else:
+                warnings.warn('Unrecognized language %s in codeblock' % condition.language)
+                cond_string = condition.as_string
+            condition_preamble = f'if ({cond_string})' + ' {'
+            condition_postamble = '}'
+
         desc = node.desc(sdfg)
 
         # Obtain a pointer for arrays and scalars
@@ -130,11 +147,13 @@ class SaveProvider(InstrumentationProvider, DataInstrumentationProviderMixin):
         strides = ', '.join(cpp.sym2cpp(s) for s in desc.strides)
 
         # Write code
+        inner_stream.write(condition_preamble, sdfg, state_id, node_id)
         inner_stream.write(preamble, sdfg, state_id, node_id)
         inner_stream.write(
             f'__state->serializer->save({ptrname}, {cpp.sym2cpp(desc.total_size - desc.start_offset)}, '
             f'"{node.data}", "{uuid}", {shape}, {strides});\n', sdfg, state_id, node_id)
         inner_stream.write(postamble, sdfg, state_id, node_id)
+        inner_stream.write(condition_postamble, sdfg, state_id, node_id)
 
 
 @registry.autoregister_params(type=dtypes.DataInstrumentationType.Restore)
@@ -178,6 +197,20 @@ class RestoreProvider(InstrumentationProvider, DataInstrumentationProviderMixin)
             # Only run on host code
             return
 
+        condition_preamble, condition_postamble = '', ''
+        condition: Optional[CodeBlock] = node.instrument_condition
+        if condition is not None and not condition.as_string == '1':
+            cond_string = None
+            if condition.language == dtypes.Language.CPP:
+                cond_string = condition.as_string
+            elif condition.language == dtypes.Language.Python:
+                cond_string = cppunparse.py2cpp(condition.code[0], expr_semicolon=False)
+            else:
+                warnings.warn('Unrecognized language %s in codeblock' % condition.language)
+                cond_string = condition.as_string
+            condition_preamble = f'if ({cond_string})' + ' {'
+            condition_postamble = '}'
+
         desc = node.desc(sdfg)
 
         # Obtain a pointer for arrays and scalars
@@ -198,8 +231,10 @@ class RestoreProvider(InstrumentationProvider, DataInstrumentationProviderMixin)
             preamble, postamble, ptrname = self._generate_copy_to_device(node, desc, ptrname)
 
         # Write code
+        inner_stream.write(condition_preamble, sdfg, state_id, node_id)
         inner_stream.write(preamble, sdfg, state_id, node_id)
         inner_stream.write(
             f'__state->serializer->restore({ptrname}, {cpp.sym2cpp(desc.total_size - desc.start_offset)}, '
             f'"{node.data}", "{uuid}");\n', sdfg, state_id, node_id)
         inner_stream.write(postamble, sdfg, state_id, node_id)
+        inner_stream.write(condition_postamble, sdfg, state_id, node_id)
