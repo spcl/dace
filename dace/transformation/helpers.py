d2acdc2f6ad816cc5c668c8372256e55d0b910fc
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-03-11 00:11:35 +0100

    Improved component creation for SingleState and MultiState constructs.

--- dace/transformation/helpers.py
+++ dace/transformation/helpers.py
@@ -311,8 +311,16 @@ def find_sdfg_control_flow(sdfg: SDFG) -> Dict[SDFGState, Set[SDFGState]]:
         if isinstance(child, cf.SingleState):
             if child.state in visited:
                 continue
-            components[child.state] = (set([child.state]), child)
-            visited[child.state] = child.state
+            in_edges = sdfg.in_edges(child.state)
+            if len(in_edges) == 1 and in_edges[0].data.assignments:
+                guard = sdfg.add_state_before(child.state, f"new_{child.state.label}")
+                edge = sdfg.edges_between(guard, child.state)[0]
+                edge.data.assignments = in_edges[0].data.assignments
+                in_edges[0].data.assignments = {}
+                components[guard] = (set([guard, child.state]), cf.MultiState(None, guard))
+            else:
+                components[child.state] = (set([child.state]), child)
+                visited[child.state] = child.state
         elif isinstance(child, (cf.ForScope, cf.WhileScope)):
             guard = child.guard
             fexit = None
@@ -401,18 +409,32 @@ def find_sdfg_control_flow(sdfg: SDFG) -> Dict[SDFGState, Set[SDFGState]]:
             visited.update({s: guard for s in states})
         else:
             raise ValueError(f"Unsupported control flow class {type(child)}")
+    
+    def sgraph_free_symbols(sgraph: graph.SubgraphView) -> Set[str]:
+        free_symbols = set()
+        for state in sgraph:
+            free_symbols |= state.free_symbols
+        for edge in sgraph.edges():
+            free_symbols |= edge.data.free_symbols
+        return set([str(s) for s in free_symbols])
 
     # Second pass to create MultiStates
     result = OrderedDict()
     guard = None
     states = set(sdfg.states())
-    for k, v in components.items():
-        if isinstance(v[1], cf.SingleState):
+    subgraph_fsymbols = []
+    for _, v in components.items():
+        subgraph_fsymbols.append(sgraph_free_symbols(graph.SubgraphView(sdfg, v[0])))
+    for i, (k, v) in enumerate(components.items()):
+        if isinstance(v[1], (cf.SingleState, cf.MultiState)):
             has_assign = False
-            for e in sdfg.in_edges(k):
-                if e.data.assignments:
-                    has_assign = True
-                    break
+            for st in v[0]:
+                for e in sdfg.in_edges(k):
+                    if e.data.assignments:
+                        for j in range(i + 1, len(components)):
+                            if any(k in subgraph_fsymbols[j] for k in e.data.assignments.keys()):
+                                has_assign = True
+                                break
             if has_assign:
                 guard = k
                 break
