df5dbb830e671e5272ad11d6b615c3c70b164334
Philipp Schaad <schaad.phil@gmail.com>
2023-03-11 15:15:26 +0100

    Symbol write scope analysis pass

--- dace/transformation/passes/analysis.py
+++ dace/transformation/passes/analysis.py
@@ -10,7 +10,7 @@ from networkx.algorithms import shortest_paths as nxsp
 
 WriteScopeDict = Dict[str, Dict[Optional[Tuple[SDFGState, nd.AccessNode]],
                                 Set[Tuple[SDFGState, Union[nd.AccessNode, InterstateEdge]]]]]
-
+SymbolScopeDict = Dict[str, Dict[InterstateEdge, Set[Union[InterstateEdge, SDFGState]]]]
 
 @properties.make_properties
 class StateReachability(ppl.Pass):
@@ -38,6 +38,41 @@ class StateReachability(ppl.Pass):
             for state in sdfg.nodes():
                 reachable[sdfg.sdfg_id][state] = set(tc.successors(state))
         return reachable
+
+
+@properties.make_properties
+class SymbolAccessSets(ppl.Pass):
+    """
+    Evaluates symbol access sets (which symbols are read/written in each state or interstate edge).
+    """
+
+    CATEGORY: str = 'Analysis'
+
+    def modifies(self) -> ppl.Modifies:
+        return ppl.Modifies.Nothing
+
+    def should_reapply(self, modified: ppl.Modifies) -> bool:
+        # If anything was modified, reapply
+        return modified & ppl.Modifies.States | ppl.Modifies.Edges | ppl.Modifies.Symbols | ppl.Modifies.Nodes
+
+    def apply_pass(self,
+                   top_sdfg: SDFG, _) -> Dict[int, Dict[Union[SDFGState, InterstateEdge], Tuple[Set[str], Set[str]]]]:
+        """
+        :return: A dictionary mapping each state to a tuple of its (read, written) data descriptors.
+        """
+        top_result: Dict[int, Dict[SDFGState, Tuple[Set[str], Set[str]]]] = {}
+        for sdfg in top_sdfg.all_sdfgs_recursive():
+            result: Dict[SDFGState, Tuple[Set[str], Set[str]]] = {}
+            for state in sdfg.nodes():
+                readset = set(state.defined_symbols().keys())
+                # No symbols may be written to inside states.
+                result[state] = (readset, set())
+                for oedge in sdfg.out_edges(state):
+                    edge_readset = oedge.data.read_symbols()
+                    edge_writeset = set(oedge.data.assignments.keys())
+                    result[oedge.data] = (edge_readset, edge_writeset)
+            top_result[sdfg.sdfg_id] = result
+        return top_result
 
 
 @properties.make_properties
@@ -155,6 +190,78 @@ class FindAccessNodes(ppl.Pass):
                     if state.out_degree(anode) > 0:
                         result[anode.data][state][0].add(anode)
             top_result[sdfg.sdfg_id] = result
+        return top_result
+
+
+@properties.make_properties
+class SymbolWriteScopes(ppl.Pass):
+    """
+    For each symbol, create a dictionary mapping each writing interstate edge to that symbol to the set of interstate
+    edges and states reading that symbol that are dominated by that write.
+    """
+
+    CATEGORY: str = 'Analysis'
+
+    def modifies(self) -> ppl.Modifies:
+        return ppl.Modifies.Nothing
+
+    def should_reapply(self, modified: ppl.Modifies) -> bool:
+        # If anything was modified, reapply
+        return modified & ppl.Modifies.Symbols | ppl.Modifies.States | ppl.Modifies.Edges | ppl.Modifies.Nodes
+
+    def depends_on(self):
+        return {SymbolAccessSets}
+
+    def _find_dominating_write(
+            self, sym: str, read: Union[SDFGState, InterstateEdge], state_idom: Dict[SDFGState, SDFGState],
+            access_sets: Dict[Union[SDFGState, InterstateEdge], Tuple[Set[str], Set[str]]]
+    ) -> Optional[InterstateEdge]:
+        last_state: SDFGState = read if isinstance(read, SDFGState) else read.src
+
+        in_edges = last_state.parent.in_edges(last_state)
+        deg = len(in_edges)
+        if deg == 0:
+            return None
+        elif deg == 1 and any([sym == k for k, _ in in_edges[0].data.assignments.keys()]):
+            return in_edges[0].data
+
+        write_isedge = None
+        n_state = state_idom[last_state] if state_idom[last_state] != last_state else None
+        while n_state is not None and write_isedge is None:
+            oedges = n_state.parent.out_edges(n_state)
+            odeg = len(oedges)
+            if odeg == 1:
+                if any([sym == k for k, _ in oedges[0].data.assignments.keys()]):
+                    write_isedge = oedges[0].data
+            else:
+                dom_edge = None
+                for cand in oedges:
+                    if any([sym == k for k, _ in cand.data.assignments.keys()]):
+                        if nxsp.has_path(n_state.parent.nx, cand.dst, last_state):
+                            if dom_edge is None:
+                                dom_edge = cand.data
+                            else:
+                                dom_edge = None
+                                break
+                write_isedge = dom_edge
+            n_state = state_idom[n_state] if state_idom[n_state] != n_state else None
+        return write_isedge
+
+    def apply_pass(self, sdfg: SDFG, pipeline_results: Dict[str, Any]) -> Dict[int, SymbolScopeDict]:
+        top_result: Dict[int, SymbolScopeDict] = dict()
+
+        for sdfg in sdfg.all_sdfgs_recursive():
+            result: SymbolScopeDict = defaultdict(lambda: defaultdict(lambda: set()))
+            symbol_access_sets: Dict[Union[SDFGState, InterstateEdge], Tuple[Set[str], Set[str]]] = pipeline_results[
+                SymbolAccessSets.__name__
+            ][sdfg.sdfg_id]
+            idom = nx.immediate_dominators(sdfg.nx, sdfg.start_state)
+            for read_loc, (reads, _) in symbol_access_sets.keys():
+                for sym in reads:
+                    dominating_write = self._find_dominating_write(sym, read_loc, idom, symbol_access_sets)
+                    result[sym][dominating_write].add(read_loc)
+            top_result[sdfg.sdfg_id] = result
+
         return top_result
 
 
