a8e5b9216bedaeb74e99235e601229b2cf4c6980
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-03-29 21:18:16 +0200

    WIP fissioning full cloudsc

--- tests/fortran/cloudsc/cloudscexp2_full.py
+++ tests/fortran/cloudsc/cloudscexp2_full.py
@@ -24,6 +24,124 @@ from dace.transformation.subgraph import helpers as xfsh
 from dace.transformation import helpers as xfh
 
 
+from dace.transformation.dataflow import MapFission, RemoveIntermediateWrite
+from dace.transformation.interstate import InlineSDFG
+from dace.transformation.interstate.move_loop_into_map import MoveMapIntoLoop, MoveMapIntoIf
+from typing import List, Set, Tuple
+
+
+def find_defined_symbols(sdfg: dace.SDFG) -> Set[str]:
+    return sdfg.symbols.keys() - sdfg.free_symbols
+
+
+def fix_sdfg_symbols(sdfg: dace.SDFG):
+    for state in sdfg.nodes():
+        for node in state.nodes():
+            if isinstance(node, dace.nodes.NestedSDFG):
+                fix_sdfg_symbols(node.sdfg)
+    for s in find_defined_symbols(sdfg):
+        del sdfg.symbols[s]
+        if s in sdfg.parent_nsdfg_node.symbol_mapping:
+            del sdfg.parent_nsdfg_node.symbol_mapping[s]
+
+
+def fix_arrays(sdfg: dace.SDFG):
+    repl_dict = {'_for_it_0': 1}
+    for sd in sdfg.all_sdfgs_recursive():
+        sd.replace_dict(repl_dict, replace_in_graph=False, replace_keys=False)
+
+
+def fix_sdfg_parents(sdfg: dace.SDFG):
+    for sd in sdfg.all_sdfgs_recursive():
+        if sd.parent is not None:
+            if sd.parent.parent != sd.parent_sdfg:
+                print(f"Fixing parent of {sd.label}")
+                sd.parent_sdfg = sd.parent.parent
+
+
+def find_toplevel_maps(sdfg: dace.SDFG) -> List[Tuple[dace.SDFG, dace.SDFGState, dace.nodes.MapEntry]]:
+    map_entries = []
+    for state in sdfg.nodes():
+        scope_dict = state.scope_dict()
+        for node in state.nodes():
+            if isinstance(node, dace.nodes.MapEntry) and scope_dict[node] is None:
+                map_entries.append((sdfg, state, node))
+            elif isinstance(node, dace.nodes.NestedSDFG) and scope_dict[node] is None:
+                map_entries.extend(find_toplevel_maps(node.sdfg))
+    return map_entries
+
+
+def count_sdfg_transient_memory(sdfg: dace.SDFG) -> int:
+    memory = 0
+    # for _, desc in sdfg.arrays.items():
+    #     if desc.transient:
+    #         memory += desc.total_size
+    for sd in sdfg.all_sdfgs_recursive():
+        for _, desc in sd.arrays.items():
+            if desc.transient:
+                memory += desc.total_size
+    return memory
+
+
+def count_map_transient_memory(sdfg: dace.SDFG, state: dace.SDFGState, map_entry: dace.nodes.MapEntry) -> int:
+    memory = 0
+    scope_children = state.scope_children()[map_entry]
+    for node in scope_children:
+        # TODO: This is too broad.
+        if isinstance(node, dace.nodes.AccessNode) and node.desc(sdfg).transient:
+            memory += sdfg.arrays[node.data].total_size
+        elif isinstance(node, dace.nodes.MapEntry):
+            memory += count_map_transient_memory(sdfg, state, node)
+        elif isinstance(node, dace.nodes.NestedSDFG):
+            memory += count_sdfg_transient_memory(node.sdfg)
+    return memory
+
+
+def fission_sdfg(sdfg: dace.SDFG, name: str = None, iteration: int = 0) -> int:
+
+    if name:
+        name = f"{name}_"
+    else:
+        name = ""
+    top_level_maps = find_toplevel_maps(sdfg)
+    count = 0
+    for sd, state, map_entry in top_level_maps:
+        mem = count_map_transient_memory(sd, state, map_entry)
+        if dace.symbolic.issymbolic(mem) or mem > 10:
+            print(f"[{sd.label}, {state}, {map_entry}]: {mem} {'(TO FISSION)' if dace.symbolic.issymbolic(mem) else ''}")
+            scope_children = state.scope_children()[map_entry]
+            if len(scope_children) == 2 and isinstance(scope_children[0], dace.nodes.NestedSDFG):
+                try:
+                    MapFission.apply_to(sd, expr_index=1, map_entry=map_entry, nested_sdfg=scope_children[0])
+                except ValueError:
+                    try:
+                        MoveMapIntoLoop.apply_to(sd, map_entry=map_entry, nested_sdfg=scope_children[0], map_exit=scope_children[1])
+                    except ValueError:
+                        try:
+                            MoveMapIntoIf.apply_to(sd, map_entry=map_entry, nested_sdfg=scope_children[0], map_exit=scope_children[1])
+                        except ValueError:
+                            print("Map cannot be moved into loop")
+                            continue
+            else:
+                try:
+                    MapFission.apply_to(sd, expr_index=0, map_entry=map_entry)
+                except ValueError:
+                    print("Map cannot be fissioned (expr_index=0)")
+                    continue
+            count += 1
+            sdfg.save(f'{name}interim_step_{iteration}.sdfg')
+            # sdfg.validate()
+            sdfg.simplify()
+            sdfg.apply_transformations_repeated(RemoveIntermediateWrite)
+            sd.apply_transformations_repeated(MapCollapse)
+            sdfg.simplify()
+            sdfg.apply_transformations_repeated(RemoveIntermediateWrite)
+            fix_sdfg_symbols(sdfg)
+            fix_arrays(sdfg)
+            sdfg.save(f'{name}fission_step_{iteration}.sdfg')
+    return count
+
+
 def read_source(filename: str, extension: str = 'f90') -> str:
     source = None
     with open(os.path.join(os.path.dirname(__file__), f'{filename}.{extension}'), 'r') as file:
@@ -72,6 +190,23 @@ def get_sdfg(source: str, program_name: str, normalize_offsets: bool = False) ->
 
     # if normalize_offsets:
     #     utils.normalize_offsets(sdfg)
+
+    for sd in sdfg.all_sdfgs_recursive():
+        sd.replace_dict({'KLON': 'NPROMA'})
+        if sd.parent_nsdfg_node is not None:
+            if 'KLON' in sd.parent_nsdfg_node.symbol_mapping:
+                del sd.parent_nsdfg_node.symbol_mapping['KLON']
+    for sd in sdfg.all_sdfgs_recursive():
+        assert 'KLON' not in sd.symbols
+        assert 'KLON' not in (str(s) for s in sd.free_symbols)
+        for state in sd.states():
+            for node in state.nodes():
+                if isinstance(node, dace.nodes.Tasklet):
+                    assert 'KLON' not in node.code.as_string
+                elif isinstance(node, dace.nodes.NestedSDFG):
+                    for s, m in node.symbol_mapping.items():
+                        assert 'KLON' not in str(s)
+                        assert 'KLON' not in (str(s) for s in m.free_symbols)
     
     for sd in sdfg.all_sdfgs_recursive():
         promoted = ScalarToSymbolPromotion().apply_pass(sd, {})
@@ -105,6 +240,26 @@ def validate_sdfg(sdfg, inputs, outputs, outputs_f) -> bool:
 
 
 def debug_auto_optimize(sdfg: dace.SDFG, inputs, outputs, ref_outputs):
+
+    auto_optimize(sdfg, dace.DeviceType.Generic)
+
+    if not validate_sdfg(sdfg, inputs, outputs, ref_outputs):
+        return
+
+    fix_sdfg_symbols(sdfg)
+    count = 1
+    iteration = 0
+    while count > 0:
+        count = fission_sdfg(sdfg, sdfg.name, iteration)
+        print(f"Fissioned {count} maps")
+        if not validate_sdfg(sdfg, inputs, outputs, ref_outputs):
+            return
+        iteration += 1
+    sdfg.simplify()
+    sdfg.save('CLOUDSC_final.sdfg')
+
+    return validate_sdfg(sdfg, inputs, outputs, ref_outputs)
+
 
     device = dace.DeviceType.Generic
     validate = False
@@ -258,7 +413,7 @@ parameters = {
     'NCLDDIAG': 1,
     'NAERCLD': 1,
 
-    'NBLOCKS': 1,
+    'NBLOCKS': 4,
     # 'NBLOCKS': 512,
     # 'LDMAINCALL': np.bool_(True),  # boolean (LOGICAL)
     # 'LDSLPHY': np.bool_(True),  # boolean (LOGICAL),
@@ -282,10 +437,10 @@ parameters = {
     'LCLDBUDGET': np.int32(np.bool_(True)),  # boolean (LOGICAL)
     'NGPBLKS': 10,
     'NUMOMP': 10,
-    'NGPTOT': 4*1,
+    'NGPTOT': 4*4,
     # 'NGPTOT': 65536,
-    'NGPTOTG': 4*1,
-    'NGPTOTG': 65536,
+    'NGPTOTG': 4*4,
+    # 'NGPTOTG': 65536,
     'NPROMA': 4,
     # 'NPROMA': 128,
     'NBETA': 1,
@@ -691,12 +846,31 @@ def test_program(program: str, device: dace.DeviceType, normalize_offsets: bool)
     fsource = read_source(program)
     program_name, routine_name = programs[program]
     ffunc = get_fortran(fsource, program_name, routine_name)
-    sdfg = get_sdfg(fsource, program_name, normalize_offsets)
-    if device == dace.DeviceType.GPU:
-        auto_optimize(sdfg, device)
-    # sdfg.simplify()
-    # utils.make_dynamic_map_inputs_unique(sdfg)
+    # sdfg = get_sdfg(fsource, program_name, normalize_offsets)
+    # if device == dace.DeviceType.GPU:
+    #     auto_optimize(sdfg, device)
+    # # sdfg.simplify()
+    # # utils.make_dynamic_map_inputs_unique(sdfg)
     # auto_optimize(sdfg, dace.DeviceType.Generic)
+    # sdfg.save('CLOUDSCOUTER_autoopt.sdfg')
+
+    # sdfg = dace.SDFG.from_file('CLOUDSCOUTER_autoopt.sdfg')
+    # fix_sdfg_symbols(sdfg)
+    sdfg = dace.SDFG.from_file('CLOUDSCOUTER_fission_step_6.sdfg')
+    for sd in sdfg.all_sdfgs_recursive():
+        sd.openmp_sections = False
+    count = 1
+    iteration = 7
+    while count > 0:
+        count = fission_sdfg(sdfg, sdfg.name, iteration)
+        print(f"Fissioned {count} maps")
+        iteration += 1
+        sdfg.compile()
+    sdfg.simplify()
+
+    for sd, state, map_entry in find_toplevel_maps(sdfg):
+        print(f"[{sd.label}, {state}, {map_entry}]: {count_map_transient_memory(sd, state, map_entry)}")
+
 
     rng = np.random.default_rng(42)
     inputs = get_inputs(program, rng)
@@ -706,20 +880,27 @@ def test_program(program: str, device: dace.DeviceType, normalize_offsets: bool)
     print("Running Fortran ...")
     ffunc(**{k.lower(): v for k, v in inputs.items()}, **{k.lower(): v for k, v in outputs_f.items()})
     print("Running DaCe ...")
-    # sdfg(**inputs, **outputs_d)
+    # sdfg = dace.SDFG.from_file('CLOUDSCOUTER_fission_step_0.sdfg')
+    # sdfg = dace.SDFG.from_file('CLOUDSCOUTER_interim_step_1.sdfg')
+    # sdfg.name = 'CLOUDSCOUTER_interim_step_1'
+    # for sd in sdfg.all_sdfgs_recursive():
+    #     sd.openmp_sections = False
+    # fission_sdfg(sdfg, 'CLOUDSCOUTER', 1)
+    # sdfg.name = 'CLOUDSCOUTER_fission_step_1'
+    sdfg(**inputs, **outputs_d)
 
-    # for k in outputs_f.keys():
-    #     farr = outputs_f[k]
-    #     darr = outputs_d[k]
-    #     if np.allclose(farr, darr):
-    #         print(f"{k}: OK!")
-    #     else:
-    #         print(f"{k}: relative error is {np.linalg.norm(farr - darr) / np.linalg.norm(farr)}")
-    #     # assert np.allclose(farr, darr)
+    for k in outputs_f.keys():
+        farr = outputs_f[k]
+        darr = outputs_d[k]
+        if np.allclose(farr, darr):
+            print(f"{k}: OK!")
+        else:
+            print(f"{k}: relative error is {np.linalg.norm(farr - darr) / np.linalg.norm(farr)}")
+        # assert np.allclose(farr, darr)
     
     # # print(outputs_f['DEBUG_EPSILON'])
     # # print(outputs_d['DEBUG_EPSILON'])
-    debug_auto_optimize(sdfg, inputs, outputs_d, outputs_f)
+    # debug_auto_optimize(sdfg, inputs, outputs_d, outputs_f)
 
 
 if __name__ == "__main__":
