00ff22b9ea945d6bb8e76ba7a306c4d8499854d3
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-04-20 20:44:08 +0200

    Added test.

--- dace/transformation/dataflow/map_interchange.py
+++ dace/transformation/dataflow/map_interchange.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+# Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
 """ Implements the map interchange transformation. """
 
 from dace.sdfg import SDFG, SDFGState
