d0e7064b66b4c2010e0b1d46a6907f34c4074175
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-04-29 16:04:47 +0200

    Added tests.

--- tests/sdfg/forloop_test.py
+++ tests/sdfg/forloop_test.py
@@ -23,5 +23,41 @@ def test_simple_forloop():
     assert val[0] == (9 * 10) // 2
 
 
+def test_sdfg_state_add_forloop():
+
+    sdfg = dace.SDFG('simple_forloop_sdfg')
+    sdfg.add_array('a', [1], dace.int32)
+
+    state = sdfg.add_state('simple_forloop_state')
+    inp_acc = dace.nodes.AccessNode('a')
+    le, lx = state.add_forloop('simple_forloop', {'i': dace.subsets.Range([(0, 9, 1)])})
+    task = dace.nodes.Tasklet('simple_task', {'__inp'}, {'__out'}, '__out = __inp + i')
+    out_acc = dace.nodes.AccessNode('a')
+    state.add_memlet_path(inp_acc, le, task, memlet=dace.Memlet('a[0]'), dst_conn='__inp')
+    state.add_memlet_path(task, lx, out_acc, memlet=dace.Memlet('a[0]'), src_conn='__out')
+
+    val = np.array([0], dtype=np.int32)
+    sdfg(a=val)
+    assert val[0] == (9 * 10) // 2
+
+
+def test_sdfg_state_add_forlooped_tasklet():
+
+    sdfg = dace.SDFG('simple_forloop_sdfg')
+    sdfg.add_array('a', [1], dace.int32)
+
+    state = sdfg.add_state('simple_forloop_state')
+    state.add_forlooped_tasklet('simple_forloop', {'i': dace.subsets.Range([(0, 9, 1)])},
+                                {'__inp': dace.Memlet('a[0]')},
+                                '__out = __inp + i', {'__out': dace.Memlet('a[0]')},
+                                external_edges=True)
+
+    val = np.array([0], dtype=np.int32)
+    sdfg(a=val)
+    assert val[0] == (9 * 10) // 2
+
+
 if __name__ == '__main__':
     test_simple_forloop()
+    test_sdfg_state_add_forloop()
+    test_sdfg_state_add_forlooped_tasklet()
