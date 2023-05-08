a44652664349040e7d7feb38a6d14b809a4c96ca
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-05-08 13:34:42 +0200

    Enable `dace.subsets.Range.from_array` even when Data do not have an offset.

--- dace/subsets.py
+++ dace/subsets.py
@@ -190,7 +190,7 @@ class Range(Subset):
     def from_array(array: 'dace.data.Data'):
         """ Constructs a range that covers the full array given as input. """
         result = Range([(0, s - 1, 1) for s in array.shape])
-        if any(o != 0 for o in array.offset):
+        if hasattr(array, 'offset') and any(o != 0 for o in array.offset):
             result.offset(array.offset, True)
         return result
 
