fa91834b98fd7103df415e17662cde300b9529de
Samuel Martin <martisam@student.ethz.ch>
2023-05-09 17:55:16 +0200

    Added bandwidth to printing

--- thesis_playground/src/scripts/print_complete_results.py
+++ thesis_playground/src/scripts/print_complete_results.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from tabulate import tabulate
 
-from utils.ncu import get_achieved_work, get_achieved_bytes
+from utils.ncu import get_achieved_work, get_achieved_bytes, get_runtime, get_peak_performance, get_achieved_performance
 from measurements.flop_computation import get_number_of_bytes_2
 from utils.complete_results import get_actions, get_program_measurements, get_roofline_data
 from scripts import Script
@@ -19,10 +19,13 @@ class PrintCompleteResults(Script):
 
         columns = [
                     ('program', lambda msr, action, roof: msr.program),
-                    ('W [flop]', lambda msr, action, roof: roof[0].get_total_flops()),
-                    ('W (ncu) [flop]', lambda msr, action, roof: get_achieved_work(action)['dW']),
-                    ('Q [bytes]', lambda msr, action, roof: get_number_of_bytes_2(msr.parameters, msr.program)[0]),
-                    ('D (ncu) [bytes]', lambda msr, action, roof: get_achieved_bytes(action)),
+                    ('W [flop]', lambda msr, action, roof: roof[0].get_total_flops(), ','),
+                    ('W (ncu) [flop]', lambda msr, action, roof: get_achieved_work(action)['dW'], ','),
+                    ('Q [bytes]', lambda msr, action, roof: get_number_of_bytes_2(msr.parameters, msr.program)[0], ','),
+                    ('D (ncu) [bytes]', lambda msr, action, roof: get_achieved_bytes(action), ','),
+                    ('Kernel time [s]', lambda msr, action, roof: get_runtime(action), '.3e'),
+                    ('Bandwidth [%]', lambda msr, action, roof: float(get_achieved_performance(action)[1] /
+                                                                get_peak_performance(action)[1]) * 100, '.2f'),
                 ]
 
         ncu_actions = get_actions(args.name)
@@ -33,4 +36,6 @@ class PrintCompleteResults(Script):
             roofline = roofline_data[msr.program]
             tabulate_data.append([col[1](msr, action, roofline) for col in columns])
 
-        print(tabulate(tabulate_data, headers=[col[0] for col in columns], intfmt=','))
+        floatfmt = [c[2] if isinstance(val, float) else None for c, val in zip(columns, tabulate_data[0])]
+        intfmt = [c[2] if isinstance(val, int) else None for c, val in zip(columns, tabulate_data[0])]
+        print(tabulate(tabulate_data, headers=[col[0] for col in columns], intfmt=intfmt, floatfmt=floatfmt))
