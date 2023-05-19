1d91c1a9217d37adfa8dd61d931d0da11d122c4c
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-05-19 10:31:53 +0200

    Updated tests.

--- tests/python_frontend/experimental_test.py
+++ tests/python_frontend/experimental_test.py
@@ -62,28 +62,8 @@ def test_expand_if_python():
 @pytest.mark.skip
 def test_expand_if_dace():
 
-    def function_0(array_0, array_1, scalar):
-
-        a = array_0 + 5
-        b = array_1 - 5
-        c = (a + b) / 2
-
-        if scalar > 0:
-            tmp_array_0 = array_0
-            tmp_array_1 = array_1
-        else:
-            tmp_array_0 = array_1
-            tmp_array_1 = array_0
-        
-        tmp_array_0[:] = c
-        d = tmp_array_0 * 0.25
-        e = tmp_array_1 * 0.75
-        f = (d + e) / 2
-        tmp_array_1[:] = f
-    
-    @dace.program
-    @dace.expand_if
-    def function_1(array_0, array_1, scalar):
+    @dace.program(expand_if=True)
+    def function(array_0, array_1, scalar):
 
         a = array_0 + 5
         b = array_1 - 5
@@ -109,8 +89,37 @@ def test_expand_if_dace():
     B_val = B_ref.copy()
     scalar = rng.random(dtype=np.float32)
 
-    function_0(A_ref, B_ref, scalar)
-    function_1(A_val, B_val, scalar)
+    function.f(A_ref, B_ref, scalar)
+    function(A_val, B_val, scalar)
+
+    assert np.allclose(A_ref, A_val)
+    assert np.allclose(B_ref, B_val)
+
+
+def test_if_array_none():
+
+    @dace.program(expand_if=True)
+    def if_array_none(array, scalar, optional_array):
+        if optional_array is not None:
+            tmp_array = optional_array
+        else:
+            tmp_array = array
+        tmp_array[:] = scalar
+    
+    rng = np.random.default_rng(42)
+    A_ref = rng.random((10, ), dtype=np.float32)
+    B_ref = rng.random((10, ), dtype=np.float32)
+    A_val = A_ref.copy()
+    B_val = B_ref.copy()
+    scalar = rng.random(dtype=np.float32)
+
+    if_array_none.f(A_ref, scalar, None)
+    if_array_none(A_val, scalar, None)
+
+    assert np.allclose(A_ref, A_val)
+
+    if_array_none.f(A_ref, scalar, B_ref)
+    if_array_none(A_val, scalar, B_val)
 
     assert np.allclose(A_ref, A_val)
     assert np.allclose(B_ref, B_val)
@@ -119,3 +128,4 @@ def test_expand_if_dace():
 if __name__ == '__main__':
     test_expand_if_python()
     test_expand_if_dace()
+    test_if_array_none()
