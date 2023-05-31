40037e2c740da60799a2d8257c5acf3fd41df750
Philipp Schaad <schaad.phil@gmail.com>
2023-05-31 18:52:34 +0200

    Add documenting comments to the work-depth analysis

--- dace/sdfg/work_depth_analysis.py
+++ dace/sdfg/work_depth_analysis.py
@@ -14,6 +14,9 @@ def sdfg_work_depth(sdfg: SDFG, sym_map: Dict[str, int]) -> Tuple[sp.Expr, sp.Ex
     print('SDFG has', len(sdfg.nodes()), 'states')
     print('Calculating work and depth for all states individually...')
 
+    # First determine the work and depth of each state individually.
+    # Keep track of the work and depth for each state in a dictionary, where work and depth are multiplied by the number
+    # of times the state will be executed.
     state_depths: Dict[SDFGState, sp.Expr] = {}
     state_works: Dict[SDFGState, sp.Expr] = {}
     for state in sdfg.nodes():
@@ -32,7 +35,12 @@ def sdfg_work_depth(sdfg: SDFG, sym_map: Dict[str, int]) -> Tuple[sp.Expr, sp.Ex
     dummy_exit = sdfg.add_state('dummy_exit')
     for state in sdfg.nodes():
         if hasattr(state, 'condition_edge') and hasattr(state, 'is_loop_guard') and state.is_loop_guard:
+            # This is a loop guard.
             loop_begin = state.condition_edge.dst
+            # Determine loop states through a depth first search from the start of the loop. Everything reached before
+            # arriving back at the loop guard is part of the loop.
+            # TODO: This is hacky. Loops should report the loop states directly. This may fail or behave unexpectedly
+            #       for break/return statements inside of loops.
             loop_states = set(sdutil.dfs_conditional(sdfg, sources=[loop_begin], condition=lambda _, s: s != state))
             loop_exit = None
             exit_edge = None
@@ -62,9 +70,9 @@ def sdfg_work_depth(sdfg: SDFG, sym_map: Dict[str, int]) -> Tuple[sp.Expr, sp.Ex
     state_depths[dummy_exit] = sp.sympify(0)
     state_works[dummy_exit] = sp.sympify(0)
 
-    # Perform a BFS traversal of the state machine and calculate the maximum depth at each state. Only advance to the
-    # next state in the BFS if all incoming edges have been visited, to ensure the maximum depth expression has been
-    # calculated.
+    # Perform a BFS traversal of the state machine and calculate the maximum work / depth at each state. Only advance to
+    # the next state in the BFS if all incoming edges have been visited, to ensure the maximum work / depth expressions
+    # have been calculated.
     traversal_q = deque()
     traversal_q.append((sdfg.start_state, sp.sympify(0), sp.sympify(0), None))
     visited = set()
@@ -102,7 +110,21 @@ def sdfg_work_depth(sdfg: SDFG, sym_map: Dict[str, int]) -> Tuple[sp.Expr, sp.Ex
     return sp.simplify(max_work).subs(sym_map), sp.simplify(max_depth).subs(sym_map)
 
 
+"""
+Analyze the work and depth of a scope.
+This works by constructing a proxy graph of the scope and then finding the maximum depth path in that graph between
+the source and sink. The proxy graph is constructed to remove any multi-edges between nodes and to remove nodes that
+do not contribute to the depth. Additionally, nested scopes are summarized into single nodes. All of this is necessary
+to reduce the number of possible paths in the graph, as much as possible, since they all have to be brute-force
+enumerated to find the maximum depth path.
+:note: This is terribly inefficient and should be improved.
+:param state: The state in which the scope to analyze is contained.
+:param sym_map: A dictionary mapping symbols to their values.
+:param entry: The entry node of the scope to analyze. If None, the entire state is analyzed.
+:return: A tuple containing the work and depth of the scope.
+"""
 def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryNode = None) -> Tuple[sp.Expr, sp.Expr]:
+    # Construct a proxy graph of the scope with dummy source and sink nodes.
     proxy = nx.DiGraph()
     source = nd.Node()
     sink = nd.Node()
@@ -110,13 +132,15 @@ def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryN
     proxy.add_node(source)
     proxy.add_node(sink)
 
-    # TODO: Document this. SC deadline..
+    # Iteratively add all nodes from the scope to the proxy graph.
     work = sp.sympify(0)
     max_depth = sp.sympify(0)
     scope_nodes = state.scope_children()[entry]
     scope_exit = None if entry is None else state.exit_node(entry)
     for node in scope_nodes:
         if isinstance(node, nd.EntryNode):
+            # If the scope contains an entry node, we need to recursively analyze the scope of the entry node first.
+            # The resulting work/depth are summarized into a single node in the proxy graph (the entry node).
             s_work, s_depth = scope_work_depth(state, sym_map, node)
             work += s_work
             node_depths[node] = s_depth
@@ -140,6 +164,9 @@ def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryN
                     if not proxy.has_edge(node, oedge.dst) and not isinstance(oedge.dst, nd.ExitNode):
                         proxy.add_edge(node, oedge.dst)
         elif node == scope_exit:
+            # The exit node is not added to the proxy graph (unless it does not have any incoming edges, in which
+            # case it is added with an edge to it from the dummy source). All other incoming edges are redirected to
+            # the dummy sink node in the proxy.
             iedges = state.in_edges(node)
             if len(iedges) == 0 and not proxy.has_edge(source, node):
                 proxy.add_edge(source, node)
@@ -151,10 +178,14 @@ def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryN
                     elif not proxy.has_edge(iedge.src, sink) and not isinstance(iedge.src, nd.ExitNode):
                         proxy.add_edge(iedge.src, sink)
         else:
+            # All other nodes are added to the proxy graph and their work/depth is calculated individually.
             if isinstance(node, nd.Tasklet):
+                # We assume each tasklet has a work of 1. WARNING: this is not always true! Tasklets should definitely
+                # be analyzed to determine their work.
                 work += 1
                 node_depths[node] = sp.sympify(1)
             elif isinstance(node, nd.NestedSDFG):
+                # Nested SDFGs are recursively analyzed first.
                 nsdfg_work, nsdfg_depth = sdfg_work_depth(node.sdfg, sym_map)
                 work += nsdfg_work
                 node_depths[node] = nsdfg_depth
@@ -177,6 +208,7 @@ def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryN
                     if not proxy.has_edge(node, oedge.dst) and not isinstance(oedge.dst, nd.ExitNode):
                         proxy.add_edge(node, oedge.dst)
     if entry is not None:
+        # If the scope being analyzed is a map, multiply the work by the number of iterations of the map.
         if isinstance(entry, nd.MapEntry):
             nmap: nd.Map = entry.map
             range: Range = nmap.range
@@ -185,6 +217,9 @@ def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryN
         else:
             print('WARNING: Only Map scopes are supported in work analysis for now. Assuming 1 iteration.')
 
+    # Calculate the maximum depth of the scope by finding the 'deepest' path from the source to the sink in the proxy
+    # graph, where the deepest path corresponds to the source-sink path with the maximum depth sum. This is done by
+    # brute-force enumeration of all possible source-sink paths. This is not very efficient.
     for path in nx.all_simple_paths(proxy, source, sink):
         depth = sp.sympify(0)
         for node in path:
@@ -194,21 +229,42 @@ def scope_work_depth(state: SDFGState, sym_map: Dict[str, int], entry: nd.EntryN
 
     return sp.simplify(work).subs(sym_map), sp.simplify(max_depth).subs(sym_map)
 
+"""
+Analyze the work and depth of a state.
+:param state: The state to analyze.
+:param sym_map: A dictionary mapping symbols to their values.
+:return: A tuple containing the work and depth of the state.
+"""
 def state_work_depth(state: SDFGState, sym_map: Dict[str, int]) -> Tuple[sp.Expr, sp.Expr]:
     work, depth = scope_work_depth(state, sym_map, None)
     return sp.simplify(work).subs(sym_map), sp.simplify(depth).subs(sym_map)
 
 
+"""
+Analyze the work and depth of an SDFG.
+Optionally, a dictionary mapping symbols to their values can be provided to concretize the analysis.
+Note that this also significantly speeds up the analysis due to sympy not having to perform the analysis symbolically.
+:note: SDFGs should have split interstate edges. This means there should be no interstate edges containing both a
+       condition and an assignment.
+:param sdfg: The SDFG to analyze.
+:param sym_map: A dictionary mapping symbols to their values.
+:return: A tuple containing the work and depth of the SDFG
+"""
 def analyze_sdfg(sdfg: SDFG, sym_map: Dict[str, int]) -> Tuple[sp.Expr, sp.Expr]:
+    # Run state propagation for all SDFGs recursively. This is necessary to determine the number of times each state
+    # will be executed, or to determine upper bounds for that number (such as in the case of branching)
     print('Propagating states...')
     for sd in sdfg.all_sdfgs_recursive():
         propagation.propagate_states(sd)
 
+    # Check if the SDFG has any dynamically unbounded executions, i.e., if there are any states that have neither a
+    # statically known number of executions, nor an upper bound on the number of executions. Warn if this is the case.
     print('Checking for dynamically unbounded executions...')
     for sd in sdfg.all_sdfgs_recursive():
         if any([s.executions == 0 and s.dynamic_executions for s in sd.nodes()]):
             print('WARNING: SDFG has dynamic executions. The analysis may fail in unexpected ways or be inaccurate.')
 
+    # Analyze the work and depth of the SDFG.
     print('Analyzing SDFG...')
     work, depth = sdfg_work_depth(sdfg, sym_map)
 
