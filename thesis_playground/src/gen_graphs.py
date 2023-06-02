729bb835bfd226aee801258ef00938156ecf36e0
Samuel Martin <martisam@student.ethz.ch>
2023-06-02 15:15:28 +0200

    Forgot to add some files

--- thesis_playground/src/gen_graphs.py
+++ thesis_playground/src/gen_graphs.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 
 import dace
 
+from execute.parameters import ParametersProvider
 from utils.general import get_programs_data, get_sdfg, reset_graph_files, read_source, enable_debug_flags, optimize_sdfg
 
 
@@ -13,6 +14,12 @@ def main():
         action='store_true',
         help='Does not compile the SDFGs into C++ code, only creates the SDFGs and runs the transformations')
     parser.add_argument('--debug', action='store_true', default=False, help="Configure for debug build")
+    parser.add_argument('--not-specialise', action='store_true', help='Do not specialise symbols')
+    parser.add_argument('--KLON', type=int, default=None)
+    parser.add_argument('--KLEV', type=int, default=None)
+    parser.add_argument('--NBLOCKS', type=int, default=None)
+    parser.add_argument('--KIDIA', type=int, default=None)
+    parser.add_argument('--KFDIA', type=int, default=None)
 
     device = dace.DeviceType.GPU
     args = parser.parse_args()
@@ -27,7 +34,14 @@ def main():
     program_name = programs[args.program]
     sdfg = get_sdfg(fsource, program_name)
 
-    optimize_sdfg(sdfg, device, verbose_name=args.program)
+    add_args = {}
+    if not args.not_specialise:
+        params = ParametersProvider(args.program)
+        params.update_from_args(args)
+        print(f"Use {params} for specialisation")
+        add_args['symbols'] = params.get_dict()
+
+    optimize_sdfg(sdfg, device, verbose_name=args.program, **add_args)
     sdfg.instrument = dace.InstrumentationType.Timer
     if not args.only_graph:
         sdfg.compile()
