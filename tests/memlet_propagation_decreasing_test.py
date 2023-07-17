1d92d5d16f4d25a6a3a167fbae29db66ce5c6099
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-07-17 16:27:28 +0200

    Updated test to have separate input/output arrays.

--- tests/memlet_propagation_decreasing_test.py
+++ tests/memlet_propagation_decreasing_test.py
@@ -6,16 +6,18 @@ from dace import nodes
 
 def test_decreasing_propagation():
 
+    p = np.random.randn(19, 19)
     q = np.random.randn(19, 19)
     ref = q.copy()
 
     @dace.program
-    def copy_nw_corner(q: dace.float64[19, 19]):
+    def copy_nw_corner(p: dace.float64[19, 19], q: dace.float64[19, 19]):
         for j in dace.map[15:19]:
             for i in dace.map[0:3]:
-                q[i, j] = q[j - 12, 17 - i]
+                q[i, j] = p[j - 12, 17 - i]
 
     sdfg = copy_nw_corner.to_sdfg()
+    sdfg.view()
     me = None
     state = None
     for n, s in sdfg.all_nodes_recursive():
@@ -30,8 +32,8 @@ def test_decreasing_propagation():
     subset = edges[0].data.src_subset
     assert (subset.ranges == [(3, 6, 1), (15, 17, 1)])
 
-    copy_nw_corner(q)
-    copy_nw_corner.f(ref)
+    copy_nw_corner(p, q)
+    copy_nw_corner.f(p, ref)
     assert (np.allclose(q, ref))
 
 
