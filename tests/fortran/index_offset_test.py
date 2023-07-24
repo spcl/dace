fdd5a27997680a3f4385fde90470b57a604bbb72
Marcin Copik <mcopik@gmail.com>
2023-07-24 20:22:27 +0200

    Support shape attribute specification in the Fortran frontend

--- tests/fortran/index_offset_test.py
+++ tests/fortran/index_offset_test.py
@@ -18,6 +18,46 @@ import dace.frontend.fortran.ast_transforms as ast_transforms
 import dace.frontend.fortran.ast_utils as ast_utils
 import dace.frontend.fortran.ast_internal_classes as ast_internal_classes
 
+def test_fortran_frontend_index_offset_attributes():
+    """
+    Tests that the Fortran frontend can parse array accesses and that the accessed indices are correct.
+    """
+    test_string = """
+                    PROGRAM index_offset_test
+                    implicit none
+                    double precision, dimension(50:54) :: d
+                    !double precision, dimension(5) :: d
+                    !double precision d(50:54)
+                    CALL index_test_function(d)
+                    end
+
+                    SUBROUTINE index_test_function(d)
+                    !double precision d(50:54)
+                    !double precision d(5)
+                    double precision, dimension(50:54) :: d
+                    !double precision, intent(inout) :: d(50:54)
+
+                    do i=50,54
+                       d(i) = i * 2.0
+                    end do
+
+                    END SUBROUTINE index_test_function
+                    """
+    sdfg = fortran_parser.create_sdfg_from_string(test_string, "index_offset_test")
+    sdfg.simplify(verbose=True)
+    sdfg.compile()
+
+    assert len(sdfg.data('d').shape) == 1
+    assert sdfg.data('d').shape[0] == 5
+    assert len(sdfg.data('d').offset) == 1
+    assert sdfg.data('d').offset[0] == -1
+
+    a = np.full([60], 42, order="F", dtype=np.float64)
+    sdfg(d=a)
+    for i in range(50,54):
+        # offset -1 is already added
+        assert a[i-1] == i * 2
+
 def test_fortran_frontend_index_offset():
     """
     Tests that the Fortran frontend can parse array accesses and that the accessed indices are correct.
@@ -56,5 +96,5 @@ def test_fortran_frontend_index_offset():
 
 if __name__ == "__main__":
 
-    #test_fortran_frontend_index_offset()
-    test_fortran_frontend_index_offset_dimensions()
+    test_fortran_frontend_index_offset()
+    test_fortran_frontend_index_offset_attributes()
