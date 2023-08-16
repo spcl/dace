fbc1e8ae31cdeb118e691f95411e93109905a632
Samuel Martin <samuel.martin@macrolino.ch>
2023-08-16 10:56:48 +0200

    add option to not use outer loop first in gen_graphs

--- thesis_playground/src/gen_graphs.py
+++ thesis_playground/src/gen_graphs.py
@@ -19,6 +19,14 @@ def main():
     parser.add_argument('--k-caching', action='store_true', default=False, help="use k-caching")
     parser.add_argument('--change-stride', action='store_true', default=False, help="change stride")
     parser.add_argument('--device', choices=['CPU', 'GPU'], default='GPU')
+    parser.add_argument('--verbose-name',
+                        type=str,
+                        default=None,
+                        help='Foldername under which intermediate SDFGs should be stored, uses the program name by '
+                        'default')
+    parser.add_argument('--use-dace-auto-opt', default=False, action='store_true',
+                        help='Use DaCes auto_opt instead of mine')
+    parser.add_argument('--no-outer-loop-first', action='store_true', default=False, help='Disable outer loops first')
     add_cloudsc_size_arguments(parser)
 
     args = parser.parse_args()
@@ -28,7 +36,10 @@ def main():
     if args.debug:
         enable_debug_flags()
 
-    reset_graph_files(args.program)
+    verbose_name = args.program
+    if args.verbose_name is not None:
+        verbose_name = args.verbose_name
+    reset_graph_files(verbose_name)
 
     programs = get_programs_data()['programs']
     fsource = read_source(args.program)
@@ -47,7 +58,11 @@ def main():
 
     add_args['k_caching'] = args.k_caching
     add_args['change_stride'] = args.change_stride
-    sdfg = optimize_sdfg(sdfg, device, verbose_name=args.program, **add_args)
+    if args.use_dace_auto_opt:
+        add_args['use_my_auto_opt'] = False
+    if args.no_outer_loop_first:
+        add_args['outside_first'] = False
+    sdfg = optimize_sdfg(sdfg, device, verbose_name=verbose_name, **add_args)
     sdfg.instrument = dace.InstrumentationType.Timer
     if not args.only_graph:
         sdfg.compile()
