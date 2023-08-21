395308bff19af5866906187273591c8d6e39e7dc
Samuel Martin <martisam@student.ethz.ch>
2023-08-21 15:33:47 +0200

    Added testcase exposing bug

--- tests/trivial_map_elimination_test.py
+++ tests/trivial_map_elimination_test.py
@@ -25,6 +25,33 @@ def trivial_map_sdfg():
     return sdfg
 
 
+def trivial_map_init_sdfg():
+    sdfg = dace.SDFG('trivial_map_range_expanded')
+    sdfg.add_array('B', [5, 1], dace.float64)
+    state = sdfg.add_state()
+
+    # Nodes
+    map_entry_outer, map_exit_outer = state.add_map('map_outer', dict(j='0:5'))
+    map_entry_inner, map_exit_inner = state.add_map('map_inner', dict(i='0:1'))
+
+    tasklet = state.add_tasklet('tasklet', {}, {'b'}, 'b = 1')
+    write = state.add_write('B')
+
+    # Edges
+    state.add_memlet_path(map_entry_outer, map_entry_inner, memlet=dace.Memlet())
+    state.add_memlet_path(map_entry_inner, tasklet, memlet=dace.Memlet())
+
+    state.add_memlet_path(tasklet, map_exit_inner, memlet=dace.Memlet.simple('B', 'j, i'), src_conn='b',
+                          dst_conn='IN_B')
+    state.add_memlet_path(map_exit_inner, map_exit_outer, memlet=dace.Memlet.simple('B', 'j, 0'), src_conn='OUT_B',
+                          dst_conn='IN_B')
+    state.add_memlet_path(map_exit_outer, write, memlet=dace.Memlet.simple('B', '0:5, 0'),
+                          src_conn='OUT_B')
+
+    sdfg.validate()
+    return sdfg
+
+
 class TrivialMapEliminationTest(unittest.TestCase):
     def test_can_be_applied(self):
         graph = trivial_map_sdfg()
@@ -54,6 +81,40 @@ class TrivialMapEliminationTest(unittest.TestCase):
         B = [n for n in state.nodes() if isinstance(n, dace.sdfg.nodes.AccessNode) and n.data == 'B'][0]
         out_memlet = state.in_edges(B)[0]
         self.assertEqual(out_memlet.data.subset, dace.subsets.Range([(0, 0, 1)]))
+
+
+class TrivialMapInitEliminationTest(unittest.TestCase):
+    def test_can_be_applied(self):
+        graph = trivial_map_init_sdfg()
+        graph.save('trivial_map_expanded_sdfg.sdfg')
+
+        count = graph.apply_transformations(TrivialMapElimination, validate=False, validate_all=False)
+        graph.save('trivial_map_expanded_sdfg_applied.sdfg')
+        graph.validate()
+
+        self.assertGreater(count, 0)
+
+    # def test_removes_map(self):
+    #     graph = trivial_map_sdfg()
+
+    #     graph.apply_transformations(TrivialMapElimination)
+
+    #     state = graph.nodes()[0]
+    #     map_entries = [n for n in state.nodes() if isinstance(n, dace.sdfg.nodes.MapEntry)]
+    #     self.assertEqual(len(map_entries), 0)
+
+    # def test_raplaces_map_params_in_scope(self):
+    #     # Tests if the 'i' in the range of the memlet to B gets replaced
+    #     # with the value 'i' obtains in the map, namely '0'.
+
+    #     graph = trivial_map_sdfg()
+
+    #     graph.apply_transformations(TrivialMapElimination)
+
+    #     state = graph.nodes()[0]
+    #     B = [n for n in state.nodes() if isinstance(n, dace.sdfg.nodes.AccessNode) and n.data == 'B'][0]
+    #     out_memlet = state.in_edges(B)[0]
+    #     self.assertEqual(out_memlet.data.subset, dace.subsets.Range([(0, 0, 1)]))
 
 
 if __name__ == '__main__':
