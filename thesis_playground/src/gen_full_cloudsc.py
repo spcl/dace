ae7661d5f0ccb91bc21baaa7b6a1d53ec12f191b
Samuel Martin <martisam@student.ethz.ch>
2023-08-28 16:07:38 +0200

    Add debug build in gen_full_cloudsc.py

--- thesis_playground/src/gen_full_cloudsc.py
+++ thesis_playground/src/gen_full_cloudsc.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 
 from utils.generate_sdfg import get_optimised_sdfg
-from utils.general import remove_build_folder
+from utils.general import remove_build_folder, enable_debug_flags
 from utils.log import setup_logging
 from utils.execute_dace import RunConfig
 from utils.paths import get_full_cloudsc_log_dir
@@ -18,6 +18,7 @@ def main():
     parser.add_argument('--version', default=2, type=int)
     parser.add_argument('--baseline', action='store_true', default=False)
     parser.add_argument('--log-level', default='info')
+    parser.add_argument('--debug-build', action='store_true', default=False)
     args = parser.parse_args()
 
     if args.version == 3:
@@ -30,6 +31,7 @@ def main():
     logger.info("Use program: %s", program)
 
     params = ParametersProvider(program, update={'NBLOCKS': 16384})
+    # params = ParametersProvider(program, update={'NBLOCKS': 20})
     if args.baseline:
         run_config = RunConfig(k_caching=False, change_stride=False, outside_loop_first=False,
                                move_assignment_outside=False)
@@ -38,6 +40,8 @@ def main():
     logger.debug(run_config)
     remove_build_folder(program)
     sdfg = get_optimised_sdfg(program, run_config, params, ['NBLOCKS'], instrument=False, storage_on_gpu=False)
+    if args.debug_build:
+        enable_debug_flags()
     sdfg.compile()
     sdfg.save(os.path.join(get_full_cloudsc_log_dir(), f"{program}.sdfg"))
     with open(os.path.join(get_full_cloudsc_log_dir(), f"signature_dace_{program}.txt"), 'w') as file:
