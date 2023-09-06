cee8eceb74d0e8f967183a92caae3fdc0b7c5c81
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-09-06 11:48:05 +0200

    Added writing test.

--- tests/python_frontend/structures/structure_test.py
+++ tests/python_frontend/structures/structure_test.py
@@ -34,5 +34,40 @@ def test_read_structure():
     assert np.allclose(B, ref)
 
 
+def test_write_structure():
+
+    M, N, nnz = (dace.symbol(s) for s in ('M', 'N', 'nnz'))
+    CSR = dace.data.Structure(dict(indptr=dace.int32[M + 1], indices=dace.int32[nnz], data=dace.float32[nnz]),
+                              name='CSRMatrix')
+    
+    @dace.program
+    def dense_to_csr_python(A: dace.float32[M, N], B: CSR):
+        idx = 0
+        for i in range(M):
+            B.indptr[i] = idx
+            for j in range(N):
+                if A[i, j] != 0:
+                    B.data[idx] = A[i, j]
+                    B.indices[idx] = j
+                    idx += 1
+        B.indptr[M] = idx
+    
+    rng = np.random.default_rng(42)
+    tmp = sparse.random(20, 20, density=0.1, format='csr', dtype=np.float32, random_state=rng)
+    A = tmp.toarray()
+    B = tmp.tocsr(copy=True)
+    B.indptr[:] = -1
+    B.indices[:] = -1
+    B.data[:] = -1
+
+    outB = CSR.dtype._typeclass.as_ctypes()(indptr=B.indptr.__array_interface__['data'][0],
+                                            indices=B.indices.__array_interface__['data'][0],
+                                            data=B.data.__array_interface__['data'][0])
+
+    func = dense_to_csr_python.compile()
+    func(A=A, B=outB, M=tmp.shape[0], N=tmp.shape[1], nnz=tmp.nnz)
+
+
 if __name__ == '__main__':
     test_read_structure()
+    test_write_structure()
