a0f02fae1b099e58d362179e8578518384e977f1
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-09-06 16:28:41 +0200

    Added mini-app test.

--- tests/python_frontend/structures/structure_python_test.py
+++ tests/python_frontend/structures/structure_python_test.py
@@ -68,6 +68,82 @@ def test_write_structure():
     func(A=A, B=outB, M=tmp.shape[0], N=tmp.shape[1], nnz=tmp.nnz)
 
 
+def test_rgf():
+
+    class BTD:
+
+        def __init__(self, diag, upper, lower):
+            self.diag = diag
+            self.upper = upper
+            self.lower = lower
+
+    n, nblocks = dace.symbol('n'), dace.symbol('nblocks')
+    BlockTriDiagonal = dace.data.Structure(
+        dict(diag=dace.complex128[nblocks, n, n],
+             upper=dace.complex128[nblocks, n, n],
+             lower=dace.complex128[nblocks, n, n]),
+        name='BlockTriDiagonalMatrix')
+    
+    @dace.program
+    def rgf_leftToRight(A: BlockTriDiagonal, B: BlockTriDiagonal, n_: dace.int32, nblocks_: dace.int32):
+
+        # Storage for the incomplete forward substitution
+        tmp = np.zeros_like(A.diag)
+        identity = np.zeros_like(tmp[0])
+
+        # 1. Initialisation of tmp
+        tmp[0] = np.linalg.inv(A.diag[0])
+        for i in dace.map[0:identity.shape[0]]:
+            identity[i, i] = 1
+
+        # 2. Forward substitution
+        # From left to right
+        for i in range(1, nblocks_):
+            tmp[i] = np.linalg.inv(A.diag[i] - A.lower[i-1] @ tmp[i-1] @ A.upper[i-1])
+
+        # 3. Initialisation of last element of B
+        B.diag[-1] = tmp[-1]
+
+        # 4. Backward substitution
+        # From right to left
+
+        for i in range(nblocks_-2, -1, -1): 
+            B.diag[i]  =  tmp[i] @ (identity + A.upper[i] @ B.diag[i+1] @ A.lower[i] @ tmp[i])
+            B.upper[i] = -tmp[i] @ A.upper[i] @ B.diag[i+1]
+            B.lower[i] =  np.transpose(B.upper[i])
+    
+    rng = np.random.default_rng(42)
+
+    A_diag = rng.random((10, 20, 20)) + 1j * rng.random((10, 20, 20))
+    A_upper = rng.random((10, 20, 20)) + 1j * rng.random((10, 20, 20))
+    A_lower = rng.random((10, 20, 20)) + 1j * rng.random((10, 20, 20)) 
+    inpBTD = BlockTriDiagonal.dtype._typeclass.as_ctypes()(diag=A_diag.__array_interface__['data'][0],
+                                                           upper=A_upper.__array_interface__['data'][0],
+                                                           lower=A_lower.__array_interface__['data'][0])
+    
+    B_diag = np.zeros((10, 20, 20), dtype=np.complex128)
+    B_upper = np.zeros((10, 20, 20), dtype=np.complex128)
+    B_lower = np.zeros((10, 20, 20), dtype=np.complex128)
+    outBTD = BlockTriDiagonal.dtype._typeclass.as_ctypes()(diag=B_diag.__array_interface__['data'][0],
+                                                           upper=B_upper.__array_interface__['data'][0],
+                                                           lower=B_lower.__array_interface__['data'][0])
+    
+    func = rgf_leftToRight.compile()
+    func(A=inpBTD, B=outBTD, n_=A_diag.shape[1], nblocks_=A_diag.shape[0], n=A_diag.shape[1], nblocks=A_diag.shape[0])
+
+    A = BTD(A_diag, A_upper, A_lower)
+    B = BTD(np.zeros((10, 20, 20), dtype=np.complex128),
+            np.zeros((10, 20, 20), dtype=np.complex128),
+            np.zeros((10, 20, 20), dtype=np.complex128))
+    
+    rgf_leftToRight.f(A, B, A_diag.shape[1], A_diag.shape[0])
+
+    assert np.allclose(B.diag, B_diag)
+    assert np.allclose(B.upper, B_upper)
+    assert np.allclose(B.lower, B_lower)
+
+
 if __name__ == '__main__':
     test_read_structure()
     test_write_structure()
+    test_rgf()
