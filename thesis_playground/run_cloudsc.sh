27cafcc9ff5d805efe7afea437d4d1fd543aa509
Samuel Martin <martisam@student.ethz.ch>
2023-09-21 15:34:37 +0200

    Run cloudsc script

--- thesis_playground/run_cloudsc.sh
+++ thesis_playground/run_cloudsc.sh
@@ -1,5 +1,6 @@
 cloudsc_path='/users/msamuel/dwarf-p-cloudsc-dace'
-dace_signature_file='full_cloudsc_logs/signature_dace_cloudscexp3.txt'
+dace_signature_file='full_cloudsc_logs/signature_dace_cloudscexp4.txt'
+cloudsc_version=4
 
 echo "Using signature file at $dace_signature_file"
 
@@ -44,22 +45,30 @@ adapter_file="$cloudsc_path/src/cloudsc_fortran/working.cc"
 echo "#include <cstdlib>" > $adapter_file 
 echo "#include <dace/dace.h>" >> $adapter_file
 echo "#include <chrono>" >> $adapter_file
-echo "typedef void * CLOUDSCOUTERHandle_t;" >> $adapter_file
-echo "extern \"C\" CLOUDSCOUTERHandle_t __dace_init_CLOUDSCOUTER();" >> $adapter_file
-echo "extern \"C\" void __dace_exit_CLOUDSCOUTER(CLOUDSCOUTERHandle_t handle);" >> $adapter_file
-echo "extern \"C\" void __program_CLOUDSCOUTER(CLOUDSCOUTERHandle_t handle, $(cat $dace_signature_file));" >> $adapter_file
+echo "typedef void * CLOUDSCOUTER${cloudsc_version}Handle_t;" >> $adapter_file
+echo "extern \"C\" CLOUDSCOUTER${cloudsc_version}Handle_t __dace_init_CLOUDSCOUTER${cloudsc_version}();" >> $adapter_file
+echo "extern \"C\" void __dace_exit_CLOUDSCOUTER${cloudsc_version}(CLOUDSCOUTER${cloudsc_version}Handle_t handle);" >> $adapter_file
+echo "extern \"C\" void __program_CLOUDSCOUTER${cloudsc_version}(CLOUDSCOUTER${cloudsc_version}Handle_t handle, $(cat $dace_signature_file));" >> $adapter_file
 echo "" >> $adapter_file
 echo "void cloudsc2_internal($(cat $dace_signature_file))" >> $adapter_file
 echo "{" >> $adapter_file
-echo "    CLOUDSCOUTERHandle_t handle;" >> $adapter_file
-echo "    handle = __dace_init_CLOUDSCOUTER();" >> $adapter_file
+echo "    CLOUDSCOUTER${cloudsc_version}Handle_t handle;" >> $adapter_file
+echo "    handle = __dace_init_CLOUDSCOUTER${cloudsc_version}();" >> $adapter_file
 echo "    auto start = std::chrono::high_resolution_clock::now();" >> $adapter_file
-echo "    __program_CLOUDSCOUTER(handle, $parameters);" >> $adapter_file
+echo "    __program_CLOUDSCOUTER${cloudsc_version}(handle, $parameters);" >> $adapter_file
 echo "    auto end = std::chrono::high_resolution_clock::now();" >> $adapter_file
 echo "    printf(\"Time %d [ms]\n\", std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count());" >> $adapter_file
-echo "    __dace_exit_CLOUDSCOUTER(handle);" >>  $adapter_file
+echo "    __dace_exit_CLOUDSCOUTER${cloudsc_version}(handle);" >>  $adapter_file
 echo "}" >> $adapter_file
 echo "DACE_EXPORTED void cloudsc2_($parameters_pointer)" >> $adapter_file
 echo "{" >> $adapter_file
 echo "    cloudsc2_internal($parameters_dereference);" >> $adapter_file
 echo "}" >> $adapter_file
+
+
+
+current_dir=$PWD
+cd "$cloudsc_path/build"
+make && ./bin/dwarf-cloudsc-fortran 1 16384 1
+cd $current_dir
+
