d55154076bd03b64547dd5474c560a0170b9a292
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-10-06 10:04:16 +0200

    Added new test.

--- tests/python_frontend/structures/structure_python_test.py
+++ tests/python_frontend/structures/structure_python_test.py
@@ -68,6 +68,44 @@ def test_write_structure():
     func(A=A, B=outB, M=tmp.shape[0], N=tmp.shape[1], nnz=tmp.nnz)
 
 
+def test_local_structure():
+
+    M, N, nnz = (dace.symbol(s) for s in ('M', 'N', 'nnz'))
+    CSR = dace.data.Structure(dict(indptr=dace.int32[M + 1], indices=dace.int32[nnz], data=dace.float32[nnz]),
+                              name='CSRMatrix')
+    
+    @dace.program
+    def dense_to_csr_local_python(A: dace.float32[M, N], B: CSR):
+        tmp = dace.define_local_structure(CSR)
+        idx = 0
+        for i in range(M):
+            tmp.indptr[i] = idx
+            for j in range(N):
+                if A[i, j] != 0:
+                    tmp.data[idx] = A[i, j]
+                    tmp.indices[idx] = j
+                    idx += 1
+        tmp.indptr[M] = idx
+        B.indptr[:] = tmp.indptr[:]
+        B.indices[:] = tmp.indices[:]
+        B.data[:] = tmp.data[:]
+    
+    rng = np.random.default_rng(42)
+    tmp = sparse.random(20, 20, density=0.1, format='csr', dtype=np.float32, random_state=rng)
+    A = tmp.toarray()
+    B = tmp.tocsr(copy=True)
+    B.indptr[:] = -1
+    B.indices[:] = -1
+    B.data[:] = -1
+
+    outB = CSR.dtype._typeclass.as_ctypes()(indptr=B.indptr.__array_interface__['data'][0],
+                                            indices=B.indices.__array_interface__['data'][0],
+                                            data=B.data.__array_interface__['data'][0])
+
+    func = dense_to_csr_local_python.compile()
+    func(A=A, B=outB, M=tmp.shape[0], N=tmp.shape[1], nnz=tmp.nnz)
+
+
 def test_rgf():
 
     class BTD:
@@ -143,6 +181,7 @@ def test_rgf():
 
 
 if __name__ == '__main__':
-    test_read_structure()
-    test_write_structure()
+    # test_read_structure()
+    # test_write_structure()
+    test_local_structure()
     test_rgf()
