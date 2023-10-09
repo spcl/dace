6c90205424cbe4d72d4ff7e48c923fb52564bfce
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2023-10-09 12:19:07 +0200

    Added py312 mark.

--- tests/python_frontend/type_statement_test.py
+++ tests/python_frontend/type_statement_test.py
@@ -1,25 +1,19 @@
 # Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
 import dace
 import pytest
-import sys
 
 
+@pytest.mark.py312
 def test_type_statement():
 
-    if sys.version_info >= (3, 12):
-
-        @dace.program
-        def type_statement():
-            type Scalar[T] = T
-            A: Scalar[dace.float32] = 0
-            return A
-        
-        with pytest.raises(dace.frontend.python.common.DaceSyntaxError):
-            type_statement()
+    @dace.program
+    def type_statement():
+        type Scalar[T] = T
+        A: Scalar[dace.float32] = 0
+        return A
     
-    else:
-
-        assert True
+    with pytest.raises(dace.frontend.python.common.DaceSyntaxError):
+        type_statement()
 
 
 if __name__ == '__main__':
