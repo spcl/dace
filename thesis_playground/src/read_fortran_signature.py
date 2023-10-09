e5268940637b5d1c178000c35a40daca29eb5a93
Samuel Martin <samuel.martin@macrolino.ch>
2023-10-09 10:58:54 +0200

    Removed thesis_playground folder

--- thesis_playground/src/read_fortran_signature.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from utils.general import generate_arguments_fortran
-from execute.parameters import ParametersProvider
-
-generate_arguments_fortran('mwe_map_similar_size', np.random.default_rng(42), ParametersProvider('cloudsc_vert_loop_10'))
-
-
