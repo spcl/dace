a56f8df7278d3d35b142194fcea67ce00bdea99a
Timo Schneider <timos@inf.ethz.ch>
2023-10-17 19:00:54 +0200

    add a deprecation warning whenever we a dace struct dtype is instantiated

--- dace/dtypes.py
+++ dace/dtypes.py
@@ -1,6 +1,7 @@
 # Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
 """ A module that contains various DaCe type definitions. """
 from __future__ import print_function
+import warnings
 import ctypes
 import aenum
 import inspect
@@ -766,6 +767,7 @@ class struct(typeclass):
         self.ctype_unaligned = name
         self.dtype = self
         self._parse_field_and_types(**fields_and_types)
+        warnings.warn('DaCe struct dtype is deprecated!')
 
     @property
     def fields(self):
