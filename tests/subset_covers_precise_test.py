be528a0ca3259e7ac989c4970de587440495cd07
matteonussbauemer <m.l.nussbaumer@student.tudelft.nl>
2023-10-30 21:46:26 +0100

    add some tests for indices

--- tests/subset_covers_precise_test.py
+++ tests/subset_covers_precise_test.py
@@ -2,13 +2,12 @@
 
 import pytest
 import dace
-from dace.subsets import Subset, Range
+from dace.subsets import Indices, Subset, Range
 from dace.config import Config
 
 
 def test_integer_overlap_no_cover():
     # two overlapping subsets, neither of them covering the other
-
     subset1 = Range.from_string("0:10:1")
     subset2 = Range.from_string("5:11:1")
 
@@ -100,6 +99,57 @@ def test_symbolic_boundaries_not_symbolic_positive():
     assert (subset2.covers_precise(subset1) is False)
 
 
+def test_range_indices():
+    subset1 = Indices.from_string('0')
+    subset2 = Range.from_string('0:2:1')
+    assert (subset2.covers_precise(subset1))
+    assert (subset1.covers_precise(subset2) is False)
+    subset1 = Indices.from_string('0')
+    subset2 = Range.from_string('0:1:1')
+    assert (subset2.covers_precise(subset1))
+    assert (subset1.covers_precise(subset2))
+    subset1 = Indices.from_string('0, 1')
+    subset2 = Range.from_string('0:2:1, 2:4:1')
+    assert (subset2.covers_precise(subset1) is False)
+    assert (subset1.covers_precise(subset2) is False)
+
+def test_index_index():
+    subset1 = Indices.from_string('1')
+    subset2 = Indices.from_string('1')
+    assert (subset2.covers_precise(subset1))
+    assert (subset1.covers_precise(subset2))
+    subset1 = Indices.from_string('1')
+    subset2 = Indices.from_string('2')
+    assert (subset2.covers_precise(subset1) is False)
+    assert (subset1.covers_precise(subset2) is False)
+    subset1 = Indices.from_string('1, 2')
+    subset2 = Indices.from_string('1, 2')
+    assert (subset2.covers_precise(subset1))
+    assert (subset1.covers_precise(subset2))
+    subset1 = Indices.from_string('2, 1')
+    subset2 = Indices.from_string('1, 2')
+    assert (subset2.covers_precise(subset1) is False)
+    assert (subset1.covers_precise(subset2) is False)
+    subset1 = Indices.from_string('i')
+    subset2 = Indices.from_string('j')
+    assert (subset2.covers_precise(subset1) is False)
+    assert (subset1.covers_precise(subset2) is False)
+    subset1 = Indices.from_string('i')
+    subset2 = Indices.from_string('i')
+    assert (subset2.covers_precise(subset1))
+    assert (subset1.covers_precise(subset2))
+    subset1 = Indices.from_string('i, j')
+    subset2 = Indices.from_string('i, k')
+    assert (subset2.covers_precise(subset1) is False)
+    assert (subset1.covers_precise(subset2) is False)
+    subset1 = Indices.from_string('i, j')
+    subset2 = Indices.from_string('i, j')
+    assert (subset2.covers_precise(subset1))
+    assert (subset1.covers_precise(subset2))
+
+
+
+
 if __name__ == "__main__":
     test_integer_overlap_no_cover()
     test_integer_bounding_box_cover_coprime_step()
@@ -107,3 +157,5 @@ if __name__ == "__main__":
     test_integer_bounding_box_symbolic_step()
     test_symbolic_boundaries()
     test_symbolic_boundaries_not_symbolic_positive()
+    test_range_indices()
+    test_index_index()
