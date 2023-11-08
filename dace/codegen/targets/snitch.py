781327887cb443d30a9cdbf70c7ac917be9c5920
OpenRefactory, Inc <56681071+openrefactory@users.noreply.github.com>
2023-11-07 23:04:26 -0800

    Comment: Updated based on reviewer's comment.

--- dace/codegen/targets/snitch.py
+++ dace/codegen/targets/snitch.py
@@ -801,6 +801,8 @@ class SnitchCodeGen(TargetCodeGenerator):
         """Given a list of ssr configurations, allocate SSR streamers to the data and change
         the corresponding storage types"""
 
+        if len(ssr_configs) == 0:
+            return
         allocated = 0
         for streamer, sc in enumerate(ssr_configs):
             if sc["map"] is None:
