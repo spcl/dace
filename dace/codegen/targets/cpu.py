49de4c1b7f2ad6f9d6c6765308001fa5044cba42
Tal Ben-Nun <tbennun@gmail.com>
2023-11-28 06:55:41 -0800

    Fix consume serialization

--- dace/codegen/targets/cpu.py
+++ dace/codegen/targets/cpu.py
@@ -1913,7 +1913,7 @@ class CPUCodeGen(TargetCodeGenerator):
                                               'size_t')
 
         # Take quiescence condition into account
-        if node.consume.condition.code is not None:
+        if node.consume.condition is not None:
             condition_string = "[&]() { return %s; }, " % cppunparse.cppunparse(node.consume.condition.code, False)
         else:
             condition_string = ""
@@ -1932,7 +1932,7 @@ class CPUCodeGen(TargetCodeGenerator):
             "{num_pes}, {condition}"
             "[&](int {pe_index}, {element_or_chunk}) {{".format(
                 chunksz=node.consume.chunksize,
-                cond="" if node.consume.condition.code is None else "_cond",
+                cond="" if node.consume.condition is None else "_cond",
                 condition=condition_string,
                 stream_in=input_stream.data,  # TODO: stream arrays
                 element_or_chunk=chunk,
