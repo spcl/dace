5928fe8744289d6766507b1655dca486ea8cbb2d
Tal Ben-Nun <tbennun@gmail.com>
2023-12-03 12:50:33 -0800

    Add validation check for empty SDFGs

--- dace/sdfg/validation.py
+++ dace/sdfg/validation.py
@@ -202,6 +202,8 @@ def validate_sdfg(sdfg: 'dace.sdfg.SDFG', references: Set[int] = None, **context
             raise InvalidSDFGError("Invalid name", sdfg, None)
 
         all_blocks = set(sdfg.all_control_flow_blocks())
+        if len(all_blocks) == 0:
+            raise InvalidSDFGError('SDFG contains no states or control flow blocks', sdfg, None)
         if len(all_blocks) != len(set([s.label for s in all_blocks])):
             raise InvalidSDFGError('Found multiple blocks with the same name', sdfg, None)
 
