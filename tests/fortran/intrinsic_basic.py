a9f3e46ae7f098b4f6ba59087ad2982c8f1b09ae
Marcin Copik <mcopik@gmail.com>
2023-12-06 22:50:26 +0100

    Support DIM argument for SIZE intrinsic

--- tests/fortran/intrinsic_basic.py
+++ tests/fortran/intrinsic_basic.py
@@ -41,18 +41,31 @@ def test_fortran_frontend_bit_size_symbolic():
     test_string = """
                     PROGRAM intrinsic_math_test_bit_size
                     implicit none
-                    integer :: arrsize = 2
+                    integer :: arrsize
+                    integer :: arrsize2
+                    integer :: arrsize3
                     integer :: res(arrsize)
-                    CALL intrinsic_math_test_function(arrsize, res)
+                    integer :: res2(arrsize, arrsize2, arrsize3)
+                    integer :: res3(arrsize+arrsize2, arrsize2 * 5, arrsize3 + arrsize2*arrsize)
+                    CALL intrinsic_math_test_function(arrsize, arrsize2, arrsize3, res, res2, res3)
                     end
 
-                    SUBROUTINE intrinsic_math_test_function(arrsize, res)
+                    SUBROUTINE intrinsic_math_test_function(arrsize, arrsize2, arrsize3, res, res2, res3)
                     implicit none
                     integer :: arrsize
+                    integer :: arrsize2
+                    integer :: arrsize3
                     integer :: res(arrsize)
+                    integer :: res2(arrsize, arrsize2, arrsize3)
+                    integer :: res3(arrsize+arrsize2, arrsize2 * 5, arrsize3 + arrsize2*arrsize)
 
                     res(1) = SIZE(res)
-                    res(2) = SIZE(res)*2
+                    res(2) = SIZE(res2)
+                    res(3) = SIZE(res3)
+                    res(4) = SIZE(res)*2
+                    res(5) = SIZE(res)*SIZE(res2)*SIZE(res3)
+                    res(6) = SIZE(res2, 1) + SIZE(res2, 2) + SIZE(res2, 3)
+                    res(7) = SIZE(res3, 1) + SIZE(res3, 2) + SIZE(res3, 3)
 
                     END SUBROUTINE intrinsic_math_test_function
                     """
@@ -62,9 +75,21 @@ def test_fortran_frontend_bit_size_symbolic():
     sdfg.compile()
 
     size = 24
+    size2 = 5
+    size3 = 7
     res = np.full([size], 42, order="F", dtype=np.int32)
-    sdfg(res=res, arrsize=size)
+    res2 = np.full([size, size2, size3], 42, order="F", dtype=np.int32)
+    res3 = np.full([size+size2, size2*5, size3 + size*size2], 42, order="F", dtype=np.int32)
+    sdfg(res=res, res2=res2, res3=res3, arrsize=size, arrsize2=size2, arrsize3=size3)
     print(res)
+
+    assert res[0] == size
+    assert res[1] == size*size2*size3
+    assert res[2] == (size + size2) * (size2 * 5) * (size3 + size2*size)
+    assert res[3] == size * 2 
+    assert res[4] == res[0] * res[1] * res[2]
+    assert res[5] == size + size2 + size3
+    assert res[6] == size + size2 + size2*5 + size3 + size*size2
 
 
 if __name__ == "__main__":
