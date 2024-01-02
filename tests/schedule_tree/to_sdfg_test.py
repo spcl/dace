649b21637208aaa0825d38e5060648832817defb
Tal Ben-Nun <tbennun@gmail.com>
2024-01-02 07:18:53 -0800

    Copyright year

--- tests/schedule_tree/to_sdfg_test.py
+++ tests/schedule_tree/to_sdfg_test.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
+# Copyright 2019-2024 ETH Zurich and the DaCe authors. All rights reserved.
 """
 Tests components in conversion of schedule trees to SDFGs.
 """
