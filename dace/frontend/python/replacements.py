21b191d0e456d9db9656f6069c7c21be2b0e5922
Tal Ben-Nun <tbennun@gmail.com>
2024-01-08 01:23:57 -0800

    Implement `numpy.fft.{fft,ifft}` and library node

--- dace/frontend/python/replacements.py
+++ dace/frontend/python/replacements.py
@@ -5268,3 +5268,85 @@ def _hsplit(visitor: ProgramVisitor, sdfg: SDFG, state: SDFGState, ary: str,
 def _vsplit(visitor: ProgramVisitor, sdfg: SDFG, state: SDFGState, ary: str,
             indices_or_sections: Union[symbolic.SymbolicType, List[symbolic.SymbolicType], str]):
     return _split_core(visitor, sdfg, state, ary, indices_or_sections, axis=0, allow_uneven=False)
+
+
+############################################################################################################
+# Fast Fourier Transform numpy package (numpy.fft)
+
+def _real_to_complex(real_type: dace.typeclass):
+    if real_type == dace.float32:
+        return dace.complex64
+    elif real_type == dace.float64:
+        return dace.complex128
+    else:
+        return real_type
+
+
+def _fft_core(pv: 'ProgramVisitor',
+              sdfg: SDFG,
+              state: SDFGState,
+              a: str,
+              n: Optional[dace.symbolic.SymbolicType] = None,
+              axis=-1,
+              norm: StringLiteral = StringLiteral('backward'),
+              is_inverse: bool = False):
+    from dace.libraries.fft.nodes import FFT, IFFT  # Avoid import loops
+    if axis != 0 and axis != -1:
+        raise NotImplementedError('Only one dimensional arrays are supported at the moment')
+    if not isinstance(a, str) or a not in sdfg.arrays:
+        raise ValueError('Input must be a valid array')
+
+    libnode = FFT('fft') if not is_inverse else IFFT('ifft')
+
+    desc = sdfg.arrays[a]
+    N = desc.shape[axis]
+
+    # If n is not None, either pad input or slice and add a view
+    if n is not None:
+        raise NotImplementedError
+
+    # Compute factor
+    if norm == 'forward':
+        factor = (1 / N) if not is_inverse else 1
+    elif norm == 'backward':
+        factor = 1 if not is_inverse else (1 / N)
+    elif norm == 'ortho':
+        factor = sp.sqrt(1 / N)
+    else:
+        raise ValueError('norm argument can only be one of "forward", "backward", or "ortho".')
+    libnode.factor = factor
+
+    # Compute output type from input type
+    if is_inverse and desc.dtype not in (dace.complex64, dace.complex128):
+        raise TypeError(f'Inverse FFT only accepts complex inputs, got {desc.dtype}')
+    dtype = _real_to_complex(desc.dtype)
+
+    name, odesc = sdfg.add_temp_transient_like(desc, dtype)
+    r = state.add_read(a)
+    w = state.add_write(name)
+    state.add_edge(r, None, libnode, '_inp', Memlet.from_array(a, desc))
+    state.add_edge(libnode, '_out', w, None, Memlet.from_array(name, odesc))
+
+    return name
+
+
+@oprepo.replaces('numpy.fft.fft')
+def _fft(pv: 'ProgramVisitor',
+         sdfg: SDFG,
+         state: SDFGState,
+         a: str,
+         n: Optional[dace.symbolic.SymbolicType] = None,
+         axis=-1,
+         norm: StringLiteral = StringLiteral('backward')):
+    return _fft_core(pv, sdfg, state, a, n, axis, norm, False)
+
+
+@oprepo.replaces('numpy.fft.ifft')
+def _ifft(pv: 'ProgramVisitor',
+          sdfg: SDFG,
+          state: SDFGState,
+          a,
+          n=None,
+          axis=-1,
+          norm: StringLiteral = StringLiteral('backward')):
+    return _fft_core(pv, sdfg, state, a, n, axis, norm, True)
