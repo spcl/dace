eef71e40c08b84e44d8c7e72d9298052098a8192
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2024-01-23 13:07:36 +0100

    When triggering recursive allocation of nested data, use the SDFG dataname, not the code-generation name.

--- dace/codegen/targets/cpu.py
+++ dace/codegen/targets/cpu.py
@@ -355,8 +355,9 @@ class CPUCodeGen(TargetCodeGenerator):
                         ctypedef = dtypes.pointer(v.dtype).ctype if isinstance(v, data.Array) else v.dtype.ctype
                         defined_type = DefinedType.Scalar if isinstance(v, data.Scalar) else DefinedType.Pointer
                         self._dispatcher.declared_arrays.add(f"{name}->{k}", defined_type, ctypedef)
-                        self.allocate_array(sdfg, dfg, state_id, nodes.AccessNode(f"{name}.{k}"), v, function_stream,
-                                            declaration_stream, allocation_stream)
+                        # NOTE: The access node dataname must "navigate" the structure with dots (not arrows).
+                        self.allocate_array(sdfg, dfg, state_id, nodes.AccessNode(f"{node.data}.{k}"), v,
+                                            function_stream, declaration_stream, allocation_stream)
             return
         if isinstance(nodedesc, (data.StructureView, data.View)):
             return self.allocate_view(sdfg, dfg, state_id, node, function_stream, declaration_stream, allocation_stream)
