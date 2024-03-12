8534fb3b1766f8873d78bfd55cf4963a8c78fc08
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-03-12 15:06:59 +0100

    Create test with overlapping ranges with two different access nodes

--- tests/warn_on_potential_data_race_test.py
+++ tests/warn_on_potential_data_race_test.py
@@ -121,7 +121,50 @@ def test_memlet_range_write_read_overlap_ranges():
         with dace.config.set_temporary('experimental', 'check_race_conditions', value=True):
             sdfg(N=N, A=A, B=B, C=C)
 
+def test_memlet_overlap_ranges_two_access_nodes():
+    sdfg = dace.SDFG('memlet_range_write_read_overlap_ranges')
+    state = sdfg.add_state()
+    N = dace.symbol("N", dtype=dace.int32)
+    sdfg.add_array("A", (N,), dace.int32)
+    A1 = state.add_access("A")
+    A2 = state.add_access("A")
+    sdfg.add_array("B", (N,), dace.int32)
+    B1 = state.add_access("B")
+    B2 = state.add_access("B")
+
+    state.add_mapped_tasklet(
+        name="first_tasklet",
+        code="b = a + 10",
+        inputs={"a": dace.Memlet(data="A", subset="k")},
+        outputs={"b": dace.Memlet(data="B", subset="k")},
+        map_ranges={"k": "0:N"},
+        external_edges=True,
+        input_nodes={"A": A1},
+        output_nodes={"B": B1}
+    )
+    state.add_mapped_tasklet(
+        name="second_tasklet",
+        code="b = a - 20",
+        inputs={"a": dace.Memlet(data="A", subset="k")},
+        outputs={"b": dace.Memlet(data="B", subset="k")},
+        map_ranges={"k": "0:N"},
+        external_edges=True,
+        input_nodes={"A": A2},
+        output_nodes={"B": B2}
+    )
+
+    N = 6
+    A = np.arange(N, dtype=np.int32)
+    B = np.zeros((N,), dtype=np.int32)
+    C = 20 * A
+
+    with pytest.warns(UserWarning):
+        with dace.config.set_temporary('experimental', 'check_race_conditions', value=True):
+            sdfg(N=N, A=A, B=B, C=C)
+
+
 if __name__ == '__main__':
     test_memlet_range_not_overlap_ranges()
     test_memlet_range_write_write_overlap_ranges()
     test_memlet_range_write_read_overlap_ranges()
+    test_memlet_overlap_ranges_two_access_nodes()
