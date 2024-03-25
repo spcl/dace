5c41bc479782c56df6ebdcf93fcdf4fecbc32654
Jan Kleine <jan.kleine@inf.ethz.ch>
2024-03-25 14:10:15 +0100

    SPMMD add status check

--- dace/libraries/sparse/nodes/spmmd.py
+++ dace/libraries/sparse/nodes/spmmd.py
@@ -157,7 +157,7 @@ class MKLSPMMDExpansion(library.ExpandTransformation):
         code += f"""
             sparse_status_t __status = {func_pref}_sp2md(__A_op, __A_descr, __A, __B_op, __B_descr, __B, {node.alpha}, {node.beta}, _C, {layout}, {C.shape[1]});
             if (__status != SPARSE_STATUS_SUCCESS) {{
-                exit((int) __status);
+                printf("MKL SP2MD failed with status %d\\n", __status);
             }}
         """
 
