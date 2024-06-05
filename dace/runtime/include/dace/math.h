1d692d18f96f93ece3ae59dc87768c9f0a0cd9ec
Ben Weber <dev.ben.weber@gmail.com>
2024-06-05 17:51:02 +0200

    Removed `common_type` pow overload

--- dace/runtime/include/dace/math.h
+++ dace/runtime/include/dace/math.h
@@ -507,13 +507,6 @@ namespace dace
             return result;
         }
 
-        template<typename T, typename U>
-        DACE_CONSTEXPR DACE_HDFI typename std::common_type<T, U>::type pow(const T& a, const U& b)
-        {   
-            using c_type = typename std::common_type<T, U>::type;
-            return (c_type)std::pow((c_type)a, (c_type)b);
-        }
-
         template<typename T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
         DACE_CONSTEXPR DACE_HDFI T ifloor(const T& a)
         {
