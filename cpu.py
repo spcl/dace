9ea89e73553e1e120c6dfb0588f9eca321c64c9e
Sameeranjoshi <joshisameeran17@gmail.com>
2024-07-18 16:38:38 -0600

    cpu, gpu basic tests

---
+++ cpu.py
@@ -0,0 +1,15 @@
+import dace
+import numpy as np
+
+@dace.program
+def cpu_getstarted(A, B):
+    return A + B
+
+if __name__ == "__main__":
+    #a = np.random.rand(2,3)
+    a = 10
+    b = 20
+    print ("before dace(CPU) (a,b)", a, b)
+    print("after dace(CPU)", cpu_getstarted(a, b))
+    sdfg = cpu_getstarted.to_sdfg(a, b)
+    # sdfg.apply_gpu_transformations()
