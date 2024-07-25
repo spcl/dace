01c8bf5483955fb1c5141f82d4687ad3f98a31a9
Sameeranjoshi <joshisameeran17@gmail.com>
2024-07-24 21:16:18 -0600

    MPI basic test

---
+++ mpi.py
@@ -0,0 +1,22 @@
+
+import dace
+import numpy as np
+from dace.transformation.dataflow import MPITransformMap
+
+        
+@dace.program
+def mpi_vector_add(A: dace.int32[20], B: dace.int32[20], C: dace.int32[20]):
+    for i in dace.map[0:20]:       # parallelization construct
+       C[i] =  A[i] + B[i]
+
+if __name__ == '__main__':
+    sdfg = mpi_vector_add.to_sdfg(simplify=False)   # compiled SDFG
+    sdfg.apply_transformations(MPITransformMap)
+    
+    # call with values
+    A = np.ones((20), dtype=np.int32)   # 1,1,1,1,...
+    B = np.ones((20), dtype=np.int32)   # 1,1,1,1,...
+    C = np.zeros((20), dtype=np.int32)  # 0,0,0,0,...
+    sdfg(A, B, C)
+    # ref = np.full(20, 2, dtype=np.int32)     # 2,2,2,2,...
+    # assert np.array_equal(ref, C)
