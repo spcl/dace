d57aa3ef355601d4488c655bb57c14f1c97542d1
thrudprimrose <budanaz.yakup@gmail.com>
2024-09-05 17:18:02 +0200

    Add optional setting map parameters (not forced to auto)

--- dace/sdfg/nodes.py
+++ dace/sdfg/nodes.py
@@ -793,7 +793,10 @@ class MapEntry(EntryNode):
         result = {}
         # Add map params
         for p, rng in zip(self._map.params, self._map.range):
-            result[p] = dtypes.result_type_of(infer_expr_type(rng[0], symbols), infer_expr_type(rng[1], symbols))
+            if p in self.map.param_types.keys():
+                result[p] = dtypes.typeclass(self.map.param_types[p])
+            else:
+                result[p] = dtypes.result_type_of(infer_expr_type(rng[0], symbols), infer_expr_type(rng[1], symbols))
 
         # Add dynamic inputs
         dyn_inputs = set(c for c in self.in_connectors if not c.startswith('IN_'))
@@ -877,6 +880,7 @@ class Map(object):
     # List of (editable) properties
     label = Property(dtype=str, desc="Label of the map")
     params = ListProperty(element_type=str, desc="Mapped parameters")
+    param_types = DictProperty(key_type=str, value_type=str, desc="Types of mapped parameters")
     range = RangeProperty(desc="Ranges of map parameters", default=sbs.Range([]))
     schedule = EnumProperty(dtype=dtypes.ScheduleType, desc="Map schedule", default=dtypes.ScheduleType.Default)
     unroll = Property(dtype=bool, desc="Map unrolling")
