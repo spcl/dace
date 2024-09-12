7edeaff9822337b70e9b25dbcf7519597dc22fed
Luigi Fusco <luigifusco98@gmail.com>
2024-09-12 16:15:10 +0200

    add map split transformation

--- dace/transformation/dataflow/__init__.py
+++ dace/transformation/dataflow/__init__.py
@@ -14,6 +14,7 @@ from .map_unroll import MapUnroll
 from .trivial_map_elimination import TrivialMapElimination
 from .trivial_map_range_elimination import TrivialMapRangeElimination
 from .otf_map_fusion import OTFMapFusion
+from .map_split import MapSplit
 
 # Data movement
 from .strip_mining import StripMining
