6d368704f556138724a9f804c4f713d45e1454a3
Luigi Fusco <luigifusco98@gmail.com>
2024-09-13 13:54:25 +0200

    fix test

--- tests/transformations/conditional_elimination_test.py
+++ tests/transformations/conditional_elimination_test.py
@@ -4,28 +4,34 @@ import dace
 from dace.transformation.passes import ResolveCondition
 from dace.transformation.dataflow import MapFusion
 import numpy as np
+import pytest
 
-
-N = dace.symbol('N', dtype=dace.int32)
-
-
-@dace.program
-def simple_program(flag: dace.bool, arr: dace.float32[N]):
-    tmp1 = np.empty_like(arr)
-    tmp2 = np.empty_like(arr)
-    for i in dace.map[0:N]:
-        tmp1[i] = arr[i] + 1
-    if flag:
-        for i in dace.map[0:N]:
-            tmp2[i] = tmp1[i] * 2
-    else:
-        for i in dace.map[0:N]:
-            tmp2[i] = tmp1[i] * 3
-    for i in dace.map[0:N]:
-        arr[i] = tmp2[i] + 1
+@pytest.mark.parametrize("condition, after_pass, after_simplify, map_fusion_applications", [
+    ('flag', 3, 1, 2),
+    ('not flag', 3, 1, 2),
+    ('dummy', 4, 4, 0)
+])
 
 
 def test_application_and_fusion(condition: str, after_pass: int, after_simplify: int, map_fusion_applications: int):
+    N = dace.symbol('N', dtype=dace.int32)
+
+    @dace.program
+    def simple_program(flag: dace.bool, arr: dace.float32[N]):
+        tmp1 = np.empty_like(arr)
+        tmp2 = np.empty_like(arr)
+        for i in dace.map[0:N]:
+            tmp1[i] = arr[i] + 1
+        if flag:
+            for i in dace.map[0:N]:
+                tmp2[i] = tmp1[i] * 2
+        else:
+            for i in dace.map[0:N]:
+                tmp2[i] = tmp1[i] * 3
+        for i in dace.map[0:N]:
+            arr[i] = tmp2[i] + 1
+
+
     sdfg = simple_program.to_sdfg()
 
     assert len(sdfg.nodes()) == 4
