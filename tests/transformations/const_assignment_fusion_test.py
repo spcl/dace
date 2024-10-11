378673ae33496c88faa95e529f4833791157db58
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2024-10-11 13:35:10 +0200

    if the ranges are not exactly the same, still try to fuse by adding a   grid-stride loop as a guard.

--- tests/transformations/const_assignment_fusion_test.py
+++ tests/transformations/const_assignment_fusion_test.py
@@ -7,6 +7,7 @@ import dace
 from dace.transformation.dataflow.const_assignment_fusion import ConstAssignmentMapFusion, ConstAssignmentStateFusion
 from dace.transformation.interstate import StateFusionExtended
 
+K = dace.symbol('K')
 M = dace.symbol('M')
 N = dace.symbol('N')
 
@@ -152,6 +153,47 @@ def test_free_floating_fusion():
 
 
 @dace.program
+def assign_top_face(A: dace.float32[K, M, N]):
+    for t1, t2 in dace.map[0:M, 0:N]:
+        A[0, t1, t2] = 1
+
+
+@dace.program
+def assign_bottom_face(A: dace.float32[K, M, N]):
+    for t1, t2 in dace.map[0:M, 0:N]:
+        A[K - 1, t1, t2] = 1
+
+
+@dace.program
+def assign_bounary_3d(A: dace.float32[K, M, N], B: dace.float32[K, M, N]):
+    assign_top_face(A)
+    assign_bottom_face(B)
+
+
+def test_fusion_with_multiple_indices():
+    A = np.random.uniform(size=(3, 4, 5)).astype(np.float32)
+    B = np.random.uniform(size=(3, 4, 5)).astype(np.float32)
+
+    # Construct SDFG with the maps on separate states.
+    g = assign_bounary_3d.to_sdfg(simplify=True, validate=True, use_cache=False)
+    g.save(os.path.join('_dacegraphs', '3d-0.sdfg'))
+    g.validate()
+    actual_A = deepcopy(A)
+    actual_B = deepcopy(B)
+    g(A=actual_A, B=actual_B, K=3, M=4, N=5)
+
+    g.apply_transformations(ConstAssignmentMapFusion)
+    g.save(os.path.join('_dacegraphs', '3d-1.sdfg'))
+    g.validate()
+    our_A = deepcopy(A)
+    our_B = deepcopy(B)
+    g(A=our_A, B=our_B, K=3, M=4, N=5)
+
+    # print(our_A)
+    assert np.allclose(our_A, actual_A)
+
+
+@dace.program
 def assign_bounary_with_branch(A: dace.float32[M, N], B: dace.float32[M, N]):
     assign_top_row_branched(A)
     assign_bottom_row(B)
@@ -185,3 +227,4 @@ if __name__ == '__main__':
     test_interstate_fusion()
     test_free_floating_fusion()
     test_fusion_with_branch()
+    test_fusion_with_multiple_indices()
