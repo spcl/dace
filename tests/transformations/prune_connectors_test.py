41ff418a4d255f13be0fcaf28066ed341270fd2b
Tal Ben-Nun <tbennun@users.noreply.github.com>
2024-10-11 12:07:20 -0700

    Update prune_connectors_test.py

--- tests/transformations/prune_connectors_test.py
+++ tests/transformations/prune_connectors_test.py
@@ -207,7 +207,7 @@ def _make_read_write_sdfg(
         istate.add_nedge(
             inner_A,
             inner_B,
-            dace.Memlet("inner_A[0:4, 0:4] -> 0:4, 0:4"),
+            dace.Memlet("inner_A[0:4, 0:4] -> [0:4, 0:4]"),
         )
     else:
         # Because the `data` filed of the involved memlets differs the read to
@@ -216,7 +216,7 @@ def _make_read_write_sdfg(
         istate.add_nedge(
             inner_A,
             inner_B,
-            dace.Memlet("inner_B[0:4, 0:4] -> 0:4, 0:4"),
+            dace.Memlet("inner_B[0:4, 0:4] -> [0:4, 0:4]"),
         )
 
     # Add the nested SDFG
