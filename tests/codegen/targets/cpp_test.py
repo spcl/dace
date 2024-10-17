0027888495201f9a0c6578d9546fabf4e989954b
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2024-10-17 18:42:41 +0200

    Add even more tests to cover strides and offsets.

--- tests/codegen/targets/cpp_test.py
+++ tests/codegen/targets/cpp_test.py
@@ -15,63 +15,132 @@ class ReshapeStrides(unittest.TestCase):
 
         # To smaller-sized shape
         target_dims = [1]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, [1])
+        self.assertEqual(strides, [1])
 
         # To equal-sized shape
         target_dims = [1, 1]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, [1, 1])
+        self.assertEqual(strides, [1, 1])
 
         # To larger-sized shape
         target_dims = [1, 1, 1]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, [1, 1, 1])
+        self.assertEqual(strides, [1, 1, 1])
 
     def test_multidim_array_some_dims_unit(self):
         r = Range([(0, 1, 1), (0, 0, 1)])
 
         # To smaller-sized shape
         target_dims = [2]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [1])
 
         # To equal-sized shape
         target_dims = [2, 1]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [1, 1])
+        # To equal-sized shape, but units first.
+        target_dims = [1, 2]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [2, 1])
 
-        # To equal-sized shape
+        # To larger-sized shape.
         target_dims = [2, 1, 1]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [1, 1, 1])
+        # To larger-sized shape, but units first.
+        target_dims = [1, 1, 2]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [2, 2, 1])
 
     def test_multidim_array_different_shape(self):
         r = Range([(0, 4, 1), (0, 5, 1)])
 
         # To smaller-sized shape
         target_dims = [30]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [1])
 
         # To equal-sized shape
         target_dims = [15, 2]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [2, 1])
+
+        # To larger-sized shape
+        target_dims = [3, 5, 2]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [10, 2, 1])
+
+    def test_from_strided_range(self):
+        r = Range([(0, 4, 2), (0, 6, 2)])
+
+        # To smaller-sized shape
+        target_dims = [12]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [1])
 
         # To equal-sized shape
-        target_dims = [3, 5, 2]
-        self.assertEqual(r.num_elements_exact(), reduce(mul, target_dims))
+        target_dims = [4, 3]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
         reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
         self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [3, 1])
+
+        # To larger-sized shape
+        target_dims = [2, 3, 2]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [6, 2, 1])
+
+    def test_from_strided_and_offset_range(self):
+        r = Range([(10, 14, 2), (10, 16, 2)])
+
+        # To smaller-sized shape
+        target_dims = [12]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [1])
+
+        # To equal-sized shape
+        target_dims = [4, 3]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [3, 1])
+
+        # To larger-sized shape
+        target_dims = [2, 3, 2]
+        self.assertEqual(reduce(mul, r.size_exact()), reduce(mul, target_dims))
+        reshaped, strides = cpp.reshape_strides(r, None, None, target_dims)
+        self.assertEqual(reshaped, target_dims)
+        self.assertEqual(strides, [6, 2, 1])
 
 
 class RedundantArrayCrashesCodegenTest(unittest.TestCase):
