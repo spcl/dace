c1d1bd7d51112b55e99e4c3facf8f6dcc3def1e9
Yakup Budanaz <budanaz.yakup@gmail.com>
2024-10-18 14:13:56 +0200

    Clean imports, minor reformatting

--- dace/sdfg/data_group.py
+++ dace/sdfg/data_group.py
@@ -1,62 +1,48 @@
 # Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
-import typing
-import aenum
-import copy as cp
-import ctypes
-import functools
-
-from abc import ABC, abstractmethod
 from collections import OrderedDict
-from numbers import Number
-from typing import Any, Dict, List, Optional, Sequence, Set, Tuple, Union
+from typing import Any, Set, Union
 
 from dace.data import Data
-import numpy
-import sympy as sp
 
-try:
-    from numpy.typing import ArrayLike
-except (ModuleNotFoundError, ImportError):
-    ArrayLike = Any
-
-import dace.dtypes as dtypes
 from dace import serialize, symbolic
-from dace.codegen import cppunparse
-from dace.properties import (DebugInfoProperty, DictProperty, EnumProperty, ListProperty, NestedDataClassProperty,
-                             OrderedDictProperty, Property, ShapeProperty, SymbolicProperty, TypeClassProperty,
-                             make_properties)
+from dace.properties import OrderedDictProperty, Property, make_properties
+
 
 def _members_to_json(members):
     if members is None:
         return None
     return [(k, serialize.to_json(v)) for k, v in members.items()]
 
+
 def _members_from_json(obj, context=None):
     if obj is None:
         return {}
     return OrderedDict((k, serialize.from_json(v, context)) for k, v in obj)
 
+
 @make_properties
 class DataGroup:
     name = Property(dtype=str, default="", allow_none=False)
-    members = OrderedDictProperty(default=OrderedDict(),
-                                  desc="Dictionary of structure members",
-                                  from_json=_members_from_json,
-                                  to_json=_members_to_json)
+    members = OrderedDictProperty(
+        default=OrderedDict(),
+        desc="Dictionary of structure members",
+        from_json=_members_from_json,
+        to_json=_members_to_json,
+    )
 
     def __init__(self, name):
         self.name = name
         self.members = OrderedDict()
         self._validate()
 
-    def add_member(self, name: str, member : Union[Data, 'DataGroup']):
+    def add_member(self, name: str, member: Union[Data, "DataGroup"]):
         if name is None or name == "":
             name = len(self.members)
         self.members[name] = member
 
     @property
     def free_symbols(self) -> Set[symbolic.SymbolicType]:
-        """ Returns a set of undefined symbols in this data descriptor. """
+        """Returns a set of undefined symbols in this data descriptor."""
         result = set()
         for k, v in self.members.items():
             result |= v.free_symbols
@@ -86,7 +72,9 @@ class DataGroup:
         return hash(serialize.dumps(self))
 
     def __repr__(self):
-        members_repr = ', '.join(f'{k}: {v.__class__.__name__}' for k, v in self.members.items())
+        members_repr = ", ".join(
+            f"{k}: {v.__class__.__name__}" for k, v in self.members.items()
+        )
         return f"DataGroup(name='{self.name}', members={{ {members_repr} }})"
 
     def __str__(self):
