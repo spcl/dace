b1f10ac57c487f1e1d6a9be6420097ef4c08a519
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2024-10-28 23:27:36 +0100

    Add further explanation (especially about explicit assumptions) and some cosmetic rephrasing of expressions.

--- tests/transformations/if_extraction_test.py
+++ tests/transformations/if_extraction_test.py
@@ -1,10 +1,14 @@
 # Copyright 2019-2024 ETH Zurich and the DaCe authors. All rights reserved.
+import os
+
+from dace.symbolic import simplify
 
 import dace
 from dace.transformation.interstate import IfExtraction
 from dace.sdfg.nodes import NestedSDFG
 
 N = dace.symbol('N', dtype=dace.int32)
+
 
 @dace.program
 def simple_application(flag: dace.bool, arr: dace.float32[N]):
@@ -27,20 +31,30 @@ def dependant_application(flag: dace.bool, arr: dace.float32[N]):
 
 
 def test_simple_application():
-    sdfg = simple_application.to_sdfg(simplify=True)
+    g = simple_application.to_sdfg(simplify=False, validate=False, use_cache=False)
+    g.simplify(verbose=True)  # Simplify (for convenience) to get the actual test graph.
+    g.save(os.path.join('_dacegraphs', 'simple-0.sdfg'))
+    g.validate()
+    g.compile()
 
-    assert len(sdfg.nodes()) == 1
+    # Before, the outer graph had only one nested SDFG.
+    assert len(g.nodes()) == 1
 
-    assert sdfg.apply_transformations_repeated([IfExtraction]) == 1
+    assert g.apply_transformations_repeated([IfExtraction]) == 1
+    g.save(os.path.join('_dacegraphs', 'simple-1.sdfg'))
+    g.validate()
+    g.compile()
 
-    assert len(sdfg.nodes()) == 4
-    assert sdfg.start_state.is_empty()
-    
-    sdfg.simplify()
-    
-    for s in sdfg.nodes():
+    # But now, the outer graph have four: two copies of the original nested SDFGs and two for branch management.
+    assert len(g.nodes()) == 4
+    assert g.start_state.is_empty()
+
+    g.simplify()
+
+    for s in g.nodes():
         for n in s.nodes():
             assert not isinstance(n, NestedSDFG)
+
 
 def test_fails_due_to_dependency():
     sdfg = dependant_application.to_sdfg(simplify=True)
