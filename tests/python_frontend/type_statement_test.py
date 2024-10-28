bceaf39cae5331b364796f7707ef85230ac8ca3f
Tal Ben-Nun <talbn@llnl.gov>
2024-10-28 12:50:07 -0700

    Revert "Wrap Python 3.12 test with a proper test"
    This reverts commit b28f82b92415c3cdc7ec787747892a42b34ed365.

--- tests/python_frontend/type_statement_test.py
+++ tests/python_frontend/type_statement_test.py
@@ -1,22 +1,22 @@
-# Copyright 2019-2024 ETH Zurich and the DaCe authors. All rights reserved.
+# Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
 import dace
 import pytest
-import sys
 
-# Check if Python version is 3.12 or higher
-if sys.version_info >= (3, 12):
-    def test_type_statement():
 
-        @dace.program
-        def type_statement():
-            type Scalar[T] = T
-            A: Scalar[dace.float32] = 0
-            return A
+# TODO: Investigate why pytest parses the DaCeProgram, even when the test is not supposed to run.
+# @pytest.mark.py312
+# def test_type_statement():
 
-        with pytest.raises(dace.frontend.python.common.DaceSyntaxError):
-            type_statement()
+#     @dace.program
+#     def type_statement():
+#         type Scalar[T] = T
+#         A: Scalar[dace.float32] = 0
+#         return A
+    
+#     with pytest.raises(dace.frontend.python.common.DaceSyntaxError):
+#         type_statement()
 
 
 if __name__ == '__main__':
-    if sys.version_info >= (3, 12):
-        test_type_statement()
+    # test_type_statement()
+    pass
