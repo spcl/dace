945b5ce4ad26ba9047467d95e75e9aec48792506
Philipp Schaad <schaad.phil@gmail.com>
2024-10-31 12:51:56 +0100

    Fix jupyter's version of SDFV (#1714)
    This requires https://github.com/spcl/dace-webclient/pull/179 to be
    merged before being ready.

--- dace/sdfg/sdfg.py
+++ dace/sdfg/sdfg.py
@@ -1464,9 +1464,11 @@ class SDFG(ControlFlowRegion):
     var sdfg_{uid} = {sdfg};
 </script>
 <script>
-    var sdfv_{uid} = new SDFV();
-    var renderer_{uid} = new SDFGRenderer(sdfv_{uid}, parse_sdfg(sdfg_{uid}),
-        document.getElementById('contents_{uid}'));
+    new SDFGRenderer(
+        checkCompatLoad(parse_sdfg(sdfg_{uid})),
+        document.getElementById("contents_{uid}"),
+        undefined, null, null, false, null, null
+    );
 </script>""".format(
             # Dumping to a string so that Jupyter Javascript can parse it
             # recursively
