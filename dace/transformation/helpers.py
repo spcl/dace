58293be05fc8509415dd04dfac09c9052af08979
Tal Ben-Nun <tbennun@gmail.com>
2024-11-01 22:00:47 -0700

    State fission: remove extraneous input connectors from boundary nodes (which no longer have incoming edges)

--- dace/transformation/helpers.py
+++ dace/transformation/helpers.py
@@ -688,6 +688,9 @@ def state_fission(subgraph: graph.SubgraphView, label: Optional[str] = None) -> 
     for e in orig_edges:
         newstate.add_edge(new_nodes[e.src], e.src_conn, new_nodes[e.dst], e.dst_conn, e.data)
 
+    for bn in boundary_nodes:
+        bn._in_connectors.clear()
+
     return newstate
 
 
