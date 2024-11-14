2ecb2d61710f5628599198cfd4537096d13cc23e
Philip Mueller <philip.mueller@cscs.ch>
2024-11-14 14:04:03 +0100

    Now they work on GPU and also updated the functionality.

--- dace/runtime/include/dace/nan.h
+++ dace/runtime/include/dace/nan.h
@@ -13,96 +13,103 @@ namespace dace
         // Defines a typeless Pi
         struct typeless_nan
         {
+            DACE_CONSTEXPR DACE_HDFI typeless_nan() noexcept = default;
+            DACE_CONSTEXPR DACE_HDFI typeless_nan(const typeless_nan&) noexcept = default;
+            DACE_CONSTEXPR DACE_HDFI typeless_nan(typeless_nan&&) noexcept = default;
+            DACE_CONSTEXPR DACE_HDFI typeless_nan& operator=(const typeless_nan&) noexcept = default;
+            DACE_CONSTEXPR DACE_HDFI typeless_nan& operator=(typeless_nan&&) noexcept = default;
+            DACE_HDFI ~typeless_nan() noexcept = default;
+
             operator int() const = delete;
-            operator float() const
+            DACE_CONSTEXPR DACE_HDFI operator float() const
             {
                 return std::numeric_limits<float>::quiet_NaN();
             }
-            operator double() const
+            DACE_CONSTEXPR DACE_HDFI operator double() const
             {
                 return std::numeric_limits<double>::quiet_NaN();
             }
-            operator long double() const
+            DACE_CONSTEXPR DACE_HDFI operator long double() const
             {
                 return std::numeric_limits<long double>::quiet_NaN();
             }
-            typeless_nan operator+() const
+            DACE_CONSTEXPR DACE_HDFI typeless_nan operator+() const
             {
                 return typeless_nan{};
             }
-            typeless_nan operator-() const
+            DACE_CONSTEXPR DACE_HDFI typeless_nan operator-() const
             {
                 return typeless_nan{};
             }
         };
 
         template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator*(const T&,  const typeless_nan&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator*(const T&,  const typeless_nan&) noexcept { return typeless_nan{}; }
 
         template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator*(const typeless_nan&,  const T&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator*(const typeless_nan&,  const T&) noexcept { return typeless_nan{}; }
 
-        inline typeless_nan
-        operator*(const typeless_nan&,  const typeless_nan&) { return typeless_nan{}; }
-
-
-        template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator+(const T&,  const typeless_nan&) { return typeless_nan{}; }
-
-        template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator+(const typeless_nan&,  const T&) { return typeless_nan{}; }
-
-        inline typeless_nan
-        operator+(const typeless_nan&,  const typeless_nan&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI typeless_nan
+        operator*(const typeless_nan&,  const typeless_nan&) noexcept { return typeless_nan{}; }
 
 
         template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator-(const T&,  const typeless_nan&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator+(const T&,  const typeless_nan&) noexcept { return typeless_nan{}; }
 
         template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator-(const typeless_nan&,  const T&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator+(const typeless_nan&,  const T&) noexcept { return typeless_nan{}; }
 
-        inline typeless_nan
-        operator-(const typeless_nan&,  const typeless_nan&) { return typeless_nan{}; }
-
-
-        template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator/(const T&,  const typeless_nan&) { return typeless_nan{}; }
-
-        template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator/(const typeless_nan&,  const T&) { return typeless_nan{}; }
-
-        inline typeless_nan
-        operator/(const typeless_nan&,  const typeless_nan&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI typeless_nan
+        operator+(const typeless_nan&,  const typeless_nan&) noexcept { return typeless_nan{}; }
 
 
         template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator%(const T&,  const typeless_nan&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator-(const T&,  const typeless_nan&) noexcept { return typeless_nan{}; }
 
         template<typename T>
-        DACE_CONSTEXPR typename std::enable_if<std::is_floating_point<T>::value, typeless_nan>::type
-        operator%(const typeless_nan&,  const T&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator-(const typeless_nan&,  const T&) noexcept { return typeless_nan{}; }
 
-        inline typeless_nan
-        operator%(const typeless_nan&,  const typeless_nan&) { return typeless_nan{}; }
+        DACE_CONSTEXPR DACE_HDFI typeless_nan
+        operator-(const typeless_nan&,  const typeless_nan&) noexcept { return typeless_nan{}; }
+
+
+        template<typename T>
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator/(const T&,  const typeless_nan&) noexcept { return typeless_nan{}; }
+
+        template<typename T>
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator/(const typeless_nan&,  const T&) noexcept { return typeless_nan{}; }
+
+        DACE_CONSTEXPR DACE_HDFI typeless_nan
+        operator/(const typeless_nan&,  const typeless_nan&) noexcept { return typeless_nan{}; }
+
+
+        template<typename T>
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator%(const T&,  const typeless_nan&) noexcept { return typeless_nan{}; }
+
+        template<typename T>
+        DACE_CONSTEXPR DACE_HDFI std::enable_if_t<std::is_floating_point<T>::value, typeless_nan>
+        operator%(const typeless_nan&,  const T&) noexcept { return typeless_nan{}; }
+
+        DACE_CONSTEXPR DACE_HDFI typeless_nan
+        operator%(const typeless_nan&,  const typeless_nan&) noexcept { return typeless_nan{}; }
 
     }
 }
 
 	//These functions allows to perfrom operations with `typeless_nan` instances.
-#	define FADAPT(F) DACE_CONSTEXPR ::dace::math::typeless_nan F (::dace::math::typeless_nan) { return ::dace::math::typeless_nan{}; }
-#	define FADAPT2(F) template<typename T1> DACE_CONSTEXPR dace::math::typeless_nan F (T1&&, dace::math::typeless_nan) { return ::dace::math::typeless_nan{}; }; \
-			  template<typename T2> DACE_CONSTEXPR dace::math::typeless_nan F (dace::math::typeless_nan, T2&&) { return ::dace::math::typeless_nan{}; }; \
-			  DACE_CONSTEXPR ::dace::math::typeless_nan F (dace::math::typeless_nan, dace::math::typeless_nan) { return ::dace::math::typeless_nan{}; }
+#	define FADAPT(F) DACE_CONSTEXPR DACE_HDFI ::dace::math::typeless_nan F (::dace::math::typeless_nan) noexcept { return ::dace::math::typeless_nan{}; }
+#	define FADAPT2(F) template<typename T1> DACE_CONSTEXPR DACE_HDFI dace::math::typeless_nan F (T1&&, dace::math::typeless_nan) noexcept { return ::dace::math::typeless_nan{}; }; \
+			  template<typename T2> DACE_CONSTEXPR DACE_HDFI dace::math::typeless_nan F (dace::math::typeless_nan, T2&&) noexcept { return ::dace::math::typeless_nan{}; }; \
+			  DACE_CONSTEXPR DACE_HDFI ::dace::math::typeless_nan F (dace::math::typeless_nan, dace::math::typeless_nan) noexcept { return ::dace::math::typeless_nan{}; }
         FADAPT(tanh); FADAPT(cos); FADAPT(sin); FADAPT(sqrt); FADAPT(tan);
         FADAPT(acos); FADAPT(asin); FADAPT(atan); FADAPT(log); FADAPT(exp);
         FADAPT(floor); FADAPT(ceil); FADAPT(round); FADAPT(abs);
