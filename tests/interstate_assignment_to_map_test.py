3186c896a26df5a00cc730291e6b34d24894d025
Yakup Budanaz <budanaz.yakup@gmail.com>
2024-11-20 19:16:14 +0100

    Add skeleton files

---
+++ tests/interstate_assignment_to_map_test.py
@@ -0,0 +1,10 @@
+import dace
+
+def _get_interstate_dependent_sdfg(interstate_edge_str: str):
+    pass
+
+def test_interstate_assignment():
+    sdfg = _get_interstate_dependent_sdfg("i = 5")
+
+if __name__ == "__main__":
+    test_interstate_assignment()
