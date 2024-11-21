0b8f50e40a5f883dee61ae845c89b5dda3c76562
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2024-11-21 16:04:22 +0100

    [Multi SDFG, Fortran] Fix `intrinsic_blas_test.py`: The `simplify()` pass is broken in `multi_sdfg` branch (probably because it didn't get the bug fixes from `main` branch). Most tests (if any) should not rely on the `simplify()` pass anyway. (#1787)

--- tests/fortran/intrinsic_blas_test.py
+++ tests/fortran/intrinsic_blas_test.py
@@ -1,32 +1,22 @@
 # Copyright 2019-2023 ETH Zurich and the DaCe authors. All rights reserved.
 
 import numpy as np
-import pytest
 
-from dace.frontend.fortran import fortran_parser
+from tests.fortran.fotran_test_helper import create_singular_sdfg_from_string, SourceCodeBuilder
+
 
 def test_fortran_frontend_dot():
-    test_string = """
-                    PROGRAM intrinsic_math_test_min_max
-                    implicit none
-                    double precision, dimension(5) :: arg1
-                    double precision, dimension(5) :: arg2
-                    double precision, dimension(2) :: res1
-                    CALL intrinsic_math_test_function(arg1, arg2, res1)
-                    end
-
-                    SUBROUTINE intrinsic_math_test_function(arg1, arg2, res1)
-                    double precision, dimension(5) :: arg1
-                    double precision, dimension(5) :: arg2
-                    double precision, dimension(2) :: res1
-
-                    res1(1) = DOT_PRODUCT(arg1, arg2)
-
-                    END SUBROUTINE intrinsic_math_test_function
-                    """
-
-    sdfg = fortran_parser.create_sdfg_from_string(test_string, "intrinsic_math_test_min_max", False)
-    sdfg.simplify(verbose=True)
+    sources, main = SourceCodeBuilder().add_file("""
+subroutine main(arg1, arg2, res1)
+  double precision, dimension(5) :: arg1
+  double precision, dimension(5) :: arg2
+  double precision, dimension(2) :: res1
+  res1(1) = dot_product(arg1, arg2)
+end subroutine main
+""").check_with_gfortran().get()
+    sdfg = create_singular_sdfg_from_string(sources, 'main', normalize_offsets=False)
+    # TODO: We should re-enable `simplify()` once we merge it.
+    # sdfg.simplify()
     sdfg.compile()
 
     size = 5
@@ -36,34 +26,25 @@ def test_fortran_frontend_dot():
 
     for i in range(size):
         arg1[i] = i + 1
-        arg2[i] = i + 5 
+        arg2[i] = i + 5
 
     sdfg(arg1=arg1, arg2=arg2, res1=res1)
 
     assert res1[0] == np.dot(arg1, arg2)
+
 
 def test_fortran_frontend_dot_range():
-    test_string = """
-                    PROGRAM intrinsic_math_test_min_max
-                    implicit none
-                    double precision, dimension(5) :: arg1
-                    double precision, dimension(5) :: arg2
-                    double precision, dimension(2) :: res1
-                    CALL intrinsic_math_test_function(arg1, arg2, res1)
-                    end
-
-                    SUBROUTINE intrinsic_math_test_function(arg1, arg2, res1)
-                    double precision, dimension(5) :: arg1
-                    double precision, dimension(5) :: arg2
-                    double precision, dimension(2) :: res1
-
-                    res1(1) = DOT_PRODUCT(arg1(1:3), arg2(1:3))
-
-                    END SUBROUTINE intrinsic_math_test_function
-                    """
-
-    sdfg = fortran_parser.create_sdfg_from_string(test_string, "intrinsic_math_test_min_max", False)
-    sdfg.simplify(verbose=True)
+    sources, main = SourceCodeBuilder().add_file("""
+subroutine main(arg1, arg2, res1)
+  double precision, dimension(5) :: arg1
+  double precision, dimension(5) :: arg2
+  double precision, dimension(2) :: res1
+  res1(1) = dot_product(arg1(1:3), arg2(1:3))
+end subroutine main
+""").check_with_gfortran().get()
+    sdfg = create_singular_sdfg_from_string(sources, 'main', normalize_offsets=False)
+    # TODO: We should re-enable `simplify()` once we merge it.
+    # sdfg.simplify()
     sdfg.compile()
 
     size = 5
@@ -73,16 +54,12 @@ def test_fortran_frontend_dot_range():
 
     for i in range(size):
         arg1[i] = i + 1
-        arg2[i] = i + 5 
+        arg2[i] = i + 5
 
     sdfg(arg1=arg1, arg2=arg2, res1=res1)
-    print(arg1)
-    print(arg2)
-    print(res1)
-
     assert res1[0] == np.dot(arg1, arg2)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     test_fortran_frontend_dot()
-    #test_fortran_frontend_dot_range()
+    test_fortran_frontend_dot_range()
