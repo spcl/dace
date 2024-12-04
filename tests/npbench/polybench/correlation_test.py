6429d91a45fbf43f352828eb8b6e45a6f7a2368d
Philip Mueller <philip.mueller@cscs.ch>
2024-12-04 09:51:46 +0100

    Chacnged the test.
    This test failed due to numerical instabilities.
    It passed once I changed the arguments, which to me does not make sense, as I think if `a` is close to `b` then `b` should also be close to `a`.
    So I changed the test to an absoluet check.

--- tests/npbench/polybench/correlation_test.py
+++ tests/npbench/polybench/correlation_test.py
@@ -70,7 +70,7 @@ def run_correlation(device_type: dace.dtypes.DeviceType):
 
     if device_type in {dace.dtypes.DeviceType.CPU, dace.dtypes.DeviceType.GPU}:
         # Parse the SDFG and apply autopot
-        sdfg = correlation_kernel.to_sdfg()
+        sdfg = correlation_kernel.to_sdfg(simplify=True)
         sdfg = auto_optimize(sdfg, device_type)
         last_value = os.environ.get('DACE_testing_serialization', '0')
         os.environ['DACE_testing_serialization'] = '0'
@@ -83,7 +83,8 @@ def run_correlation(device_type: dace.dtypes.DeviceType):
     # Compute ground truth and validate result
 
     corr_ref = ground_truth(M, float_n_ref, data_ref)
-    assert np.allclose(corr, corr_ref)
+    diff = corr_ref - corr
+    assert np.abs(diff).max() <= 10e-10
     return sdfg
 
 
