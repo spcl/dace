867f5fe08cf8563c58dd75e7bd538221a9fb925e
Marcin Copik <mcopik@gmail.com>
2024-12-06 16:12:35 +0100

    More tests

--- tests/fortran/intrinsic_bound_test.py
+++ tests/fortran/intrinsic_bound_test.py
@@ -15,7 +15,8 @@ from tests.fortran.fortran_test_helper import SourceCodeBuilder, create_singular
     * Arrays with assumed shape with offsets.
     * Arrays inside structures.
     * Arrays inside structures with local override.
-    * Arrays inside structures with multiple layers of indirection (TODO).
+    * Arrays inside structures with multiple layers of indirection.
+    * Arrays inside structures with multiple layers of indirection + assumed size.
 """
 
 def test_fortran_frontend_bound():
@@ -338,10 +339,67 @@ MODULE test_bounds
     SUBROUTINE intrinsic_bound_test_function_impl(container, res)
         TYPE(array_container), INTENT(IN) :: container
         INTEGER, DIMENSION(4) :: res
-        ! if we handle the refs correctly, this override won't fool us
-        integer, dimension(3, 10) :: data
 
         res(1) = LBOUND(container%middle%inner%inner_data, 1)   ! Should return -1
+        res(2) = LBOUND(container%middle%inner%inner_data, 2)  ! Should return 0
+        res(3) = UBOUND(container%middle%inner%inner_data, 1)  ! Should return 2
+        res(4) = UBOUND(container%middle%inner%inner_data, 2)  ! Should return 3
+    END SUBROUTINE
+END MODULE
+""", 'main').check_with_gfortran().get()
+    sdfg = create_singular_sdfg_from_string(sources, 'test_bounds.intrinsic_bound_test_function', normalize_offsets=True)
+    sdfg.simplify(verbose=True)
+    sdfg.compile()
+
+    size = 4
+    res = np.full([size], 42, order="F", dtype=np.int32)
+    sdfg(res=res)
+
+    assert np.allclose(res, [-1, 0, 2, 3])
+
+def test_fortran_frontend_bound_structure_recursive_allocatable():
+    sources, main = SourceCodeBuilder().add_file("""
+MODULE test_types
+    IMPLICIT NONE
+
+    TYPE inner_container
+        INTEGER, ALLOCATABLE, DIMENSION(:, :) :: inner_data
+    END TYPE
+
+    TYPE middle_container
+        INTEGER, ALLOCATABLE, DIMENSION(:, :) :: middle_data
+        TYPE(inner_container) :: inner
+    END TYPE
+
+    TYPE array_container
+        INTEGER, ALLOCATABLE, DIMENSION(:, :) :: outer_data
+        TYPE(middle_container) :: middle
+    END TYPE
+END MODULE
+
+MODULE test_bounds
+    USE test_types
+    IMPLICIT NONE
+
+    CONTAINS
+
+    SUBROUTINE intrinsic_bound_test_function(res)
+        IMPLICIT NONE
+        TYPE(array_container) :: container
+        INTEGER, DIMENSION(4) :: res
+
+        ALLOCATE(container%middle%inner%inner_data(-1:2, 0:3))
+        CALL intrinsic_bound_test_function_impl(container, res)
+        DEALLOCATE(container%middle%inner%inner_data)
+
+    END SUBROUTINE
+
+    SUBROUTINE intrinsic_bound_test_function_impl(container, res)
+        IMPLICIT NONE
+        TYPE(array_container), INTENT(IN) :: container
+        INTEGER, DIMENSION(4) :: res
+
+        res(1) = LBOUND(container%middle%inner%inner_data, 1)  ! Should return -1
         res(2) = LBOUND(container%middle%inner%inner_data, 2)  ! Should return 0
         res(3) = UBOUND(container%middle%inner%inner_data, 1)  ! Should return 2
         res(4) = UBOUND(container%middle%inner%inner_data, 2)  ! Should return 3
@@ -367,4 +425,5 @@ if __name__ == "__main__":
     #test_fortran_frontend_bound_allocatable_offsets()
     #test_fortran_frontend_bound_structure()
     #test_fortran_frontend_bound_structure_override()
-    test_fortran_frontend_bound_structure_recursive()
+    #test_fortran_frontend_bound_structure_recursive()
+    test_fortran_frontend_bound_structure_recursive_allocatable()
