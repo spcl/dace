e662b44fec26c8db3f36764d317a24fa65f5a482
Marcin Copik <mcopik@gmail.com>
2024-12-18 18:38:53 +0100

    bugfix

--- dace/frontend/fortran/ast_transforms.py
+++ dace/frontend/fortran/ast_transforms.py
@@ -3135,7 +3135,7 @@ class ReplaceStructArgsLibraryNodesVisitor(NodeVisitor):
     def __init__(self):
         self.nodes: List[ast_internal_classes.FNode] = []
 
-        FUNCS_TO_REPLACE = [
+        self.FUNCS_TO_REPLACE = [
             "transpose",
             "matmul"
         ]
@@ -3143,7 +3143,7 @@ class ReplaceStructArgsLibraryNodesVisitor(NodeVisitor):
     def visit_Call_Expr_Node(self, node: ast_internal_classes.Call_Expr_Node):
 
         name = node.name.name.split('__dace_')
-        if len(name) == 2 or name[1] in FUNCS_TO_REPLACE:
+        if len(name) == 2 and name[1].lower() in self.FUNCS_TO_REPLACE:
             self.nodes.append(node)
 
     def visit_Execution_Part_Node(self, node: ast_internal_classes.Execution_Part_Node):
