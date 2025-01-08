93722601026202406b86409860cc57d52bb2fcf4
acalotoiu <alex@DESKTOP-0FDBKFG.>
2025-01-08 19:38:00 +0100

    fixes

--- tests/fortran/global_test.py
+++ tests/fortran/global_test.py
@@ -19,106 +19,71 @@ import dace.frontend.fortran.ast_utils as ast_utils
 import dace.frontend.fortran.ast_internal_classes as ast_internal_classes
 
 
+
+from tests.fortran.fortran_test_helper import SourceCodeBuilder
+from dace.frontend.fortran.fortran_parser import create_singular_sdfg_from_string
+
 def test_fortran_frontend_global():
     """
     Tests that the Fortran frontend can parse complex global includes.
     """
-    test_string = """
-        PROGRAM global_test
-            implicit none
-            USE global_test_module_subroutine, ONLY: global_test_function
-            
-            REAL :: d(4), a(4,4,4)
-
-            CALL global_test_function(d)
-
-        end
-                    
-
-                    """
-    sources={}
-    sources["global_test"]=test_string
-    sources["global_test_module_subroutine.f90"]="""
-                    MODULE global_test_module_subroutine
-                    
-                    CONTAINS
-                    
-                    SUBROUTINE global_test_function(d)
-                    USE global_test_module, ONLY: outside_init,simple_type
-                    USE nested_one, ONLY: nested
-                    double precision d(4)
-                    double precision :: a(4,4,4)
-                    integer :: i
-                   
-                 
-
-            
-                    TYPE(simple_type) :: ptr_patch
-            
-                    double precision d(4)
-                    ptr_patch%w(:,:,:)=5.5
-
-                    i=outside_init
-                    CALL nested(i,ptr_patch%w)
-                    d(i+1)=5.5+ptr_patch%w(3,3,3)
-                    
-                    END SUBROUTINE global_test_function
-                    END MODULE global_test_module_subroutine
-                    """
-    sources["global_test_module.f90"]="""
-                    MODULE global_test_module
-                    IMPLICIT NONE
-                    TYPE simple_type
-                        double precision,POINTER :: w(:,:,:)
-                        integer a
-                
-                    END TYPE simple_type
-                    integer outside_init=1
-                    END MODULE global_test_module
-                    """
-    
-    sources["nested_one.f90"]="""
-                    MODULE nested_one
-                    IMPLICIT NONE
-                    CONTAINS
-                    SUBROUTINE nested(i,a)
-                    USE nested_two, ONLY: nestedtwo
-                    integer :: i
-                    double precision :: a(:,:,:)
-
-                    i=0
-                    CALL nestedtwo(i)
-                    a(i+1,i+1,i+1)=5.5
-                    END SUBROUTINE nested
-                    
-                    END MODULE nested_one
-                    """
-    sources["nested_two.f90"]="""
-                    MODULE nested_two
-                    IMPLICIT NONE
-                    CONTAINS
-                    SUBROUTINE nestedtwo(i)
-                    USE global_test_module, ONLY: outside_init
-                    integer :: i
-                    
-                    i = outside_init+1
-                    
-                    END SUBROUTINE nestedtwo
-                    
-                    END MODULE nested_two
-                    """
-
-    sdfg = fortran_parser.create_sdfg_from_string(test_string, "global_test",sources=sources,normalize_offsets=True)
+    sources, main = SourceCodeBuilder().add_file("""
+module global_test_module
+  implicit none
+  type simple_type
+    double precision, pointer :: w(:, :, :)
+    integer a
+  end type simple_type
+  integer :: outside_init = 1
+end module global_test_module
+""").add_file("""
+module nested_two
+  implicit none
+contains
+  subroutine nestedtwo(i)
+    use global_test_module, only: outside_init
+    integer :: i
+    i = outside_init + 1
+  end subroutine nestedtwo
+end module nested_two
+""").add_file("""
+module nested_one
+  implicit none
+contains
+  subroutine nested(i, a)
+    use nested_two, only: nestedtwo
+    integer :: i
+    double precision :: a(:, :, :)
+    i = 0
+    call nestedtwo(i)
+    a(i + 1, i + 1, i + 1) = 5.5
+  end subroutine nested
+end module nested_one
+""").add_file("""
+subroutine main(d)
+  use global_test_module, only: outside_init, simple_type
+  use nested_one, only: nested
+  double precision :: d(4)
+  double precision :: a(4, 4, 4)
+  integer :: i
+  type(simple_type) :: ptr_patch
+  ptr_patch%w(:, :, :) = 5.5
+  i = outside_init
+  call nested(i, ptr_patch%w)
+  d(i + 1) = 5.5 + ptr_patch%w(3, 3, 3)
+end subroutine main
+""").check_with_gfortran().get()
+    sdfg = create_singular_sdfg_from_string(sources, 'main')
     sdfg.simplify(verbose=True)
     sdfg.save('test.sdfg')
     a = np.full([4], 42, order="F", dtype=np.float64)
-    a2 = np.full([4,4,4], 42, order="F", dtype=np.float64)
-    #TODO Add validation - but we need python structs for this.
-    #sdfg(d=a,a=a2)
-    #assert (a[0] == 42)
-    #assert (a[1] == 5.5)
-    #assert (a[2] == 42)
+    a2 = np.full([4, 4, 4], 42, order="F", dtype=np.float64)
+    # TODO Add validation - but we need python structs for this.
+    # sdfg(d=a,a=a2)
+    # assert (a[0] == 42)
+    # assert (a[1] == 5.5)
+    # assert (a[2] == 42)
+
 
 if __name__ == "__main__":
-
     test_fortran_frontend_global()
--- tests/fortran/long_tasklet_test.py
+++ tests/fortran/long_tasklet_test.py
@@ -5,50 +5,44 @@ from dace.frontend.fortran import fortran_parser
 import dace.frontend.fortran.ast_transforms as ast_transforms
 import dace.frontend.fortran.ast_internal_classes as ast_internal_classes
 import numpy as np
+from tests.fortran.fortran_test_helper import SourceCodeBuilder
+from dace.frontend.fortran.fortran_parser import create_singular_sdfg_from_string
+
 
 def test_fortran_frontend_long_tasklet():
     """
     Tests that the Fortran frontend can parse array accesses and that the accessed indices are correct.
     """
-    test_string = """
-                    PROGRAM long_tasklet_test
-                    implicit none
-
-                    
-                    type test_type
-                        integer :: indices(5)
-                        integer :: start
-                        integer :: end
-                    end type
-
-                    double precision d(5)
-                    double precision, dimension(5) :: arr
-                    double precision, dimension(50:54) :: arr3
-                    CALL long_tasklet_test_function(d)
-                    end
-
-                    SUBROUTINE long_tasklet_test_function(d)
-                    double precision d(5)
-                    double precision, dimension(50:54) :: arr4
-                    double precision, dimension(5) :: arr
-                    type(test_type) :: ind
-
-                    arr(:)=2.0
-                    ind%indices(:)=1
-                    d(2)=5.5
-                    d(1)=arr(1)*arr(ind%indices(1))!+arr(2,2,2)*arr(ind%indices(2,2,2),2,2)!+arr(3,3,3)*arr(ind%indices(3,3,3),3,3)
-
-                    END SUBROUTINE long_tasklet_test_function
-                    """
-    sources={}
-    sources["long_tasklet_test"]=test_string
-    sdfg = fortran_parser.create_sdfg_from_string(test_string, "long_tasklet_test", True, sources=sources)
+    sources, main = SourceCodeBuilder().add_file("""
+module lib
+  implicit none
+  type test_type
+    integer :: indices(5)
+    integer :: start
+    integer :: end
+  end type
+end module lib
+""").add_file("""
+subroutine main(d)
+  use lib
+  implicit none
+  double precision d(5)
+  double precision, dimension(50:54) :: arr4
+  double precision, dimension(5) :: arr
+  type(test_type) :: ind
+  arr(:) = 2.0
+  ind%indices(:) = 1
+  d(2) = 5.5
+  d(1) = arr(1)*arr(ind%indices(1))!+arr(2,2,2)*arr(ind%indices(2,2,2),2,2)!+arr(3,3,3)*arr(ind%indices(3,3,3),3,3)
+end subroutine main
+""").check_with_gfortran().get()
+    sdfg = create_singular_sdfg_from_string(sources, 'main')
     sdfg.simplify(verbose=True)
     a = np.full([5], 42, order="F", dtype=np.float64)
     sdfg(d=a)
     assert (a[1] == 5.5)
     assert (a[0] == 4)
- 
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
     test_fortran_frontend_long_tasklet()
