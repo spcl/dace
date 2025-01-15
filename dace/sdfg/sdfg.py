5bdc6c1f9a9db99ca5a2bcf8beeb7b58ac5bc12b
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2025-01-15 16:48:58 +0100

    On a second thought, just copy it from `main` branch, where it has been dealt with years ago.

--- dace/sdfg/sdfg.py
+++ dace/sdfg/sdfg.py
@@ -109,6 +109,8 @@ def _replace_dict_keys(d, old, new):
         warnings.warn(f"Trying to replace key with the same name {old} ... skipping.")
         return
     if old in d:
+        if new in d:
+            warnings.warn('"%s" already exists in SDFG' % new)
         d[new] = d[old]
         del d[old]
 
