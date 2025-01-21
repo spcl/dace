7e46eacc0d9899d50654cc8fe3f711e4a8ed04d4
acalotoiu <alex@DESKTOP-0FDBKFG.>
2025-01-21 17:14:04 +0100

    local

--- tests/fortran/view_collapse_test.py
+++ tests/fortran/view_collapse_test.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from dace.frontend.fortran.fortran_parser import create_singular_sdfg_from_string
 from tests.fortran.fortran_test_helper import SourceCodeBuilder
-
+import dace
 
 def test_fortran_frontend_view_collapse():
     """
@@ -19,11 +19,13 @@ contains
   subroutine view_collapse_test_function(dd)
     use lib1, only: outside_init
     double precision dd
-    real:: bob = epsilon(1.0)
+    real:: bob = 1
+          
     if (dd>3) then
       dd = 5.5 + bob 
     else
-      dd = 6.5
+      dd = 336.5
+              
     end if
   end subroutine view_collapse_test_function
 end module lib2
@@ -41,11 +43,14 @@ subroutine main(d)
 end subroutine main
 """).check_with_gfortran().get()
     sdfg = create_singular_sdfg_from_string(sources, 'main')
+    
     sdfg.simplify(verbose=True)
+
     a = np.full([4,4], 42, order="F", dtype=np.float64)
+    #sdfg=dace.sdfg.utils.load_precompiled_sdfg(".dacecache/main/")
     sdfg(d=a, outside_init=0)
     assert (a[0,0] == 42)
-    assert (a[1,0] == 5.5)
+    assert (a[1,0] == 6.5)
     assert (a[2,0] == 42)
 
 
