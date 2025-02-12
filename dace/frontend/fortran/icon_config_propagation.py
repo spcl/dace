626928daac35de6a1cf150250882fcaadadc89b8
Pratyai Mazumder <pratyai.mazumder@gmail.com>
2025-02-12 06:16:10 +0100

    clean up icon config propagation file.

--- dace/frontend/fortran/icon_config_propagation.py
+++ dace/frontend/fortran/icon_config_propagation.py
@@ -30,34 +30,6 @@ def find_path_recursive(base_dir):
     return fortran_files
 
 
-def read_lines_between(file_path: str, start_str: str, end_str: str) -> list[str]:
-    lines_between = []
-    with open(file_path, 'r') as file:
-        capture = False
-        for line in file:
-            if start_str in line:
-                capture = True
-                continue
-            if end_str in line:
-                if capture:
-                    capture = False
-                    break
-            if capture:
-                lines_between.append(line.strip())
-    return lines_between[1:]
-
-
-def parse_assignments(assignments: list[str]) -> list[tuple[str, str]]:
-    parsed_assignments = []
-    for assignment in assignments:
-        # Remove comments
-        assignment = assignment.split('!')[0].strip()
-        if '=' in assignment:
-            a, b = assignment.split('=', 1)
-            parsed_assignments.append((a.strip(), b.strip()))
-    return parsed_assignments
-
-
 def config_injection_list(root: str = 'dace/frontend/fortran/conf_files') -> List[ConstTypeInjection]:
     cfgs = [
         (('radiation_aerosol', 'aerosol_type'), f"{root}/aerosol_obj.txt"),
@@ -85,30 +57,22 @@ if __name__ == "__main__":
         already_parsed_ast = None
 
     base_dir_ecrad = f"{base_icon_path}/externals/ecrad"
-    base_dir_icon = f"{base_icon_path}/src"
     fortran_files = find_path_recursive(base_dir_ecrad)
-    inc_list = [f"{base_icon_path}/externals/ecrad/include"]
 
-    # Construct the primary ECRad AST.
+    # Construct the primary ECRAD AST.
     parse_cfg = ParseConfig(
         sources=[Path(f) for f in fortran_files],
         entry_points=[('radiation_interface', 'radiation')],
         config_injections=config_injection_list('dace/frontend/fortran/conf_files'),
     )
-    #already_parsed_ast=None
     if already_parsed_ast is None:
         ecrad_ast = create_fparser_ast(parse_cfg)
         already_parsed_ast_bool = False
     else:
-        mini_parser=pf().create(std="f2008")
+        mini_parser = pf().create(std="f2008")
         ecrad_ast = mini_parser(ffr(file_candidate=already_parsed_ast))
         already_parsed_ast_bool = True
 
-    # previous steps were used to generate the initial list of assignments for ECRAD
-    # this includes user config and internal enumerations of ICON
-    # the previous ASTs can be now disregarded
-    # we only keep the list of assignments and propagate it to ECRAD parsing.
-    print(f"{base_icon_path}/{icon_file}")
     already_parsed_ast_bool = False
     fortran_parser.create_sdfg_from_fortran_file_with_options(
         parse_cfg,
