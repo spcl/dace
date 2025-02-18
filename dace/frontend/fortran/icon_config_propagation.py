0b664675bdd7d53b381b10c71aad043fdc173eea
acalotoiu <acalotoiu@inf.ethz.ch>
2025-02-18 20:37:48 +0100

    icon config

--- dace/frontend/fortran/icon_config_propagation.py
+++ dace/frontend/fortran/icon_config_propagation.py
@@ -30,7 +30,35 @@ def find_path_recursive(base_dir):
     return fortran_files
 
 
-def config_injection_list(root: str = 'dace/frontend/fortran/conf_files') -> List[ConstTypeInjection]:
+def read_lines_between(file_path: str, start_str: str, end_str: str) -> list[str]:
+    lines_between = []
+    with open(file_path, 'r') as file:
+        capture = False
+        for line in file:
+            if start_str in line:
+                capture = True
+                continue
+            if end_str in line:
+                if capture:
+                    capture = False
+                    break
+            if capture:
+                lines_between.append(line.strip())
+    return lines_between[1:]
+
+
+def parse_assignments(assignments: list[str]) -> list[tuple[str, str]]:
+    parsed_assignments = []
+    for assignment in assignments:
+        # Remove comments
+        assignment = assignment.split('!')[0].strip()
+        if '=' in assignment:
+            a, b = assignment.split('=', 1)
+            parsed_assignments.append((a.strip(), b.strip()))
+    return parsed_assignments
+
+
+def config_injection_list(root: str = '/home/alex/dace/dace/frontend/fortran/conf_files') -> List[ConstTypeInjection]:
     cfgs = [
         (('radiation_aerosol', 'aerosol_type'), f"{root}/aerosol_obj.txt"),
         (('radiation_cloud', 'cloud_type'), f"{root}/cloud_obj.txt"),
@@ -62,8 +90,16 @@ if __name__ == "__main__":
     # Construct the primary ECRAD AST.
     parse_cfg = ParseConfig(
         sources=[Path(f) for f in fortran_files],
-        entry_points=[('radiation_interface', 'radiation')],
-        config_injections=config_injection_list('dace/frontend/fortran/conf_files'),
+        entry_points=[('radiation_single_level', 'get_albedos')],
+        #entry_points=[('radiation_ifs_rrtm', 'gas_optics')],
+        #entry_points=[('radiation_cloud', 'crop_cloud_fraction')],
+        #entry_points=[('radiation_cloud_optics', 'cloud_optics_fn_438')],
+        #entry_points=[('radiation_mcica_lw', 'solver_mcica_lw')],
+        #entry_points=[('radiation_mcica_sw', 'solver_mcica_sw')],
+
+        #entry_points=[('radiation_aerosol_optics', 'add_aerosol_optics')],
+        
+        config_injections=config_injection_list('/home/alex/dace/dace/frontend/fortran/conf_files'),
     )
     if already_parsed_ast is None:
         ecrad_ast = create_fparser_ast(parse_cfg)
