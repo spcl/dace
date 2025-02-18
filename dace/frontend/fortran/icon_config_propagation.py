0996ce71fce13f813e158a30e3b2fd0ba2242b11
acalotoiu <acalotoiu@inf.ethz.ch>
2025-02-18 22:26:49 +0100

    revert for push

--- dace/frontend/fortran/icon_config_propagation.py
+++ dace/frontend/fortran/icon_config_propagation.py
@@ -21,8 +21,17 @@ if __name__ == "__main__":
         entry_point_module = sys.argv[5]
         entry_point_function = sys.argv[6]
     else:
-        entry_point_module = 'radiation_interface'
-        entry_point_function = 'radiation'
+        entry_point_module = 'radiation_mcica_lw'
+        entry_point_function = 'solver_mcica_lw'
+        #entry_points=[('radiation_single_level', 'get_albedos')],
+        #entry_points=[('radiation_ifs_rrtm', 'gas_optics')],
+        #entry_points=[('radiation_cloud', 'crop_cloud_fraction')],
+        #entry_points=[('radiation_cloud_optics', 'cloud_optics_fn_438')],
+        #entry_points=[('radiation_mcica_lw', 'solver_mcica_lw')],
+        #entry_points=[('radiation_mcica_sw', 'solver_mcica_sw')],
+
+        #entry_points=[('radiation_aerosol_optics', 'add_aerosol_optics')],
+        
 
     if already_parsed_ast:
         fortran_files = find_all_f90_files(Path(already_parsed_ast))
@@ -32,16 +41,7 @@ if __name__ == "__main__":
     # Construct the primary ECRAD AST.
     parse_cfg = ParseConfig(
         sources=[Path(f) for f in fortran_files],
-        entry_points=[('radiation_single_level', 'get_albedos')],
-        #entry_points=[('radiation_ifs_rrtm', 'gas_optics')],
-        #entry_points=[('radiation_cloud', 'crop_cloud_fraction')],
-        #entry_points=[('radiation_cloud_optics', 'cloud_optics_fn_438')],
-        #entry_points=[('radiation_mcica_lw', 'solver_mcica_lw')],
-        #entry_points=[('radiation_mcica_sw', 'solver_mcica_sw')],
-
-        #entry_points=[('radiation_aerosol_optics', 'add_aerosol_optics')],
         
-        config_injections=config_injection_list('/home/alex/dace/dace/frontend/fortran/conf_files'),
         entry_points=[(entry_point_module, entry_point_function)],
         config_injections=ecrad_config_injection_list('dace/frontend/fortran/conf_files'),
     )
