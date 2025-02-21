1b6c37343a9a3982a79060aa44579f41a8a4835a
Anon <ax.shen@outlook.com>
2025-02-21 21:42:57 +0100

    new systolic

--- dace/transformation/soft_hier/__init__.py
+++ dace/transformation/soft_hier/__init__.py
@@ -1,4 +1,5 @@
 from .canon import CanonTransformer
 from .systolic import SystolocTransformer
 from .new_cannon import CannonTransformer
+from .new_systolic import SystolicTransformer
 from .split_hbm_load import SplitHBMLoad
