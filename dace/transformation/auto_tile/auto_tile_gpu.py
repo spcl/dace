25e6c38f382c31de395c47247373a5219901fcbf
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-02-24 12:30:35 +0100

    Fixkeyword

--- dace/transformation/auto_tile/auto_tile_gpu.py
+++ dace/transformation/auto_tile/auto_tile_gpu.py
@@ -76,7 +76,7 @@ def _tile_gpu(
 
         copy_inputs = copy.deepcopy(inputs)
         non_transformed_time = auto_tile_util.run_and_measure_time(
-            kernel_sdfg=_kernel_sdfg, repeats=copy_inputs,
+            kernel_sdfg=_kernel_sdfg, inputs=copy_inputs,
             repeats=6, warmup=1, dev_type=dace.dtypes.StorageType.GPU_Device,
             instr_type=dace.dtypes.InstrumentationType.GPU_Events
         )
@@ -352,7 +352,7 @@ def _tile_gpu(
             else:
                 copy_inputs_2 = copy.deepcopy(inputs)
                 time = auto_tile_util.run_and_measure_time(
-                    kernel_sdfg=kernel_sdfg, repeats=copy_inputs_2,
+                    kernel_sdfg=kernel_sdfg, inputs=copy_inputs_2,
                     repeats=6, warmup=1, dev_type=dace.dtypes.StorageType.GPU_Device,
                     instr_type=dace.dtypes.InstrumentationType.GPU_Events)
                 output_from_transformed = copy_inputs_2[output_name]
