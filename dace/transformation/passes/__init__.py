e25c71c54acee9cb151c52634177d44bf18ac335
Berke Ates <berke@ates.ch>
2025-03-13 12:27:08 +0100

    added DuplicateConstArrays to init

--- dace/transformation/passes/__init__.py
+++ dace/transformation/passes/__init__.py
@@ -4,6 +4,7 @@ from .consolidate_edges import ConsolidateEdges
 from .constant_propagation import ConstantPropagation
 from .dead_dataflow_elimination import DeadDataflowElimination
 from .dead_state_elimination import DeadStateElimination
+from .duplicate_const_arrays import DuplicateConstArrays
 from .fusion_inline import FuseStates, InlineSDFGs
 from .optional_arrays import OptionalArrayInference
 from .pattern_matching import PatternMatchAndApply, PatternMatchAndApplyRepeated, PatternApplyOnceEverywhere
