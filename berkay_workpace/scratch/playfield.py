9d81a379bdc27adcd15b2201909b0d5fa54b0108
aydogdub <aydogduberkay62@gmail.com>
2025-05-12 20:57:05 +0200

    small test of GPU_Warp schedule

--- berkay_workpace/scratch/playfield.py
+++ berkay_workpace/scratch/playfield.py
@@ -1,6 +1,8 @@
 import dace
 import random
 import cupy as cp
+from dace.frontend.python.interface import inline
+
 
 from dace import registry
 from dace.sdfg.scope import ScopeSubgraphView
@@ -11,35 +13,43 @@ from dace.codegen.targets.cpp import sym2cpp
 from IPython.display import Code
 from dace.config import Config
 
-print(Config.get('compiler', 'cuda', 'implementation'))
+
+@dace.program
+def reduce_add_sync(mask: dace.uint32, value: dace.uint32):
+
+    result = dace.define_local_scalar(dace.uint32)
+    
+    with dace.tasklet(dace.Language.CPP):
+        inp_mask << mask
+        inp_value << value
+        out_result >> result
+        """
+        out_result = __reduce_add_sync(inp_mask, inp_value);
+        """
+    return result
+
 
 
 @dace.program
-def warpLevel(A: dace.float64[512] @ dace.dtypes.StorageType.GPU_Global, B: dace.float64[512] @ dace.dtypes.StorageType.GPU_Global):
-    for i in dace.map[0:512:512] @ dace.dtypes.ScheduleType.GPU_Device:
+def warpLevel(A: dace.uint32[512] @ dace.dtypes.StorageType.GPU_Global, B: dace.uint32[512] @ dace.dtypes.StorageType.GPU_Global):
+    for _ in dace.map[0:512:512] @ dace.dtypes.ScheduleType.GPU_Device:
         for j in dace.map[0:512] @ dace.dtypes.ScheduleType.GPU_ThreadBlock:
-            for k in dace.map[0:16] @ dace.dtypes.ScheduleType.GPU_Warp:
-                A[k] = A[k] + 1
 
+            value = A[j]
+            mask = 0xffffffff
+            result = 0
+
+            for _ in dace.map[0:16] @ dace.dtypes.ScheduleType.GPU_Warp:
+                
+                result = reduce_add_sync(mask, value)
+            
+            B[j] = result
+
+
+A = cp.ones(512, cp.uint32)
+B = cp.random.rand(512).astype(cp.uint32)
 
 sdfg = warpLevel.to_sdfg()
-Code(sdfg.generate_code()[0].clean_code, language='cpp')
+sdfg(A=A, B=B)
 
-
-"""
-"""
-
-
-"""
-@dace.program
-def vector_copy3(A: dace.float64[64] @ dace.dtypes.StorageType.GPU_Global, B: dace.float64[64] @ dace.dtypes.StorageType.GPU_Global):
-    for j in dace.map[0:32] @ dace.dtypes.ScheduleType.GPU_ThreadBlock:
-        A[j] = B[j]
-
-sdfg = vector_copy3.to_sdfg()
-Code(sdfg.generate_code()[0].clean_code, language='cpp')
-
-
-
-
-"""
+print(B)
