6f517977df3ee669157115316c8432dc8dd4a014
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-06-16 10:43:26 +0200

    1

--- dace/transformation/dataflow/add_threadblock_map.py
+++ dace/transformation/dataflow/add_threadblock_map.py
@@ -21,9 +21,18 @@ class AddThreadBlockMap(transformation.SingleStateTransformation):
     map_entry = transformation.PatternNode(nodes.MapEntry)
 
     # Properties
-    thread_block_size_x = SymbolicProperty(dtype=int, default=None, allow_none=True, desc="Number threads in the threadBlock X Dim")
-    thread_block_size_y = SymbolicProperty(dtype=int, default=None, allow_none=True, desc="Number threads in the threadBlock Y Dim")
-    thread_block_size_z = SymbolicProperty(dtype=int, default=None, allow_none=True, desc="Number threads in the threadBlock Z Dim")
+    thread_block_size_x = SymbolicProperty(dtype=int,
+                                           default=None,
+                                           allow_none=True,
+                                           desc="Number threads in the threadBlock X Dim")
+    thread_block_size_y = SymbolicProperty(dtype=int,
+                                           default=None,
+                                           allow_none=True,
+                                           desc="Number threads in the threadBlock Y Dim")
+    thread_block_size_z = SymbolicProperty(dtype=int,
+                                           default=None,
+                                           allow_none=True,
+                                           desc="Number threads in the threadBlock Z Dim")
 
     @classmethod
     def expressions(cls):
@@ -31,7 +40,7 @@ class AddThreadBlockMap(transformation.SingleStateTransformation):
 
     def can_be_applied(self, graph, expr_index, sdfg, permissive=False):
         if self.thread_block_size_x is None or self.thread_block_size_y is None or self.thread_block_size_z is None:
-            x,y,z = dace.config.Config.get('compiler', 'cuda', 'default_block_size').split(',')
+            x, y, z = dace.config.Config.get('compiler', 'cuda', 'default_block_size').split(',')
             try:
                 self.thread_block_size_x = int(x)
                 self.thread_block_size_y = int(y)
