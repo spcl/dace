f03f62249ff2ec5d2d5dc87430f29bceba376ce3
affifboudaoud <hk_boudaoud@esi.dz>
2025-06-25 14:59:03 +0200

    Added SciPy to dependencies + formatting

--- setup.py
+++ setup.py
@@ -9,24 +9,49 @@ from setuptools import find_packages, setup
 # Find runtime and external library files by obtaining the module path and
 # trimming the absolute path of the resulting files.
 dace_path = os.path.dirname(os.path.abspath(__file__)) + '/dace/'
-runtime_files = [f[len(dace_path):] for f in glob.glob(dace_path + 'runtime/include/**/*', recursive=True)]
-library_files = [f[len(dace_path):] for f in glob.glob(dace_path + 'libraries/**/include/**/*', recursive=True)]
-cmake_files = [f[len(dace_path):] for f in glob.glob(dace_path + 'codegen/**/*.cmake', recursive=True)]
-viewer_files = [
-    f[len(dace_path):] for f in (glob.glob(dace_path + 'viewer/webclient/dist/*.js', recursive=True) +
-                                 glob.glob(dace_path + 'viewer/webclient/external_libs/**/*', recursive=True) +
-                                 glob.glob(dace_path + 'viewer/webclient/*.css', recursive=True) +
-                                 glob.glob(dace_path + 'viewer/webclient/*.html', recursive=True) +
-                                 glob.glob(dace_path + 'viewer/templates/**/*', recursive=True) +
-                                 glob.glob(dace_path + 'viewer/**/LICENSE', recursive=True))
+runtime_files = [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'runtime/include/**/*', recursive=True)
 ]
-cub_files = [f[len(dace_path):] for f in glob.glob(dace_path + 'external/cub/cub/**/*', recursive=True)
-             ] + [dace_path + 'external/cub/LICENSE.TXT']
-hlslib_files = [f[len(dace_path):] for f in glob.glob(dace_path + 'external/hlslib/cmake/**/*', recursive=True)] + [
-    f[len(dace_path):] for f in glob.glob(dace_path + 'external/hlslib/include/**/*', recursive=True)
+library_files = [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'libraries/**/include/**/*', recursive=True)
+]
+cmake_files = [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'codegen/**/*.cmake', recursive=True)
+]
+viewer_files = [
+    f[len(dace_path):] for f in (
+        glob.glob(dace_path + 'viewer/webclient/dist/*.js', recursive=True) +
+        glob.glob(dace_path + 'viewer/webclient/external_libs/**/*',
+                  recursive=True) +
+        glob.glob(dace_path + 'viewer/webclient/*.css', recursive=True) +
+        glob.glob(dace_path + 'viewer/webclient/*.html', recursive=True) +
+        glob.glob(dace_path + 'viewer/templates/**/*', recursive=True) +
+        glob.glob(dace_path + 'viewer/**/LICENSE', recursive=True))
+]
+cub_files = [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'external/cub/cub/**/*', recursive=True)
+] + [dace_path + 'external/cub/LICENSE.TXT']
+hlslib_files = [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'external/hlslib/cmake/**/*',
+                       recursive=True)
+] + [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'external/hlslib/include/**/*',
+                       recursive=True)
 ] + [dace_path + 'external/hlslib/LICENSE.md']
-rtllib_files = [f[len(dace_path):] for f in glob.glob(dace_path + 'external/rtllib/cmake/**/*', recursive=True)] + [
-    f[len(dace_path):] for f in glob.glob(dace_path + 'external/rtllib/templates/**/*', recursive=True)
+rtllib_files = [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'external/rtllib/cmake/**/*',
+                       recursive=True)
+] + [
+    f[len(dace_path):]
+    for f in glob.glob(dace_path + 'external/rtllib/templates/**/*',
+                       recursive=True)
 ]
 
 # See if CMake is available and if not, install as a dependency
@@ -35,8 +60,10 @@ try:
     cmake_path = shutil.which('cmake')
     if cmake_path:
         # CMake is available, check version
-        output = subprocess.check_output([cmake_path, '--version']).decode('utf-8')
-        cmake_version = tuple(int(t) for t in output.splitlines()[0].split(' ')[-1].split('.'))
+        output = subprocess.check_output([cmake_path,
+                                          '--version']).decode('utf-8')
+        cmake_version = tuple(
+            int(t) for t in output.splitlines()[0].split(' ')[-1].split('.'))
         # If version meets minimum requirements, CMake is not necessary
         if cmake_version >= (3, 17):
             cmake_requires = []
@@ -64,23 +91,46 @@ setup(name='dace',
           "Operating System :: OS Independent",
       ],
       python_requires='>=3.9, <3.14',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      packages=find_packages(
+          exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
       package_data={
           '': [
-              '*.yml', 'codegen/CMakeLists.txt', 'codegen/tools/*.cpp', 'external/moodycamel/*.h',
-              'external/moodycamel/LICENSE.md', 'codegen/Xilinx_HLS.tcl.in'
-          ] + runtime_files + cub_files + viewer_files + hlslib_files + library_files + rtllib_files + cmake_files
+              '*.yml', 'codegen/CMakeLists.txt', 'codegen/tools/*.cpp',
+              'external/moodycamel/*.h', 'external/moodycamel/LICENSE.md',
+              'codegen/Xilinx_HLS.tcl.in'
+          ] + runtime_files + cub_files + viewer_files + hlslib_files +
+          library_files + rtllib_files + cmake_files
       },
       include_package_data=True,
       install_requires=[
-          'numpy < 2.0', 'networkx >= 2.5', 'astunparse', 'sympy >= 1.9', 'pyyaml', 'ply',
-          'fparser >= 0.1.3', 'aenum >= 3.1', 'dataclasses', 'dill',
-          'pyreadline;platform_system=="Windows"', 'typing-compat; python_version < "3.8"', 'packaging',
-          'onnx', 'torch', 'onnxsim', 'protobuf',
+          'numpy',
+          'networkx >= 2.5',
+          'astunparse',
+          'sympy >= 1.9',
+          'pyyaml',
+          'ply',
+          'fparser >= 0.1.3',
+          'aenum >= 3.1',
+          'dataclasses',
+          'dill',
+          'pyreadline;platform_system=="Windows"',
+          'typing-compat; python_version < "3.8"',
+          'packaging',
+          'onnx',
+          'torch',
+          'onnxsim',
+          'protobuf',
+          'scipy',
       ] + cmake_requires,
       extras_require={
-          'testing': ['coverage', 'pytest-cov', 'scipy', 'absl-py', 'opt_einsum', 'pymlir', 'click', 'transformers'],
-          'docs': ['jinja2<3.2.0', 'sphinx-autodoc-typehints', 'sphinx-rtd-theme>=0.5.1', 'matplotlib']
+          'testing': [
+              'coverage', 'pytest-cov', 'absl-py', 'opt_einsum', 'pymlir',
+              'click', 'transformers'
+          ],
+          'docs': [
+              'jinja2<3.2.0', 'sphinx-autodoc-typehints',
+              'sphinx-rtd-theme>=0.5.1', 'matplotlib'
+          ]
       },
       entry_points={
           'console_scripts': [
