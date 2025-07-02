ba99a54ad2e0ebdc2b6f7298960a9c689c8d73d1
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-02 16:46:39 +0200

    Add a strided copy test that triggered the error previously

---
+++ tests/python_frontend/strided_copy_test.py
@@ -0,0 +1,15 @@
+import dace
+import pytest
+
+def test_strided_copy():
+    @dace.program
+    def strided_copy(dst: dace.uint32[20], src: dace.uint32[40]):
+        dst[0:20:2] = src[0:40:4]
+
+    base_sdfg = strided_copy.to_sdfg(simplify=False)
+    base_sdfg.validate()
+    base_sdfg.simplify()
+    base_sdfg.validate()
+
+if __name__ == "__main__":
+    test_strided_copy()
