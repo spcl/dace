80392fad2393b0ef15d7cba3976d4507df1f512c
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-03 18:16:39 +0200

    Refactor

--- tests/transformations/map_tiling_with_strides_test.py
+++ tests/transformations/map_tiling_with_strides_test.py
@@ -1,6 +1,5 @@
 # Copyright 2019-2025 ETH Zurich and the DaCe authors. All rights reserved.
 
-
 import dace
 import numpy as np
 from dace.transformation.dataflow import MapTiling
@@ -9,9 +8,9 @@ import pytest
 N = dace.symbol('N')
 
 
-
 def test_map_tiling_with_strides():
     s = 33
+
     @dace.program
     def vector_copy_strides(A: dace.uint32[N], B: dace.uint32[N]):
         for i in dace.map[0:N:s] @ dace.dtypes.ScheduleType.CPU_Multicore:
@@ -21,26 +20,24 @@ def test_map_tiling_with_strides():
 
     state = sdfg.states()[0]
     sdfg_nodes = state.nodes()
-    map_entry:dace.nodes.MapEntry = [n for n in sdfg_nodes if isinstance(n, dace.nodes.MapEntry)][0]
+    map_entry: dace.nodes.MapEntry = [n for n in sdfg_nodes if isinstance(n, dace.nodes.MapEntry)][0]
 
     tile_sizes = [32]
-    MapTiling.apply_to(
-        sdfg=sdfg,
-        options={
-            "prefix": "b",
-            "tile_sizes": tile_sizes,
-            "divides_evenly": False,
-            "tile_trivial": True,
-            "skew": False
-        },
-        map_entry=map_entry
-    )
+    MapTiling.apply_to(sdfg=sdfg,
+                       options={
+                           "prefix": "b",
+                           "tile_sizes": tile_sizes,
+                           "divides_evenly": False,
+                           "tile_trivial": True,
+                           "skew": False
+                       },
+                       map_entry=map_entry)
     inner_map_entry = map_entry
     outer_map_entry = state.entry_node(inner_map_entry)
 
     b_i = dace.symbol("b_i")
     inner_rangelist = [(b_i, dace.symbolic.SymExpr("Min(N - 1, b_i + 32*33 - 1)"), 33)]
-    outer_rangelist = [(0, N - 1, 32*33)]
+    outer_rangelist = [(0, N - 1, 32 * 33)]
     inner_range = dace.subsets.Range(inner_rangelist)
     outer_range = dace.subsets.Range(outer_rangelist)
 
