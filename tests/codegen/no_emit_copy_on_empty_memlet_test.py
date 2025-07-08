38aeeb8fe56b0eb0029a633c03911580f6be35ff
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-08 16:19:36 +0200

    refactor

--- tests/codegen/no_emit_copy_on_empty_memlet_test.py
+++ tests/codegen/no_emit_copy_on_empty_memlet_test.py
@@ -1,17 +1,22 @@
 import dace
 import pytest
 
+
 def _gen_sdfg() -> dace.SDFG:
     sdfg = dace.SDFG('test_no_emit_copy_on_empty_memlet')
     state = sdfg.add_state()
 
-
     nameA, arrA = sdfg.add_array('A', [2], dace.float32)
     nameB, arrB = sdfg.add_array('B', [2], dace.float32)
 
-
-    tasklet1 = state.add_tasklet('tasklet1', {}, {}, 'printf("Hello World Before Copy\\n");', language=dace.Language.CPP, code_global="#include <stdio.h>")
-    tasklet2 = state.add_tasklet('tasklet2', {}, {}, 'printf("Hello World After Copy\\n");', language=dace.Language.CPP, code_global="#include <stdio.h>")
+    tasklet1 = state.add_tasklet('tasklet1', {}, {},
+                                 'printf("Hello World Before Copy\\n");',
+                                 language=dace.Language.CPP,
+                                 code_global="#include <stdio.h>")
+    tasklet2 = state.add_tasklet('tasklet2', {}, {},
+                                 'printf("Hello World After Copy\\n");',
+                                 language=dace.Language.CPP,
+                                 code_global="#include <stdio.h>")
 
     anA = state.add_access(nameA)
     anB = state.add_access(nameB)
@@ -20,6 +25,7 @@ def _gen_sdfg() -> dace.SDFG:
     state.add_edge(anB, None, tasklet2, None, dace.Memlet())
 
     return sdfg
+
 
 def test_no_emit_copy_on_empty_memlet():
     sdfg = _gen_sdfg()
