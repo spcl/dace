88446d5036846b3e248192bd590cb30e59172b56
Philip Mueller <philip.mueller@cscs.ch>
2025-07-08 15:51:30 +0200

    Added checks for the number of access nodes.

--- tests/inlining_test.py
+++ tests/inlining_test.py
@@ -820,6 +820,7 @@ def _make_sdfg_for_multistate_inlining_with_symbol_promotion(
 def _make_sdfg_for_multistate_inlining_with_symbol_mapping(
     outside_and_inner_symbol_have_same_meaning: bool,
     separate_write_back_state: bool,
+    inside_uses_same_name: bool,
 ) -> Tuple[dace.SDFG, dace.SDFG, dace.SDFGState, dace.nodes.NestedSDFG]:
     """
     The SDFGs created by this function are rather similar to
@@ -836,9 +837,13 @@ def _make_sdfg_for_multistate_inlining_with_symbol_mapping(
     If `separate_write_back_state` then the copy of `t` into `b` is in a separate state, otherwise
     it is in a different state. Thus, if it is `False` then the inner SDFG has only one state,
     which makes it applicable to `InlineSDFG`.
+
+    If `inside_uses_same_name` is `True` then the inner SDFG will use the same symbol name. In case
+    it is `True` and `outside_and_inner_symbol_have_same_meaning` is `False` it means that there is
+    a name clash.
     """
-    inner_symbol_name = "inner_symbol"
     outer_symbol_name = "outer_symbol"
+    inner_symbol_name = outer_symbol_name if inside_uses_same_name else "inner_symbol"
 
     # Create the inner SDFG.
     inner_sdfg = dace.SDFG(unique_name("inner_sdfg"))
@@ -1152,13 +1157,15 @@ def test_multistate_inlining_different_symbols_used(separate_write_back_state: b
         separate_write_back_state=separate_write_back_state, outside_uses_different_symbol=True)
 
 
-def _perform_test_multistate_inline_with_symbol_mapping(separate_write_back_state: bool,
-                                                        outside_and_inner_symbol_have_same_meaning: bool,
-                                                        use_InlineSDFG_transformation: bool):
-
+def _perform_test_multistate_inline_with_symbol_mapping(
+    separate_write_back_state: bool,
+    outside_and_inner_symbol_have_same_meaning: bool,
+    use_InlineSDFG_transformation: bool,
+):
     outer_sdfg, inner_sdfg, inner_state, nsdfg_node = _make_sdfg_for_multistate_inlining_with_symbol_mapping(
         outside_and_inner_symbol_have_same_meaning=outside_and_inner_symbol_have_same_meaning,
         separate_write_back_state=separate_write_back_state,
+        inside_uses_same_name=False,
     )
 
     inner_symbol_name = "inner_symbol"
@@ -1217,6 +1224,19 @@ def _perform_test_multistate_inline_with_symbol_mapping(separate_write_back_stat
         assert outer_sdfg.number_of_nodes() == 1
     else:
         assert outer_sdfg.number_of_nodes() == (4 if separate_write_back_state else 3)
+
+    ac_nodes = count_nodes(outer_sdfg, dace_nodes.AccessNode, True)
+
+    if use_InlineSDFG_transformation:
+        assert len(ac_nodes) == 4
+    else:
+        if separate_write_back_state:
+            assert len(ac_nodes) == 7
+            assert len([ac.data for ac in ac_nodes if ac.data == "t"]) == 2
+        else:
+            assert len(ac_nodes) == 6
+            assert len([ac.data for ac in ac_nodes if ac.data == "t"]) == 1
+        assert len([ac.data for ac in ac_nodes if ac.data == "T"]) == 2
 
     assert outer_sdfg.arrays.keys() == {"A", "B", "t", "T"}
     assert all((not arr.transient) and arr.shape == (20, ) for aname, arr in outer_sdfg.arrays.items() if aname in "AB")
