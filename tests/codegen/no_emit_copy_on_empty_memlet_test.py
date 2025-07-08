b499ff6d5d0b74c8545e00e4a0313004a2e1a077
Yakup Budanaz <budanaz.yakup@gmail.com>
2025-07-08 16:17:57 +0200

    Fix

--- tests/codegen/no_emit_copy_on_empty_memlet_test.py
+++ tests/codegen/no_emit_copy_on_empty_memlet_test.py
@@ -10,13 +10,13 @@ def _gen_sdfg() -> dace.SDFG:
     nameB, arrB = sdfg.add_array('B', [2], dace.float32)
 
 
-    tasklet1 = state.add_tasklet('tasklet1', {}, {}, 'prinf("Hello World Before Copy\n");', language=dace.Language.CPP, code_global="#include <stdio.h>")
-    tasklet2 = state.add_tasklet('tasklet2', {}, {}, 'prinf("Hello World After Copy\n");', language=dace.Language.CPP, code_global="#include <stdio.h>")
+    tasklet1 = state.add_tasklet('tasklet1', {}, {}, 'printf("Hello World Before Copy\\n");', language=dace.Language.CPP, code_global="#include <stdio.h>")
+    tasklet2 = state.add_tasklet('tasklet2', {}, {}, 'printf("Hello World After Copy\\n");', language=dace.Language.CPP, code_global="#include <stdio.h>")
 
     anA = state.add_access(nameA)
     anB = state.add_access(nameB)
     state.add_edge(tasklet1, None, anA, None, dace.Memlet())
-    state.add_edge(anA, None, anB, None, dace.Memlet.from_array(name=nameA, datadesc=sdfg.arrays[nameA]))
+    state.add_edge(anA, None, anB, None, dace.Memlet.from_array(dataname=nameA, datadesc=sdfg.arrays[nameA]))
     state.add_edge(anB, None, tasklet2, None, dace.Memlet())
 
     return sdfg
@@ -25,3 +25,7 @@ def test_no_emit_copy_on_empty_memlet():
     sdfg = _gen_sdfg()
     sdfg.validate()
     sdfg.compile()
+
+
+if __name__ == "__main__":
+    test_no_emit_copy_on_empty_memlet()
