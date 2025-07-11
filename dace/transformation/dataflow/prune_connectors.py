218a34ae40c5e0b7829f0b28263708c2f79ccc04
Philip Mueller <philip.mueller@cscs.ch>
2025-07-11 09:00:46 +0200

    Improved `PruneSymbols`.
    Before for every symbol, that should be removed, the whole (nested) sdfg was scanned to test if it can be removed.
    Now, the set of symbols that can not be removed is determined and then used.
    Thus the SDFG is only scanned once.

--- dace/transformation/dataflow/prune_connectors.py
+++ dace/transformation/dataflow/prune_connectors.py
@@ -153,10 +153,27 @@ class PruneSymbols(pm.SingleStateTransformation):
 
         return candidates
 
+    def _find_symbols_that_can_not_be_removed(self, sdfg: SDFG) -> Set[str]:
+        """Find the set of symbols that can not be removed."""
+
+        # The implementation of this function is such that it has the same behaviour as before,
+        #  where `dace.transformation.helpers.is_symbol_unused()` was used. However, I am sure
+        #  that it can be implemented in terms of `used_symbols()` but I am unsure if it is
+        #  so simple as `return sdfg.used_symbols()`?
+
+        unremovable_symbols: Set[str] = set()
+
+        for desc in sdfg.arrays.values():
+            unremovable_symbols.update(map(str, desc.free_symbols))
+        for state in sdfg.states():  # Previously `nodes()` was used.
+            unremovable_symbols.update(state.free_symbols)
+        for e in sdfg.edges():  # Are this all edges?
+            unremovable_symbols.update(e.data.free_symbols)
+        return unremovable_symbols
+
     def can_be_applied(self, graph: SDFGState, expr_index: int, sdfg: SDFG, permissive: bool = False) -> bool:
 
         nsdfg: nodes.NestedSDFG = self.nsdfg
-
         if len(PruneSymbols._candidates(nsdfg)) > 0:
             return True
 
@@ -164,11 +181,12 @@ class PruneSymbols(pm.SingleStateTransformation):
 
     def apply(self, graph: SDFGState, sdfg: SDFG):
         nsdfg = self.nsdfg
-
         candidates = PruneSymbols._candidates(nsdfg)
+        unremovable_symbols = self._find_symbols_that_can_not_be_removed(sdfg)
+
         for candidate in candidates:
             del nsdfg.symbol_mapping[candidate]
 
             # If not used in SDFG, remove from symbols as well
-            if helpers.is_symbol_unused(nsdfg.sdfg, candidate):
+            if candidate not in unremovable_symbols:
                 nsdfg.sdfg.remove_symbol(candidate)
