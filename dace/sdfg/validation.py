298c5fe2055db922d67e3ff9e6cc211c15bb8944
Tal Ben-Nun <tbennun@gmail.com>
2025-07-30 10:02:17 -0700

    Fix exception in validation

--- dace/sdfg/validation.py
+++ dace/sdfg/validation.py
@@ -907,8 +907,22 @@ def validate_state(state: 'dace.sdfg.SDFGState',
         if not e.data.allow_oob and e.data.other_subset is not None and not (
             (isinstance(src_node, nd.AccessNode) and isinstance(sdfg.arrays[src_node.data], dt.Stream)) or
             (isinstance(dst_node, nd.AccessNode) and isinstance(sdfg.arrays[dst_node.data], dt.Stream))):
-            src_expr = (e.data.src_subset.num_elements() * sdfg.arrays[src_node.data].veclen)
-            dst_expr = (e.data.dst_subset.num_elements() * sdfg.arrays[dst_node.data].veclen)
+            if isinstance(src_node, nd.AccessNode):
+                src_expr = e.data.src_subset.num_elements() * sdfg.arrays[src_node.data].veclen
+            elif isinstance(src_node, nd.CodeNode) and e.src.out_connectors[e.src_conn] is not None:
+                # Get data from connector
+                src_expr = e.data.src_subset.num_elements() * e.src.out_connectors[e.src_conn].veclen
+            else:
+                src_expr = e.data.src_subset.num_elements()
+
+            if isinstance(dst_node, nd.AccessNode):
+                dst_expr = e.data.dst_subset.num_elements() * sdfg.arrays[dst_node.data].veclen
+            elif isinstance(dst_node, nd.CodeNode) and e.dst.in_connectors[e.dst_conn] is not None:
+                # Get data from connector
+                dst_expr = e.data.dst_subset.num_elements() * e.dst.in_connectors[e.dst_conn].veclen
+            else:
+                dst_expr = e.data.dst_subset.num_elements()
+
             if symbolic.inequal_symbols(src_expr, dst_expr):
                 error = InvalidSDFGEdgeError('Dimensionality mismatch between src/dst subsets', sdfg, state_id, eid)
                 # NOTE: Make an exception for Views and reference sets
