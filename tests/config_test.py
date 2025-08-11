15ac7c353a50a16dc9671d17fd6c8eb4dbceab74
Philip Mueller <philip.mueller@cscs.ch>
2025-08-11 09:12:56 +0200

    Added a test for the configuration isolation when multiple threads are involved.

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,5 +1,7 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
 from dace.config import Config, set_temporary, temporary_config
+import threading
+import time
 
 
 def test_set_temporary():
@@ -62,8 +64,57 @@ def test_set_temporary_exception():
         raise RuntimeError("No exception was raised.")
 
 
+def test_config_isolation_multi_thread():
+
+    thread_one_has_started_working = threading.Event()
+    thread_two_has_started_working = threading.Event()
+    thread_one_has_stoped_working = threading.Event()
+    CONFIG_KEY = "compiler.cuda.backend"
+
+    def thread1():
+        # Because child processes do not inherent the configuration state of their
+        #  parent they are set to the default.
+        initial_value = Config.get(CONFIG_KEY)
+        assert initial_value != "master"
+
+        with temporary_config():
+            Config.set(CONFIG_KEY, value="thread1")
+            thread_one_has_started_working.set()
+            assert Config.get(CONFIG_KEY) == "thread1"
+            assert thread_two_has_started_working.wait(timeout=10)
+            assert Config.get(CONFIG_KEY) == "thread1"
+
+        assert initial_value == Config.get(CONFIG_KEY)
+        thread_one_has_stoped_working.set()
+
+    def thread2():
+        assert thread_one_has_started_working.wait(timeout=10)
+        initial_value = Config.get(CONFIG_KEY)
+        assert initial_value not in ["master", "thread1"]
+
+        with temporary_config():
+            Config.set(CONFIG_KEY, value="thread2")
+            assert Config.get(CONFIG_KEY) == "thread2"
+            thread_two_has_started_working.set()
+
+            # The restore event of the first thread should not have affected the
+            #  configuration of the second thread.
+            assert thread_one_has_stoped_working.wait(timeout=10)
+            assert Config.get(CONFIG_KEY) == "thread2"
+
+    with temporary_config():
+        Config.set(CONFIG_KEY, value="master")
+        threads = [threading.Thread(target=thread1), threading.Thread(target=thread2)]
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        assert Config.get(CONFIG_KEY) == "master"
+
+
 if __name__ == '__main__':
     test_set_temporary()
     test_temporary_config()
     test_temporary_config_exception()
     test_set_temporary_exception()
+    test_config_isolation_multi_thread()
