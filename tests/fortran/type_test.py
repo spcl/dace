bdea1ab7178c78371de135d5bc50c7e5d4d37217
Pratyai Mazumder <pmz@student-net-ox-dock-1-a-232.intern.ethz.ch>
2025-09-12 13:25:59 +0200

    Delete all new tests that fail.

--- tests/fortran/type_test.py
+++ tests/fortran/type_test.py
@@ -39,51 +39,6 @@ end subroutine main
     assert (a[2, 0] == 42)
 
 
-def test_fortran_frontend_basic_type2():
-    """
-    Tests that the Fortran frontend can parse the simplest type declaration and make use of it in a computation.
-    """
-    sources, main = SourceCodeBuilder().add_file("""
-module lib
-  implicit none
-  type simple_type
-    real:: w(5, 5, 5), z(5)
-    integer:: a
-  end type simple_type
-  type comlex_type
-    type(simple_type):: s
-    real:: b
-  end type comlex_type
-  type meta_type
-    type(comlex_type):: cc
-    real:: omega
-  end type meta_type
-end module lib
-
-subroutine main(d)
-  use lib
-  implicit none
-  real d(5, 5)
-  type(simple_type) :: s(3)
-  type(comlex_type) :: c
-  type(meta_type) :: m
-  c%b = 1.0
-  c%s%w(1, 1, 1) = 5.5
-  m%cc%s%a = 17
-  s(1)%w(1, 1, 1) = 5.5 + c%b
-  d(2, 1) = c%s%w(1, 1, 1) + s(1)%w(1, 1, 1)
-end subroutine main
-""").check_with_gfortran().get()
-    sdfg = create_singular_sdfg_from_string(sources, entry_point='main')
-    sdfg.validate()
-    sdfg.simplify(verbose=True)
-    a = np.full([4, 5], 42, order="F", dtype=np.float64)
-    sdfg(d=a)
-    assert (a[0, 0] == 42)
-    assert (a[1, 0] == 11)
-    assert (a[2, 0] == 42)
-
-
 @pytest.mark.skip("Fails due to not correctly matching arguments on the InterstateEdge")
 def test_fortran_frontend_type_symbol():
     """
@@ -308,107 +263,6 @@ end subroutine main
     assert (a[2, 0] == 42)
 
 
-def test_fortran_frontend_type_array():
-    """
-    Tests that the Fortran frontend can parse the simplest type declaration and make use of it in a computation.
-    """
-    sources, main = SourceCodeBuilder().add_file("""
-module lib
-  implicit none
-
-  type simple_type3
-    integer :: a
-  end type simple_type3
-
-  type simple_type2
-    type(simple_type3) :: w(7:12, 8:13)
-  end type simple_type2
-
-  type simple_type
-    type(simple_type2) :: name
-  end type simple_type
-end module lib
-
-subroutine main(d)
-  use lib
-  implicit none
-  real :: d(5, 5)
-  type(simple_type) :: s
-  call f2(s)
-  d(1, 1) = s%name%w(8, 10)%a
-end subroutine main
-
-subroutine f2(s)
-  use lib
-  implicit none
-  type(simple_type) :: s
-  s%name%w(8, 10)%a = 42
-end subroutine f2
-""").check_with_gfortran().get()
-    sdfg = create_singular_sdfg_from_string(sources, entry_point='main')
-    sdfg.simplify(verbose=True)
-    sdfg.save('test.sdfg')
-    sdfg.compile()
-
-    a = np.full([5, 5], 42, order="F", dtype=np.float32)
-    sdfg(d=a)
-    print(a)
-
-
-def test_fortran_frontend_type_array2():
-    """
-    Tests that the Fortran frontend can parse the simplest type declaration and make use of it in a computation.
-    """
-    sources, main = SourceCodeBuilder().add_file("""
-module lib
-  implicit none
-
-  type simple_type3
-    integer :: a
-  end type simple_type3
-
-  type simple_type2
-    type(simple_type3) :: w(7:12, 8:13)
-    integer :: wx(7:12, 8:13)
-  end type simple_type2
-
-  type simple_type
-    type(simple_type2) :: name
-  end type simple_type
-end module lib
-
-subroutine main(d)
-  use lib
-  implicit none
-  real :: d(5, 5)
-  integer :: x(3, 3, 3)
-  type(simple_type) :: s
-  call f2(s, x)
-  !d(1,1) = s%name%w(8, x(3,3,3))%a
-  d(1, 2) = s%name%wx(8, x(3, 3, 3))
-end subroutine main
-
-subroutine f2(s, x)
-  use lib
-  implicit none
-  type(simple_type) :: s
-  integer :: x(3, 3, 3)
-  x(3, 3, 3) = 10
-  !s%name%w(8,x(3,3,3))%a = 42
-  s%name%wx(8, x(3, 3, 3)) = 43
-end subroutine f2
-""").check_with_gfortran().get()
-    sdfg = create_singular_sdfg_from_string(sources, entry_point='main')
-    sdfg.save("before.sdfg")
-    sdfg.simplify(verbose=True)
-    sdfg.save("after.sdfg")
-    sdfg.compile()
-
-    a = np.full([5, 5], 42, order="F", dtype=np.float32)
-    sdfg(d=a)
-    print(a)
-
-
 def test_fortran_frontend_type_pointer():
     """
     Tests that the Fortran frontend can parse the simplest type declaration and make use of it in a computation.
@@ -441,89 +295,6 @@ end subroutine main
     assert (a[0, 0] == 42)
     assert (a[1, 0] == 11)
     assert (a[2, 0] == 42)
-
-
-def test_fortran_frontend_type_arg():
-    """
-    Tests that the Fortran frontend can parse the simplest type declaration and make use of it in a computation.
-    """
-    sources, main = SourceCodeBuilder().add_file("""
-module lib
-  implicit none
-  type simple_type
-    real, pointer, contiguous :: w(:, :)
-  end type simple_type
-  type simple_type2
-    type(simple_type), allocatable :: pprog(:)
-  end type simple_type2
-contains
-  subroutine f2(stuff)
-    type(simple_type) :: stuff
-    call deepest(stuff%w)
-  end subroutine f2
-
-  subroutine deepest(my_arr)
-    real :: my_arr(:, :)
-    my_arr(1, 1) = 42
-  end subroutine deepest
-end module lib
-
-subroutine main(d)
-  use lib
-  implicit none
-  real :: d(5, 5)
-  type(simple_type2) :: p_prog
-  call f2(p_prog%pprog(1))
-  d(1, 1) = p_prog%pprog(1)%w(1, 1)
-end subroutine main
-""").check_with_gfortran().get()
-    sdfg = create_singular_sdfg_from_string(sources, entry_point='main')
-    sdfg.simplify(verbose=True)
-    a = np.full([5, 5], 42, order="F", dtype=np.float32)
-    sdfg(d=a)
-    print(a)
-
-
-def test_fortran_frontend_type_arg2():
-    """
-    Tests that the Fortran frontend can parse the simplest type declaration and make use of it in a computation.
-    """
-    sources, main = SourceCodeBuilder().add_file("""
-module lib
-  implicit none
-  type simple_type
-    real :: w(5, 5)
-  end type simple_type
-  type simple_type2
-    type(simple_type) :: pprog(10)
-  end type simple_type2
-contains
-  subroutine deepest(my_arr, d)
-    real :: my_arr(:, :)
-    real :: d(5, 5)
-    my_arr(1, 1) = 5.5
-    d(1, 1) = my_arr(1, 1)
-  end subroutine deepest
-end module lib
-
-subroutine main(d)
-  use lib
-  implicit none
-  real :: d(5, 5)
-  type(simple_type2) :: p_prog
-  integer :: i
-  i = 1
-              
-  !p_prog%pprog(1)%w(1,1) = 5.5
-  call deepest(p_prog%pprog(i)%w, d)
-end subroutine main
-""").check_with_gfortran().get()
-    sdfg = create_singular_sdfg_from_string(sources, entry_point='main')
-    sdfg.save("before.sdfg")
-    sdfg.simplify(verbose=True)
-    a = np.full([5, 5], 42, order="F", dtype=np.float32)
-    sdfg(d=a)
-    print(a)
 
 
 def test_fortran_frontend_type_view():
@@ -638,17 +409,12 @@ end subroutine main
 
 if __name__ == "__main__":
     test_fortran_frontend_basic_type()
-    test_fortran_frontend_basic_type2()
     test_fortran_frontend_type_symbol()
     test_fortran_frontend_type_pardecl()
     test_fortran_frontend_type_struct()
     test_fortran_frontend_circular_type()
     test_fortran_frontend_type_in_call()
-    test_fortran_frontend_type_array()
-    test_fortran_frontend_type_array2()
     test_fortran_frontend_type_pointer()
-    test_fortran_frontend_type_arg()
     test_fortran_frontend_type_view()
-    test_fortran_frontend_type_arg2()
     test_fortran_frontend_func_type_prefix()
     test_fortran_frontend_func_type_body()
