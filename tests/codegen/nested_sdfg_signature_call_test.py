11d4b463e4783dc8fe4e542d684acfda7821bf65
Alexandros Nikolaos Ziogas <31545860+alexnick83@users.noreply.github.com>
2025-09-22 10:53:29 +0200

    Fixed tests. Added read/write option.

--- tests/codegen/nested_sdfg_signature_call_test.py
+++ tests/codegen/nested_sdfg_signature_call_test.py
@@ -8,7 +8,10 @@ import copy
 import dace
 import pytest
 
+from typing import Union
+
 import dace.data as dt
+import dace.subsets as sbs
 
 N = 20  # Array size
 M = 10  # View size
@@ -16,23 +19,23 @@ M = 10  # View size
 # Parent SDFG data, Nested SDFG data, Subset
 data_combinations = [
     (dt.Scalar, dt.Scalar, None),
-    (dt.Scalar, dt.Scalar, dace.subsets.Range.from_string(f"0:1")),
-    (dt.Array, dt.Scalar, dace.subsets.Range.from_string(f"{N//4}:{N//4+1}")),
-    (dt.Array, dt.Array, dace.subsets.Range.from_string(f"0:{N}")),
-    (dt.Array, dt.Array, dace.subsets.Range.from_string(f"{N//4}:{(3*N)//4}")),
+    (dt.Scalar, dt.Scalar, sbs.Range.from_string(f"0:1")),
+    (dt.Array, dt.Scalar, sbs.Range.from_string(f"{N//4}:{N//4+1}")),
+    (dt.Array, dt.Array, sbs.Range.from_string(f"0:{N}")),
+    (dt.Array, dt.Array, sbs.Range.from_string(f"{N//4}:{(3*N)//4}")),
     (dt.Structure, dt.Structure, None),
-    (dt.Structure, dt.Structure, dace.subsets.Range.from_string("0:1")),
-    (dt.ContainerArray, dt.Structure, dace.subsets.Range.from_string(f"{N//4}:{N//4+1}")),
-    (dt.ContainerArray, dt.ContainerArray, dace.subsets.Range.from_string(f"0:{N}")),
-    (dt.ContainerArray, dt.ContainerArray, dace.subsets.Range.from_string(f"{N//4}:{(3*N)//4}")),
-    (dt.ArrayView, dt.Scalar, dace.subsets.Range.from_string(f"{M//4}:{M//4+1}")),
-    (dt.ArrayView, dt.Array, dace.subsets.Range.from_string(f"0:{M}")),
-    (dt.ArrayView, dt.Array, dace.subsets.Range.from_string(f"{M//4}:{(3*M)//4}")),
+    (dt.Structure, dt.Structure, sbs.Range.from_string("0:1")),
+    (dt.ContainerArray, dt.Structure, sbs.Range.from_string(f"{N//4}:{N//4+1}")),
+    (dt.ContainerArray, dt.ContainerArray, sbs.Range.from_string(f"0:{N}")),
+    (dt.ContainerArray, dt.ContainerArray, sbs.Range.from_string(f"{N//4}:{(3*N)//4}")),
+    (dt.ArrayView, dt.Scalar, sbs.Range.from_string(f"{M//4}:{M//4+1}")),
+    (dt.ArrayView, dt.Array, sbs.Range.from_string(f"0:{M}")),
+    (dt.ArrayView, dt.Array, sbs.Range.from_string(f"{M//4}:{(3*M)//4}")),
     (dt.StructureView, dt.Structure, None),
-    (dt.StructureView, dt.Structure, dace.subsets.Range.from_string("0:1")),
-    (dt.ContainerView, dt.Structure, dace.subsets.Range.from_string(f"{M//4}:{M//4+1}")),
-    (dt.ContainerView, dt.ContainerArray, dace.subsets.Range.from_string(f"0:{M}")),
-    (dt.ContainerView, dt.ContainerArray, dace.subsets.Range.from_string(f"{M//4}:{(3*M)//4}")),
+    (dt.StructureView, dt.Structure, sbs.Range.from_string("0:1")),
+    (dt.ContainerView, dt.Structure, sbs.Range.from_string(f"{M//4}:{M//4+1}")),
+    (dt.ContainerView, dt.ContainerArray, sbs.Range.from_string(f"0:{M}")),
+    (dt.ContainerView, dt.ContainerArray, sbs.Range.from_string(f"{M//4}:{(3*M)//4}")),
 ]
 
 # Parent SDFG data nesting combinations to test
@@ -111,7 +114,9 @@ constructors = {
 }
 
 
-def make_sdfg(data_combo, parent_nesting):
+def make_sdfg(data_combo: tuple[dt.Data, dt.Data, Union[None, sbs.Range]],
+              is_read: bool,
+              parent_nesting: tuple[dt.Data, ...] = ()):
     parent_dtype, nested_dtype, subset = data_combo
 
     sdfg = dace.SDFG("nested_sdfg_signature_call_test")
@@ -123,24 +128,39 @@ def make_sdfg(data_combo, parent_nesting):
     else:
         non_view_class = None
 
-    if issubclass(parent_dtype, dt.Array):
-        if non_view_class is not None:
+    def _add_viewed_access(is_scalar: bool):
+        if is_scalar:
+            non_view_desc = constructors[non_view_class](dtypes[non_view_class])
+            view_desc = constructors[parent_dtype](dtypes[non_view_class])
+            memlet = dace.Memlet(data="A", subset=None)
+        else:
             non_view_desc = constructors[non_view_class]((N, ), dtypes[non_view_class])
             view_desc = constructors[parent_dtype]((M, ), dtypes[non_view_class])
-            sdfg.add_datadesc("A", non_view_desc)
-            sdfg.add_datadesc("Av", view_desc)
-            non_view_access = state.add_access("A")
-            parent_access = state.add_access("Av")
-            state.add_edge(non_view_access, None, parent_access, "views",
-                           dace.Memlet(data="A", subset=f"{N//4}:{N//4+M}", other_subset=f"0:{M}"))
+            memlet = dace.Memlet(data="A", subset=f"{N//4}:{N//4+M}", other_subset=f"0:{M}")
+        sdfg.add_datadesc("A", non_view_desc)
+        sdfg.add_datadesc("Av", view_desc)
+        non_view_access = state.add_access("A")
+        view_access = state.add_access("Av")
+        if is_read:
+            state.add_edge(non_view_access, None, view_access, "views", memlet)
         else:
-            non_view_desc = constructors[parent_dtype]((N, ), dtypes[parent_dtype])
-            sdfg.add_datadesc("A", non_view_desc)
-            parent_access = state.add_access("A")
-    else:
-        desc = constructors[parent_dtype](dtypes[parent_dtype])
+            state.add_edge(view_access, "views", non_view_access, None, memlet)
+        return view_access
+
+    def _add_normal_access(is_scalar: bool):
+        if is_scalar:
+            desc = constructors[parent_dtype](dtypes[parent_dtype])
+        else:
+            desc = constructors[parent_dtype]((N, ), dtypes[parent_dtype])
         sdfg.add_datadesc("A", desc)
-        parent_access = state.add_access("A")
+        access = state.add_access("A")
+        return access
+
+    is_scalar = not issubclass(parent_dtype, dt.Array)
+    if non_view_class is not None:
+        parent_access = _add_viewed_access(is_scalar=is_scalar)
+    else:
+        parent_access = _add_normal_access(is_scalar=is_scalar)
 
     nested_sdfg = dace.SDFG("nested")
     _ = nested_sdfg.add_state("nested_state")  # Necessary to avoid errors
@@ -151,17 +171,22 @@ def make_sdfg(data_combo, parent_nesting):
         nested_desc = constructors[nested_dtype](dtypes[nested_dtype])
     nested_sdfg.add_datadesc("B", nested_desc)
 
-    nested_sdfg_node = state.add_nested_sdfg(nested_sdfg, {"B"}, {})
-    state.add_edge(parent_access, None, nested_sdfg_node, "B", dace.Memlet(data="A", subset=subset))
+    if is_read:
+        nested_sdfg_node = state.add_nested_sdfg(nested_sdfg, {"B"}, {})
+        state.add_edge(parent_access, None, nested_sdfg_node, "B", dace.Memlet(data=parent_access.data, subset=subset))
+    else:
+        nested_sdfg_node = state.add_nested_sdfg(nested_sdfg, {}, {"B"})
+        state.add_edge(nested_sdfg_node, "B", parent_access, None, dace.Memlet(data=parent_access.data, subset=subset))
 
     return sdfg
 
 
 @pytest.mark.parametrize("data_combo", data_combinations)
+@pytest.mark.parametrize("is_read", [True, False])
 @pytest.mark.parametrize("parent_nesting", [None])
-def test_nested_sdfg_signature_call(data_combo, parent_nesting):
+def test_nested_sdfg_signature_call(data_combo, is_read, parent_nesting):
 
-    sdfg = make_sdfg(data_combo, parent_nesting)
+    sdfg = make_sdfg(data_combo, is_read, parent_nesting)
 
     with dace.config.set_temporary("optimizer", "automatic_simplification", value=False):
         sdfg.compile()
@@ -174,5 +199,6 @@ if __name__ == "__main__":
         parent_nesting = None
         # for parent_nesting in parent_data_nesting:
         print(f"Testing combination: {data_combo} with parent nesting {parent_nesting}...")
-        test_nested_sdfg_signature_call(data_combo, parent_nesting)
+        test_nested_sdfg_signature_call(data_combo, True, parent_nesting)
+        test_nested_sdfg_signature_call(data_combo, False, parent_nesting)
     print("All combinations passed!")
