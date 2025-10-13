41f503c37834fd7cdf09fbecf3e8372da353d3ef
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-10-13 12:48:57 +0200

    Stuff

--- dace/transformation/passes/a.py
+++
@@ -1,5 +0,0 @@
-f_cond = 1.0 if (a[i, j] > 0.5) else 0.0
-c[i, j] = (a[i, j] * b[i, j]) * f_cond
-          + (0.0) * (1.0 - f_cond)
-d[i, j] = (1.0 - c[i, j]) * f_cond
-          + (0.0) * (1.0 - f_cond)
