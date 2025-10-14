8dcb4703293e8faea17707ecb076276545bf404c
Affifboudaoud <hk_boudaoud@esi.dz>
2025-10-14 11:37:41 +0200

    Enable make_transients_persistent in auto_opt

--- dace/transformation/auto/auto_optimize.py
+++ dace/transformation/auto/auto_optimize.py
@@ -663,8 +663,7 @@ def auto_optimize(sdfg: SDFG,
     move_small_arrays_to_stack(sdfg)
 
     # Make all independent arrays persistent
-    # disable this for now
-    # make_transients_persistent(sdfg, device)
+    make_transients_persistent(sdfg, device)
 
     if symbols:
         # Specialize for all known symbols
