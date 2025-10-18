6be15fab965511e4b4dce12e096dad158d1c89e1
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-10-18 13:02:12 +0200

    Import back changes

---
+++ dace/transformation/dataflow/move_alloc_up.py
@@ -0,0 +1,140 @@
+from typing import List, Any
+import dace
+
+# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+from dace import data as dt, symbolic, SDFG
+from dace.sdfg import nodes, utils as sdutil
+from dace.sdfg.state import SDFGState
+from dace.transformation import transformation
+import copy
+import itertools
+
+
+def offset_memlets(state: dace.SDFGState, offsets: List[Any], entry_node: dace.nodes.MapEntry, name: str):
+    all_nodes_between = state.all_nodes_between(entry_node, state.exit_node(entry_node))
+    all_edges = state.all_edges(*all_nodes_between)
+    for edge in set(all_edges):
+        if edge.data.data is None:
+            continue
+
+        if edge.data.data != name:
+            continue
+
+        old_subset = [(b, e, s) for (b, e, s) in edge.data.subset]
+
+        if len(offsets) > len(old_subset):
+            for _ in range(len(offsets) - len(old_subset)):
+                old_subset.insert(0, (dace.symbolic.SymExpr(0), dace.symbolic.SymExpr(0), 1))
+
+        if len(offsets) == len(old_subset):
+            new_subset = [(b + offset, e + offset, s) for offset, (b, e, s) in zip(offsets, old_subset)]
+        else:
+            raise Exception("hmm")
+
+        edge.data = dace.memlet.Memlet(data=edge.data.data, subset=dace.subsets.Range(new_subset))
+
+
+def move_access_node_up(state: dace.SDFGState, access_node: dace.nodes.AccessNode, do_offset_memlets: bool):
+    # Scope Lifetime array where size matches the map dimensions
+    # Move it to the upper map, make the size match the parent map, replace all subsets
+    assert state.in_degree(access_node) == 1
+
+    lvl2_ie = state.in_edges(access_node)[0]
+    lvl2_oe = state.out_edges(access_node)[0]
+    lvl2_map_entry: dace.nodes.MapEntry = lvl2_ie.src
+    lvl1_map_entry: dace.nodes.MapEntry = state.entry_node(lvl2_map_entry)
+    # Add node to lvl1
+    in_conn = lvl2_ie.src_conn.replace("OUT_", "IN_")
+    lvl1_ie = list(state.in_edges_by_connector(lvl2_map_entry, in_conn))[0]
+
+    new_size = [(e + 1 - b) for (b, e, s) in lvl1_ie.data.subset]
+
+    sdfg = state.sdfg
+    dataname = access_node.data
+    copydesc = sdfg.arrays[dataname]
+    sdfg.remove_data(dataname, False)
+
+    sdfg.add_array(name=dataname,
+                   shape=tuple(new_size),
+                   dtype=copydesc.dtype,
+                   storage=copydesc.storage,
+                   location=copydesc.location,
+                   transient=copydesc.transient,
+                   lifetime=copydesc.lifetime)
+
+    # Remove node from lvl2
+    state.remove_node(access_node)
+    lvl2_memlet = copy.deepcopy(lvl2_oe.data)
+    state.add_edge(lvl2_ie.src, lvl2_ie.src_conn, lvl2_oe.dst, lvl2_oe.dst_conn, lvl2_memlet)
+
+    new_access = state.add_access(dataname)
+
+    lvl1_ie_memlet = copy.deepcopy(lvl1_ie.data)
+    lvl1_ie_memlet.data = new_access.data
+    state.add_edge(new_access, None, lvl1_ie.dst, lvl1_ie.dst_conn, lvl1_ie_memlet)
+    state.remove_edge(lvl1_ie)
+    state.add_edge(lvl1_ie.src, lvl1_ie.src_conn, new_access, None, copy.deepcopy(lvl1_ie.data))
+
+    new_begs = [b for (b, e, s) in lvl2_ie.data.subset]
+    print(new_begs)
+    if do_offset_memlets:
+        offset_memlets(state, new_begs, lvl2_map_entry, dataname)
+
+
+def move_exit_access_node_down(state: dace.SDFGState, access_node: dace.nodes.AccessNode, do_offset_memlets: bool):
+    # Scope Lifetime array where size matches the map dimensions
+    # Move it to the upper map, make the size match the parent map, replace all subsets
+    assert state.in_degree(access_node) == 1
+
+    lvl2_ie = state.in_edges(access_node)[0]
+    lvl2_oe = state.out_edges(access_node)[0]
+
+    # Lvl3 Map -> AN ->  Lvl2 Map -> Lvl1 Map
+    #          | Lvl2_ie
+    #                | Lvl2_oe
+
+    lvl2_map_exit: dace.nodes.MapExit = lvl2_oe.dst
+    lvl2_map_entry = {
+        n
+        for n in state.nodes() if isinstance(n, dace.nodes.MapEntry) and state.exit_node(n) == lvl2_map_exit
+    }.pop()
+    lvl1_map_entry = state.entry_node(lvl2_map_entry)
+    lvl1_map_exit: dace.nodes.MapExit = state.exit_node(lvl1_map_entry)
+    # Add node to lvl1
+    out_conn = lvl2_oe.dst_conn.replace("IN_", "OUT_")
+
+    lvl1_oe = list(state.out_edges_by_connector(lvl2_map_exit, out_conn))[0]
+
+    new_size = [(e + 1 - b) for (b, e, s) in lvl1_oe.data.subset]
+
+    sdfg = state.sdfg
+    dataname = access_node.data
+    copydesc = sdfg.arrays[dataname]
+    sdfg.remove_data(dataname, False)
+
+    sdfg.add_array(name=dataname,
+                   shape=tuple(new_size),
+                   dtype=copydesc.dtype,
+                   storage=copydesc.storage,
+                   location=copydesc.location,
+                   transient=copydesc.transient,
+                   lifetime=copydesc.lifetime)
+
+    # Remove node from lvl2
+    state.remove_node(access_node)
+    lvl2_memlet = copy.deepcopy(lvl2_ie.data)
+    lvl2_memlet.data = dataname
+    state.add_edge(lvl2_ie.src, lvl2_ie.src_conn, lvl2_oe.dst, lvl2_oe.dst_conn, lvl2_memlet)
+
+    new_access = state.add_access(dataname)
+
+    lvl1_oe_memlet = copy.deepcopy(lvl1_oe.data)
+    lvl1_oe_memlet.data = new_access.data
+    state.add_edge(lvl1_oe.src, lvl1_oe.src_conn, new_access, None, lvl1_oe_memlet)
+    state.remove_edge(lvl1_oe)
+    state.add_edge(new_access, None, lvl1_oe.dst, lvl1_oe.dst_conn, copy.deepcopy(lvl1_oe.data))
+
+    new_begs = [b for (b, e, s) in lvl2_oe.data.subset]
+    print(new_begs)
+    if do_offset_memlets:
+        offset_memlets(state, new_begs, lvl2_map_entry, dataname)
---
+++ dace/transformation/passes/remove_assignment_tasklets.py
@@ -0,0 +1,73 @@
+import copy
+import dace
+# Copyright 2019-2024 ETH Zurich and the DaCe authors. All rights reserved.
+from collections import defaultdict
+from typing import Any, Dict, Optional, Set
+
+from dace import SDFG, InterstateEdge
+from dace.sdfg import nodes as nd
+from dace.sdfg.state import ConditionalBlock, LoopRegion
+from dace.transformation import pass_pipeline as ppl, transformation
+from dace.transformation.passes import analysis as ap
+from dace import properties
+import dace.sdfg.construction_utils as cutil
+
+
+@transformation.explicit_cf_compatible
+class RemoveAssignmentTasklets(ppl.Pass):
+    copy_tasklet_pattern = properties.Property(dtype=str, default='', allow_none=False)
+
+    def modifies(self) -> ppl.Modifies:
+        return ppl.Modifies.AccessNodes | ppl.Modifies.Tasklets
+
+    def should_reapply(self, modified: ppl.Modifies) -> bool:
+        return False
+
+    def depends_on(self):
+        return {}
+
+    def _arr_appears_in_loop(self, arr_name: str, loop: LoopRegion):
+        pass
+
+    def _arr_appears_in_conditional(self, arr_name: str, conditional: ConditionalBlock):
+        pass
+
+    def _apply(self, sdfg: dace.SDFG):
+        for state in sdfg.all_states():
+            nodes_to_rm = set()
+            for node in state.nodes():
+                if isinstance(node, dace.nodes.Tasklet):
+                    if len(node.in_connectors) == 1 and len(node.out_connectors) == 1:
+                        in_conn_name = next(iter(node.in_connectors.keys()))
+                        out_conn_name = next(iter(node.out_connectors.keys()))
+                        if (node.code.as_string == f"{out_conn_name} = {in_conn_name}"
+                                or node.code.as_string == f"{out_conn_name} = {in_conn_name};"
+                                or node.code.as_string == f"vector_copy({out_conn_name}, {in_conn_name});"):
+                            # Can rm this node
+                            nodes_to_rm.add(node)
+
+            print(nodes_to_rm)
+            for node in nodes_to_rm:
+                ie = state.in_edges(node)[0]
+                oe = state.out_edges(node)[0]
+
+                state.remove_node(node)
+
+                assert isinstance(ie.src, dace.nodes.AccessNode)
+                state.add_edge(
+                    ie.src, None, oe.dst, oe.dst_conn,
+                    dace.memlet.Memlet(
+                        data=oe.data.data,
+                        subset=copy.deepcopy(oe.data.subset),
+                        other_subset=copy.deepcopy(ie.data.subset),
+                    ))
+
+            for node in state.nodes():
+                if isinstance(node, dace.nodes.NestedSDFG):
+                    self._apply(node.sdfg)
+
+    def apply_pass(self, sdfg: SDFG, pipeline_results: Dict[str, Any]) -> Optional[Dict[str, Set[str]]]:
+        self._apply(sdfg)
+
+    def report(self, pass_retval: Any) -> Optional[str]:
+        return f''
