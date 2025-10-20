211fc3b2ef8241ccd6c3e1a7c3625457ec8b13e2
Philip Mueller <philip.mueller@cscs.ch>
2025-10-20 08:51:27 +0200

    First version of teh test done, now increase complexity.

--- tests/consolidate_edges_test.py
+++ tests/consolidate_edges_test.py
@@ -1,28 +1,132 @@
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+
+from typing import Tuple
 import dace
+from dace import subsets as dace_sbs
+from dace.sdfg import nodes as dace_nodes
 from dace.sdfg.utils import consolidate_edges
 
-sdfg = dace.SDFG('cetest')
-sdfg.add_array('A', [50], dace.float32)
-sdfg.add_array('B', [48], dace.float32)
-state = sdfg.add_state()
+from .transformations import utility
 
-r = state.add_read('A')
-me, mx = state.add_map('map', dict(i='1:49'))
-t = state.add_tasklet('op', {'a', 'b', 'c'}, {'out'}, 'out = a + b + c')
-w = state.add_write('B')
 
-state.add_memlet_path(r, me, t, dst_conn='a', memlet=dace.Memlet.simple('A', 'i-1'))
-state.add_memlet_path(r, me, t, dst_conn='b', memlet=dace.Memlet.simple('A', 'i'))
-state.add_memlet_path(r, me, t, dst_conn='c', memlet=dace.Memlet.simple('A', 'i+1'))
-state.add_memlet_path(t, mx, w, src_conn='out', memlet=dace.Memlet.simple('B', 'i-1'))
+def _make_cetest_sdfg():
+    sdfg = dace.SDFG('cetest')
+    sdfg.add_array('A', [50], dace.float32)
+    sdfg.add_array('B', [48], dace.float32)
+    state = sdfg.add_state()
+
+    r = state.add_read('A')
+    me, mx = state.add_map('map', dict(i='1:49'))
+    t = state.add_tasklet('op', {'a', 'b', 'c'}, {'out'}, 'out = a + b + c')
+    w = state.add_write('B')
+
+    state.add_memlet_path(r, me, t, dst_conn='a', memlet=dace.Memlet.simple('A', 'i-1'))
+    state.add_memlet_path(r, me, t, dst_conn='b', memlet=dace.Memlet.simple('A', 'i'))
+    state.add_memlet_path(r, me, t, dst_conn='c', memlet=dace.Memlet.simple('A', 'i+1'))
+    state.add_memlet_path(t, mx, w, src_conn='out', memlet=dace.Memlet.simple('B', 'i-1'))
+
+    sdfg.validate()
+
+    return sdfg, state
 
 
 def test_consolidate_edges():
+    sdfg, state = _make_cetest_sdfg()
     assert len(state.edges()) == 8
     consolidate_edges(sdfg)
     assert len(state.edges()) == 6
 
 
+def _make_sdfg_multi_usage_input(
+    N: int, ) -> Tuple[dace.SDFG, dace.SDFGState, dace_nodes.AccessNode, dace_nodes.MapEntry]:
+    sdfg = dace.SDFG(utility.unique_name("multi_usage_sdfg"))
+    state = sdfg.add_state(is_start_block=True)
+
+    multi_use_value_data, _ = sdfg.add_array(
+        "multi_use_value",
+        shape=(12, ),
+        dtype=dace.float64,
+        transient=False,
+    )
+    multi_use_value = state.add_access(multi_use_value_data)
+    me, mx = state.add_map(
+        "comp",
+        ndrange={
+            "__i": "0:10",
+            "__j": "0:30",
+        },
+    )
+
+    for i in range(N):
+        input_data = f"input_{i}"
+        output_data = f"output_{i}"
+        offset_in_i = i % 3
+        for name in [input_data, output_data]:
+            sdfg.add_array(
+                name,
+                shape=(10, 30),
+                dtype=dace.float64,
+                transient=False,
+            )
+
+        iac, oac = (state.add_access(name) for name in [input_data, output_data])
+        tlet = state.add_tasklet(
+            f"tlet_{i}",
+            inputs={"__in1", "__in2"},
+            outputs={"__out"},
+            code="__out = __in1 + __in2",
+        )
+
+        state.add_edge(multi_use_value, None, me, f"IN_muv_{i}",
+                       dace.Memlet(f"{multi_use_value_data}[{offset_in_i}:{offset_in_i + 10}]"))
+        state.add_edge(me, f"OUT_muv_{i}", tlet, "__in1", dace.Memlet(f"{multi_use_value_data}[__i + {offset_in_i}]"))
+        me.add_scope_connectors(f"muv_{i}")
+
+        state.add_edge(iac, None, me, f"IN_{input_data}", dace.Memlet(f"{input_data}[0:10, 0:3]"))
+        state.add_edge(me, f"OUT_{input_data}", tlet, "__in2", dace.Memlet(f"{input_data}[__i, __j]"))
+        me.add_scope_connectors(input_data)
+
+        state.add_edge(tlet, "__out", mx, f"IN_{output_data}", dace.Memlet(f"{output_data}[__i, __j]"))
+        state.add_edge(mx, f"OUT_{output_data}", oac, None, dace.Memlet(f"{output_data}[0:10, 0:30]"))
+        mx.add_scope_connectors(output_data)
+
+    sdfg.validate()
+
+    return sdfg, state, multi_use_value, me
+
+
+def test_multi_use_value_input():
+    N = 5
+    sdfg, state, multi_use_value, me = _make_sdfg_multi_usage_input(N=N)
+
+    initial_ac = utility.count_nodes(sdfg, dace_nodes.AccessNode, True)
+    assert multi_use_value in initial_ac
+    assert state.out_degree(multi_use_value) == N
+    assert all((oedge.data.src_subset == dace_sbs.Range.from_string("0:10") or oedge.data.src_subset ==
+                dace_sbs.Range.from_string("1:11") or oedge.data.src_subset == dace_sbs.Range.from_string("2:12"))
+               for oedge in state.out_edges(multi_use_value))
+    assert all(
+        state.out_degree(ac) == 1 and isinstance(ac, dace_nodes.AccessNode) for ac in state.source_nodes()
+        if ac is not multi_use_value)
+    assert all(state.in_degree(ac) == 1 for ac in state.sink_nodes())
+
+    ref, res = utility.make_sdfg_args(sdfg)
+    utility.compile_and_run_sdfg(sdfg, **ref)
+
+    consolidate_edges(sdfg)
+
+    ac_after = utility.count_nodes(sdfg, dace_nodes.AccessNode, True)
+    assert set(initial_ac) == set(ac_after)
+
+    assert state.out_degree(multi_use_value) == 1
+    assert all(oedge.data.src_subset == dace_sbs.Range.from_string("0:12")
+               for oedge in state.out_edges(multi_use_value))
+    assert all(state.in_degree(ac) == 1 for ac in state.sink_nodes())
+
+    utility.compile_and_run_sdfg(sdfg, **res)
+    assert utility.compare_sdfg_res(ref=ref, res=res)
+
+
 if __name__ == '__main__':
     test_consolidate_edges()
+    test_multi_use_value_input()
