6ccb8b3e6249bfd2052a94b1eba8794674d5ca25
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-11-02 23:50:20 +0100

    Add environment

---
+++ dace/libraries/standard/environments/cpu.py
@@ -0,0 +1,21 @@
+# Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
+import dace.library
+
+
+@dace.library.environment
+class CPU:
+
+    cmake_minimum_version = None
+    cmake_packages = []
+    cmake_variables = {}
+    cmake_includes = []
+    cmake_libraries = []
+    cmake_compile_flags = []
+    cmake_link_flags = []
+    cmake_files = []
+
+    headers = []
+    state_fields = []
+    init_code = ""
+    finalize_code = ""
+    dependencies = []
