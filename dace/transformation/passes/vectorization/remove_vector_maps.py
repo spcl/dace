b13a94c9d9d797a81fe7e956426a8f96c2197684
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-11-25 13:42:22 +0100

    Remove vector maps

---
+++ dace/transformation/passes/vectorization/remove_vector_maps.py
@@ -0,0 +1,31 @@
+# Copyright 2019-2025 ETH Zurich and the DaCe authors. All rights reserved.
+import copy
+from typing import Any, Dict
+from dace import SDFG, InterstateEdge, properties
+from dace.sdfg import nodes
+from dace.transformation import pass_pipeline as ppl, transformation
+from dace.transformation.passes.vectorization.vectorization_utils import *
+
+@properties.make_properties
+@transformation.explicit_cf_compatible
+class RemoveVectorMaps(ppl.Pass):
+    # This pass is testes as part of the vectorization pipeline
+    CATEGORY: str = 'Vectorization'
+
+    def modifies(self) -> ppl.Modifies:
+        return ppl.Modifies.AccessNodes | ppl.Modifies.InterstateEdges | ppl.Modifies.Tasklets | ppl.Modifies.Edges
+
+    def should_reapply(self, modified: ppl.Modifies) -> bool:
+        return False
+
+    def depends_on(self):
+        return {}
+
+    def _apply(self, sdfg: SDFG):
+        for n, g in sdfg.all_nodes_recursive():
+            if isinstance(n, nodes.MapEntry) and n.map.label.startswith("vectorloop_"):
+                remove_map(n, g)
+
+    def apply_pass(self, sdfg: SDFG, pipeline_results: Dict[str, Any]) -> None:
+        self._apply(sdfg)
+        sdfg.validate()
