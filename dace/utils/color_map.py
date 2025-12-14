2007e696a86b0263f15f5f7e70cee5e9ad5a59a1
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-12-14 13:06:54 +0100

    Add color map

---
+++ dace/utils/color_map.py
@@ -0,0 +1,6 @@
+COMPILER_COLOR_MAP = {
+    "gcc":       "#1b9e77",  # green
+    "llvm":      "#d95f02",  # orange
+    "intel":     "#7570b3",  # purple
+    "graceclang": "#e7298a",  # magenta
+}
