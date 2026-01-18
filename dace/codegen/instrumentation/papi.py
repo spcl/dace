1a4e83ebb351fd054d612794c8a799dcc4189e24
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-01-18 16:18:48 +0100

    Fix non thread safe papi init bug

--- dace/codegen/instrumentation/papi.py
+++ dace/codegen/instrumentation/papi.py
@@ -102,46 +102,55 @@ class PAPIInstrumentation(InstrumentationProvider):
         self._papi_used = True
 
     def on_sdfg_begin(self, sdfg, local_stream, global_stream, codegen):
-        if sdfg.parent is None and sdfg.instrument is dtypes.InstrumentationType.PAPI_Counters and PAPIUtils.is_papi_used(sdfg):
-            raise Exception("You cannot instrument the SDFG object in addition to inner regions. Please remove the instrumentation either from the sdfg nodes or from the sdfg object!")
-        elif sdfg.parent is None and sdfg.instrument is dtypes.InstrumentationType.PAPI_Counters and not PAPIUtils.is_papi_used(sdfg):
+        if sdfg.parent is None and sdfg.instrument is dtypes.InstrumentationType.PAPI_Counters and PAPIUtils.is_papi_used(
+                sdfg):
+            raise Exception(
+                "You cannot instrument the SDFG object in addition to inner regions. Please remove the instrumentation either from the sdfg nodes or from the sdfg object!"
+            )
+        elif sdfg.parent is None and sdfg.instrument is dtypes.InstrumentationType.PAPI_Counters and not PAPIUtils.is_papi_used(
+                sdfg):
             # Configure CMake project and counters
             self.configure_papi_native()
 
             # Add instrumentation includes and initialize PAPI
             global_stream.write('#include <dace/perf/papi.h>', sdfg)
-            local_stream.write(
-                '''dace::perf::PAPI::init();''')
+            local_stream.write('''dace::perf::PAPI::init();''')
             # start a counter on every available thread
             counter_start_code = """
 int max_threads = omp_get_max_threads();
 int _papi_event_sets[max_threads];
 std::vector<std::string> _papi_events = {{{events}}};
 int _papi_nevents = _papi_events.size();
+
 #pragma omp parallel num_threads(max_threads)
 {{
-    PAPI_register_thread();
-    int _papi_EventSet = PAPI_NULL;
-    int tretval = PAPI_create_eventset(&_papi_EventSet);
-    if (tretval != PAPI_OK){{
-        std::cerr << "PAPI error: " << "Create EventSet" << " -> " << PAPI_strerror(tretval) << std::endl;
-        std::exit(EXIT_FAILURE);
-    }}
-    _papi_event_sets[omp_get_thread_num()] = _papi_EventSet;
-    for (unsigned int i = 0; i < _papi_events.size(); ++i) {{
-        tretval = PAPI_add_named_event(_papi_EventSet, _papi_events[i].c_str());
-        if (tretval != PAPI_OK) {{
-            // If some event isn't available, print a warning and continue (but exit if critical)
-            std::cerr << "Warning: PAPI_add_event failed for event " << i
-                    << " (" << PAPI_strerror(tretval) << ").\\n";
-            _papi_nevents--;
+    #pragma omp critical
+    {{
+        int tid = omp_get_thread_num();
+        PAPI_register_thread();
+        int _papi_EventSet = PAPI_NULL;
+        int tretval = PAPI_create_eventset(&_papi_EventSet);
+        if (tretval != PAPI_OK){{
+            std::cerr << "PAPI error: " << "Create EventSet" << " -> " << PAPI_strerror(tretval) << std::endl;
+            std::exit(EXIT_FAILURE);
+        }}
+        _papi_event_sets[tid] = _papi_EventSet;
+        for (unsigned int i = 0; i < _papi_events.size(); ++i) {{
+            tretval = PAPI_add_named_event(_papi_EventSet, _papi_events[i].c_str());
+            if (tretval != PAPI_OK) {{
+                // If some event isn't available, print a warning and continue (but exit if critical)
+                std::cerr << "Warning: PAPI_add_event failed for event " << i
+                        << " (" << PAPI_strerror(tretval) << ").\\n";
+                _papi_nevents--;
+            }}
         }}
     }}
 }}
 #pragma omp parallel num_threads(max_threads)
 {{
+    int tid = omp_get_thread_num();
     if(_papi_nevents>0){{
-        PAPI_start(_papi_event_sets[omp_get_thread_num()]);
+        PAPI_start(_papi_event_sets[tid]);
     }}
 }}
 """.format(events=f"\"{'\", \"'.join(self._counters)}\"")
@@ -166,42 +175,41 @@ dace::perf::PAPIValueStore<%s> __perf_store (__state->report);''' % (', '.join(s
     def on_sdfg_end(self, sdfg, local_stream, global_stream):
         if not self._papi_used:
             return
-        
-        if sdfg.parent is None and sdfg.instrument is dtypes.InstrumentationType.PAPI_Counters and not PAPIUtils.is_papi_used(sdfg):
+
+        if sdfg.parent is None and sdfg.instrument is dtypes.InstrumentationType.PAPI_Counters and not PAPIUtils.is_papi_used(
+                sdfg):
             counter_stop_code = """
 // Stop counters
 #pragma omp parallel num_threads(max_threads)
 {{
-    size_t thread_num = omp_get_thread_num();
-    int _papi_EventSet = _papi_event_sets[thread_num];
+    size_t tid = omp_get_thread_num();
+    int _papi_EventSet = _papi_event_sets[tid];
     long long _papi_counts[_papi_nevents] = {{0}};
     int tretval = PAPI_stop(_papi_EventSet, _papi_counts);
     if (tretval != PAPI_OK){{
         std::cerr << "PAPI error: " << "Stop Counters" << " -> " << PAPI_strerror(tretval) << std::endl;
         std::exit(EXIT_FAILURE);
     }}
-    tretval = PAPI_cleanup_eventset(_papi_EventSet);
-    if (tretval != PAPI_OK) {{
-        std::cerr << "Warning: PAPI_cleanup_eventset: " << PAPI_strerror(tretval) << "\\n";
-    }}
-    tretval = PAPI_destroy_eventset(&_papi_EventSet);
-    if (tretval != PAPI_OK) {{
-        std::cerr << "Warning: PAPI_destroy_eventset: " << PAPI_strerror(tretval) << "\\n";
-    }}
-    PAPI_unregister_thread();
     #pragma omp critical
-    {{  
-        for(int i = 0; i<_papi_nevents; i++){{
-            __state->report.add_counter("{region_name}", "PAPI", _papi_events[i].c_str(), (unsigned long) _papi_counts[i], thread_num, {cfg_id}, {state_id}, {node_id});
+    {{
+        tretval = PAPI_cleanup_eventset(_papi_EventSet);
+        if (tretval != PAPI_OK) {{
+            std::cerr << "Warning: PAPI_cleanup_eventset: " << PAPI_strerror(tretval) << "\\n";
         }}
-    }} 
+        tretval = PAPI_destroy_eventset(&_papi_EventSet);
+        if (tretval != PAPI_OK) {{
+            std::cerr << "Warning: PAPI_destroy_eventset: " << PAPI_strerror(tretval) << "\\n";
+        }}
+        PAPI_unregister_thread();
+        for(int i = 0; i<_papi_nevents; i++){{
+            __state->report.add_counter("{region_name}", "PAPI", _papi_events[i].c_str(), (unsigned long) _papi_counts[i], tid, {cfg_id}, {state_id}, {node_id});
+        }}
+    }}
 }}
-""".format(region_name = (f"{sdfg.name}_{sdfg.cfg_id}"), cfg_id=sdfg.cfg_id, state_id=-1, node_id=-1)
+""".format(region_name=(f"{sdfg.name}_{sdfg.cfg_id}"), cfg_id=sdfg.cfg_id, state_id=-1, node_id=-1)
             local_stream.write(counter_stop_code)
         else:
-             local_stream.write('__perf_store.flush();', sdfg)
-        
-
+            local_stream.write('__perf_store.flush();', sdfg)
 
     def on_state_begin(self, sdfg, cfg, state, local_stream, global_stream):
         if not self._papi_used:
