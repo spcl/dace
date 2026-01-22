20b1bd2bb233cab0a02fd4add92eadd097310275
Tal Ben-Nun <tbennun@gmail.com>
2026-01-22 00:11:05 -0800

    Fix type hints for enums and numpy tests

--- dace/registry.py
+++ dace/registry.py
@@ -6,6 +6,7 @@ import aenum
 from typing import Dict, Type, TypeVar
 
 T = TypeVar('T')
+E = TypeVar('E', bound=aenum.Enum)
 
 
 def make_registry(cls: Type[T]) -> Type[T]:
@@ -58,7 +59,7 @@ def autoregister_params(**params):
     return lambda cls: autoregister(cls, **params)
 
 
-def undefined_safe_enum(cls: Type):
+def undefined_safe_enum(cls: type[E]) -> type[E]:
     """
     Decorator that adds a value ``Undefined`` to an enumeration.
     """
@@ -68,7 +69,7 @@ def undefined_safe_enum(cls: Type):
     return cls
 
 
-def extensible_enum(cls: Type):
+def extensible_enum(cls: type[E]) -> type[E]:
     """
     Decorator that adds a function called ``register`` to an enumeration,
     extending its values. Note that new values cannot be unregistered.
@@ -80,7 +81,7 @@ def extensible_enum(cls: Type):
     if not issubclass(cls, aenum.Enum):
         raise TypeError("Only aenum.Enum subclasses may be made extensible")
 
-    def _extend_enum(cls: Type, name: str, *value):
+    def _extend_enum(cls: type[E], name: str, *value):
         aenum.extend_enum(cls, name, *value)
 
     cls.register = lambda name, *args: _extend_enum(cls, name, *args)
