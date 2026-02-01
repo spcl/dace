65bffbc107bae309807931990c3206c497dc9993
Tal Ben-Nun <tbennun@gmail.com>
2026-02-01 11:01:19 -0800

    Add tests

--- tests/tasklet_test.py
+++ tests/tasklet_test.py
@@ -2,25 +2,83 @@
 import dace
 import math as mt
 import numpy as np
+import pytest
+from dace.sdfg.validation import InvalidSDFGError
 
 
-@dace.program
-def myprint(input, N, M):
+def test_decorator_syntax():
 
-    @dace.tasklet
-    def myprint():
-        a << input
-        for i in range(0, N):
-            for j in range(0, M):
-                mt.sin(a[i, j])
+    @dace.program
+    def myprint(input, N, M):
 
+        @dace.tasklet
+        def myprint():
+            a << input
+            for i in range(0, N):
+                for j in range(0, M):
+                    mt.sin(a[i, j])
 
-def test():
     input = dace.ndarray([10, 10], dtype=dace.float32)
     input[:] = np.random.rand(10, 10).astype(dace.float32.type)
 
     myprint(input, 10, 10)
 
 
+def test_invalid_array_access():
+
+    @dace.program
+    def tester(input: dace.float64[20], output: dace.float64[20]):
+        with dace.tasklet:
+            out >> output[0]
+            out = input[1]  # Invalid access, must go through memlet
+
+    with pytest.raises(InvalidSDFGError):
+        tester.to_sdfg(simplify=False)
+
+
+def test_invalid_scalar_access():
+
+    @dace.program
+    def tester(input: dace.float64, output: dace.float64[20]):
+        with dace.tasklet:
+            out >> output[0]
+            out = input  # Invalid access, must go through memlet
+
+    with pytest.raises(InvalidSDFGError):
+        tester.to_sdfg(simplify=False)
+
+
+def test_invalid_array_access_decorator_syntax():
+
+    @dace.program
+    def tester(input: dace.float64[20], output: dace.float64[20]):
+
+        @dace.tasklet
+        def tasklet():
+            out >> output[0]
+            out = input[1]  # Invalid access, must go through memlet
+
+    with pytest.raises(InvalidSDFGError):
+        tester.to_sdfg(simplify=False)
+
+
+def test_invalid_scalar_access_decorator_syntax():
+
+    @dace.program
+    def tester(input: dace.float64, output: dace.float64[20]):
+
+        @dace.tasklet
+        def tasklet():
+            out >> output[0]
+            out = input  # Invalid access, must go through memlet
+
+    with pytest.raises(InvalidSDFGError):
+        tester.to_sdfg(simplify=False)
+
+
 if __name__ == "__main__":
-    test()
+    test_decorator_syntax()
+    test_invalid_array_access()
+    test_invalid_scalar_access()
+    test_invalid_array_access_decorator_syntax()
+    test_invalid_scalar_access_decorator_syntax()
