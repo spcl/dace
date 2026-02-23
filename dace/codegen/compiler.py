360e6b632417a506e94d9659849beb20044602da
Roman Cattaneo <>
2026-02-23 17:43:49 +0100

    only write cachedir.tag if it doesn't already exist

--- dace/codegen/compiler.py
+++ dace/codegen/compiler.py
@@ -106,8 +106,10 @@ def generate_program_folder(sdfg, code_objects: List[CodeObject], out_path: str,
                 hfile.write(contents)
 
     # Write cachedir tag
-    with open(os.path.join(out_path, "CACHEDIR.TAG"), "w") as f:
-        f.write("""Signature: 8a477f597d28d172789f06886806bc55
+    cachedir_tag = os.path.join(out_path, "CACHEDIR.TAG")
+    if not os.path.exists(cachedir_tag):
+        with open(cachedir_tag, "w") as f:
+            f.write("""Signature: 8a477f597d28d172789f06886806bc55
 # This file is a cache directory tag created by DaCe.
 # For information about cache directory tags, see:
 #	http://www.brynosaurus.com/cachedir/
