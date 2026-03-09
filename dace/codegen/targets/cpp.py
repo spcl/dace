1ca59fd9abc1d75c385b86aec43bcfebd963e81e
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-03-09 11:13:54 +0100

    Fix related to fpga deprecation

--- dace/codegen/targets/cpp.py
+++ dace/codegen/targets/cpp.py
@@ -327,10 +327,6 @@ def emit_memlet_reference(dispatcher: 'TargetDispatcher',
             if memlet.data in dispatcher.frame.symbols_and_constants(sdfg):
                 ref = '*'
                 typedef = make_const(typedef)
-    elif defined_type == DefinedType.ArrayInterface:
-        base_ctype = conntype.base_type.ctype
-        typedef = f"{base_ctype}* __restrict__" if is_write else f"const {base_ctype}* __restrict__"
-        is_scalar = False
     elif defined_type == DefinedType.Scalar:
         typedef = defined_ctype if is_scalar else (defined_ctype + '* __restrict__')
         if is_write is False and not isinstance(desc, data.Structure):
