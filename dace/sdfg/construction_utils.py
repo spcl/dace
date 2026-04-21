259a1f6d842ff264ec9ba539be02b2061812d3b0
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-04-21 17:28:02 +0200

    Run lint

--- dace/sdfg/construction_utils.py
+++ dace/sdfg/construction_utils.py
@@ -1,7 +1,6 @@
 # Copyright 2019-2026 ETH Zurich and the DaCe authors. All rights reserved.
 """
-Utility functions for traversing the SDFG hierarchy to discover parent
-map scopes, loop regions, and nested SDFG boundaries.
+Utility functions for manipulating/constructing/analyzing SDFG components
 """
 from typing import List, Union
 
