317762672352978d1d18b5cf9f8f5993fbad64d2
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2026-07-09 19:26:36 +0200

    mpi: add CommRank/CommSize/CommSplit/Abort/Gatherv library nodes + modernize Gather to resolve_comm

--- dace/libraries/mpi/nodes/__init__.py
+++ dace/libraries/mpi/nodes/__init__.py
@@ -7,11 +7,16 @@ from .wait import Wait, Waitall
 from .bcast import Bcast
 from .scatter import Scatter, BlockScatter
 from .gather import Gather, BlockGather
+from .gatherv import Gatherv
 from .reduce import Reduce
 from .allreduce import Allreduce
 from .allgather import Allgather
 from .alltoall import Alltoall
 from .barrier import Barrier
 from .comm_f2c import CommF2c
+from .comm_rank import CommRank
+from .comm_size import CommSize
+from .comm_split import CommSplit
+from .abort import Abort
 from .dummy import Dummy
 from .redistribute import Redistribute
--- dace/libraries/mpi/nodes/gather.py
+++ dace/libraries/mpi/nodes/gather.py
@@ -6,7 +6,7 @@ from dace.sdfg import nodes
 from dace.symbolic import symstr
 from dace.transformation.transformation import ExpandTransformation
 from .. import environments
-from dace.libraries.mpi.nodes.node import MPINode, expanded_input_connectors, input_descriptor_name
+from dace.libraries.mpi.nodes.node import MPINode, resolve_comm, expanded_input_connectors, input_descriptor_name
 
 
 @library.expansion
@@ -25,14 +25,20 @@ class ExpandGatherMPI(ExpandTransformation):
         if root.dtype.base_type != dtypes.int32:
             raise ValueError("Gather root must be an integer!")
 
+        comm = resolve_comm(node, parent_state)
+
         code = f"""
             int _commsize;
-            MPI_Comm_size(MPI_COMM_WORLD, &_commsize);
+            MPI_Comm_size({comm}, &_commsize);
             MPI_Gather(_inbuffer, {in_count_str}, {in_mpi_dtype_str},
                        _outbuffer, ({out_count_str})/_commsize, {out_mpi_dtype_str},
-                       _root, MPI_COMM_WORLD);
+                       _root, {comm});
             """
-        tasklet = nodes.Tasklet(node.name, node.in_connectors, node.out_connectors, code, language=dtypes.Language.CPP)
+        tasklet = nodes.Tasklet(node.name,
+                                expanded_input_connectors(node, parent_state),
+                                node.out_connectors,
+                                code,
+                                language=dtypes.Language.CPP)
         return tasklet
 
 
