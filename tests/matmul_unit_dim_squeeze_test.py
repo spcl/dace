spcl/dace

commit afd0efe48238bbaba23a6eee1935fe67e8397f8f

Fixes aggressive SliceView removal and removes unnecessary squeezing in MatMult library nodes (#1884)
Co-authored-by: Tal Ben-Nun <tbennun@gmail.com>
Co-authored-by: Tal Ben-Nun <tbennun@users.noreply.github.com>

dace/libraries/blas/nodes/batched_matmul.py
@@ -22,7 +22,7 @@ class ExpandBatchedMatMulPure(ExpandTransformation):
     @staticmethod
     def make_sdfg(node, parent_state, parent_sdfg):
         # Get metadata from parent SDFG
-        ((edge_a, outer_array_a, shape_a, strides_a), (edge_b, outer_array_b, shape_b, strides_b),
+        ((edge_a, outer_array_a, shape_a, strides_a, _, _), (edge_b, outer_array_b, shape_b, strides_b, _, _),
          cdata) = _get_matmul_operands(node, parent_state, parent_sdfg)
         outedge = parent_state.out_edges(node)[0]
         cdesc = parent_sdfg.arrays[outedge.data.data]
@@ -52,7 +52,7 @@ class ExpandBatchedMatMulPure(ExpandTransformation):
 
         _, array_a = sdfg.add_array("_a", shape_a, dtype_a, strides=strides_a, storage=storage)
         _, array_b = sdfg.add_array("_b", shape_b, dtype_b, strides=strides_b, storage=storage)
-        _, array_c = sdfg.add_array("_c", shape_c, dtype_c, strides=cdata[-1], storage=storage)
+        _, array_c = sdfg.add_array("_c", shape_c, dtype_c, strides=cdata[-3], storage=storage)
 
         # Add an initialization state
         init_state = sdfg.add_state()
@@ -93,7 +93,8 @@ class ExpandBatchedMatMulMKL(ExpandTransformation):
     @staticmethod
     def expansion(node, state, sdfg):
         node.validate(sdfg, state)
-        (_, adesc, ashape, astrides), (_, bdesc, bshape, bstrides), _ = _get_matmul_operands(node, state, sdfg)
+        (_, adesc, ashape, astrides, _, _), (_, bdesc, bshape, bstrides, _,
+                                             _), _ = _get_matmul_operands(node, state, sdfg)
         cdesc: dt.Array = sdfg.arrays[state.out_edges(node)[0].data.data]
         check_access(dtypes.ScheduleType.CPU_Multicore, adesc, bdesc, cdesc)
         dtype = cdesc.dtype.base_type
@@ -162,7 +163,8 @@ class ExpandBatchedMatMulOpenBLAS(ExpandTransformation):
     @staticmethod
     def expansion(node, state, sdfg):
         node.validate(sdfg, state)
-        (_, adesc, ashape, astrides), (_, bdesc, bshape, bstrides), _ = _get_matmul_operands(node, state, sdfg)
+        (_, adesc, ashape, astrides, _, _), (_, bdesc, bshape, bstrides, _,
+                                             _), _ = _get_matmul_operands(node, state, sdfg)
         cdesc = sdfg.arrays[state.out_edges(node)[0].data.data]
         check_access(dtypes.ScheduleType.CPU_Multicore, adesc, bdesc, cdesc)
         dtype = cdesc.dtype.base_type
@@ -449,10 +451,7 @@ class BatchedMatMul(dace.sdfg.nodes.LibraryNode):
                 f'may not match', UserWarning)
         elif not res:
             raise ValueError("Inputs to matrix-matrix product must agree in the k-dimension")
-        out_subset = dc(out_memlet.subset)
-        out_subset.squeeze()
-        size2 = out_subset.size()
-        if len(size2) != 3:
+        if len(out_memlet.subset) != 3:
             raise ValueError("batched matrix-matrix product only supported on matrices")
 
 

dace/libraries/blas/nodes/matmul.py
@@ -12,23 +12,27 @@ def _get_matmul_operands(node, state, sdfg, name_lhs="_a", name_rhs="_b", name_o
     res_rhs = None
     for edge in state.all_edges(node):
         if edge.dst_conn in [name_lhs, name_rhs]:
-            subset = dc(edge.data.subset)
-            squeezed = subset.squeeze()
-            size = subset.size()
+            size = edge.data.subset.size()
+            squeezed = dc(edge.data.subset)
+            squeezed_dims = squeezed.squeeze()
+            squeezed_size = squeezed.size()
             outer_array = sdfg.data(dace.sdfg.find_input_arraynode(state, edge).data)
-            strides = [s for i, s in enumerate(outer_array.strides) if i in squeezed]
-            res = edge, outer_array, size, strides
+            strides = list(outer_array.strides)
+            squeezed_strides = [s for i, s in enumerate(outer_array.strides) if i in squeezed_dims]
+            res = edge, outer_array, size, strides, squeezed_size, squeezed_strides
             if edge.dst_conn == name_lhs:
                 res_lhs = res
             else:
                 res_rhs = res
         elif edge.src_conn == name_out:
-            subset = dc(edge.data.subset)
-            squeezed = subset.squeeze()
-            size = subset.size()
+            size = edge.data.subset.size()
+            squeezed = dc(edge.data.subset)
+            squeezed_dims = squeezed.squeeze()
+            squeezed_size = squeezed.size()
             outer_array = sdfg.data(dace.sdfg.find_output_arraynode(state, edge).data)
-            strides = [s for i, s in enumerate(outer_array.strides) if i in squeezed]
-            res_out = edge, outer_array, size, strides
+            strides = list(outer_array.strides)
+            squeezed_strides = [s for i, s in enumerate(outer_array.strides) if i in squeezed_dims]
+            res_out = edge, outer_array, size, strides, squeezed_size, squeezed_strides
     for res, name in ((res_lhs, name_lhs), (res_rhs, name_rhs), (res_out, name_out)):
         if res is None:
             raise ValueError("Matrix multiplication connector "
@@ -87,7 +91,8 @@ def _get_codegen_gemm_opts(node, state, sdfg, adesc, bdesc, cdesc, alpha, beta, 
     from dace.codegen.common import sym2cpp
     from dace.libraries.blas.blas_helpers import get_gemm_opts
 
-    (_, _, ashape, astride), (_, _, bshape, bstride), (_, _, cshape, cstride) = _get_matmul_operands(node, state, sdfg)
+    (_, _, ashape, astride, _, _), (_, _, bshape, bstride, _, _), (_, _, cshape, cstride, _,
+                                                                   _) = _get_matmul_operands(node, state, sdfg)
 
     if getattr(node, 'transA', False):
         ashape = list(reversed(ashape))
@@ -143,9 +148,10 @@ class SpecializeMatMul(dace.transformation.transformation.ExpandTransformation):
     @staticmethod
     def expansion(node, state, sdfg):
         a, b, c = _get_matmul_operands(node, state, sdfg)
-        size_a = a[2]
-        size_b = b[2]
-        if len(size_a) == 2 and len(size_b) == 2:
+        size_a = a[4]
+        size_b = b[4]
+        size_c = c[4]
+        if len(size_c) == 2 and ((len(size_a) == 2 and len(size_b) == 2) or (len(a[2]) == 2 and len(b[2]) == 2)):
             # Matrix and matrix -> GEMM
             from dace.libraries.blas.nodes.gemm import Gemm
             beta = node.beta

dace/codegen/targets/cpp.py
@@ -307,7 +307,7 @@ def emit_memlet_reference(dispatcher: 'TargetDispatcher',
     typedef = conntype.ctype
     offset = cpp_offset_expr(desc, memlet.subset)
     offset_expr = '[' + offset + ']'
-    is_scalar = not isinstance(conntype, dtypes.pointer)
+    is_scalar = not isinstance(conntype, dtypes.pointer) and not fpga.is_fpga_array(desc)
     ptrname = ptr(memlet.data, desc, sdfg, dispatcher.frame)
     ref = ''
 

dace/codegen/targets/cpu.py
@@ -177,9 +177,16 @@ class CPUCodeGen(TargetCodeGenerator):
         self._generated_nodes.add(node)
         self._locals.clear_scope(self._ldepth + 1)
 
-    def allocate_view(self, sdfg: SDFG, cfg: ControlFlowRegion, dfg: SDFGState, state_id: int, node: nodes.AccessNode,
-                      global_stream: CodeIOStream, declaration_stream: CodeIOStream,
-                      allocation_stream: CodeIOStream) -> None:
+    def allocate_view(self,
+                      sdfg: SDFG,
+                      cfg: ControlFlowRegion,
+                      dfg: SDFGState,
+                      state_id: int,
+                      node: nodes.AccessNode,
+                      global_stream: CodeIOStream,
+                      declaration_stream: CodeIOStream,
+                      allocation_stream: CodeIOStream,
+                      decouple_array_interfaces: bool = False) -> None:
         """
         Allocates (creates pointer and refers to original) a view of an
         existing array, scalar, or view.
@@ -221,7 +228,8 @@ class CPUCodeGen(TargetCodeGenerator):
                                                         name,
                                                         dtypes.pointer(nodedesc.dtype),
                                                         ancestor=0,
-                                                        is_write=is_write)
+                                                        is_write=is_write,
+                                                        decouple_array_interfaces=decouple_array_interfaces)
 
         # Test for views of container arrays and structs
         if isinstance(sdfg.arrays[viewed_dnode.data], (data.Structure, data.ContainerArray, data.ContainerView)):

dace/codegen/targets/xilinx.py
@@ -1216,8 +1216,15 @@ DACE_EXPORTED int __dace_exit_xilinx({sdfg_state_name} *__state) {{
     def allocate_view(self, sdfg: dace.SDFG, cfg: ControlFlowRegion, dfg: dace.SDFGState, state_id: int,
                       node: dace.nodes.AccessNode, global_stream: CodeIOStream, declaration_stream: CodeIOStream,
                       allocation_stream: CodeIOStream) -> None:
-        return self._cpu_codegen.allocate_view(sdfg, cfg, dfg, state_id, node, global_stream, declaration_stream,
-                                               allocation_stream)
+        return self._cpu_codegen.allocate_view(sdfg,
+                                               cfg,
+                                               dfg,
+                                               state_id,
+                                               node,
+                                               global_stream,
+                                               declaration_stream,
+                                               allocation_stream,
+                                               decouple_array_interfaces=self._decouple_array_interfaces)
 
     def generate_nsdfg_arguments(self, sdfg, cfg, dfg, state, node):
         # Connectors that are both input and output share the same name, unless

dace/libraries/blas/nodes/gemm.py
@@ -7,8 +7,7 @@ from dace import SDFG, SDFGState
 from dace.frontend.common import op_repository as oprepo
 import dace.sdfg.nodes
 from dace.transformation.transformation import ExpandTransformation
-from dace.libraries.blas.blas_helpers import (to_blastype, get_gemm_opts, check_access, dtype_to_cudadatatype,
-                                              to_cublas_computetype)
+from dace.libraries.blas.blas_helpers import to_blastype, check_access, dtype_to_cudadatatype, to_cublas_computetype
 from dace.libraries.blas.nodes.matmul import (_get_matmul_operands, _get_codegen_gemm_opts)
 from .. import environments
 import numpy as np
@@ -47,7 +46,7 @@ class ExpandGemmPure(ExpandTransformation):
     def make_sdfg(node, parent_state, parent_sdfg):
         sdfg = dace.SDFG(node.label + "_sdfg")
 
-        ((edge_a, outer_array_a, shape_a, strides_a), (edge_b, outer_array_b, shape_b, strides_b),
+        ((edge_a, outer_array_a, shape_a, strides_a, _, _), (edge_b, outer_array_b, shape_b, strides_b, _, _),
          cdata) = _get_matmul_operands(node, parent_state, parent_sdfg)
 
         dtype_a = outer_array_a.dtype.type
@@ -80,7 +79,7 @@ class ExpandGemmPure(ExpandTransformation):
 
         _, array_a = sdfg.add_array("_a", shape_a, dtype_a, strides=strides_a, storage=outer_array_a.storage)
         _, array_b = sdfg.add_array("_b", shape_b, dtype_b, strides=strides_b, storage=outer_array_b.storage)
-        _, array_c = sdfg.add_array("_c", shape_c, dtype_c, strides=cdata[-1], storage=cdata[1].storage)
+        _, array_c = sdfg.add_array("_c", shape_c, dtype_c, strides=cdata[-3], storage=cdata[1].storage)
 
         if equal_valued(1, node.alpha):
             mul_program = "__out = __a * __b"
@@ -94,7 +93,7 @@ class ExpandGemmPure(ExpandTransformation):
             state = sdfg.add_state_after(init_state, node.label + "_state")
 
         if '_cin' in node.in_connectors:
-            sdfg.add_array("_cin", shape_c, dtype_c, strides=cdata[-1], storage=cdata[1].storage)
+            sdfg.add_array("_cin", shape_c, dtype_c, strides=cdata[-3], storage=cdata[1].storage)
 
         mul_out, mul_out_array = "_c", array_c
         output_nodes = None
@@ -165,7 +164,7 @@ class ExpandGemmOpenBLAS(ExpandTransformation):
     @staticmethod
     def expansion(node, state, sdfg):
         node.validate(sdfg, state)
-        (_, adesc, ashape, astrides), (_, bdesc, bshape, bstrides), _ = _get_matmul_operands(node, state, sdfg)
+        (_, adesc, _, _, _, _), (_, bdesc, _, _, _, _), _ = _get_matmul_operands(node, state, sdfg)
         dtype = adesc.dtype.base_type
         func = to_blastype(dtype.type).lower() + 'gemm'
         alpha = f'{dtype.ctype}({node.alpha})'
@@ -464,7 +463,7 @@ class ExpandGemmPBLAS(ExpandTransformation):
     @staticmethod
     def expansion(node, state, sdfg):
         node.validate(sdfg, state)
-        (_, adesc, ashape, astrides), (_, bdesc, bshape, bstrides), _ = _get_matmul_operands(node, state, sdfg)
+        (_, adesc, ashape, _, _, _), (_, bdesc, bshape, _, _, _), _ = _get_matmul_operands(node, state, sdfg)
         dtype = adesc.dtype.base_type
 
         if not equal_valued(0, node.beta):
@@ -519,8 +518,8 @@ class ExpandGemmFPGA1DSystolic(ExpandTransformation):
         :return:
         """
 
-        ((edge_a, outer_array_a, shape_a, strides_a), (edge_b, outer_array_b, shape_b, strides_b),
-         (edge_c, outer_array_c, shape_c, strides_c)) = _get_matmul_operands(node, parent_state, parent_sdfg)
+        ((edge_a, outer_array_a, shape_a, strides_a, _, _), (edge_b, outer_array_b, shape_b, strides_b, _, _),
+         (edge_c, outer_array_c, shape_c, strides_c, _, _)) = _get_matmul_operands(node, parent_state, parent_sdfg)
 
         dtype_a = outer_array_a.dtype.type
         dtype_b = outer_array_b.dtype.type
@@ -1019,17 +1018,11 @@ class Gemm(dace.sdfg.nodes.LibraryNode):
         size2 = None
         for _, _, _, dst_conn, memlet in state.in_edges(self):
             if dst_conn == '_a':
-                subset = dc(memlet.subset)
-                subset.squeeze()
-                size0 = subset.size()
+                size0 = memlet.subset.size()
             if dst_conn == '_b':
-                subset = dc(memlet.subset)
-                subset.squeeze()
-                size1 = subset.size()
+                size1 = memlet.subset.size()
             if dst_conn == '_c':
-                subset = dc(memlet.subset)
-                subset.squeeze()
-                size2 = subset.size()
+                size2 = memlet.subset.size()
 
         if self.transA:
             size0 = list(reversed(size0))
@@ -1049,9 +1042,7 @@ class Gemm(dace.sdfg.nodes.LibraryNode):
                           UserWarning)
         elif not res:
             raise ValueError("Inputs to matrix-matrix product must agree in the k-dimension")
-        out_subset = dc(out_memlet.subset)
-        out_subset.squeeze()
-        size3 = out_subset.size()
+        size3 = out_memlet.subset.size()
         if size2 is not None:
             res = [equal(s0, s1) for s0, s1 in zip(size2, size3)]
             fail = any([r is False for r in res])

commit 638022d901f5bb6270f55087d00fd9f6cd7cf619

Squeeze only rank-reducing indices in MatMul operands
A subsets.Range entry of size 1 is ambiguous: a rank-reducing index into a larger
dimension and a genuine extent-1 dimension are both stored as (0, 0, 1). Range.squeeze()
drops both, so a full read of a (NQ, 1, NP) view looks identical to a 2D slice.

np.reshape(x, (NQ, 1, NP)) builds exactly that. SpecializeMatMul dispatched on the
squeezed sizes and saw a matrix, picking Gemm, whose validate re-read the unsqueezed
subset and raised "matrix-matrix product only supported on matrices". npbench's doitgen
hits this at every size, on both the simplified and the auto-optimized pipeline.

The descriptor is the authority: an entry of size 1 is rank-reducing only where the
descriptor's own extent is larger. Under that rule the reshaped operand stays 3D and
dispatches to BatchedMatMul, which is what numpy does, while an index into a larger
dimension still squeezes to Gemm. BatchedMatMul.validate went through the same shared
helper so both nodes agree on operand rank.

Regressed in afd0efe48, which removed the squeezing from the validate methods but left
SpecializeMatMul dispatching on squeezed sizes. That commit also dropped the (NQ, 1, NP)
reshape from the in-tree doitgen test, which is why CI stayed green.

dace/libraries/blas/nodes/batched_matmul.py
@@ -458,20 +458,13 @@ class BatchedMatMul(dace.sdfg.nodes.LibraryNode):
         in_edges = state.in_edges(self)
         if len(in_edges) != 2:
             raise ValueError("Expected exactly two inputs to batched matrix-matrix product")
-        for _, _, _, dst_conn, memlet in state.in_edges(self):
-            if dst_conn == '_a':
-                subset = dc(memlet.subset)
-                subset.squeeze()
-                size0 = subset.size()
-            if dst_conn == '_b':
-                subset = dc(memlet.subset)
-                subset.squeeze()
-                size1 = subset.size()
         out_edges = state.out_edges(self)
         if len(out_edges) != 1:
             raise ValueError("Expected exactly one output from "
                              "batched matrix-matrix product")
-        out_memlet = out_edges[0].data
+        # Sizes come from the shared operand helper, which squeezes only rank-reducing indices.
+        a, b, c = _get_matmul_operands(self, state, sdfg)
+        size0, size1, size2 = a[4], b[4], c[4]
 
         # Both inputs must be at least 2D
         if len(size0) < 2:
@@ -494,9 +487,8 @@ class BatchedMatMul(dace.sdfg.nodes.LibraryNode):
             raise ValueError("Inputs to matrix-matrix product must agree in the k-dimension")
 
         # Output must have batch dimensions
-        if len(out_memlet.subset) < 3:
-            raise ValueError(
-                f"Batched matrix-matrix product output must be at least 3D, got {len(out_memlet.subset)} dimensions")
+        if len(size2) < 3:
+            raise ValueError(f"Batched matrix-matrix product output must be at least 3D, got {len(size2)} dimensions")
 
 
 # Numpy replacement

dace/libraries/blas/nodes/matmul.py
@@ -7,6 +7,17 @@ import warnings
 from math import prod
 
 
+def unit_extent_dims(desc):
+    """Dimensions that are size-1 in the descriptor itself.
+
+    A subset entry of size 1 is ambiguous: it is either a rank-reducing index into a larger
+    dimension (``A[r, 0:N]`` on ``A[R, N]`` -- the dimension is gone) or a full read of a
+    dimension the data actually has (``V[0:N, 0, 0:P]`` on a ``(N, 1, P)`` view -- the dimension
+    is part of the shape). Only the former may be squeezed away.
+    """
+    return [i for i, extent in enumerate(desc.shape) if extent == 1]
+
+
 def _get_matmul_operands(node, state, sdfg, name_lhs="_a", name_rhs="_b", name_out="_c"):
     """Returns the matrix multiplication input edges, arrays, and shape."""
     res_lhs = None
@@ -15,9 +26,9 @@ def _get_matmul_operands(node, state, sdfg, name_lhs="_a", name_rhs="_b", name_o
         if edge.dst_conn in [name_lhs, name_rhs]:
             size = edge.data.subset.size()
             squeezed = dc(edge.data.subset)
-            squeezed_dims = squeezed.squeeze()
-            squeezed_size = squeezed.size()
             outer_array = sdfg.data(dace.sdfg.find_input_arraynode(state, edge).data)
+            squeezed_dims = squeezed.squeeze(ignore_indices=unit_extent_dims(outer_array))
+            squeezed_size = squeezed.size()
             strides = list(outer_array.strides)
             squeezed_strides = [s for i, s in enumerate(outer_array.strides) if i in squeezed_dims]
             res = edge, outer_array, size, strides, squeezed_size, squeezed_strides
@@ -28,9 +39,9 @@ def _get_matmul_operands(node, state, sdfg, name_lhs="_a", name_rhs="_b", name_o
         elif edge.src_conn == name_out:
             size = edge.data.subset.size()
             squeezed = dc(edge.data.subset)
-            squeezed_dims = squeezed.squeeze()
-            squeezed_size = squeezed.size()
             outer_array = sdfg.data(dace.sdfg.find_output_arraynode(state, edge).data)
+            squeezed_dims = squeezed.squeeze(ignore_indices=unit_extent_dims(outer_array))
+            squeezed_size = squeezed.size()
             strides = list(outer_array.strides)
             squeezed_strides = [s for i, s in enumerate(outer_array.strides) if i in squeezed_dims]
             res_out = edge, outer_array, size, strides, squeezed_size, squeezed_strides

tests/matmul_unit_dim_squeeze_test.py
@@ -1,0 +1,111 @@
+"""A ``subsets.Range`` entry of size 1 is ambiguous, and the MatMul library nodes read it two
+incompatible ways.
+
+``Range`` stores both a rank-reducing index and a genuine extent-1 dimension as the same triple
+``(0, 0, 1)``, so ``A[r, 0:N, 0:P]`` (dimension indexed away, rank 2) and ``V[0:N, 0, 0:P]``
+(full read of a ``(N, 1, P)`` view, rank 3) are indistinguishable at the subset level.
+``Range.squeeze()`` drops both.
+
+``np.reshape(x, (NQ, 1, NP))`` builds exactly the second kind: a 3D view whose middle dimension is
+part of the shape. ``SpecializeMatMul`` dispatched on the squeezed sizes and saw a matrix, so it
+picked ``Gemm``, whose ``validate`` re-read the unsqueezed subset and raised
+"matrix-matrix product only supported on matrices". npbench's doitgen hits this on every size.
+
+The descriptor is the authority: a subset entry of size 1 is rank-reducing only where the
+descriptor's own extent is larger. Under that rule the reshape stays 3D and dispatches to
+``BatchedMatMul``, while a real index into a larger dimension still squeezes to ``Gemm``.
+"""
+import numpy as np
+import pytest
+
+import dace
+from dace.libraries.blas.nodes.batched_matmul import BatchedMatMul
+from dace.libraries.blas.nodes.gemm import Gemm
+from dace.libraries.blas.nodes.matmul import MatMul
+from dace.transformation.auto.auto_optimize import auto_optimize
+
+NR, NQ, NP = (dace.symbol(s, dtype=dace.int64) for s in ('NR', 'NQ', 'NP'))
+
+
+@dace.program
+def doitgen_reshape(A: dace.float64[NR, NQ, NP], C4: dace.float64[NP, NP]):
+    # npbench polybench/doitgen, verbatim: the (NQ, 1, NP) reshape is the point of the benchmark.
+    for r in range(NR):
+        A[r, :, :] = np.reshape(np.reshape(A[r], (NQ, 1, NP)) @ C4, (NQ, NP))
+
+
+@dace.program
+def indexed_slice_matmul(A: dace.float64[NR, NQ, NP], C4: dace.float64[NP, NP]):
+    # A[r] indexes dimension 0 away: a genuine 2D operand that must still reach Gemm.
+    for r in range(NR):
+        A[r, :, :] = A[r] @ C4
+
+
+def reference(A, C4):
+    NR, NQ, NP = A.shape
+    return np.reshape(np.reshape(A, (NR, NQ, 1, NP)) @ C4, (NR, NQ, NP))
+
+
+def initialize(nr, nq, np_):
+    A = np.fromfunction(lambda i, j, k: ((i * j + k) % np_) / np_, (nr, nq, np_), dtype=np.float64)
+    C4 = np.fromfunction(lambda i, j: (i * j % np_) / np_, (np_, np_), dtype=np.float64)
+    return A, C4
+
+
+def count_nodes(sdfg, nodetype):
+    return sum(1 for node, _ in sdfg.all_nodes_recursive() if isinstance(node, nodetype))
+
+
+def specialize_matmuls(sdfg):
+    """Expand the MatMul meta-nodes one level, leaving the chosen specialization in the graph."""
+    for node, state in list(sdfg.all_nodes_recursive()):
+        if type(node) is MatMul:
+            node.expand(state)
+
+
+@pytest.mark.parametrize('optimize', [False, True])
+@pytest.mark.parametrize('sizes', [(3, 4, 5), (1, 1, 1), (8, 10, 12), (5, 1, 7)])
+def test_reshape_unit_dim_matmul(optimize, sizes):
+    nr, nq, np_ = sizes
+    A, C4 = initialize(nr, nq, np_)
+    ref = reference(A, C4)
+
+    sdfg = doitgen_reshape.to_sdfg(simplify=False)
+    sdfg.simplify()
+    if optimize:
+        auto_optimize(sdfg, dace.dtypes.DeviceType.CPU, symbols=dict(NR=nr, NQ=nq, NP=np_))
+
+    sdfg(A=A, C4=C4, NR=nr, NQ=nq, NP=np_)
+    assert np.allclose(A, ref)
+
+
+def test_reshape_unit_dim_dispatches_to_batched():
+    """The reshaped operand keeps its unit dimension, so the matmul stays batched."""
+    sdfg = doitgen_reshape.to_sdfg(simplify=False)
+    sdfg.simplify()
+    specialize_matmuls(sdfg)
+    assert count_nodes(sdfg, BatchedMatMul) == 1
+    assert count_nodes(sdfg, Gemm) == 0
+
+
+def test_indexed_dim_still_squeezes_to_gemm():
+    """An index into a larger dimension is rank-reducing, so the operand is a plain matrix."""
+    nr, nq, np_ = 3, 4, 5
+    A, C4 = initialize(nr, nq, np_)
+    ref = reference(A, C4)
+
+    sdfg = indexed_slice_matmul.to_sdfg(simplify=False)
+    sdfg.simplify()
+    specialize_matmuls(sdfg)
+    assert count_nodes(sdfg, Gemm) == 1
+    assert count_nodes(sdfg, BatchedMatMul) == 0
+
+    sdfg(A=A, C4=C4, NR=nr, NQ=nq, NP=np_)
+    assert np.allclose(A, ref)
+
+
+if __name__ == '__main__':
+    for opt in (False, True):
+        test_reshape_unit_dim_matmul(opt, (3, 4, 5))
+    test_reshape_unit_dim_dispatches_to_batched()
+    test_indexed_dim_still_squeezes_to_gemm()